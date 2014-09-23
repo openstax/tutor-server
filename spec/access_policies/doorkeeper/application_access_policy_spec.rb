71786cf22e2f4d79af1d0f77ca51017831f03cbf
Dante Soares <dms3@rice.edu>
2014-09-23 18:16:11 -0500

    Added tests against anonymous users

--- spec/access_policies/doorkeeper/application_access_policy_spec.rb
+++ spec/access_policies/doorkeeper/application_access_policy_spec.rb
@@ -2,29 +2,38 @@ require 'rails_helper'
 
 module Doorkeeper
   RSpec.describe ApplicationAccessPolicy do
+    let!(:anon)        { AnonymousUser.instance }
     let!(:user)        { FactoryGirl.create(:user) }
-    let!(:admin)        { FactoryGirl.create(:user, :administrator) }
+    let!(:admin)       { FactoryGirl.create(:user, :administrator) }
     let!(:application) { FactoryGirl.create(:doorkeeper_application) }
 
     context 'index, read, create, update, destroy' do
-      it 'can be accessed by humans administrators only' do
+      it 'cannot be accessed by non-administrators' do
+        expect(ApplicationAccessPolicy.action_allowed?(:index, anon, Application)).to eq false
+        expect(ApplicationAccessPolicy.action_allowed?(:create, anon, application)).to eq false
+        expect(ApplicationAccessPolicy.action_allowed?(:read, anon, application)).to eq false
+        expect(ApplicationAccessPolicy.action_allowed?(:update, anon, application)).to eq false
+        expect(ApplicationAccessPolicy.action_allowed?(:destroy, anon, application)).to eq false
+
         expect(ApplicationAccessPolicy.action_allowed?(:index, user, Application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:create, user, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:read, user, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:update, user, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:destroy, user, application)).to eq false
 
-        expect(ApplicationAccessPolicy.action_allowed?(:index, admin, Application)).to eq true
-        expect(ApplicationAccessPolicy.action_allowed?(:create, admin, application)).to eq true
-        expect(ApplicationAccessPolicy.action_allowed?(:read, admin, application)).to eq true
-        expect(ApplicationAccessPolicy.action_allowed?(:update, admin, application)).to eq true
-        expect(ApplicationAccessPolicy.action_allowed?(:destroy, admin, application)).to eq true
-
         expect(ApplicationAccessPolicy.action_allowed?(:index, application, Application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:create, user, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:read, application, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:update, application, application)).to eq false
         expect(ApplicationAccessPolicy.action_allowed?(:destroy, application, application)).to eq false
+      end
+
+      it 'can be accessed by human administrators only' do
+        expect(ApplicationAccessPolicy.action_allowed?(:index, admin, Application)).to eq true
+        expect(ApplicationAccessPolicy.action_allowed?(:create, admin, application)).to eq true
+        expect(ApplicationAccessPolicy.action_allowed?(:read, admin, application)).to eq true
+        expect(ApplicationAccessPolicy.action_allowed?(:update, admin, application)).to eq true
+        expect(ApplicationAccessPolicy.action_allowed?(:destroy, admin, application)).to eq true
       end
     end
   end
--- spec/access_policies/user_access_policy_spec.rb
+++ spec/access_policies/user_access_policy_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UserAccessPolicy do
+  let!(:anon)        { AnonymousUser.instance }
   let!(:user)        { FactoryGirl.create(:user) }
   let!(:application) { FactoryGirl.create(:doorkeeper_application) }
 
   context 'index' do
-    it 'can be accessed by applications and humans' do
+    it 'cannot be accessed by anonymous users' do
+      expect(UserAccessPolicy.action_allowed?(:index, anon, User)).to eq false
+    end
+
+    it 'can be accessed by applications and human users' do
       expect(UserAccessPolicy.action_allowed?(:index, user, User)).to eq true
 
       expect(UserAccessPolicy.action_allowed?(:index, application, User)).to eq true
@@ -13,14 +18,20 @@ RSpec.describe UserAccessPolicy do
   end
 
   context 'show, update and destroy' do
-    it 'can be accessed by humans only' do
-      expect(UserAccessPolicy.action_allowed?(:read, user, user)).to eq true
-      expect(UserAccessPolicy.action_allowed?(:update, user, user)).to eq true
-      expect(UserAccessPolicy.action_allowed?(:destroy, user, user)).to eq true
+    it 'cannot be accessed by anonymous users or applications' do
+      expect(UserAccessPolicy.action_allowed?(:read, anon, User)).to eq false
+      expect(UserAccessPolicy.action_allowed?(:update, anon, User)).to eq false
+      expect(UserAccessPolicy.action_allowed?(:destroy, anon, User)).to eq false
 
       expect(UserAccessPolicy.action_allowed?(:read, application, user)).to eq false
       expect(UserAccessPolicy.action_allowed?(:update, application, user)).to eq false
       expect(UserAccessPolicy.action_allowed?(:destroy, application, user)).to eq false
+    end
+
+    it 'can be accessed by humans users' do
+      expect(UserAccessPolicy.action_allowed?(:read, user, user)).to eq true
+      expect(UserAccessPolicy.action_allowed?(:update, user, user)).to eq true
+      expect(UserAccessPolicy.action_allowed?(:destroy, user, user)).to eq true
     end
   end
 end
