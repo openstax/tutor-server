d68149e17ad0755bd934002963bb0345000f1c85
JP Slavinsky <jpslav@gmail.com>
2014-10-15 08:16:32 -0700

    fixes for sprint 002, wasn't setting task opens_at

--- app/routines/create_reading.rb
+++ app/routines/create_reading.rb
@@ -9,8 +9,12 @@ protected
 
   def exec(options={})
     run(GetOrCreateResource, options.slice(:url, :content))
+
     outputs[:reading] = Reading.create(resource: outputs[:resource])
-    Task.create(details: outputs[:reading], title: 'placeholder')
+    transfer_errors_from(outputs[:reading], {type: :verbatim}, true)
+
+    task = Task.create(details: outputs[:reading], title: 'placeholder', opens_at: options[:opens_at] || Time.now)
+    transfer_errors_from(task, {type: :verbatim}, true)
   end
 
 end
