f7925538f7ddce2b0698ba0e38258ef7c86ac5c6
Philip Schatz <phil@cnx.org>
2014-10-15 10:49:57 -0400

    load UI CSS file

--- spec/views/webview/index.html.erb_spec.rb
+++ spec/views/webview/index.html.erb_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe "webview/index", :type => :view do
 
     expect(rendered).to include("<script type='text/javascript' defer>var path = '#{@path}', name = '#{@name}';</script>")
     expect(rendered).to include("<script type='text/javascript' src='//openstax.github.io/tutor-js/bundle.js' defer></script>")
+    expect(rendered).to include("<link rel='stylesheet' href='//openstax.github.io/tutor-js/tutor.css' />")
   end
 end
