723ae01a44bc1a6dfe639c9f6f58877c56d58925
Kevin Burleigh <klb@kindling>
2015-01-13 18:46:28 -0500

    moved User model exchange_identifer non-null constraint to migration which created table originally - NOT BACKWARDS COMPATIBLE

--- db/migrate/20150106202716_add_exchange_identifier_not_null_constraint_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddExchangeIdentifierNotNullConstraintToUsers < ActiveRecord::Migration
-  def change
-    change_column :users, :exchange_identifier, :string, :null => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106202716) do
+ActiveRecord::Schema.define(version: 20141110212240) do
 
   create_table "administrators", force: true do |t|
     t.integer  "user_id",    null: false
