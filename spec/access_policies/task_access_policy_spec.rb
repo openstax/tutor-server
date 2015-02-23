c58f88e73580a0c0a5dd2e843bb832c17ffce70d
Joe Sak <joe@joesak.com>
2015-02-23 18:34:32 -0500

    Add first easy tests to TaskAccessPolcy, the false cases

--- spec/access_policies/task_access_policy_spec.rb
+++ spec/access_policies/task_access_policy_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TaskAccessPolicy do
-  pending "add some examples to #{__FILE__}"
+  let(:requestor) { FactoryGirl.create(:user) }
+  let(:task) { FactoryGirl.create(:task) }
+
+  [:create, :update, :destroy, :made_up].each do |disallowed_action|
+    context "when the action is #{disallowed_action}" do
+      it 'is not allowed' do
+          expect(TaskAccessPolicy).not_to be_action_allowed(disallowed_action,
+                                                            requestor,
+                                                            task)
+      end
+    end
+  end
 end
