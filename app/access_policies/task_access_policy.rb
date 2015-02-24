d1c53c3f5204dccac0e2b59e5a38c212db7faaea
Joe Sak <joe@joesak.com>
2015-02-24 12:06:11 -0500

    Refactor TaskAccessPolicy switch case

--- app/access_policies/task_access_policy.rb
+++ app/access_policies/task_access_policy.rb
@@ -2,9 +2,7 @@ class TaskAccessPolicy
   def self.action_allowed?(action, requestor, task)
     case action
     when :read
-      requestor.is_human? && task.any_tasks?(requestor)
-    when :create, :update, :destroy
-      false
+      requestor.is_human? && task.tasked_to?(requestor)
     else
       false
     end
