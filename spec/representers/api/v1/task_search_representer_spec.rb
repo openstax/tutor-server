62d0bfdfa93b5aac0dea49101618b8bf51d7b581
Nathan Stitt <nathan@stitt.org>
2015-02-24 08:40:06 -0600

    Add Spec for JSON format of TaskSearchRepresenter
    This only partially tests the JSON attributes.
    
    due_at and opens_at keys are not currently
    being validated.

--- spec/representers/api/v1/task_search_representer_spec.rb
+++ spec/representers/api/v1/task_search_representer_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TaskSearchRepresenter, :type => :representer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "a user" do
+
+    let(:user)    { FactoryGirl.create(:user) }
+    let(:outputs) { SearchTasks.call(q: "user_id:#{user.id}").outputs   }
+    let(:default_task)   { FactoryGirl.create(:task) }
+    let(:representation) { Api::V1::TaskSearchRepresenter.new(outputs).as_json }
+
+    it "generates a JSON representation of their tasks" do
+
+      5.times{ FactoryGirl.create(:tasking, taskee: user) }
+
+      expect(representation).to include(
+        "total_count" => 5,
+        "items" => a_collection_including(
+          a_hash_including(
+            "id"           => a_value_within(1).of(5),
+            "task_plan_id" => a_value_within(1).of(5),
+            "title"        => a_string_matching(default_task.title),
+            "is_shared"    => false
+          )
+        )
+      )
+
+    end
+
+  end
+
 end
