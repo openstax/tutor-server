a6cb00eecb91a79d1ab2f30a6a44c11ca9cc427d
Nathan Stitt <nathan@stitt.org>
2015-02-24 10:02:59 -0600

    Spec for TaskedExerciseRepresenter

--- spec/representers/api/v1/tasked_exercise_representer_spec.rb
+++ spec/representers/api/v1/tasked_exercise_representer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TaskedExerciseRepresenter, :type => :representer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:exercise_content) { OpenStax::Exercises::V1.fake_client.new_exercise_hash }
+  let(:tasked_exercise) {
+    FactoryGirl.create(:tasked_exercise, content: exercise_content.to_json)
+  }
+  let(:representation) { Api::V1::TaskedExerciseRepresenter.new(tasked_exercise).as_json }
+
+  it "represents a tasked exercise" do
+    expect(representation).to include(
+      "id"           => tasked_exercise.id,
+      "type"         => "exercise",
+      "is_completed" => false,
+      "content_url"  => tasked_exercise.url,
+      "content"      => exercise_content.deep_stringify_keys
+    )
+  end
+
 end
