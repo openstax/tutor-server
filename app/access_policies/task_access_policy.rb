4ed6f209b345f92afa09f28047e5a027d308f79f
Joe Sak <joe@joesak.com>
2015-02-24 10:05:05 -0500

    Bury implementation details behind a public method

--- app/access_policies/task_access_policy.rb
+++ app/access_policies/task_access_policy.rb
@@ -2,7 +2,7 @@ class TaskAccessPolicy
   def self.action_allowed?(action, requestor, task)
     case action
     when :read
-      requestor.is_human? && task.taskings.where(user_id: requestor.id).any?
+      requestor.is_human? && task.any_tasks?(requestor)
     when :create, :update, :destroy
       false
     else
