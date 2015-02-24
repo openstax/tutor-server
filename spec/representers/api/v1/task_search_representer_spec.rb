acad73bfc8c1933f4e076643749218ff902fe1be
Nathan Stitt <nathan@stitt.org>
2015-02-24 12:02:27 -0600

    Compare items against the factory generated values

--- spec/representers/api/v1/task_search_representer_spec.rb
+++ spec/representers/api/v1/task_search_representer_spec.rb
@@ -9,20 +9,20 @@ RSpec.describe Api::V1::TaskSearchRepresenter, :type => :representer do
     let(:default_task)   { FactoryGirl.create(:task) }
     let(:representation) { Api::V1::TaskSearchRepresenter.new(outputs).as_json }
 
-    it "generates a JSON representation of their tasks" do
-
-      5.times{ FactoryGirl.create(:tasking, taskee: user) }
+    it "represents a generates a JSON representation of their tasks" do
+      task_count = rand(5..10)
+      task_count.times{ FactoryGirl.create(:tasking, taskee: user) }
 
       expect(representation).to include(
-        "total_count" => 5,
-        "items" => a_collection_including(
-          a_hash_including(
-            "id"           => a_value_within(1).of(5),
-            "task_plan_id" => a_value_within(1).of(5),
-            "title"        => a_string_matching(default_task.title),
-            "is_shared"    => false
-          )
-        )
+        "total_count" => task_count,
+        "items" => outputs[:items].map{ | item |
+          json = item.as_json.slice('id', 'title', 'task_plan_id', 'is_shared', 'steps')
+          json['opens_at']  = DateTimeUtilities.to_api_s(item.opens_at)
+          json['due_at']    = DateTimeUtilities.to_api_s(item.due_at)
+          json['is_shared'] = item.is_shared
+          json['steps']     = item.task_steps.as_json
+          json
+        }
       )
 
     end
