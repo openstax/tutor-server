a098b476ffcf27ce4e388d6942765dba3fb619dd
Kevin Burleigh <klb@kindling>
2015-02-25 20:25:40 -0500

    removed puts from subsystem initializer

--- config/initializers/subsystems.rb
+++ config/initializers/subsystems.rb
@@ -4,6 +4,6 @@ subsystem_root_files = Dir[File.join(Rails.root, "app/subsystems/*/*.rb")] - dom
 subsystem_files      = Dir[File.join(Rails.root, "app/subsystems/**/*.rb")] - domain_files - entity_files - subsystem_root_files
 
 [entity_files, subsystem_root_files, subsystem_files, domain_files].flatten.each do |file|
-  puts "=== requiring #{file} ==="
+  # puts "=== requiring #{file} ==="
   ActiveSupport.require_or_load file
 end
