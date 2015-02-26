16cd88e618f9247c9db46d1953aec3c977f034f4
Kevin Burleigh <klb@kindling>
2015-02-26 16:27:07 -0500

    removed puts

--- config/initializers/subsystems.rb
+++ config/initializers/subsystems.rb
@@ -2,6 +2,6 @@ Dir[File.join(Rails.root, "app/subsystems/*/*.rb")] \
 .select{|f| f !~ %r{app/subsytems/domain/}} \
 .select{|f| f =~ %r{app/subsystems/(.*?)/\1}}
 .sort.each do |ss_root_file|
-  puts "ss_root_file: #{ss_root_file}"
+  # puts "ss_root_file: #{ss_root_file}"
   ActiveSupport.require_or_load ss_root_file
 end
