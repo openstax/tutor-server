790b6cae500b29001cf37914958bd964663521a6
Kevin Burleigh <klb@kindling>
2015-02-26 04:11:51 -0500

    ensuring that require order is deterministic

--- config/initializers/subsystems.rb
+++ config/initializers/subsystems.rb
@@ -1,7 +1,7 @@
-domain_files         = Dir[File.join(Rails.root, "app/subsystems/domain/**/*.rb")]
-entity_files         = Dir[File.join(Rails.root, "app/subsystems/entity/**/*.rb")] - domain_files
-subsystem_root_files = Dir[File.join(Rails.root, "app/subsystems/*/*.rb")] - domain_files - entity_files
-subsystem_files      = Dir[File.join(Rails.root, "app/subsystems/**/*.rb")] - domain_files - entity_files - subsystem_root_files
+domain_files         = Dir[File.join(Rails.root, "app/subsystems/domain/**/*.rb")].sort
+entity_files         = Dir[File.join(Rails.root, "app/subsystems/entity/**/*.rb")].sort - domain_files
+subsystem_root_files = Dir[File.join(Rails.root, "app/subsystems/*/*.rb")].sort - domain_files - entity_files
+subsystem_files      = Dir[File.join(Rails.root, "app/subsystems/**/*.rb")].sort - domain_files - entity_files - subsystem_root_files
 
 [entity_files, subsystem_root_files, subsystem_files, domain_files].flatten.each do |file|
   # puts "=== requiring #{file} ==="
