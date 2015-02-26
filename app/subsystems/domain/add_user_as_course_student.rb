0f89b59290fde4a8d72f14486c6d9d410912e69a
Kevin Burleigh <klb@kindling>
2015-02-26 15:18:43 -0500

    removed 'unnecessary' checks for called routine errors

--- app/subsystems/domain/add_user_as_course_student.rb
+++ app/subsystems/domain/add_user_as_course_student.rb
@@ -10,18 +10,13 @@ class Domain::AddUserAsCourseStudent
 
   def exec(user:, course:)
     result = run(Domain::UserIsCourseTeacher, user: user, course: course)
-    fatal_error(code: :could_not_determine_if_user_is_course_teacher, offending_inputs: [user, course]) if result.errors.any?
-
     unless result.outputs.user_is_course_teacher
       result = run(Domain::UserIsCourseStudent, user: user, course: course)
-      fatal_error(code: :could_not_determine_if_user_is_course_student, offending_inputs: [user, course]) if result.errors.any?
-      fatal_error(code: :user_is_already_a_course_student, offending_inputs: [user, course]) if result.outputs.user_is_course_student
+      fatal_error(code: :user_is_already_a_course_student, offending_inputs: [user, course]) \
+        if result.outputs.user_is_course_student
     end
 
-    result = run(Role::CreateUserRole, user)
-    fatal_error(code: :could_not_add_student_role, offending_inputs: [user, course]) if result.errors.any?
-
-    result = run(CourseMembership::AddStudent, course: course, role: outputs.role)
-    fatal_error(code: :could_not_add_student, offending_inputs: [user, course]) if result.errors.any?
+    run(Role::CreateUserRole, user)
+    run(CourseMembership::AddStudent, course: course, role: outputs.role)
   end
 end
--- app/subsystems/domain/add_user_as_course_teacher.rb
+++ app/subsystems/domain/add_user_as_course_teacher.rb
@@ -9,13 +9,10 @@ class Domain::AddUserAsCourseTeacher
 
   def exec(user:, course:)
     result = run(Domain::UserIsCourseTeacher, user: user, course: course)
-    fatal_error(code: :could_not_determine_if_user_is_course_teacher, offending_inputs: [user, course]) if result.errors.any?
-    fatal_error(code: :user_is_already_teacher_of_course, offending_inputs: [user, course]) if result.outputs.user_is_course_teacher
+    fatal_error(code: :user_is_already_teacher_of_course, offending_inputs: [user, course]) \
+      if result.outputs.user_is_course_teacher
 
-    result = run(Role::CreateUserRole, user)
-    fatal_error(code: :could_not_add_teacher_role, offending_inputs: [user, course]) if result.errors.any?
-
-    result = run(CourseMembership::AddTeacher, course: course, role: outputs.role)
-    fatal_error(code: :could_not_add_teacher, offending_inputs: [user, course]) if result.errors.any?
+    run(Role::CreateUserRole, user)
+    run(CourseMembership::AddTeacher, course: course, role: outputs.role)
   end
 end
