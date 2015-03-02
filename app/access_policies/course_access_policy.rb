d0bcd6de2f30105aea90589295e01f1258d166b8
JP Slavinsky <jpslav@gmail.com>
2015-03-02 16:40:58 -0600

    commenting out unfinished access control stuff for courses

--- app/access_policies/course_access_policy.rb
+++ app/access_policies/course_access_policy.rb
@@ -2,7 +2,6 @@ class CourseAccessPolicy
   def self.action_allowed?(action, requestor, course)
     case action
     when :readings
-      debugger
       requestor.is_human? # && 
       # Domain::UserIsCourseTeacher.call(user: requestor.human_user, 
       #                                  course: course)
