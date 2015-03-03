c1df807bae0f9e8069f0118979a3a74732d9f710
Joe Sak <joe@joesak.com>
2015-03-03 12:15:07 -0600

    Remove unecessary include

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,6 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include SigninHelper
-  config.include ActionView::RecordIdentifier
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
