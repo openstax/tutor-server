0a8b44f7be5fccc6e8c9a33ef23607ed19dd34d5
Kevin Burleigh <klb@kindling>
2015-03-04 11:41:42 -0600

    fixed failing webview spec

--- spec/views/webview/index.html.erb_spec.rb
+++ spec/views/webview/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "webview/index", :type => :view do
   it "renders the webview" do
     render
 
-    expect(rendered).to include("<script type='text/javascript' src='http://localhost:8000/dist/tutor.js' defer></script>")
-    expect(rendered).to include("<link rel='stylesheet' href='http://localhost:8000/dist/tutor.css' />")
+    expect(rendered).to include("<script type='text/javascript' src='#{Rails.application.secrets[:openstax_tutor_js_url]}' defer></script>")
+    expect(rendered).to include("<link rel='stylesheet' href='#{Rails.application.secrets[:openstax_tutor_css_url]}' />")
   end
 end
