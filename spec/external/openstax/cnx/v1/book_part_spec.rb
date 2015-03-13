4f1d723c593b634e407994e48a755850d4be08c2
Dante Soares <dms3@rice.edu>
2015-03-13 18:31:21 -0500

    Added specs for Cnx Book and BookPart

--- spec/external/openstax/cnx/v1/book_part_spec.rb
+++ spec/external/openstax/cnx/v1/book_part_spec.rb
@@ -1,5 +1,110 @@
 require 'rails_helper'
+require 'vcr_helper'
 
 RSpec.describe OpenStax::Cnx::V1::BookPart, :type => :external do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  cnx_book_infos = HashWithIndifferentAccess.new(
+    stable: [
+      { id: "7db9aa72-f815-4c3b-9cb6-d50cf5318b58@2.2",
+        contents: [
+          { id: "subcol",
+            contents: [
+              { id: "1491e74e-ed39-446f-a602-e7ab881af101@1",
+                title: "Introduction" },
+              { id: "092bbf0d-0729-42ce-87a6-fd96fd87a083@4",
+                title: "Force"},
+              { id: "61445f78-00e2-45ae-8e2c-461b17d9b4fd@3",
+                title: "Newton's First Law of Motion: Inertia"}
+            ],
+            title: "Forces and Newton's Laws of Motion" }
+        ],
+        title: 'Updated Tutor HS Physics Content - legacy',
+        expected: {
+          part_classes: [OpenStax::Cnx::V1::BookPart]
+        }
+      },
+      { id: "subcol",
+        contents: [
+          { id: "1491e74e-ed39-446f-a602-e7ab881af101@1",
+            title: "Introduction" },
+          { id: "092bbf0d-0729-42ce-87a6-fd96fd87a083@4",
+            title: "Force"},
+          { id: "61445f78-00e2-45ae-8e2c-461b17d9b4fd@3",
+            title: "Newton's First Law of Motion: Inertia"}
+        ],
+        title: "Forces and Newton's Laws of Motion",
+        expected: {
+          part_classes: [OpenStax::Cnx::V1::Page,
+                         OpenStax::Cnx::V1::Page,
+                         OpenStax::Cnx::V1::Page]
+        }
+      }
+    ],
+    latest: [
+      { id: "7db9aa72-f815-4c3b-9cb6-d50cf5318b58",
+        contents: [
+          { id: "subcol",
+            contents: [
+              { id: "1491e74e-ed39-446f-a602-e7ab881af101@1",
+                title: "Introduction" },
+              { id: "092bbf0d-0729-42ce-87a6-fd96fd87a083@4",
+                title: "Force"},
+              { id: "61445f78-00e2-45ae-8e2c-461b17d9b4fd@3",
+                title: "Newton's First Law of Motion: Inertia"}
+            ],
+            title: "Forces and Newton's Laws of Motion" }
+        ],
+        title: 'Updated Tutor HS Physics Content - legacy',
+        expected: {
+          part_classes: [OpenStax::Cnx::V1::BookPart]
+        }
+      },
+      { id: "subcol",
+        contents: [
+          { id: "1491e74e-ed39-446f-a602-e7ab881af101@1",
+            title: "Introduction" },
+          { id: "092bbf0d-0729-42ce-87a6-fd96fd87a083@4",
+            title: "Force"},
+          { id: "61445f78-00e2-45ae-8e2c-461b17d9b4fd@3",
+            title: "Newton's First Law of Motion: Inertia"}
+        ],
+        title: "Forces and Newton's Laws of Motion",
+        expected: {
+          part_classes: [OpenStax::Cnx::V1::Page,
+                         OpenStax::Cnx::V1::Page,
+                         OpenStax::Cnx::V1::Page]
+        }
+      }
+    ]
+  )
+
+  cnx_book_infos.each do |name, infos|
+    context "with #{name.to_s} content" do
+      it "provides info about the book part for the given hash" do
+        infos.each do |hash|
+          book_part = OpenStax::Cnx::V1::BookPart.new(
+            hash: hash.except(:expected)
+          )
+          expect(book_part.hash).not_to be_blank
+          expect(book_part.path).to be_blank
+          expect(book_part.title).to eq hash[:title]
+          expect(book_part.contents).not_to be_blank
+          expect(book_part.parts).not_to be_empty
+        end
+      end
+
+      it "can retrieve its children parts" do
+        infos.each do |hash|
+          book_part = OpenStax::Cnx::V1::BookPart.new(
+            hash: hash.except(:expected)
+          )
+
+          expect(book_part.parts.collect{|p| p.class}).to(
+            eq hash[:expected][:part_classes]
+          )
+        end
+      end
+    end
+  end
+
 end
--- spec/external/openstax/cnx/v1/book_spec.rb
+++ spec/external/openstax/cnx/v1/book_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 require 'vcr_helper'
 
-RSpec.describe OpenStax::Cnx::V1::Book, :type => :external,
-                                        :vcr => VCR_OPTS do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe OpenStax::Cnx::V1::Book, :type => :external, :vcr => VCR_OPTS do
+
+  cnx_book_ids = {
+    stable: '7db9aa72-f815-4c3b-9cb6-d50cf5318b58@2.2',
+    latest: '7db9aa72-f815-4c3b-9cb6-d50cf5318b58'
+  }
+
+  cnx_book_ids.each do |name, id|
+    context "with #{name.to_s} content" do
+      it "provides info about the book with the given id" do
+        book = OpenStax::Cnx::V1::Book.new(id: id)
+        expect(book.id).to eq id
+        expect(book.hash).not_to be_blank
+        expect(book.title).to eq "Updated Tutor HS Physics Content - legacy"
+        expect(book.tree).not_to be_nil
+        expect(book.root_book_part).to be_a OpenStax::Cnx::V1::BookPart
+      end
+    end
+  end
+
 end
