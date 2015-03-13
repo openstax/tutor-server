7cfd47120933095bb85b6a8aa18c06e6729980a4
Dante Soares <dms3@rice.edu>
2015-03-13 10:22:13 -0500

    Changed webview spec to reflect the fact that the webview tags got moved into the layout

--- spec/views/webview/index.html.erb_spec.rb
+++ spec/views/webview/index.html.erb_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "webview/index", :type => :view do
-  it "renders the webview" do
+  it "does not contain any content" do
     render
 
-    expect(rendered).to include("<script type='text/javascript' src='http://localhost:8000/dist/tutor.js' defer></script>")
-    expect(rendered).to include("<link rel='stylesheet' href='http://localhost:8000/dist/tutor.css' />")
+    expect(rendered).to be_blank
   end
 end
