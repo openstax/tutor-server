be8eb13026acf1b92f1fe7d1d774e8ebf72009f4
Nathan Stitt <nathan@stitt.org>
2015-03-19 11:12:46 -0500

    Create factory to create task_plan with tasks
    And use it for generating stats to test against

--- spec/representers/api/v1/i_reading_stats_representer_spec.rb
+++ spec/representers/api/v1/i_reading_stats_representer_spec.rb
@@ -2,57 +2,52 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::IReadingStatsRepresenter, :type => :representer do
 
+
+  let(:number_of_students){ 2 }
+
   let(:task_plan) {
-    FactoryGirl.create(:task_plan)
+    FactoryGirl.create :tasked_task_plan,
+                       number_of_students: number_of_students
   }
-  let(:statistics){
-    CalculateIReadingStats.call(plan:task_plan).outputs[:stats]
+
+  let(:stats){
+    CalculateIReadingStats.call(plan: task_plan).outputs.stats
   }
-  let(:representation) { Api::V1::IReadingStatsRepresenter.new(statistics).as_json }
+
+  let(:representation) { Api::V1::IReadingStatsRepresenter.new(stats).as_json }
 
   it "represents a tasked exercise's stats" do
     expect(representation).to include(
       "course" => a_hash_including(
-        "total_count"              => a_value_between(0,100),
-        "complete_count"           => a_value_between(0,100),
-        "partially_complete_count" => a_value_between(0,100)#,
-        # "current_pages"            => a_collection_including(
-        #   a_hash_including(
-        #     "student_count"   => a_value_between(0,100),
-        #     "correct_count"   => a_value_between(0,100),
-        #     "incorrect_count" => a_value_between(0,100),
-        #     "page" => a_hash_including(
-        #       "id"     => be_a_kind_of(Fixnum),
-        #       "number" => a_string_matching(/\d+/),
-        #       "title"  => be_a_kind_of(String)
-        #     )
-        #   )
-        # ),
-        # "spaced_pages" => a_collection_including(
-        #   a_hash_including(
-        #     "student_count"   => a_value_between(0,100),
-        #     "correct_count"   => a_value_between(0,100),
-        #     "incorrect_count" => a_value_between(0,100),
-        #     "page" => a_hash_including(
-        #       "id"     => be_a_kind_of(Fixnum),
-        #       "number" => a_string_matching(/\d+/),
-        #       "title"  => be_a_kind_of(String)
-        #     ) ,
-            # "previous_attempt" => a_hash_including(
-            #   "student_count"   => a_value_between(0,100),
-            #   "correct_count"   => a_value_between(0,100),
-            #   "incorrect_count" => a_value_between(0,100),
-            #   "page" => a_hash_including(
-            #     "id"     => be_a_kind_of(Fixnum),
-            #     "number" => a_string_matching(/\d+.\d+/),
-            #     "title"  => be_a_kind_of(String)
-            #   )
-            # )
-      #     )
-      #   )
+        "total_count"              => 2,
+        "complete_count"           => 0,
+        "partially_complete_count" => 0,
+        "current_pages"            => a_collection_containing_exactly(
+          a_hash_including(
+            "student_count"   => 2,
+            "correct_count"   => 0,
+            "incorrect_count" => 0,
+            "page" => a_hash_including(
+              "id"     => 1,
+              "number" => 1,
+              "title"  => "Force"
+            )
+          )
+        ),
+        "spaced_pages" => a_collection_containing_exactly(
+          a_hash_including(
+            "student_count"   => 2,
+            "correct_count"   => 0,
+            "incorrect_count" => 0,
+            "page" => a_hash_including(
+              "id"     => 0,
+              "number" => 0,
+              "title"  => ""
+            )
+          )
+        )
       )
     )
-
   end
 
 
