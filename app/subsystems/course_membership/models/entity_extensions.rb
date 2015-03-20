cb23d83ef3640d44518b90644b6c165f536486c8
Nathan Stitt <nathan@stitt.org>
2015-03-20 18:09:29 -0500

    Specify foreign_key for associations to be entity

--- app/subsystems/course_membership/models/entity_extensions.rb
+++ app/subsystems/course_membership/models/entity_extensions.rb
@@ -1,4 +1,4 @@
 class Entity::Role < ActiveRecord::Base
-  has_many :students, subsystem: :course_membership
-  has_many :teachers, subsystem: :course_membership
+  has_many :students, subsystem: :course_membership, foreign_key: 'entity_role_id'
+  has_many :teachers, subsystem: :course_membership, foreign_key: 'entity_role_id'
 end
