32d453a8c51cbb65ac9929f199ce765d90515330
Joe Sak <joe@joesak.com>
2015-03-23 14:36:08 -0400

    Move UserMapper to Domain::MapUsersAccounts

---
+++ app/subsystems/domain/map_users_accounts.rb
@@ -0,0 +1,19 @@
+module Domain::MapUsersAccounts
+
+  def self.account_to_user(account)
+    return UserProfile::Profile.anonymous if account.is_anonymous?
+
+    user = UserProfile::Profile.where(account_id: account.id).first
+    return user if user.present?
+
+    outcome = UserProfile::FindOrCreate.call(ccount_id: account.id)
+    raise outcome.errors.first.message unless outcome.errors.none?
+
+    outcome.outputs.user
+  end
+
+  def self.user_to_account(user)
+    user.account
+  end
+
+end
