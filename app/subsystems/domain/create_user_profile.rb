4d80da5df8c3804edbc8cec55a4d968ae06f34d5
Joe Sak <joe@joesak.com>
2015-03-23 13:22:33 -0400

    Remove the old CreateUser routine

--- app/subsystems/domain/create_user_profile.rb
+++ app/subsystems/domain/create_user_profile.rb
@@ -1,11 +1,12 @@
 class Domain::CreateUserProfile
   lev_routine
 
-  uses_routine UserProfile::CreateUser
+  uses_routine UserProfile::FindOrCreate,
+    as: :create_user_profile
 
   protected
 
   def exec(attributes = {})
-    run(UserProfile::FindOrCreate, attributes)
+    run(:create_user_profile, attributes)
   end
 end
