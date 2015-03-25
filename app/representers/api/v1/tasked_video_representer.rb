5e1858c3963aed32aa659902eeacc7a9dbf5d245
karen chan <karen@karen-chan.com>
2015-03-25 08:23:30 +0100

    Change TaskedVideoRepresenter to not inherit from TaskedReadingRepresenter
    jpslav said he doesn't want a coupling between it and a reading step.

--- app/representers/api/v1/tasked_video_representer.rb
+++ app/representers/api/v1/tasked_video_representer.rb
@@ -1,4 +1,36 @@
 module Api::V1
-  class TaskedVideoRepresenter < TaskedReadingRepresenter
+  class TaskedVideoRepresenter < Roar::Decorator
+
+    include TaskStepProperties
+
+    property :url,
+             type: String,
+             writeable: false,
+             readable: true,
+             as: :content_url,
+             schema_info: {
+               required: false,
+               description: "The source URL for this Video"
+             }
+
+    property :title,
+             type: String,
+             writeable: false,
+             readable: true,
+             schema_info: {
+               required: true,
+               description: "The title of this Video"
+             }
+
+    property :content,
+             type: String,
+             writeable: false,
+             readable: true,
+             as: :content_html,
+             schema_info: {
+               required: false,
+               description: "The content as HTML"
+             }
+
   end
 end
