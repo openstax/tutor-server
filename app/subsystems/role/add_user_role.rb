69acaa2c5b5f1586689cf3bef076733bd994c04d
Nathan Stitt <nathan@stitt.org>
2015-03-30 09:22:05 -0500

    Use entity_user_id for role, not the profile id

--- app/subsystems/role/add_user_role.rb
+++ app/subsystems/role/add_user_role.rb
@@ -4,7 +4,7 @@ class Role::AddUserRole
   protected
 
   def exec(user:, role:)
-    ss_map = Role::Models::User.create(entity_user_id: user.id, entity_role_id: role.id)
+    ss_map = Role::Models::User.create(entity_user_id: user.entity_user_id, entity_role_id: role.id)
     transfer_errors_from(ss_map, {type: :verbatim}, true)
   end
 end
