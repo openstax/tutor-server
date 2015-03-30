493ad4d901c596311f3150ba0bfb1180a23e54e8
Nathan Stitt <nathan@stitt.org>
2015-03-30 09:46:09 -0500

    Use include matcher, the order returned can differ

--- spec/routines/get_course_task_plans_spec.rb
+++ spec/routines/get_course_task_plans_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe GetCourseTaskPlans, :type => :routine do
   it 'gets all task_plans in a course' do
     out = GetCourseTaskPlans.call(course: course).outputs
     expect(out[:total_count]).to eq 3
-    expect(out[:items]).to eq [task_plan_1, task_plan_2, task_plan_3]
+    expect(out[:items]).to include(task_plan_1, task_plan_2, task_plan_3)
   end
 end
