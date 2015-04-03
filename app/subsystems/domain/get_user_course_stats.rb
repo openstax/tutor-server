ffbb122f204607bf07aff031969ca6f61fe39b8d
Joe Sak <joe@joesak.com>
2015-04-03 15:49:44 -0400

    Add tests and new routines/representers for Course Stats API endpoint

---
+++ app/subsystems/domain/get_user_course_stats.rb
@@ -0,0 +1,34 @@
+class Domain::GetUserCourseStats
+  lev_routine express_output: :course_stats
+
+  uses_routine CourseProfile::GetProfile,
+    translations: { outputs: { type: :verbatim } },
+    as: :get_course_profile
+
+  uses_routine Domain::GetCourseBooks,
+    translations: { outputs: { type: :verbatim } },
+    as: :get_course_books
+
+  protected
+  def exec(user:, course:)
+    run(:get_course_profile, course.id)
+    run(:get_course_books, course: course)
+    compile_course_stats
+  end
+
+  private
+  def compile_course_stats
+    outputs[:course_stats] = {
+      title: outputs.profile.name,
+      topics: collect_book_parts
+    }
+  end
+
+  def collect_book_parts
+    outputs.book_parts.collect do |book_part|
+      { id: book_part.id,
+        title: book_part.title,
+        number: book_part.path }
+    end
+  end
+end
