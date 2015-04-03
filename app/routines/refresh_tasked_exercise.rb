1be7c74da8a9f430b00d22c2e4a77d30b58ca568
Dante Soares <dms3@rice.edu>
2015-04-03 17:33:48 -0500

    Clean up unused variables, plus better comments

--- app/routines/refresh_tasked_exercise.rb
+++ app/routines/refresh_tasked_exercise.rb
@@ -10,8 +10,6 @@ class RefreshTaskedExercise
   def exec(tasked_exercise:)
     task_step = tasked_exercise.task_step
     run(:recover, tasked_exercise: tasked_exercise)
-    task = outputs[:task]
-    recovery_step = outputs[:recovery_step]
 
     outputs[:refresh_step] = refresh_step_for(task_step: task_step)
   end
@@ -21,7 +19,12 @@ class RefreshTaskedExercise
   def refresh_step_for(task_step:)
     current_step = task_step
 
-    # Hack for Sprint 9; Replace with final version before alpha
+    # Hack for Sprint 9; Replace with final version before MVP
+    # This hack returns the last completed TaskedReading's URL,
+    # which is the current CNX module's URL.
+    # The FE is responsible for displaying it to the user.
+    # Will not work if the exercise is the first step in an iReading
+    # (but it shouldn't be)
     while current_step.tasked_type.demodulize != 'TaskedReading' do
       current_step = current_step.previous_by_number
       return {} if current_step.nil?
