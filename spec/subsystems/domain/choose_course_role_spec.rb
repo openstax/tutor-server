405d27529c2277b9affbba22a28374fea91417a0
Nathan Stitt <nathan@stitt.org>
2015-04-06 11:13:49 -0500

    Use new style Entity::XXX.create methods

--- spec/subsystems/domain/choose_course_role_spec.rb
+++ spec/subsystems/domain/choose_course_role_spec.rb
@@ -2,25 +2,24 @@ require 'rails_helper'
 
 describe Domain::ChooseCourseRole do
 
-  let(:teacher){ Entity::CreateUser.call.outputs.user }
-  let(:student){ Entity::CreateUser.call.outputs.user }
+  let(:teacher){ Entity::User.create! }
+  let(:student){ Entity::User.create! }
 
-  let(:interloper){ Entity::CreateUser.call.outputs.user }
+  let(:interloper){ Entity::User.create! }
 
-  let(:course){ Entity::CreateCourse.call.outputs.course }
+  let(:course){ Entity::Course.create! }
 
   let!(:teacher_role){
-    role=Entity::CreateRole.call(:teacher).outputs.role
+    role=Entity::Role.create!(role_type: :teacher)
     Role::AddUserRole[user: teacher, role: role]
     CourseMembership::AddTeacher[course: course, role: role]
     role
   }
 
   let!(:student_role){
-    role=Entity::CreateRole.call(:student).outputs.role
+    role=Entity::Role.create!
     Role::AddUserRole[user: student, role: role]
-    # CourseMembership::AddStudent routine also creates a user?
-    CourseMembership::Models::Student.create(entity_course_id: course.id, entity_role_id: role.id)
+    CourseMembership::AddStudent[course: course, role: role]
     role
   }
 
@@ -28,7 +27,7 @@ describe Domain::ChooseCourseRole do
   context "when a role is provided" do
 
     context "and the user has it" do
-      subject { Domain::VerifyCourseRole.call(user: student, course: course, role: student_role) }
+      subject { Domain::ChooseCourseRole.call(user: student, course: course, role: student_role) }
       it "returns the user's role" do
         expect(subject.outputs.role).to eq(student_role)
       end
@@ -36,7 +35,7 @@ describe Domain::ChooseCourseRole do
 
     context "and the user lacks it" do
       subject(:result) {
-        Domain::VerifyCourseRole.call(user: student, course: course, role: teacher_role)
+        Domain::ChooseCourseRole.call(user: student, course: course, role: teacher_role)
       }
 
       describe "errors" do
@@ -57,7 +56,7 @@ describe Domain::ChooseCourseRole do
   context "when a role is not given" do
     context "and the user does not have any roles on the course" do
       subject(:result) {
-        Domain::VerifyCourseRole.call(user: interloper, course: course)
+        Domain::ChooseCourseRole.call(user: interloper, course: course)
       }
 
       describe "errors" do
@@ -75,7 +74,7 @@ describe Domain::ChooseCourseRole do
 
     context "and the user has a single role" do
       subject(:result) {
-        Domain::VerifyCourseRole.call(user: teacher, course: course)
+        Domain::ChooseCourseRole.call(user: teacher, course: course)
       }
 
       describe "errors" do
@@ -94,10 +93,10 @@ describe Domain::ChooseCourseRole do
       context "when one is a teacher" do
         let(:role_type){ :any }
         subject(:found) {
-          role=Entity::CreateRole.call(:student).outputs.role
+          role=Entity::Role.create!(role_type: :student)
           Role::AddUserRole[user: teacher, role: role]
           CourseMembership::Models::Student.create(entity_course_id: course.id, entity_role_id: role.id)
-          Domain::VerifyCourseRole.call(
+          Domain::ChooseCourseRole.call(
             user: teacher, course: course, role_type: role_type
           ).outputs.role
         }
@@ -113,11 +112,11 @@ describe Domain::ChooseCourseRole do
       end
 
       it "fails with an error if one is not a teacher" do
-        role=Entity::CreateRole.call(:student).outputs.role
+        role=Entity::Role.create(role_type: :student)
         Role::AddUserRole[user: student, role: role]
         CourseMembership::Models::Student.create!(entity_course_id: course.id, entity_role_id: role.id)
 
-        errors = Domain::VerifyCourseRole.call(user: student, course: course).errors
+        errors = Domain::ChooseCourseRole.call(user: student, course: course).errors
         expect(errors).not_to be_empty
         expect(errors.first.code).to eq(:multiple_roles)
       end
