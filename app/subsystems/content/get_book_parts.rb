9915cec3873e0815dcf36564b175e44db2c9670d
Joe Sak <joe@joesak.com>
2015-04-06 11:20:51 -0400

    Use entity books between the routines instead of SS models

--- app/subsystems/content/get_book_parts.rb
+++ app/subsystems/content/get_book_parts.rb
@@ -2,9 +2,9 @@ class Content::GetBookParts
   lev_routine express_output: :book_parts
 
   protected
-  def exec(course_books:)
+  def exec(books:)
     outputs[:book_parts] = Content::Models::BookPart.where(
-      entity_book_id: course_books.collect(&:entity_book_id)
+      entity_book_id: books.collect(&:id)
     )
   end
 end
--- app/subsystems/course_content/get_course_books.rb
+++ app/subsystems/course_content/get_course_books.rb
@@ -4,8 +4,8 @@ class CourseContent::GetCourseBooks
   protected
 
   def exec(course:)
-    outputs[:course_books] = CourseContent::Models::CourseBook.where(course: course.id)
-    book_ids = outputs.course_books.collect(&:entity_book_id)
+    course_books = CourseContent::Models::CourseBook.where(course: course.id)
+    book_ids = course_books.collect(&:entity_book_id)
     outputs[:books] = Entity::Book.find(book_ids)
   end
 end
