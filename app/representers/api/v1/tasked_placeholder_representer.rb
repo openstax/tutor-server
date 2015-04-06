44c7774dd14efdc85b4d3ce79d81552540065cae
Dante Soares <dms3@rice.edu>
2015-04-06 16:24:44 -0500

    Added a TaskedPlaceholderRepresenter and fixed the sprint 9 homework task to allow the student to see the tasks and the teacher to make tasks for the student

---
+++ app/representers/api/v1/tasked_placeholder_representer.rb
@@ -0,0 +1,7 @@
+module Api::V1
+  class TaskedPlaceholderRepresenter < Roar::Decorator
+
+    include TaskStepProperties
+
+  end
+end
--- lib/tasks/sprint/sprint_009/homework.rb
+++ lib/tasks/sprint/sprint_009/homework.rb
@@ -16,33 +16,51 @@ module Sprint009
       Domain::AddBookToCourse.call(book: book, course: course)
       Domain::AddUserAsCourseTeacher.call(course: course,
                                           user: teacher.entity_user)
+      Domain::AddUserAsCourseStudent.call(course: course,
+                                          user: student.entity_user)
 
-      assistant = FactoryGirl.create(
+      r_assistant = FactoryGirl.create(
+        :tasks_assistant,
+        code_class_name: "Tasks::Assistants::IReadingAssistant"
+      )
+      hw_assistant = FactoryGirl.create(
         :tasks_assistant,
         code_class_name: "Tasks::Assistants::HomeworkAssistant"
       )
-      tp = FactoryGirl.create :tasks_task_plan, assistant: assistant,
+
+      FactoryGirl.create(:tasks_course_assistant,
+                         course: course,
+                         assistant: r_assistant,
+                         tasks_task_plan_type: 'reading')
+      FactoryGirl.create(:tasks_course_assistant,
+                         course: course,
+                         assistant: hw_assistant,
+                         tasks_task_plan_type: 'homework')
+
+      course.reload
+
+      tp = FactoryGirl.create :tasks_task_plan, assistant: hw_assistant,
                                                 settings: { exercise_ids: [
         1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25
       ] }
       tp.tasking_plans << FactoryGirl.create(:tasks_tasking_plan,
-                                             target: student, task_plan: tp)
+                                             target: course, task_plan: tp)
       DistributeTasks.call(tp)
 
-      tp = FactoryGirl.create :tasks_task_plan, assistant: assistant,
+      tp = FactoryGirl.create :tasks_task_plan, assistant: hw_assistant,
                                                 settings: { exercise_ids: [
         2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24
       ] }
       tp.tasking_plans << FactoryGirl.create(:tasks_tasking_plan,
-                                             target: student, task_plan: tp)
+                                             target: course, task_plan: tp)
       DistributeTasks.call(tp)
 
-      tp = FactoryGirl.create :tasks_task_plan, assistant: assistant,
+      tp = FactoryGirl.create :tasks_task_plan, assistant: hw_assistant,
                                                 settings: { exercise_ids: [
         1, 2, 3, 5, 8, 13, 21
       ] }
       tp.tasking_plans << FactoryGirl.create(:tasks_tasking_plan,
-                                             target: student, task_plan: tp)
+                                             target: course, task_plan: tp)
       DistributeTasks.call(tp)
     end
 
