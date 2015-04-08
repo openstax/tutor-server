faa5ca3847cfacf22a68050a2e9f178c76471801
Dante Soares <dms3@rice.edu>
2015-04-08 15:30:00 -0500

    Made TaskedExercise less lazy and improved factories

--- spec/factories/content/exercises.rb
+++ spec/factories/content/exercises.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :content_exercise, class: '::Content::Models::Exercise' do
-    url { Faker::Internet.url }
-    title { Faker::Lorem.words(3).join(' ') }
     content { OpenStax::Exercises::V1.fake_client.new_exercise_hash.to_json }
+
+    transient do
+      wrapper { OpenStax::Exercises::V1::Exercise.new(content) }
+    end
+
+    url { wrapper.url }
+    title { wrapper.title }
   end
 end
--- spec/factories/tasks/tasked_exercises.rb
+++ spec/factories/tasks/tasked_exercises.rb
@@ -10,29 +10,26 @@
 FactoryGirl.define do
   factory :tasks_tasked_exercise, class: '::Tasks::Models::TaskedExercise' do
     transient do
-      tasked_to { FactoryGirl.build(:user) }
+      tasked_to { build(:user) }
       skip_task false
     end
 
-    task_step nil
     association :exercise, factory: :content_exercise
+    content { exercise.content }
+    url { exercise.url }
+    title { exercise.title }
 
     after(:build) do |tasked_exercise, evaluator|
-      if tasked_exercise.content.nil?
-        exercise_hash = OpenStax::Exercises::V1.fake_client.new_exercise_hash
-        tasked_exercise.content = exercise_hash.to_json
-      end
-
       options = { tasked: tasked_exercise }
       options[:task] = nil if evaluator.skip_task
 
-      tasked_exercise.task_step ||= FactoryGirl.build(:tasks_task_step, options)
+      tasked_exercise.task_step ||= build(:tasks_task_step, options)
     end
 
     trait :with_tasking do
       after(:build) do |tasked, evaluator|
-        FactoryGirl.create(:tasks_tasking, role: evaluator.tasked_to,
-                                           task: tasked.task_step.task.entity_task)
+        create(:tasks_tasking, role: evaluator.tasked_to,
+                               task: tasked.task_step.task.entity_task)
       end
     end
 
