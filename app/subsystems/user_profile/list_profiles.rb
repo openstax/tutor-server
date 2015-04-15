632e24154c316371b61ac73638aa469e7056ce0a
Joe Sak <joe@joesak.com>
2015-04-15 16:13:10 -0400

    Use nested ss class to get namespace access

--- app/subsystems/user_profile/list_profiles.rb
+++ app/subsystems/user_profile/list_profiles.rb
@@ -1,18 +1,19 @@
-class UserProfile::ListProfiles
-  lev_routine express_output: :profiles
+module UserProfile
+  class ListProfiles
+    lev_routine express_output: :profiles
 
-  protected
-  def exec
-    profiles = UserProfile::Models::Profile.all
-    outputs[:profiles] = profiles.collect do |profile|
-      {
-        id: profile.id,
-        account_id: profile.account_id,
-        entity_user_id: profile.entity_user_id,
-        full_name: profile.full_name,
-        username: profile.account.username
-      }
+    protected
+    def exec
+      profiles = Models::Profile.all
+      outputs[:profiles] = profiles.collect do |profile|
+        {
+          id: profile.id,
+          account_id: profile.account_id,
+          entity_user_id: profile.entity_user_id,
+          full_name: profile.full_name,
+          username: profile.account.username
+        }
+      end
     end
   end
 end
-
