3ead8c3325ed7e4ddb6e2f7d1a83b34309bc8616
Joe Sak <joe@joesak.com>
2015-04-15 22:11:08 -0400

    Update the FE rake task for sprint 9

--- lib/tasks/sprint/sprint_009/course_stats.rb
+++ lib/tasks/sprint/sprint_009/course_stats.rb
@@ -1,16 +1,24 @@
 module Sprint009
   class CourseStats
+    lev_routine express_output: :course
 
-    lev_routine
-
-    uses_routine Domain::GetUserCourseStats,
+    uses_routine CreateCourse,
       translations: { outputs: { type: :verbatim } },
-      as: :get_course_stats
+      as: :create_course
 
+    uses_routine FetchAndImportBook,
+      translations: { outputs: { type: :verbatim } },
+      as: :fetch_and_import_book
+
+    uses_routine AddBookToCourse,
+      translations: { outputs: { type: :verbatim } },
+      as: :add_book_to_course
 
     protected
-    def exec(course:)
-      run(:get_course_stats, user: nil, course: course)
+    def exec
+      run(:create_course)
+      run(:fetch_and_import_book, id: '031da8d3-b525-429c-80cf-6c8ed997733a')
+      run(:add_book_to_course, course: outputs.course, book: outputs.book)
     end
   end
 end
