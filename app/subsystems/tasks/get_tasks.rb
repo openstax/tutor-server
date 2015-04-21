9473207b087b0839654e8328b66aa2140ae41500
Kevin Burleigh <klb@kindling>
2015-04-20 21:18:14 -0500

    replace use of (broken?) entity_extensions - hopefully temporary

--- app/subsystems/tasks/get_tasks.rb
+++ app/subsystems/tasks/get_tasks.rb
@@ -10,8 +10,15 @@ class Tasks::GetTasks
   def exec(roles:)
     role_ids = verify_and_get_id_array(roles, Entity::Role)
 
-    outputs[:tasks] = Entity::Task.joins{taskings}
-                                  .where{taskings.entity_role_id.in role_ids}
+    entity_tasks = Tasks::Models::Tasking.
+                     where{entity_role_id.in role_ids}.
+                     collect{|tasking| tasking.task}
+
+    # This was temporarily removed due to a problem (with entity_extensions?):
+    # outputs[:tasks] = Entity::Task.joins{taskings}
+    #                               .where{taskings.entity_role_id.in role_ids}
+
+    outputs[:tasks] = entity_tasks
   end
 
 end
