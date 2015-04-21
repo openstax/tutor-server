c14ea7e421fd4b7d02057776ef33e528829d88f1
Joe Sak <joe@joesak.com>
2015-04-21 10:41:41 -0400

    Add book_part_id to CnxPage

--- app/subsystems/content/get_page.rb
+++ app/subsystems/content/get_page.rb
@@ -7,9 +7,14 @@ class Content::GetPage
   def exec(id:)
     page = Content::Models::Page.includes(:book_part).find(id)
     outputs[:page] = OpenStax::Cnx::V1::Page.new(
-      id: id, content: page.content, hash: {},
-      chapter_section: page.chapter_section, title: page.title, url: page.url,
-      book_part_title: page.book_part.try(:title)
+      id: id,
+      book_part_title: page.book_part.try(:title),
+      book_part_id: page.book_part.try(:id),
+      content: page.content,
+      hash: {},
+      path: page.path,
+      title: page.title,
+      url: page.url
     )
   end
 
