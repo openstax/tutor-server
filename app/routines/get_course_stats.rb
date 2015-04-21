048095883432b141937cd58a25e6ddbe1a2e77e2
Joe Sak <joe@joesak.com>
2015-04-21 10:32:31 -0400

    Get task steps for the course stats

--- app/routines/get_course_stats.rb
+++ app/routines/get_course_stats.rb
@@ -1,9 +1,13 @@
 class GetCourseStats
   lev_routine express_output: :course_stats
 
-  uses_routine Tasks::GetTasks,
+  uses_routine Tasks::GetRoleTaskSteps,
     translations: { outputs: { type: :verbatim } },
-    as: :get_tasks
+    as: :get_role_task_steps
+
+  uses_routine Content::GetPage,
+    translations: { outputs: { type: :verbatim } },
+    as: :get_page
 
   uses_routine CourseContent::GetCourseBooks,
     translations: { outputs: { type: :verbatim } },
@@ -15,35 +19,42 @@ class GetCourseStats
 
   protected
   def exec(role:, course:)
-    run(:get_tasks, roles: role)
+    run(:get_role_task_steps, roles: role)
     run(:get_course_books, course: course)
     run(:get_book_toc, book: outputs.books.first, visitor_names: :toc)
-    compile_course_stats
+
+    outputs[:course_stats] = { title: root_book_title,
+                               fields: compile_fields }
   end
 
   private
-  def compile_course_stats
-    outputs[:course_stats] = {
-      title: outputs.toc.first.title, # should be root book
-      fields: compile_toc_for_fields
-    }
+  def root_book_title
+    outputs.toc.first.title
   end
 
-  def compile_toc_for_fields
-    binding.pry
-    outputs.toc.from(1).map do |toc| # skip the root book for fields
-      next if toc.title.match(/preface/i)
-      translate_toc(toc)
+  def compile_fields
+    task_steps_grouped_by_book_part.collect do |book_part_id, task_steps|
+      book_part = find_book_part(book_part_id)
+
+      { id: book_part.id,
+        current_level: rand(0.0..1.0),
+        page_ids: task_steps.collect(&:page_id),
+        practice_count: rand(30),
+        questions_answered_count: rand(50),
+        title: book_part.title,
+        unit: book_part.path }
     end
   end
 
-  def translate_toc(toc)
-    { id: toc.id,
-      title: toc.title,
-      questions_answered_count: rand(50),
-      current_level: rand(0.0..1.0),
-      practice_count: rand(30),
-      unit: toc.path,
-      page_ids: (toc.children || []).collect(&:id) }
+  def task_steps_grouped_by_book_part
+    outputs.task_steps.group_by do |task_step|
+      run(:get_page, id: task_step.page_id).outputs.page.book_part_id
+    end
+  end
+
+  def find_book_part(id)
+    toc_children = outputs.toc.collect(&:children).flatten
+    book_part = outputs.toc.first { |bp| bp.id == id }
+    book_part ||= toc_children.first { |bp| bp.id == id }
   end
 end
