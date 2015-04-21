f18e6186266ec0f817d8d1ef648d7d08e1eac4d8
Kevin Burleigh <klb@kindling>
2015-04-20 19:17:12 -0500

    fix bug in CourseMembership::GetCourseRoles and add specs

--- app/subsystems/course_membership/get_course_roles.rb
+++ app/subsystems/course_membership/get_course_roles.rb
@@ -11,20 +11,31 @@ class CourseMembership::GetCourseRoles
     types = [types].flatten.uniq
 
     if types.include?(:any)
-      types = ROLE_TYPES
+      types = ROLE_TYPES - [:any]
     end
 
     roles = types.collect do |type|
       case type
       when :student
-        Entity::Role.joins { students }
-                    .where { students.entity_course_id == course.id }
+        CourseMembership::Models::Student.where{entity_course_id == course.id}
+
+        # Temporarily removed because of a problem (with entity_extensions?):
+        # Entity::Role.joins { students }
+        #             .where { students.entity_course_id == course.id }
+
       when :teacher
-        Entity::Role.joins { teachers }
-                    .where { teachers.entity_course_id == course.id }
+        CourseMembership::Models::Teacher.where{entity_course_id == course.id}
+
+        # Temporarily removed because of a problem (with entity_extensions?):
+        # Entity::Role.joins { teachers }
+        #             .where { teachers.entity_course_id == course.id }
+
+      else
+        raise ArgumentError, "invalid type: #{type} (valid types are #{ROLE_TYPES})"
       end
     end
 
-    outputs[:roles] = roles.flatten
+    outputs[:roles] = roles.flatten.collect{|elem| elem.role}
   end
+
 end
