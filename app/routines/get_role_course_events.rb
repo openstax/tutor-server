2b4b8bb7b33a77e28a7a72121ed72cf86db40714
Kevin Burleigh <klb@kindling>
2015-04-20 21:17:12 -0500

    return Tasks (to parallel TaskPlans) from GetRoleCourseEvents

--- app/routines/get_role_course_events.rb
+++ app/routines/get_role_course_events.rb
@@ -5,8 +5,7 @@ class GetRoleCourseEvents
                as: :get_plans,
                translations: { outputs: { map: { items: :plans } } }
   uses_routine Tasks::GetTasks,
-               as: :get_tasks,
-               translations: { outputs: { type: :verbatim } }
+               as: :get_tasks
 
   protected
 
@@ -20,10 +19,15 @@ class GetRoleCourseEvents
 
   def collect_teacher_events(course:, role:)
     run(:get_plans, course: course)
+
     run(:get_tasks, roles: role)
+    entity_task_ids = outputs["[:get_tasks, :tasks]"].collect{|entity_task| entity_task.id}
+    outputs[:tasks] = Tasks::Models::Task.where{entity_task_id.in entity_task_ids}
   end
 
   def collect_student_events(course:, role:)
     run(:get_tasks, roles: role)
+    entity_task_ids = outputs["[:get_tasks, :tasks]"].collect{|entity_task| entity_task.id}
+    outputs[:tasks] = Tasks::Models::Task.where{entity_task_id.in entity_task_ids}
   end
 end
