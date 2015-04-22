2e7b540097d9f782975d16b63d24249d80770402
Joe Sak <joe@joesak.com>
2015-04-22 12:23:52 -0500

    Refactor

--- lib/tasks/sprint/sprint_009/course_stats.rb
+++ lib/tasks/sprint/sprint_009/course_stats.rb
@@ -23,7 +23,7 @@ module Sprint009
       as: :add_user_as_course_student
 
     uses_routine DistributeTasks,
-      translations: { outputs: { map: { tasks: :temp_tasks } } },
+      translations: { outputs: { map: { tasks: :distributed_tasks } } },
       as: :distribute_tasks
 
     protected
@@ -46,7 +46,7 @@ module Sprint009
       student_role = Entity::Role.last
 
       puts "=== Create assignments ==="
-      create_assignments(role: student_role)
+      outputs[:tasks] = create_assignments(role: student_role)
 
       puts "=== Creating student history ==="
       outputs.tasks.collect(&:task_steps).find_each do |task_step|
@@ -57,40 +57,44 @@ module Sprint009
 
     private
     def create_assignments(role:)
-      ireading_assistant = FactoryGirl.create(:tasks_assistant,
-        code_class_name: 'Tasks::Assistants::IReadingAssistant'
-      )
-      homework_assistant = FactoryGirl.create(:tasks_assistant,
-        code_class_name: 'Tasks::Assistants::HomeworkAssistant'
-      )
+      tasks = []
 
-      ireading_task_plan = FactoryGirl.create(:tasks_task_plan,
-        owner: outputs.course,
-        assistant: ireading_assistant,
-        title: 'Reading',
-        settings: {
-          page_ids: outputs.page_data.from(1).collect(&:id)
-        }
-      )
-
-      homework_task_plan = FactoryGirl.create(:tasks_task_plan,
-        owner: outputs.course,
-        assistant: homework_assistant,
-        title: 'Homework',
-        settings: {
-          page_ids: outputs.page_data.from(1).collect(&:id)
-        }
-      )
-
-      outputs[:tasks] = []
       [ireading_task_plan, homework_task_plan].each do |task_plan|
         task_plan.tasking_plans << FactoryGirl.create(:tasks_tasking_plan,
                                                       task_plan: task_plan,
                                                       target: role)
 
         run(:distribute_tasks, task_plan)
-        outputs[:tasks] << outputs.temp_tasks
+        tasks << outputs.distributed_tasks
       end
+
+      tasks
+    end
+
+    def ireading_task_plan
+      assistant = FactoryGirl.create(:tasks_assistant,
+        code_class_name: 'Tasks::Assistants::IReadingAssistant')
+
+      FactoryGirl.create(:tasks_task_plan,
+        owner: outputs.course,
+        assistant: assistant,
+        title: 'Reading',
+        settings: {
+          page_ids: outputs.page_data.from(1).collect(&:id)
+        })
+    end
+
+    def homework_task_plan
+      assistant = FactoryGirl.create(:tasks_assistant,
+        code_class_name: 'Tasks::Assistants::HomeworkAssistant')
+
+      FactoryGirl.create(:tasks_task_plan,
+        owner: outputs.course,
+        assistant: assistant,
+        title: 'Homework',
+        settings: {
+          page_ids: outputs.page_data.from(1).collect(&:id)
+        })
     end
   end
 end
