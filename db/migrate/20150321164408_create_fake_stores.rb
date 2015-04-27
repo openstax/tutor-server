1152c68ed6b1304acc7739d33dc1c485bc81ee24
Dante Soares <dms3@rice.edu>
2015-04-27 15:55:51 -0500

    Enabled fake stores in production; added user passwords

--- db/migrate/20150321164408_create_fake_stores.rb
+++ db/migrate/20150321164408_create_fake_stores.rb
@@ -1,15 +1,11 @@
-if !Rails.env.production?
+class CreateFakeStores < ActiveRecord::Migration
+  def change
+    create_table :fake_stores do |t|
+      t.text :data
+      t.string :name, null: false
+      t.timestamps null: false
 
-  class CreateFakeStores < ActiveRecord::Migration
-    def change
-      create_table :fake_stores do |t|
-        t.text :data
-        t.string :name, null: false
-        t.timestamps null: false
-
-        t.index :name, unique: true
-      end
+      t.index :name, unique: true
     end
   end
-
 end
