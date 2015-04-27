956cb3cd1321a0e0d7b2e6773390ba5e679695e1
Kevin Burleigh <klb@kindling>
2015-04-27 18:42:28 -0500

    add parser methods to Content::Models::Exercise

--- app/subsystems/content/models/exercise.rb
+++ app/subsystems/content/models/exercise.rb
@@ -9,6 +9,15 @@ class Content::Models::Exercise < Tutor::SubSystems::BaseModel
 
   has_many :tags, through: :exercise_tags
 
+  delegate :uid, :questions, :question_formats, :question_answers, :question_answer_ids,
+           :correct_question_answers, :correct_question_answer_ids, :feedback_map,
+           :content_without_correctness, :tags, :los, to: :parser
+
+  # We depend on the parser because we do not save the parsed content
+  def parser
+    @parser ||= OpenStax::Exercises::V1::Exercise.new(content)
+  end
+
   def tags_with_teks
     # Include tek tags
     tags.collect { |t| [t, t.teks_tags] }.flatten.uniq
