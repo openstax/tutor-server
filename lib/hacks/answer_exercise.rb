8f2bce34b9fc072f12b29f8e51f84245eef7a451
Dante Soares <dms3@rice.edu>
2015-04-28 17:06:47 -0500

    Use JP's Hack routine... and set the open and due dates

--- lib/hacks/answer_exercise.rb
+++ lib/hacks/answer_exercise.rb
@@ -2,24 +2,27 @@ module Hacks
   class AnswerExercise
     lev_routine
 
+    uses_routine MarkTaskStepCompleted, as: :mark_completed
+
     protected
 
-    def exec(task_step:, is_correct:)
+    def exec(task_step:, is_correct:, free_response: nil, completed: true)
       tasked = task_step.tasked
       raise "Task step isn't an exercise" if !tasked.is_a?(Tasks::Models::TaskedExercise)
 
-      answer_id = if is_correct
-        tasked.correct_answer_id
+      if is_correct
+        free_response ||= 'A sentence explaining all the things!'
+        answer_id = tasked.correct_answer_id
       else
+        free_response ||= 'A sentence explaining all the wrong things...'
         wrong_answer_ids = tasked.answer_ids.reject{|id| id == tasked.correct_answer_id}
         raise "No incorrect answers to choose" if wrong_answer_ids.size == 0
-        wrong_answer_ids.first
+        answer_id = wrong_answer_ids.shuffle.first
       end
 
-      tasked.update_attributes(free_response: '.', answer_id: answer_id)
+      tasked.update_attributes(free_response: free_response, answer_id: answer_id)
 
-      MarkTaskStepCompleted[task_step: task_step]
-      # task_step.update_attributes(completed_at: Time.now)
+      run(:mark_completed, task_step: task_step) if completed
     end
   end
 end
