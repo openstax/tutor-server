582d46f0e09cff735920fdc53c11e8de2e3e999a
Dante Soares <dms3@rice.edu>
2015-04-29 16:54:47 -0500

    Improved the setup:001 task spec a bit

--- spec/lib/tasks/setup_001_spec.rb
+++ spec/lib/tasks/setup_001_spec.rb
@@ -5,7 +5,7 @@ require 'tasks/setup_001'
 RSpec.describe Setup001, type: :request, version: :v1, speed: :slow, vcr: VCR_OPTS do
 
   it "doesn't catch on fire" do
-    Setup001.call
+    expect(Setup001.call.errors).to be_empty
   end
 
 end
