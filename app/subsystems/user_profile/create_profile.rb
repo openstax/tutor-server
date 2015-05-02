97ee62b786991c7e4af71a534a872e93db17e83c
Joe Sak <joe@joesak.com>
2015-05-01 23:15:46 -0400

    Add actual profile interface

--- app/subsystems/user_profile/create_profile.rb
+++ app/subsystems/user_profile/create_profile.rb
@@ -7,19 +7,17 @@ module UserProfile
       as: :create_account
 
     protected
-    def exec(username: nil, password: nil, openstax_uid: nil, access_token: nil,
-             first_name: nil, last_name: nil, full_name: nil, title: nil,
-             entity_user_id: nil, account_id: nil, exchange_identifier: nil)
+    def exec(username: nil, password: nil, entity_user_id: nil, account_id: nil,
+             exchange_identifier: nil)
+      if username.nil? && account_id.nil?
+        raise ArgumentError, 'Username required without an account id'
+      end
+
       outputs[:profile] = Models::Profile.create!({
         exchange_identifier: exchange_identifier || OpenStax::Exchange.create_identifier,
         entity_user_id: entity_user_id || new_entity_user_id,
         account_id: account_id || new_account_id({ username: username,
-                                                   openstax_uid: openstax_uid,
-                                                   access_token: access_token,
-                                                   first_name: first_name,
-                                                   last_name: last_name,
-                                                   full_name: full_name,
-                                                   title: title })
+                                                   password: password })
       })
     end
 
