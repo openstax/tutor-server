b300e2b41144ee9cde472362a414913244d79552
Nathan Stitt <nathan@stitt.org>
2015-05-05 10:05:37 -0500

    Return the currently logged in user's name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,22 +9,15 @@ class Api::V1::UsersController < Api::V1::ApiController
   end
 
   api :GET, '/user', <<-EOS
-    If requested by a session/OAuth user, returns header ok (200).
+    If requested by a session/OAuth user, returns a JSON object containing only the users name.
     Otherwise returns header forbidden (403).
   EOS
   def show
-    ## TODO: Eventually convert this to something like:
-    ##         standard_real(current_human_user)
-    ##       once error/exception handling mechasmism
-    ##       have been updated.
-    status = if current_human_user.nil?
-               :forbidden
-             elsif current_human_user.is_anonymous?
-               :forbidden
-             else
-               :ok
-             end
-    head status
+    if current_human_user.nil? || current_human_user.is_anonymous?
+      head :forbidden
+    else
+      render json: { name: current_human_user.name }
+    end
   end
 
 end
