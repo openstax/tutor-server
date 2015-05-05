17f334bfdd15ccadfc70978b170f12502cdceb8d
Joe Sak <joe@joesak.com>
2015-05-05 16:56:29 -0400

    Begin the export performance book job with Axlsx

--- app/subsystems/queues/jobs/export_performance_book_job.rb
+++ app/subsystems/queues/jobs/export_performance_book_job.rb
@@ -4,10 +4,30 @@ module Queues
       queue_as :default
 
       def perform(role:, course:)
-        File.open('/tmp/sup.xlsx', 'w') do |f|
-          f.write('something')
-        end
+        axlsx_package = Axlsx::Package.new
+        workbook = axlsx_package.workbook
+
+        create_summary_worksheet(workbook: workbook, course: course)
+
+        axlsx_package.serialize('./tmp/sup.xlsx')
+
         Tasks::Models::PerformanceBookExport.create!(filename: 'sup')
+      end
+
+      private
+      def create_summary_worksheet(workbook:, course:)
+        center = nil
+        rich_text = Axlsx::RichText.new
+        profile = GetCourseProfile[course: course]
+
+        workbook.styles do |style|
+          center = style.add_style :alignment => { :horizontal=> :center }
+          rich_text.add_run(profile.name, b: true)
+        end
+
+        workbook.add_worksheet(name: 'Summary') do |sheet|
+          sheet.add_row [rich_text], style: center
+        end
       end
     end
   end
--- spec/subsystems/queues/jobs/export_performance_book_job_spec.rb
+++ spec/subsystems/queues/jobs/export_performance_book_job_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Queues::Jobs::ExportPerformanceBookJob do
-  it 'creates an xlsx with a worksheet that has a course summary' do
-    job = described_class.new
-    user = Entity::User.create!
-    course = Entity::Course.create!
-    AddUserAsCourseTeacher[course: course, user: user]
-    role = Entity::Role.last
+  let(:job) { described_class.new }
+  let(:user) { Entity::User.create! }
+  let(:course) { Entity::Course.last }
+  let(:role) { Entity::Role.last }
 
+  before do
+    CreateCourse[name: 'Physics']
+    AddUserAsCourseTeacher[course: course, user: user]
+  end
+
+  it 'creates an xlsx with a worksheet that has a course summary' do
+    pending
     job.perform(role: role, course: course)
+
     export = Tasks::Models::PerformanceBookExport.last
     file = File.open(export.filepath)
-    binding.pry
   end
 end
