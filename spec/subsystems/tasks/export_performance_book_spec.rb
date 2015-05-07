d7b9f23c7b7f51ba6de9527bb49b3acd90969def
Joe Sak <joe@joesak.com>
2015-05-06 21:57:14 -0400

    Add the necessary setup for a performance book (I hope)

--- spec/subsystems/tasks/export_performance_book_spec.rb
+++ spec/subsystems/tasks/export_performance_book_spec.rb
@@ -1,4 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::ExportPerformanceBook do
+  let(:course) { Entity::Course.create! }
+  let(:teacher) { FactoryGirl.create :user_profile }
+  let(:homework_assistant) { FactoryGirl.create :tasks_assistant,
+    code_class_name: 'Tasks::Assistants::HomeworkAssistant' }
+
+  before do
+    student_1 = FactoryGirl.create :user_profile
+    student_2 = FactoryGirl.create :user_profile
+    book = FetchAndImportBook[id: '7db9aa72-f815-4c3b-9cb6-d50cf5318b58']
+    reading_assistant = FactoryGirl.create(:tasks_assistant,
+      code_class_name: 'Tasks::Assistants::IReadingAssistant')
+
+    CourseContent::AddBookToCourse.call(course: course, book: book)
+    AddUserAsCourseTeacher[course: course, user: teacher.entity_user]
+    AddUserAsCourseStudent[course: course, user: student_1.entity_user]
+    AddUserAsCourseStudent[course: course, user: student_2.entity_user]
+
+    page_ids = Content::Models::Page.all.map(&:id)
+
+    reading_taskplan = Tasks::Models::TaskPlan.create!(
+      title: 'Reading task plan',
+      owner: course,
+      type: 'reading',
+      assistant: reading_assistant,
+      opens_at: Time.now,
+      due_at: Time.now + 1.week,
+      settings: { page_ids: page_ids.first(2).collect(&:to_s) }
+    )
+
+    reading_taskplan.tasking_plans << Tasks::Models::TaskingPlan.create!(target: course,
+                                                                         task_plan: reading_taskplan)
+    DistributeTasks[reading_taskplan]
+
+    homework_taskplan = Tasks::Models::TaskPlan.create!(
+      title: 'Homework task plan',
+      owner: course,
+      type: 'homework',
+      assistant: homework_assistant,
+      opens_at: Time.now,
+      due_at: Time.now + 1.day,
+      settings: { exercise_ids: Content::Models::Exercise.first(5).collect(&:id).map(&:to_s),
+                  exercises_count_dynamic: 2 }
+    )
+
+    homework_taskplan.tasking_plans << Tasks::Models::TaskingPlan.create!(target: course,
+                                                                          task_plan: homework_taskplan)
+    DistributeTasks[homework_taskplan]
+
+    homework2_taskplan = Tasks::Models::TaskPlan.create!(
+      title: 'Homework 2 task plan',
+      owner: course,
+      type: 'homework',
+      assistant: homework_assistant,
+      opens_at: Time.now,
+      due_at: Time.now + 2.week,
+      settings: { exercise_ids: Content::Models::Exercise.last(2).collect(&:id).map(&:to_s),
+                  exercises_count_dynamic: 2 }
+    )
+
+    homework2_taskplan.tasking_plans << Tasks::Models::TaskingPlan.create!(target: course,
+                                                                           task_plan: homework2_taskplan)
+
+    DistributeTasks[homework2_taskplan]
+
+    student_1_role = GetUserCourseRoles[course: course, user: student_1.entity_user].first
+    student_2_role = GetUserCourseRoles[course: course, user: student_2.entity_user].first
+
+    student_1_tasks = Tasks::Models::Task
+      .joins { taskings }
+      .where { taskings.entity_role_id == my { student_1_role.id } }
+      .where { task_type.in Tasks::Models::Task.task_types.values_at(:reading, :homework) }
+      .order { due_at }
+      .includes { task_steps.tasked }
+
+    student_2_tasks = Tasks::Models::Task
+      .joins { taskings }
+      .where { taskings.entity_role_id == my { student_2_role.id } }
+      .where { task_type.in Tasks::Models::Task.task_types.values_at(:reading, :homework) }
+      .order { due_at }
+      .includes { task_steps.tasked }
+
+    # User 1 answered everything in homework task plan correctly
+    student_1_tasks[0].task_steps.each do |ts|
+      Hacks::AnswerExercise[task_step: ts, is_correct: true]
+    end
+
+    # User 1 completed the reading task plan
+    student_1_tasks[1].task_steps.each do |ts|
+      MarkTaskStepCompleted[task_step: ts]
+    end
+
+    # User 2 answered 2 questions correctly and 2 incorrectly in
+    # homework task plan
+    student_2_tasks[0].task_steps.first(2).each do |ts|
+      Hacks::AnswerExercise[task_step: ts, is_correct: true]
+    end
+    student_2_tasks[0].task_steps.last(2).each do |ts|
+      Hacks::AnswerExercise[task_step: ts, is_correct: false]
+    end
+
+    # User 2 started the reading task plan
+    MarkTaskStepCompleted[task_step: student_2_tasks[1].task_steps.first]
+  end
+
+  it 'does not blow up' do
+    expect(1).to eq(1)
+  end
 end
