2f34147a8199e2bc458addcdbaaf3074108f7449
Kevin Burleigh <klb@kindling>
2015-05-07 14:16:27 -0500

    avoid use of mis-tagged exercise in assignment

--- spec/controllers/api/v1/courses_controller_spec.rb
+++ spec/controllers/api/v1/courses_controller_spec.rb
@@ -682,7 +682,10 @@ RSpec.describe Api::V1::CoursesController, type: :controller, api: true,
           assistant: hw_assistant,
           opens_at: Time.now,
           due_at: Time.now + 2.week,
-          settings: { exercise_ids: Content::Models::Exercise.last(2).collect{ |e| e.id.to_s },
+          settings: { exercise_ids: Content::Models::Exercise.all
+                                                             .sort_by{|ex| ex.url}
+                                                             .last(3).first(2) ## the last exercise is mis-tagged
+                                                             .collect{ |e| e.id.to_s },
                       exercises_count_dynamic: 2 }
         )
         tp.tasking_plans << Tasks::Models::TaskingPlan.create!(target: course, task_plan: tp)
