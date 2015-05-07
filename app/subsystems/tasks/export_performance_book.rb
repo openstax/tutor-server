711b87d89897db135b568efbe26eeb8d49f6f1da
Joe Sak <joe@joesak.com>
2015-05-07 10:31:21 -0400

    Get ready for Axlsx to do its thing with GetPerformanceBook

--- app/subsystems/tasks/export_performance_book.rb
+++ app/subsystems/tasks/export_performance_book.rb
@@ -6,33 +6,93 @@ module Tasks
       translations: { outputs: { type: :verbatim } },
       as: :get_course_profile
 
+    uses_routine GetPerformanceBook,
+      translations: { outputs: { type: :verbatim } },
+      as: :get_performance_book
+
     protected
     def exec(role:, course:)
       run(:get_course_profile, course: course)
+      run(:get_performance_book, course: course, role: role)
 
-      file = Axlsx::Package.new
-      workbook = file.workbook
-
-      create_summary_worksheet(workbook: workbook, course: course)
-
-      file.serialize('./tmp/sup.xlsx')
+      Axlsx::Package.new do |file|
+        create_summary_worksheet(file: file)
+        create_data_worksheet(file: file)
+        file.serialize('./tmp/sup.xlsx')
+      end
 
       Models::PerformanceBookExport.create!(filename: 'sup')
     end
 
     private
-    def create_summary_worksheet(workbook:)
+    def create_summary_worksheet(file:)
       title_style = nil
-      course_name = Axlsx::RichText.new
+      course_name, created_date = Axlsx::RichText.new, Axlsx::RichText.new
 
-      workbook.styles do |style|
-        title_style = style.add_style :alignment => { :horizontal=> :center }
-        course_name.add_run(outputs.profile.name, b: true)
+      file.workbook.styles do |style|
+        title_style = style.add_style alignment: { horizontal: :center }
       end
 
-      workbook.add_worksheet(name: 'Summary') do |sheet|
-        sheet.add_row [title_style], style: title_style
+      course_name.add_run(outputs.profile.name, b: true)
+      created_date.add_run(Date.today)
+
+      file.workbook.add_worksheet(name: 'Summary') do |sheet|
+        sheet.add_row [course_name], style: title_style
+        sheet.add_row [created_date]
       end
     end
+
+    def create_data_worksheet(file:)
+      file.workbook.add_worksheet(name: 'Data') do |sheet|
+        sheet.add_row(outputs.performance_book.data_headings.collect(&:title))
+      end
+    end
+
+#{"data_headings"=>[
+    #{"title"=>"Homework task plan", "class_average"=>75.0},
+    #{"title"=>"Reading task plan"},
+    #{"title"=>"Homework 2 task plan"}],
+ #"students"=>
+  #[{"name"=>"658c3baf3e25f616bdea88fcb5b04ffa",
+    #"role"=>2,
+    #"data"=>
+     #[{"status"=>"completed",
+    #"type"=>"homework",
+    #"id"=>3,
+    #"exercise_count"=>5,
+    #"correct_exercise_count"=>5,
+    #"recovered_exercise_count"=>0},
+    #
+    #{"status"=>"completed",
+    #"type"=>"reading",
+    #"id"=>1},
+    #
+    #{"status"=>"not_started",
+    #"type"=>"homework",
+    #"id"=>5,
+    #"exercise_count"=>3,
+    #"correct_exercise_count"=>0,
+    #"recovered_exercise_count"=>0}]},
+    #
+   #{"name"=>"4e5fa4ca7f9f69e3daf16310a90609f8",
+    #"role"=>3,
+    #"data"=>
+     #[{"status"=>"in_progress",
+    #"type"=>"homework",
+    #"id"=>4,
+    #"exercise_count"=>5,
+    #"correct_exercise_count"=>2,
+    #"recovered_exercise_count"=>0},
+    #
+      #{"status"=>"in_progress",
+    #"type"=>"reading",
+    #"id"=>2},
+    #
+      #{"status"=>"not_started",
+    #"type"=>"homework",
+    #"id"=>6,
+    #"exercise_count"=>3,
+    #"correct_exercise_count"=>0,
+    #"recovered_exercise_count"=>0}]}]}
   end
 end
--- spec/subsystems/tasks/export_performance_book_spec.rb
+++ spec/subsystems/tasks/export_performance_book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::ExportPerformanceBook do
-  let(:course) { Entity::Course.create! }
+  let(:course) { CreateCourse[name: 'Physics'] }
   let(:teacher) { FactoryGirl.create :user_profile }
 
   before do
@@ -10,7 +10,7 @@ RSpec.describe Tasks::ExportPerformanceBook do
 
   it 'does not blow up' do
     role = GetUserCourseRoles[course: course, user: teacher.entity_user].first
-    book = Tasks::GetPerformanceBook[course: course, role: role]
-    binding.pry
+    described_class[role: role, course: course]
+    expect(1).to eq(1)
   end
 end
