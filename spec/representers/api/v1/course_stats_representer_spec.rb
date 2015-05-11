c5b58bc490c1368bd7ce9b7369cfce26790a712c
Joe Sak <joe@joesak.com>
2015-05-11 10:39:07 -0400

    Add basic tests for Api::V1::CourseStatsRepresenter

--- spec/representers/api/v1/course_stats_representer_spec.rb
+++ spec/representers/api/v1/course_stats_representer_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CourseStatsRepresenter do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a title' do
+    stats = Hashie::Mash.new({ title: 'Hello' })
+
+    decorated = described_class.new(stats)
+
+    expect(decorated.to_hash['title']).to eq('Hello')
+  end
+
+  it 'has page ids' do
+    stats = Hashie::Mash.new({ title: 'Hello', page_ids: ['1', '2'] })
+
+    decorated = described_class.new(stats)
+
+    expect(decorated.to_hash['page_ids']).to eq(['1', '2'])
+  end
+
+  it 'has children' do
+    stats = Hashie::Mash.new({ title: 'Hello',
+                               page_ids: ['1', '2'],
+                               children: ['anything?'] })
+
+    decorated = described_class.new(stats)
+
+    expect(decorated.to_hash['children']).to eq(%w(anything?))
+  end
 end
