e6e389f9d271395e432e6c38cad3140e5f613d0a
Dante Soares <dms3@rice.edu>
2015-05-11 11:51:10 -0500

    Temporary sim solution

--- app/external/openstax/cnx/v1/fragment/interactive.rb
+++ app/external/openstax/cnx/v1/fragment/interactive.rb
@@ -7,8 +7,8 @@ module OpenStax::Cnx::V1::Fragment
     # For fragments missing a proper title
     DEFAULT_TITLE = nil
 
-    # CSS to find the interactive
-    INTERACTIVE_CSS = '.os-embed'
+    # CSS to find the simulation url
+    URL_CSS = 'a.ost-iframe-embeddable, .os-embed iframe'
 
     def initialize(node:, title: nil)
       @node  = node
@@ -18,28 +18,17 @@ module OpenStax::Cnx::V1::Fragment
     attr_reader :node
 
     def title
-      @title ||= node.at_css(TITLE_CSS).try(:content).try(:strip) || \
-                 DEFAULT_TITLE
-    end
-
-    def interactive(node = node)
-      @interactive ||= node.at_css(INTERACTIVE_CSS)
-
-    end
-
-    def to_html
-      # Remove the media tag and replace it with just its text
-      if @to_html.nil?
-        node_copy = node.dup
-        interactive_copy = interactive(node = node_copy)
-        interactive_copy.replace(interactive_copy.text)
-        @to_html = node_copy.to_html
-      end
-      @to_html
+      @title ||= node.at_css(TITLE_CSS).try(:content).try(:strip) || DEFAULT_TITLE
     end
 
     def url
-      @url ||= interactive.try(:xpath, 'iframe/@src').to_s
+      url_node = node.at_css(URL_CSS)
+      @url ||= url_node.try(:[], 'href') || url_node.try(:[], 'src')
+    end
+
+    def to_html
+      # Leave content as-is for now
+      @to_html ||= node.to_html
     end
 
     def visit(visitor:, depth: 0)
