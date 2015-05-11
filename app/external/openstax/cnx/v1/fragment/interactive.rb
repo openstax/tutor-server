69b52d74fd13ea8991d65d3a682e7ac3ef8d40c8
Dante Soares <dms3@rice.edu>
2015-05-11 13:11:40 -0500

    Replaced sim link and figure with iframe

--- app/external/openstax/cnx/v1/fragment/interactive.rb
+++ app/external/openstax/cnx/v1/fragment/interactive.rb
@@ -7,8 +7,11 @@ module OpenStax::Cnx::V1::Fragment
     # For fragments missing a proper title
     DEFAULT_TITLE = nil
 
-    # CSS to find the simulation url
-    URL_CSS = 'a.ost-iframe-embeddable, .os-embed iframe'
+    # CSS to find the simulation container (will be replaced with iframe)
+    CONTAINER_CSS = '.os-iframe-embeddable, .os-embed'
+
+    # XPath to find the simulation url
+    URL_XPATH = './/a/@href | .//iframe/@src'
 
     def initialize(node:, title: nil)
       @node  = node
@@ -22,12 +25,19 @@ module OpenStax::Cnx::V1::Fragment
     end
 
     def url
-      url_node = node.at_css(URL_CSS)
-      @url ||= url_node.try(:[], 'href') || url_node.try(:[], 'src')
+      @url ||= container.at_xpath(URL_XPATH).value
     end
 
     def to_html
-      # Leave content as-is for now
+      return @to_html unless @to_html.nil?
+
+      # Replace container tag with iframe with hardcoded width and height
+      iframe = Nokogiri::XML::Node.new('iframe', node.document)
+      iframe['src'] = url
+      iframe['width'] = 960
+      iframe['height'] = 500
+      container.replace(iframe)
+
       @to_html ||= node.to_html
     end
 
@@ -35,6 +45,12 @@ module OpenStax::Cnx::V1::Fragment
       visitor.pre_order_visit(elem: self, depth: depth)
       visitor.in_order_visit(elem: self, depth: depth)
       visitor.post_order_visit(elem: self, depth: depth)
+    end
+
+    protected
+
+    def container
+      @container ||= node.at_css(CONTAINER_CSS)
     end
 
   end
--- spec/external/openstax/cnx/v1/fragment/interactive_spec.rb
+++ spec/external/openstax/cnx/v1/fragment/interactive_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe OpenStax::Cnx::V1::Fragment::Interactive, :type => :external, :vc
 <li>When force is equal, no one wins.</li>
 </ol>
 <p id="fs-idp52126304">Next, click on the Friction tab. Try selecting different objects for the person to push. Slide the applied force button to the right to apply force to the right and to the left to apply force to the left. The force will continue to be applied as long as you hold the button down. See the arrow representing friction change in magnitude and direction depending on how much force you apply. Try increasing or decreasing the friction force to see how this affects the motion.</p>
-<div data-type="media" id="fs-idp5640912" class="os-embed" data-alt=" "> <iframe width="960" height="785" src="http://archive.cnx.org/specials/e2ca52af-8c6b-450e-ac2f-9300b38e8739/moving-man/"></iframe>\n</div>\n\n  </div>
+<iframe src="http://archive.cnx.org/specials/e2ca52af-8c6b-450e-ac2f-9300b38e8739/moving-man/" width="960" height="500"></iframe>\n\n  </div>
 EOF
   ] }
 
