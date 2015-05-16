a1b71686c2ddd64fbd44c8d4f2e39a4367d20a4d
Kevin Burleigh <klb@kindling>
2015-05-16 18:25:05 -0500

    add tasked and step saves

--- app/subsystems/tasks/placeholder_strategies/homework_personalized.rb
+++ app/subsystems/tasks/placeholder_strategies/homework_personalized.rb
@@ -27,6 +27,8 @@ class Tasks::PlaceholderStrategies::HomeworkPersonalized
       step.tasked.destroy!
       tasked_exercise = TaskExercise[task_step: step, exercise: exercise]
       # inject_debug_content!(step.tasked, "This exercise is part of the #{step.group_type}")
+      tasked_exercise.save!
+      step.save!
     end
 
     task
