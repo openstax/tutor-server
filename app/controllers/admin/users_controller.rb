bb208094162c9a7b6abd161547becf64d6833bc5
karen chan <karen@karen-chan.com>
2015-05-18 13:40:42 +0200

    Add full name field to admin create and edit form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :get_user, only: [:edit, :update]
+
   def index
     @users = GetAllUserProfiles[]
   end
@@ -6,14 +8,38 @@ class Admin::UsersController < Admin::BaseController
   def create
     handle_with(Admin::UsersCreate,
                 complete: -> (*) {
+                  update_account(@handler_result.outputs[:profile], [:full_name])
                   flash[:notice] = 'The user has been added.'
                   redirect_to admin_users_path
                 })
+  end
+
+  def edit
+  end
+
+  def update
+    update_account(@user, [:username, :full_name])
+    flash[:notice] = 'The user has been updated.'
+    redirect_to admin_users_path
   end
 
   def become
     account = GetAccount[params[:id]]
     sign_in(account)
     redirect_to root_path
+  end
+
+  private
+  def get_user
+    @user = UserProfile::Models::Profile.find(params[:id])
+  end
+
+  def update_account(profile, fields)
+    attributes = {}
+    fields.each do |field|
+      attributes[field] = params[:user][field] if params[:user][field].present?
+    end
+
+    profile.account.update_attributes(attributes) if attributes.present?
   end
 end
