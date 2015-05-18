bbd85227f16f8bf2df4410f41e97208b33ff97be
karen chan <karen@karen-chan.com>
2015-05-18 13:40:42 +0200

    Add teacher/student role and full name fields to admin user management page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :get_user_and_roles, only: [:edit, :update]
+
   def index
     @users = GetAllUserProfiles[]
   end
@@ -6,14 +8,47 @@ class Admin::UsersController < Admin::BaseController
   def create
     handle_with(Admin::UsersCreate,
                 complete: -> (*) {
+                  add_roles(@handler_result.outputs[:profile].entity_user)
+                  update_account(@handler_result.outputs[:profile], [:full_name])
                   flash[:notice] = 'The user has been added.'
                   redirect_to admin_users_path
                 })
+  end
+
+  def edit
+  end
+
+  def update
+    add_roles(@user.entity_user)
+    update_account(@user, [:username, :full_name])
+    flash[:notice] = 'The user has been updated.'
+    redirect_to admin_users_path
   end
 
   def become
     account = GetAccount[params[:id]]
     sign_in(account)
     redirect_to root_path
+  end
+
+  private
+  def get_user_and_roles
+    @user = UserProfile::Models::Profile.find(params[:id])
+    @roles = Role::GetUserRoles.call(@user.entity_user).outputs[:roles]
+    @roles = @roles.collect { |r| r.role_type }.uniq
+  end
+
+  def add_roles(user)
+    Role::CreateUserRole[user, :teacher] unless params[:roles_teacher].nil? || @roles.include?('teacher')
+    Role::CreateUserRole[user, :student] unless params[:roles_student].nil? || @roles.include?('student')
+  end
+
+  def update_account(profile, fields)
+    attributes = {}
+    fields.each do |field|
+      attributes[field] = params[:user][field] if params[:user][field].present?
+    end
+
+    profile.account.update_attributes(attributes) if attributes.present?
   end
 end
