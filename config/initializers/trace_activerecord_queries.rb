9da142cc19d004194b697561bc0c6c34460f84df
JP Slavinsky <jpslav@gmail.com>
2015-05-29 08:20:25 -0700

    fixed spec error when ActiveRecordQueryTrace was unknown, also removed auto shutoff of that gem in demo

--- config/initializers/trace_activerecord_queries.rb
+++ config/initializers/trace_activerecord_queries.rb
@@ -1,3 +1,10 @@
-ActiveRecordQueryTrace.enabled = true if Rails.env.development? && EnvUtilities.load_boolean(
-  name: 'TRACE_ACTIVERECORD_QUERIES', default: false
-)
+if (
+     Rails.env.development? &&
+     EnvUtilities.load_boolean(name: 'TRACE_ACTIVERECORD_QUERIES', default: false)
+    )
+
+  ActiveRecordQueryTrace.enabled = true
+  puts "ActiveRecord Query Trace is enabled! This will be way too slow!"
+end
+
+
--- lib/tasks/demo_001.rb
+++ lib/tasks/demo_001.rb
@@ -1,12 +1,5 @@
 require_relative 'demo_base'
 
-if ActiveRecordQueryTrace.enabled
-  logger = Logger.new(STDOUT)
-  logger.info "ActiveRecord Query Trace is enabled! This will be way too slow!"
-  logger.info "DISABLING ActiveRecord Query Trace"
-  ActiveRecordQueryTrace.enabled = false
-end
-
 class Demo001 < DemoBase
 
   lev_routine
