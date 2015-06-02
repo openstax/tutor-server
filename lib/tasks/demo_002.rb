2cf028fbbe506f3ce307a5398c3d6e89ff42541f
JP Slavinsky <jpslav@gmail.com>
2015-06-01 18:58:03 -0700

    Demo scenario refinements

--- lib/tasks/demo_002.rb
+++ lib/tasks/demo_002.rb
@@ -8,18 +8,23 @@ class Demo002 < DemoBase
 
   def exec(print_logs: true, book_version: :latest, random_seed: nil)
 
-    # TODO change to October date once Timecop better integrated
-    # initial_date = Time.now # Chronic.parse("October 14, 2015")
-    initial_date = (Time.now.midnight + 1.day) - (0.001).seconds
+    anchor_date = Time.now.next_week(:tuesday).advance(hours: 15)
 
-    log("Assignments will be due around #{initial_date}.")
+    monday_1    = anchor_date - 8.days
+    wednesday_1 = anchor_date - 6.days
+    friday_1    = anchor_date - 4.days
+    monday_2    = anchor_date - 1.day
+
+    log("Assignments will be due in the week+ leading up to #{anchor_date}.")
+
+    course = Entity::Course.last
 
     ### First Chapter 3 iReading
     #
 
     responses_list = new_responses_list(
       assignment_type: :reading,
-      step_types: %w( r r i e r r r e v e e r i e e r r e r r r e r e e ),
+      step_types: %w( r r i e r r r e v e e e ),
       entries: [
                   98,
                   67,
@@ -29,7 +34,7 @@ class Demo002 < DemoBase
                   :incomplete,
                   :not_started,
                   78,
-                  %w( 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ),  # explicit example, could also be `100`
+                  %w( 1 1 1 1 1 1 1 1 1 1 1 1 ),  # explicit example, could also be `100`
                   86,
                   100,
                   82,
@@ -44,11 +49,49 @@ class Demo002 < DemoBase
                ]
     )
 
-    course = Entity::Course.last
     assign_ireading(course: course,
-                    chapter_sections: [[3, 0], [3, 1], [3, 2]],
-                    title: 'Reading 3.0 - 3.2',
-                    due_at: initial_date - 5.days).each_with_index do |ireading, index|
+                    chapter_sections: [[3, 0], [3, 1]],
+                    title: 'Reading 3.0 - 3.1',
+                    due_at: monday_1).each_with_index do |ireading, index|
+
+      work_task(task: ireading, responses: responses_list[index])
+
+    end
+
+    ### Second Chapter 3 iReading
+    #
+
+    responses_list = new_responses_list(
+      assignment_type: :reading,
+      step_types: %w( r i e e r r e r r r e r e e e ),
+      entries: [
+                  94,
+                  79,
+                  40,
+                  80,
+                  60,
+                  :incomplete,
+                  :not_started,
+                  72,
+                  %w( 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ),  # explicit example, could also be `100`
+                  80,
+                  100,
+                  88,
+                  73,
+                  80,
+                  79,
+                  40,
+                  :incomplete,
+                  :incomplete,
+                  87,
+                  86
+               ]
+    )
+
+    assign_ireading(course: course,
+                    chapter_sections: [[3, 2]],
+                    title: 'Reading 3.2',
+                    due_at: wednesday_1).each_with_index do |ireading, index|
 
       work_task(task: ireading, responses: responses_list[index])
 
@@ -87,7 +130,7 @@ class Demo002 < DemoBase
     assign_homework(course: course,
                     chapter_sections: [[3, 0], [3, 1], [3, 2]],
                     num_exercises: 10,
-                    due_at: initial_date - 3.days).each_with_index do |hw, index|
+                    due_at: friday_1).each_with_index do |hw, index|
 
       work_task(task: hw, responses: responses_list[index])
 
@@ -98,7 +141,7 @@ class Demo002 < DemoBase
 
     responses_list = new_responses_list(
       assignment_type: :reading,
-      step_types: %w( r i e e ),
+      step_types: %w( r r r v e i e e e e ),
       entries: [
                   94,
                   88,
@@ -124,52 +167,15 @@ class Demo002 < DemoBase
     )
 
     assign_ireading(course: course,
-                    chapter_sections: [[4, 0], [4, 1]],
-                    title: 'Reading 4.0 - 4.1',
-                    due_at: initial_date - 0.days).each_with_index do |ireading, index|
+                    chapter_sections: [[4, 0], [4, 1], [4, 2]],
+                    title: 'Reading 4.0 - 4.2',
+                    due_at: monday_2).each_with_index do |ireading, index|
 
       work_task(task: ireading, responses: responses_list[index])
 
     end
 
-    ### First Chapter 4 HW
-    #
-
-    responses_list = new_responses_list(
-      assignment_type: :homework,
-      step_types: %w( e e e e e e e e e e ),
-      entries: [
-                 :incomplete,
-                 :incomplete,
-                 95,
-                 72,
-                 89,
-                 70,
-                 96,
-                 87,
-                 100,
-                 84,
-                 100,
-                 83,
-                 70,
-                 88,
-                 83,
-                 90,
-                 77,
-                 76,
-                 91,
-                 85
-               ]
-    )
-
-    assign_homework(course: course,
-                    chapter_sections: [[4, 0], [4, 1]],
-                    num_exercises: 8,
-                    due_at: initial_date - 0.days).each_with_index do |hw, index|
-
-      work_task(task: hw, responses: responses_list[index])
-
-    end
+    Timecop.travel_all(anchor_date)
 
   end
 end
