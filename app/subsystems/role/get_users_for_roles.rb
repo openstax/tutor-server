35bf91e71b5e028cb04fe111b9c60109a5c9737d
Joe Sak <joe@joesak.com>
2015-06-03 17:25:20 -0400

    Nitpick

--- app/subsystems/role/get_users_for_roles.rb
+++ app/subsystems/role/get_users_for_roles.rb
@@ -4,7 +4,7 @@ class Role::GetUsersForRoles
   protected
 
   def exec(roles)
-    role_ids = [roles].flatten.compact.collect{|r| r.id}
+    role_ids = [roles].flatten.compact.collect(&:id)
     ss_maps = Role::Models::User.includes(:user).where{entity_role_id.in role_ids}
     outputs[:users] = ss_maps.collect(&:user).uniq
   end
