a6ddf174905535a02fa2510460e7a4afec2f0462
Joe Sak <joe@joesak.com>
2015-06-03 13:30:56 -0400

    Nitpick

--- app/subsystems/course_membership/get_students.rb
+++ app/subsystems/course_membership/get_students.rb
@@ -7,6 +7,6 @@ class CourseMembership::GetStudents
     ss_maps = CourseMembership::Models::Student
                 .includes(:role)
                 .where{entity_course_id == course.id}
-    outputs[:students] = ss_maps.collect{|ss_map| ss_map.role}
+    outputs[:students] = ss_maps.collect(&:role)
   end
 end
