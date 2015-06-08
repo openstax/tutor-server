df5d1a5ba096be09ac37d4ab16a2e6cc784aa26f
Joe Sak <joe@joesak.com>
2015-06-08 11:58:12 -0400

    Clean up GetStudents code

--- app/subsystems/course_membership/get_students.rb
+++ app/subsystems/course_membership/get_students.rb
@@ -4,9 +4,9 @@ class CourseMembership::GetStudents
   protected
 
   def exec(course)
-    ss_maps = CourseMembership::Models::Student
-                .includes(:role)
-                .where{entity_course_id == course.id}
-    outputs[:students] = ss_maps.collect(&:role)
+    outputs[:students] = CourseMembership::Models::Student
+                           .includes(:role)
+                           .where(course: course)
+                           .collect(&:role)
   end
 end
