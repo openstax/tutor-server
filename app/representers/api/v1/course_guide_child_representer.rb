8ebd675e1c059b87e3243c2a39b43c5df19529c6
Joe Sak <joe@joesak.com>
2015-07-07 15:01:35 -0400

    Update representers for CourseGuide documentation

---
+++ app/representers/api/v1/course_guide_child_representer.rb
@@ -0,0 +1,51 @@
+module Api::V1
+  class CourseGuideChildRepresenter < Roar::Decorator
+    include Roar::JSON
+    include Representable::Coercion
+
+    property :id,
+             type: String,
+             readable: true,
+             writeable: false,
+             schema_info: { required: true }
+
+    property :title,
+             type: String,
+             readable: true,
+             writeable: false,
+             schema_info: { required: true }
+
+    collection :chapter_section,
+               readable: true,
+               writeable: false,
+               schema_info: { items: { type: 'string' } }
+
+    property :questions_answered_count,
+             type: Integer,
+             readable: true,
+             writeable: false,
+             schema_info: { required: true }
+
+    property :current_level,
+             type: Float,
+             readable: true,
+             writeable: false,
+             schema_info: { required: true }
+
+    property :practice_count,
+             type: Integer,
+             readable: true,
+             writeable: false,
+             schema_info: { required: true }
+
+    collection :page_ids,
+               readable: true,
+               writeable: false,
+               schema_info: { items: { type: 'string' } }
+
+    collection :children,
+               readable: true,
+               writeable: false,
+               extend: CourseGuideChildrenRepresenter
+  end
+end
