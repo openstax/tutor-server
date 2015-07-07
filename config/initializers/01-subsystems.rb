52c4caf78a4254e18c25fbb2972f8024d117d24d
Nathan Stitt <nathan@stitt.org>
2015-07-07 11:56:41 -0500

    Fix issue with entity/role being loaded twice
    If the role subsystem was loaded before entity/role
    Rails would get confused on whether entity/role.rb
    had already been defined and reload the file twice
    
    This would then trigger an exception when Entity::Role's
    enum was defined for the second time.

--- config/initializers/01-subsystems.rb
+++ config/initializers/01-subsystems.rb
@@ -1,5 +1,9 @@
 require 'tutor/subsystems'
 
+# Manually require entity/role
+# If it's not required first, it will conflict when files are loaded from the Role subsystem
+require 'entity/role'
+
 # Only these namespaces have been configured and should be extended
 # Once they are all configured, we can remove this line entirely,
 # but will still need to require the subsystems on rails boot, before any models are loaded
