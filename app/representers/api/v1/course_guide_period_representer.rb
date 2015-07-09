7c43769929477f6e6c74213654b51083015cb4dc
Joe Sak <joe@joesak.com>
2015-07-08 20:30:56 -0400

    Split period guide rep out to its own file

---
+++ app/representers/api/v1/course_guide_period_representer.rb
@@ -0,0 +1,21 @@
+module Api::V1
+  class CourseGuidePeriodRepresenter < Roar::Decorator
+    include Roar::JSON
+
+    property :title,
+             type: String,
+             readable: true,
+             writeable: false
+
+    collection :page_ids,
+               readable: true,
+               writeable: false,
+               getter: -> (*) { page_ids && page_ids.map(&:to_s) },
+               schema_info: { items: { type: 'string' } }
+
+    collection :children,
+               readable: true,
+               writeable: false,
+               decorator: CourseGuideChildRepresenter
+  end
+end
