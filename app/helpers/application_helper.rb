ab1e1f7f333953deb331e44bdcd8ed23724defb6
Joe Sak <joe@joesak.com>
2015-07-13 14:49:16 -0400

    Bootstrapify admin/districts, subsequently flash messages too

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def bootstrap_flash
+    HashWithIndifferentAccess.new({ success: 'alert-success',
+                                    error: 'alert-error',
+                                    alert: 'alert-block',
+                                    notice: 'alert-info' })
+  end
 end
