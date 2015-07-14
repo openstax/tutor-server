762cabaec46d851cad3461d8303957535e369ebb
karen chan <karen@karen-chan.com>
2015-07-14 23:40:10 +0200

    Add tests for admin periods controller #destroy

---
+++ spec/controllers/admin/periods_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Admin::PeriodsController do
+  let!(:admin) { FactoryGirl.create(:user_profile, :administrator) }
+
+  let!(:course) { Entity::Course.create }
+  let!(:period) { CreatePeriod[course: course, name: '1st'] }
+
+  before { controller.sign_in(admin) }
+
+  describe 'DELETE #destroy' do
+    context 'when there are no students' do
+      it 'deletes a period' do
+        expect {
+          delete :destroy, course_id: course.id, id: period.id
+        }.to change { CourseMembership::Models::Period.count }.by(-1)
+        expect(flash[:notice]).to eq 'Period "1st" deleted.'
+      end
+    end
+
+    context 'when there are students' do
+      let!(:student) { FactoryGirl.create(:user_profile).entity_user }
+      let!(:add_student) { AddUserAsPeriodStudent.call(user: student, period: period) }
+
+      it 'displays an error message' do
+        expect {
+          delete :destroy, course_id: course.id, id: period.id
+        }.not_to change { CourseMembership::Models::Period.count }
+        expect(flash[:error]).to eq [
+          'Students must be moved to another period before this period can be deleted']
+      end
+    end
+  end
+end
