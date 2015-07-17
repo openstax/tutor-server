d63f81440e69a3c231ad11cd078c55ffbb00efb3
Joe Sak <joe@joesak.com>
2015-07-17 16:23:09 -0400

    Add due_at, last_worked_at, to PerformanceReport student data representer

--- app/representers/api/v1/performance_report_representer.rb
+++ app/representers/api/v1/performance_report_representer.rb
@@ -30,6 +30,18 @@ module Api::V1
       property :recovered_exercise_count,
                type: Integer,
                readable: true
+
+      property :due_at,
+               type: String,
+               readable: true,
+               writeable: false,
+               getter: ->(*) { DateTimeUtilities.to_api_s(due_at) }
+
+      property :last_worked_at,
+               type: String,
+               readable: true,
+               writeable: false,
+               getter: ->(*) { DateTimeUtilities.to_api_s(last_worked_at) }
     end
 
     class Students < Roar::Decorator
