352215033b3e97dc69967285672f984dabdefe0f
karen chan <karen@karen-chan.com>
2015-07-20 19:17:09 +0200

    Add remove other books option to AddBookToCourse

--- app/models/entity/course.rb
+++ app/models/entity/course.rb
@@ -5,5 +5,8 @@ class Entity::Course < Tutor::SubSystems::BaseModel
   has_many :teachers, subsystem: :course_membership
   has_many :students, subsystem: :course_membership
 
+  has_many :course_books, subsystem: :course_content
+  has_many :books, through: :course_books
+
   delegate :name, to: :profile
 end
