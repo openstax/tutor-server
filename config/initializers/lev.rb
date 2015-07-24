b3ed581bf8daa57d9c19160eafc68bb84b77cec8
Dante Soares <dms3@rice.edu>
2015-07-24 15:45:54 -0500

    Change the Lev status store to use Redis

--- config/initializers/lev.rb
+++ config/initializers/lev.rb
@@ -1,3 +1,9 @@
+redis_secrets = Rails.application.secrets['redis']
+
 Lev.configure do |config|
   config.raise_fatal_errors = false
+  config.status_store = Lev::RedisStore.new(
+    url: redis_secrets['url'],
+    namespace: redis_secrets['namespaces']['lev']
+  )
 end
