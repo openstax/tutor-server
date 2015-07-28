7ac61183c4daeb2a09089ea64374c3618064ad18
Joe Sak <joe@joesak.com>
2015-07-28 12:15:52 -0400

    Obey the TODO, and move practice widget task to Tasks SS

--- app/routines/reset_practice_widget.rb
+++ app/routines/reset_practice_widget.rb
@@ -1,110 +1,73 @@
 class ResetPracticeWidget
   lev_routine express_output: :task
 
-  uses_routine GetPracticeWidget
-  uses_routine Tasks::CreateTasking
+  uses_routine GetPracticeWidget,
+    translations: { outputs: { type: :verbatim } },
+    as: :get_practice_widget
+
+  uses_routine Tasks::CreateTasking,
+    translations: { outputs: { type: :verbatim } },
+    as: :create_tasking
+
+  uses_routine Tasks::CreatePracticeWidgetTask,
+    translations: { outputs: { type: :verbatim } },
+    as: :create_practice_widget_task
 
   protected
-
   def exec(role:, exercise_source:, page_ids: [], book_part_ids: [], randomize: true)
     page_ids = [page_ids].flatten
     book_part_ids = [book_part_ids].flatten
 
     # Get the existing practice widget and remove incomplete exercises from it
     # so they can be used in later practice
-
-    existing_practice_task = run(GetPracticeWidget, role: role).outputs.task
+    # run(:get_practice_widget, role: role)
     # TODO actually do the step removal
 
     # Gather relevant LO's from pages and book_parts
-
     los = Content::GetLos[page_ids: page_ids, book_part_ids: book_part_ids]
 
     # Gather 5 exercises
-
     exercises = case exercise_source
-    when :fake
-      get_fake_exercises(count: 5)
-    when :local
-      get_local_exercises(count: 5, role: role, tags: los, randomize: randomize)
-    when :biglearn
-      get_biglearn_exercises(count: 5, role: role, los: los)
-    else
-      raise ArgumentError, "exercise_source: must be one of [:fake, :local, :biglearn]"
-    end
+                when :fake
+                  get_fake_exercises(5)
+                when :local
+                  get_local_exercises(5, role, los, { randomize: randomize })
+                when :biglearn
+                  get_biglearn_exercises(5, role, los)
+                else
+                  raise ArgumentError,
+                        "exercise_source: must be one of [:fake, :local, :biglearn]"
+                end
 
     # Create the new practice widget task, and put the exercises into steps
-
-    # TODO move this whole routine into Tasks, use run(...) here
-
     task_type = if book_part_ids.count == 1
                   :chapter_practice
                 elsif page_ids.count == 1
                   :page_practice
-                else
-                  :mixed_practice
                 end
 
-    # In a multi-web server environment, it is possible for one server to create
-    # the practice task and another to request it very quickly and if the server
-    # times are not completely sync'd the request can be reject because the task
-    # looks non open.  When we have PracticeTasks maybe they can not have an opens_at
-    # but for now HACK it by setting it to open in the near past.
-    task_time = 10.minutes.ago
+    run(:create_practice_widget_task, add_related_content: exercise_source != :fake,
+                                      exercises: exercises,
+                                      task_type: task_type)
 
-    task = Tasks::BuildTask[task_type: task_type,
-                            title: 'Practice',
-                            opens_at: task_time,
-                            feedback_at: task_time]
+    run(:create_tasking, role: role, task: outputs.task.entity_task)
 
-    exercises.each do |exercise|
-      step = Tasks::Models::TaskStep.new(task: task)
-
-      step.tasked = TaskExercise[exercise: exercise, task_step: step]
-      step.add_related_content(get_related_content_for(exercise)) unless exercise_source == :fake
-
-      task.task_steps << step
-    end
-
-    task.save!
-
-    # Assign it to role inside the Task subsystem (might not have much in there now)
-
-    run(Tasks::CreateTasking, role: role, task: task.entity_task)
-
-    # return the Task
-
-    outputs[:task] = task.entity_task
+    outputs.task = outputs.task.entity_task
   end
 
-  def get_related_content_for(content_exercise)
-    page = content_exercise_page(content_exercise)
-
-    { title: page.title, chapter_section: page.chapter_section }
-  end
-
-  def content_exercise_page(content_exercise)
-    los = content_exercise.los + content_exercise.aplos
-
-    pages = Content::Models::Page.joins{page_tags.tag}
-                                 .where{page_tags.tag.value.in los}
-
-    raise "#{pages.count} pages found for exercise #{content_exercise.url}" unless pages.one?
-    pages.first
-  end
-
-  def get_fake_exercises(count:)
+  def get_fake_exercises(count)
     count.times.collect do
       exercise_content = OpenStax::Exercises::V1.fake_client.new_exercise_hash
-      exercise = OpenStax::Exercises::V1::Exercise.new(content: exercise_content.to_json)
+      exercise = OpenStax::Exercises::V1::Exercise.new content: exercise_content.to_json
     end
   end
 
-  def get_local_exercises(count:, role:, tags:, randomize: true)
+  def get_local_exercises(count, role, tags, options = {})
+    options = { randomize: true }.merge(options)
     exercises = SearchLocalExercises[not_assigned_to: role,
                                      tag: tags,
                                      match_count: 1].to_a
-    exercises = exercises.shuffle if randomize
+    exercises = exercises.shuffle if options[:randomize]
 
     count.times.collect do
       unless exercise = exercises.pop
@@ -112,7 +75,7 @@ class ResetPracticeWidget
         exercises = SearchLocalExercises[assigned_to: role,
                                          tag: tags,
                                          match_count: 1].to_a
-        exercises = exercises.shuffle if randomize
+        exercises = exercises.shuffle if options[:randomize]
 
         unless exercise = exercises.pop
           fatal_error(code: :no_exercises_found,
@@ -124,7 +87,7 @@ class ResetPracticeWidget
     end
   end
 
-  def get_biglearn_exercises(count:, role:, los:)
+  def get_biglearn_exercises(count, role, los)
     condition = biglearn_condition(los)
 
     urls = OpenStax::Biglearn::V1.get_projection_exercises(
@@ -132,18 +95,14 @@ class ResetPracticeWidget
       difficulty: 0.5, allow_repetitions: true
     )
 
-    exercises = Content::Models::Exercise.where{url.in urls}.all.collect do |content_exercise|
+    Content::Models::Exercise.where{url.in urls}.all.collect do |content_exercise|
       OpenStax::Exercises::V1::Exercise.new(content: content_exercise.content)
     end
-    exercises
   end
 
   def biglearn_condition(los)
-    condition = {
+    {
       _or: los
     }
-
-    condition
   end
-
 end
