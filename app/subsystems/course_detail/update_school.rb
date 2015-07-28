ec249c6240c224584a61b7fe15967b88d1e50c9a
Joe Sak <joe@joesak.com>
2015-07-28 15:16:51 -0400

    Protect update school from empty web form

--- app/subsystems/course_detail/update_school.rb
+++ app/subsystems/course_detail/update_school.rb
@@ -5,7 +5,13 @@ module CourseDetail
     protected
     def exec(id:, attributes: {})
       school = Models::School.find(id)
+
+      if attributes[:course_detail_district_id].to_i.zero?
+        attributes.delete(:course_detail_district_id)
+      end
+
       school.update_attributes(attributes)
+
       outputs.school = { id: school.id,
                          district_id: school.course_detail_district_id,
                          name: school.name }
