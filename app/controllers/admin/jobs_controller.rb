7c423df6e0a388f6f1e065dd60611f31161de57f
Joe Sak <joe@joesak.com>
2015-07-29 14:52:43 -0400

    Fix the admin jobs controller inheritence

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,8 +1,10 @@
 require 'will_paginate/array'
 
-class Admin::JobsController < ApplicationController
-  def index
-    @page_header = "Queued jobs"
-    @jobs = Lev::Status.all.paginate(page: params[:page], per_page: 20)
+module Admin
+  class JobsController < BaseController
+    def index
+      @page_header = "Queued jobs"
+      @jobs = Lev::Status.all.paginate(page: params[:page], per_page: 20)
+    end
   end
 end
