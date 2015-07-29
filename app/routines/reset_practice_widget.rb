e45acdd56d4b8c2a5887aed0d7b904551618490b
Dante Soares <dms3@rice.edu>
2015-07-29 15:07:09 -0500

    Fixed some issues with ResetPracticeWidget; Better error messages

--- app/routines/reset_practice_widget.rb
+++ app/routines/reset_practice_widget.rb
@@ -12,6 +12,9 @@ class ResetPracticeWidget
     as: :create_practice_widget_task
 
   protected
+
+  BASE_RELATION = Content::Models::Exercise.preload(exercise_tags: {tag: {page_tags: :page}})
+
   def exec(role:, exercise_source:, page_ids: [], book_part_ids: [], randomize: true)
     page_ids = [page_ids].flatten
     book_part_ids = [book_part_ids].flatten
@@ -25,28 +28,41 @@ class ResetPracticeWidget
     los = Content::GetLos[page_ids: page_ids, book_part_ids: book_part_ids]
 
     # Gather 5 exercises
+    count = 5
     exercises = case exercise_source
                 when :fake
-                  get_fake_exercises(5)
+                  get_fake_exercises(count)
                 when :local
-                  get_local_exercises(5, role, los, { randomize: randomize })
+                  get_local_exercises(count, role, los, randomize: randomize)
                 when :biglearn
-                  get_biglearn_exercises(5, role, los)
+                  get_biglearn_exercises(count, role, los)
                 else
                   raise ArgumentError,
                         "exercise_source: must be one of [:fake, :local, :biglearn]"
                 end
 
-    # Create the new practice widget task, and put the exercises into steps
-    task_type = if book_part_ids.count == 1
-                  :chapter_practice
-                elsif page_ids.count == 1
-                  :page_practice
-                end
+    num_exercises = exercises.size
 
-    run(:create_practice_widget_task, add_related_content: exercise_source != :fake,
-                                      exercises: exercises,
-                                      task_type: task_type)
+    if num_exercises < count
+      # Not enough exercises
+      fatal_error(
+        code: :not_enough_exercises,
+        message: "Not enough exercises to build the Practice Widget. LO's: #{los.inspect}. Needed: #{count}. Got: #{num_exercises}"
+      )
+    end
+
+    # Figure out the type of practice
+    task_type = :mixed_practice
+    task_type = :chapter_practice if book_part_ids.count > 0 && page_ids.count == 0
+    task_type = :page_practice if book_part_ids.count == 0 && page_ids.count > 0
+
+    related_content_array = exercise_source == :fake ? [] : \
+                            exercises.collect{ |ex| get_related_content_for(ex) }
+
+    # Create the new practice widget task, and put the exercises into steps
+    run(:create_practice_widget_task, exercises: exercises,
+                                      task_type: task_type,
+                                      related_content_array: related_content_array)
 
     run(:create_tasking, role: role, task: outputs.task.entity_task)
 
@@ -62,37 +78,51 @@ class ResetPracticeWidget
 
   def get_local_exercises(count, role, tags, options = {})
     options = { randomize: true }.merge(options)
-    exercises = SearchLocalExercises[not_assigned_to: role,
-                                     tag: tags,
-                                     match_count: 1].to_a
-    exercises = exercises.shuffle if options[:randomize]
-
-    count.times.collect do
-      unless exercise = exercises.pop
-        # We ran out of exercises, so start repeating them
-        exercises = SearchLocalExercises[assigned_to: role,
+    exercise_pool = SearchLocalExercises[relation: BASE_RELATION,
+                                         not_assigned_to: role,
                                          tag: tags,
-                                         match_count: 1].to_a
-        exercises = exercises.shuffle if options[:randomize]
+                                         match_count: 1]
+    exercise_pool = exercise_pool.shuffle if options[:randomize]
+    exercises = exercise_pool.first(count)
+    num_exercises = exercises.size
 
-        unless exercise = exercises.pop
-          fatal_error(code: :no_exercises_found,
-                      message: "No exercises matched the given tags: #{tags.inspect}")
-        end
-      end
-
-      exercise
+    if num_exercises < count
+      # We ran out of exercises, so start repeating them
+      exercise_pool = SearchLocalExercises[relation: BASE_RELATION,
+                                           assigned_to: role,
+                                           tag: tags,
+                                           match_count: 1]
+      exercise_pool = exercise_pool.shuffle if options[:randomize]
+      exercises = exercises + exercise_pool.first(count - num_exercises)
     end
+
+    exercises
   end
 
   def get_biglearn_exercises(count, role, los)
     urls = OpenStax::Biglearn::V1.get_projection_exercises(
-      role: role , tag_search: { _or: los }, count: 5,
-      difficulty: 0.5, allow_repetitions: true
+      role: role , tag_search: { _or: los }, count: 5, difficulty: 0.5, allow_repetitions: true
     )
 
-    Content::Models::Exercise.where{url.in urls}.all.collect do |content_exercise|
-      OpenStax::Exercises::V1::Exercise.new(content: content_exercise.content)
+    exercises = SearchLocalExercises[relation: BASE_RELATION, url: urls]
+  end
+
+  def get_related_content_for(exercise)
+    page = exercise_page(exercise)
+
+    { title: page.title, chapter_section: page.chapter_section }
+  end
+
+  def exercise_page(exercise)
+    tags = exercise.exercise_tags.collect{ |et| et.tag }
+    los = tags.select{ |tag| tag.lo? || tag.aplo? }
+    pages = los.collect{ |lo| lo.page_tags.collect{ |pt| pt.page } }.flatten.compact
+
+    if pages.one?
+      pages.first
+    else
+      raise "#{pages.count} pages found for exercise #{exercise.url}"
     end
   end
+
 end
--- app/subsystems/tasks/create_practice_widget_task.rb
+++ app/subsystems/tasks/create_practice_widget_task.rb
@@ -7,9 +7,8 @@ module Tasks
       as: :build_task
 
     protected
-    def exec(add_related_content:, task_type: :mixed_practice, exercises:)
-      task_type ||= :mixed_practice # in case 'nil' is passed deliberately
 
+    def exec(exercises:, task_type: :mixed_practice, related_content_array: [])
       # In a multi-web server environment, it is possible for one server to create
       # the practice task and another to request it very quickly and if the server
       # times are not completely sync'd the request can be reject because the task
@@ -22,14 +21,13 @@ module Tasks
                        opens_at: task_time,
                        feedback_at: task_time)
 
-      exercises.each do |exercise|
+      exercises.each_with_index do |exercise, ii|
         step = Tasks::Models::TaskStep.new(task: outputs.task)
 
         step.tasked = TaskExercise[exercise: exercise, task_step: step]
 
-        if add_related_content
-          step.add_related_content(get_related_content_for(exercise))
-        end
+        related_content = related_content_array[ii]
+        step.add_related_content(related_content) unless related_content.nil?
 
         outputs.task.task_steps << step
       end
@@ -37,23 +35,5 @@ module Tasks
       outputs.task.save!
     end
 
-    private
-    def get_related_content_for(content_exercise)
-      page = content_exercise_page(content_exercise)
-
-      { title: page.title, chapter_section: page.chapter_section }
-    end
-
-    def content_exercise_page(content_exercise)
-      los = content_exercise.los + content_exercise.aplos
-      pages = Content::Models::Page.joins{page_tags.tag}
-                                   .where{page_tags.tag.value.in los}
-
-      if pages.one?
-        pages.first
-      else
-        raise "#{pages.count} pages found for exercise #{content_exercise.url}"
-      end
-    end
   end
 end
