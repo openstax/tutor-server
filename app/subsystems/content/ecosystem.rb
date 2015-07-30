41ce0e112c40c977fd935778309469e40b3a010d
Kevin Burleigh <klb@kindling>
2015-07-30 18:49:11 -0500

    add Content::Ecosystem#reading_core_exercises; clean up specs

--- app/subsystems/content/ecosystem.rb
+++ app/subsystems/content/ecosystem.rb
@@ -7,8 +7,11 @@ class Content::Ecosystem
   def uuid
     uuid = @strategy.uuid
 
-    raise StrategyError \
-      unless uuid.is_a? Uuid
+    raise_collection_class_error(
+      collection: [uuid],
+      klass:      Content::Ecosystem::Uuid,
+      error:      Content::Ecosystem::StrategyError
+    )
 
     uuid
   end
@@ -16,8 +19,11 @@ class Content::Ecosystem
   def books
     books = @strategy.books
 
-    raise StrategyError \
-      if books.detect{|obj| !obj.is_a?(Book)}
+    raise_collection_class_error(
+      collection: books,
+      klass:      Content::Ecosystem::Book,
+      error:      Content::Ecosystem::StrategyError
+    )
 
     books
   end
@@ -25,10 +31,38 @@ class Content::Ecosystem
   def exercises
     exercises = @strategy.exercises
 
-    raise StrategyError \
-      if exercises.detect{|obj| !obj.is_a?(Exercise)}
+    raise_collection_class_error(
+      collection: exercises,
+      klass:      Content::Ecosystem::Exercise,
+      error:      Content::Ecosystem::StrategyError
+    )
 
     exercises
   end
 
+  def reading_core_exercises(pages:)
+    pages_arr = Array(pages).flatten.compact
+
+    raise_collection_class_error(
+      collection: pages_arr,
+      klass:      Content::Ecosystem::Page,
+      error:      ArgumentError
+    )
+
+    exercises = @strategy.reading_core_exercises(pages: pages_arr)
+
+    raise_collection_class_error(
+      collection: exercises,
+      klass:      Content::Ecosystem::Exercise,
+      error:      Content::Ecosystem::StrategyError
+    )
+
+    exercises
+  end
+
+  private
+
+  def raise_collection_class_error(collection:, klass:, error:)
+    raise error if collection.detect{|obj| !obj.is_a? klass}
+  end
 end
--- spec/subsystems/content/ecosystem_spec.rb
+++ spec/subsystems/content/ecosystem_spec.rb
@@ -3,138 +3,210 @@ require 'rails_helper'
 module Content
   describe Ecosystem do
 
+    let!(:valid_uuid)   { Content::Ecosystem::Uuid.new }
+    let!(:invalid_uuid) { Object.new }
+
+    let!(:valid_book)   { Content::Ecosystem::Book.new(strategy: Object.new) }
+    let!(:invalid_book) { Object.new }
+
+    let!(:valid_page)   { Content::Ecosystem::Page.new(strategy: Object.new) }
+    let!(:invalid_page) { Object.new }
+
+    let!(:valid_exercise)   { Content::Ecosystem::Exercise.new(strategy: Object.new) }
+    let!(:invalid_exercise) { Object.new }
+
+    let(:strategy) {
+      double("strategy").tap do |dbl|
+        allow(dbl).to receive(:uuid).with(no_args)
+                     .and_return(strategy_uuid)
+
+        allow(dbl).to receive(:books).with(no_args)
+                     .and_return(strategy_books)
+
+        allow(dbl).to receive(:exercises).with(no_args)
+                     .and_return(strategy_exercises)
+
+        allow(dbl).to receive(:reading_core_exercises).with(pages: strategy_expected_pages)
+                     .and_return(strategy_reading_core_exercises)
+
+        allow(dbl).to receive(:reading_dynamic_exercises).with(pages: strategy_expected_pages)
+                     .and_return(strategy_reading_dynamic_exercises)
+
+        allow(dbl).to receive(:homework_core_exercises).with(pages: strategy_expected_pages)
+                     .and_return(strategy_homework_core_exercises)
+
+        allow(dbl).to receive(:homework_dynamic_exercises).with(pages: strategy_expected_pages)
+                     .and_return(strategy_homework_dynamic_exercises)
+
+        allow(dbl).to receive(:practice_widget_exercises).with(pages: strategy_expected_pages)
+                     .and_return(strategy_practice_widget_exercises)
+      end
+    }
+
+    let(:strategy_uuid)      { valid_uuid }
+    let(:strategy_books)     { [valid_book, valid_book] }
+    let(:strategy_exercises) { [valid_exercise, valid_exercise] }
+
+    let(:strategy_expected_pages) { [valid_page, valid_page] }
+
+    let(:strategy_reading_core_exercises)     { [valid_exercise, valid_exercise] }
+    let(:strategy_reading_dynamic_exercises)  { [valid_exercise, valid_exercise] }
+    let(:strategy_homework_core_exercises)    { [valid_exercise, valid_exercise] }
+    let(:strategy_homework_dynamic_exercises) { [valid_exercise, valid_exercise] }
+    let(:strategy_practice_widget_exercises)  { [valid_exercise, valid_exercise] }
+
+    let(:ecosystem) { Ecosystem.new(strategy: strategy) }
+
+
     context "construction" do
       it "accepts a strategy object" do
         expect{
-          Ecosystem.new(strategy: Object.new)
+          Ecosystem.new(strategy: strategy)
         }.to_not raise_error
       end
     end
 
+
     context "unique identifier" do
-      let(:ecosystem) {
-        Ecosystem.new(strategy: strategy)
-      }
+      it "delegates to its strategy" do
+        ecosystem.uuid
+        expect(strategy).to have_received(:uuid)
+      end
 
-      let(:strategy) {
-        double("strategy").tap{ |dbl|
-          allow(dbl).to receive(:uuid).with(no_args)
-                       .and_return(strategy_uuid)
-        }
-      }
+      context "strategy returns Content::Ecosystem::Uuid" do
+        let(:strategy_uuid) { valid_uuid }
 
-      let(:strategy_uuid) { Ecosystem::Uuid.new }
-
-      context "delegation" do
-        it "delegates to its strategy" do
-          ecosystem.uuid
-          expect(strategy).to have_received(:uuid).with(no_args)
+        it "returns the strategy's uuid" do
+          uuid = ecosystem.uuid
+          expect(uuid).to eq(strategy_uuid)
         end
+      end
 
-        context "strategy returns Ecosystem::Uuid" do
-          it "returns the strategy's uuid" do
-            expect(ecosystem.uuid).to eq(strategy_uuid)
-          end
-        end
+      context "strategy doesn't return an Content::Ecosystem::Uuid" do
+        let!(:strategy_uuid) { invalid_uuid }
 
-        context "strategy doesn't return an Ecosystem::Uuid" do
-          let(:strategy_uuid) { Object.new }
-
-          it "raises Ecosystem::StrategyError" do
-            expect{
-              ecosystem.uuid
-            }.to raise_error(Ecosystem::StrategyError)
-          end
+        it "raises Content::Ecosystem::StrategyError" do
+          expect{
+            ecosystem.uuid
+          }.to raise_error(Content::Ecosystem::StrategyError)
         end
       end
     end
+
 
     context "fetching books" do
-      let(:ecosystem) {
-        Ecosystem.new(strategy: strategy)
-      }
+      it "delegates to its strategy" do
+        ecosystem.books
+        expect(strategy).to have_received(:books)
+      end
 
-      let(:strategy) {
-        double("strategy").tap{ |dbl|
-          allow(dbl).to receive(:books).with(no_args)
-                       .and_return(strategy_books)
+      context "strategy returns Content::Ecosystem::Books" do
+        let(:strategy_books) {
+          [ valid_book, valid_book ]
         }
-      }
 
-      let(:strategy_books) {
-        [ Ecosystem::Book.new(strategy: Object.new),
-          Ecosystem::Book.new(strategy: Object.new) ]
-      }
-
-      context "delegation" do
-        it "delegates to its strategy" do
-          ecosystem.books
-          expect(strategy).to have_received(:books).with(no_args)
+        it "returns the strategy's books" do
+          books = ecosystem.books
+          expect(books).to eq(strategy_books)
         end
+      end
 
-        context "strategy returns Ecosystem::Books" do
-          it "returns the strategy's books" do
-            expect(ecosystem.books).to eq(strategy_books)
-          end
-        end
+      context "strategy doesn't return Content::Ecosystem::Books" do
+        let(:strategy_books) {
+          [ valid_book, invalid_book, valid_book ]
+        }
 
-        context "strategy doesn't return Ecosystem::Books" do
-          let(:strategy_books) {
-            [ Ecosystem::Book.new(strategy: Object.new),
-              Object.new,
-              Ecosystem::Book.new(strategy: Object.new) ]
-          }
-
-          it "raises Ecosystem::StrategyError" do
-            expect{
-              ecosystem.books
-            }.to raise_error(Ecosystem::StrategyError)
-          end
+        it "raises Content::Ecosystem::StrategyError" do
+          expect{
+            ecosystem.books
+          }.to raise_error(Content::Ecosystem::StrategyError)
         end
       end
     end
 
+
     context "fetching all exercises" do
-      let(:ecosystem) {
-        Ecosystem.new(strategy: strategy)
-      }
+      it "delegates to its strategy" do
+        ecosystem.exercises
+        expect(strategy).to have_received(:exercises)
+      end
 
-      let(:strategy) {
-        double("strategy").tap{ |dbl|
-          allow(dbl).to receive(:exercises).with(no_args)
-                       .and_return(strategy_exercises)
+      context "strategy returns Content::Ecosystem::Exercises" do
+        let(:strategy_exercises) {
+          [ valid_exercise, valid_exercise ]
         }
-      }
 
-      let(:strategy_exercises) {
-        [ Ecosystem::Exercise.new(strategy: Object.new),
-          Ecosystem::Exercise.new(strategy: Object.new) ]
-      }
+        it "returns the strategy's exercises" do
+          exercises = ecosystem.exercises
+          expect(exercises).to eq(strategy_exercises)
+        end
+      end
 
+      context "strategy doesn't return Content::Ecosystem::Exercises" do
+        let(:strategy_exercises) {
+          [ valid_exercise, invalid_exercise, valid_exercise ]
+        }
+
+        it "raises Ecosystem::StrategyError" do
+          expect{
+            ecosystem.exercises
+          }.to raise_error(Content::Ecosystem::StrategyError)
+        end
+      end
+    end
+
+
+    context "fetching reading core exercise pool" do
       context "delegation" do
-        it "delegates to its strategy" do
-          ecosystem.exercises
-          expect(strategy).to have_received(:exercises).with(no_args)
-        end
+        context "single page" do
+          let!(:page) { valid_page }
+          let!(:strategy_expected_pages) { [page] }
 
-        context "strategy returns Ecosystem::Exercises" do
-          it "returns the strategy's exercises" do
-            expect(ecosystem.exercises).to eq(strategy_exercises)
+          it "delegates to its strategy with the correct pages:" do
+            ecosystem.reading_core_exercises(pages: page)
+            expect(strategy).to have_received(:reading_core_exercises)
           end
         end
 
-        context "strategy doesn't return Ecosystem::Exercises" do
-          let(:strategy_exercises) {
-            [ Ecosystem::Exercise.new(strategy: Object.new),
-              Object.new,
-              Ecosystem::Exercise.new(strategy: Object.new) ]
-          }
+        context "multiple pages" do
+          let!(:pages) { [valid_page, valid_page] }
+          let!(:strategy_expected_pages) { pages }
 
-          it "raises Ecosystem::StrategyError" do
-            expect{
-              ecosystem.exercises
-            }.to raise_error(Ecosystem::StrategyError)
+          it "delegates to its strategy with the correct pages:" do
+            ecosystem.reading_core_exercises(pages: pages)
+            expect(strategy).to have_received(:reading_core_exercises)
           end
+        end
+      end
+
+      context "strategy returns Content::Ecosystem::Exercises" do
+        let!(:pages) { [valid_page, valid_page] }
+        let(:strategy_reading_core_exercises) { [ valid_exercise, valid_exercise ] }
+
+        it "returns the strategy's exercises" do
+          exercises = ecosystem.reading_core_exercises(pages: pages)
+          expect(exercises).to eq(strategy_reading_core_exercises)
+        end
+      end
+
+      context "strategy doesn't return Content::Ecosystem::Exercises" do
+        let!(:pages) { [valid_page, valid_page] }
+        let(:strategy_reading_core_exercises) { [ valid_exercise, invalid_exercise, valid_exercise ] }
+
+        it "raises Content::Ecosystem::StrategyError" do
+          expect{
+            ecosystem.reading_core_exercises(pages: pages)
+          }.to raise_error(Content::Ecosystem::StrategyError)
+        end
+      end
+
+      context "invalid pages:" do
+        let!(:pages) { [valid_page, invalid_page, valid_page] }
+
+        it "raises ArgumentError" do
+          expect{
+            ecosystem.reading_core_exercises(pages: pages)
+          }.to raise_error(ArgumentError)
         end
       end
     end
