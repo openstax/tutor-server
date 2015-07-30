f143d1dee9024c7cc0276da15227e66cc401bef6
Kevin Burleigh <klb@kindling>
2015-07-30 06:10:00 -0500

    add Content::Ecosystem#uuid and specs

--- spec/subsystems/content/ecosystem_spec.rb
+++ spec/subsystems/content/ecosystem_spec.rb
@@ -11,7 +11,49 @@ module Content
       end
     end
 
+    context "unique identifier" do
+      let(:ecosystem) {
+        Ecosystem.new(strategy: strategy)
+      }
+
+      let(:strategy) {
+        double("strategy").tap{ |dbl|
+          allow(dbl).to receive(:uuid).with(no_args)
+                       .and_return(uuid)
+        }
+      }
+
+      let(:uuid) { Ecosystem::Uuid.new }
+
+      context "delegation" do
+        it "delegates to its strategy" do
+          ecosystem.uuid
+          expect(strategy).to have_received(:uuid).with(no_args)
+        end
+
+        context "strategy returns Ecosystem::Uuid" do
+          it "returns the strategy's uuid" do
+            expect(ecosystem.uuid).to eq(uuid)
+          end
+        end
+
+        context "strategy doesn't return an Ecosystem::Uuid" do
+          let(:uuid) { Object.new }
+
+          it "raises Ecosystem::StrategyError" do
+            expect{
+              ecosystem.uuid
+            }.to raise_error(Ecosystem::StrategyError)
+          end
+        end
+      end
+    end
+
     context "fetching books" do
+      let(:ecosystem) {
+        Ecosystem.new(strategy: strategy)
+      }
+
       let(:strategy) {
         double("strategy").tap{ |dbl|
           allow(dbl).to receive(:books).with(no_args)
@@ -19,31 +61,25 @@ module Content
         }
       }
 
-      let(:ecosystem) {
-        Ecosystem.new(strategy: strategy)
+      let(:strategy_books) {
+        [ Ecosystem::Book.new(strategy: Object.new),
+          Ecosystem::Book.new(strategy: Object.new) ]
       }
 
-      context "happy paths" do
+      context "delegation" do
+        it "delegates to its strategy" do
+          ecosystem.books
+          expect(strategy).to have_received(:books).with(no_args)
+        end
+
         context "strategy returns Ecosystem::Books" do
-          let!(:strategy_books) {
-            [ Ecosystem::Book.new(strategy: Object.new),
-              Ecosystem::Book.new(strategy: Object.new) ]
-          }
-
-          it "delegates to its strategy" do
-            ecosystem.books
-            expect(strategy).to have_received(:books).with(no_args)
-          end
-
           it "returns the strategy's books" do
             expect(ecosystem.books).to eq(strategy_books)
           end
         end
-      end
 
-      context "error paths" do
         context "strategy doesn't return Ecosystem::Books" do
-          let!(:strategy_books) {
+          let(:strategy_books) {
             [ Ecosystem::Book.new(strategy: Object.new),
               Object.new,
               Ecosystem::Book.new(strategy: Object.new) ]
