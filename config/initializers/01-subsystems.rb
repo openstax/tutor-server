d4aa3d4f3a65724ef61cba9ef6cc23b21e0ac84d
Kevin Burleigh <klb@kindling>
2015-07-30 21:30:14 -0500

    add Ecosystem subsystem to initializers

--- config/initializers/01-subsystems.rb
+++ config/initializers/01-subsystems.rb
@@ -7,4 +7,4 @@ require 'entity/role'
 # Only these namespaces have been configured and should be extended
 # Once they are all configured, we can remove this line entirely,
 # but will still need to require the subsystems on rails boot, before any models are loaded
-Tutor::SubSystems.valid_namespaces = %w(content course_content course_profile entity tasks role course_membership course_detail)
+Tutor::SubSystems.valid_namespaces = %w(content course_content course_profile ecosystem entity tasks role course_membership course_detail)
