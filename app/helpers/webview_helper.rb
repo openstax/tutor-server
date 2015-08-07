b92690be9e89e8f94d2ffb7889ebdc1e3926ce4e
Nathan Stitt <nathan@stitt.org>
2015-08-06 21:31:13 -0500

    Provide bootstrap data to app via script tag

---
+++ app/helpers/webview_helper.rb
@@ -0,0 +1,13 @@
+module WebviewHelper
+
+  # Generates data for the FE to read as it boots up
+  def bootstrap_data
+    {
+      user: Api::V1::UserProfileRepresenter.new(current_user),
+      courses: Api::V1::CoursesRepresenter.new(
+        CollectCourseInfo[user: current_user.entity_user, with: [:roles, :periods]]
+      )
+    }
+  end
+
+end
