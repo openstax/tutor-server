2bc423b19efd2b32c9d7b8aa2fe1afb177374f9b
Joe Sak <joe@joesak.com>
2015-08-10 12:47:42 -0400

    Update fullname getting in teacher names

--- app/routines/get_teacher_names.rb
+++ app/routines/get_teacher_names.rb
@@ -1,20 +1,20 @@
 class GetTeacherNames
-  lev_routine
+  lev_routine express_output: :teacher_names
 
   uses_routine GetCourseTeacherUsers,
     translations: { outputs: { type: :verbatim } },
     as: :get_teacher_users
 
-  uses_routine UserProfile::GetUserFullNames,
-    translations: { outputs: { map: {full_names: :teacher_names} } },
-    as: :get_full_names
+  uses_routine UserProfile::GetProfiles,
+    translations: { outputs: { type: :verbatim } },
+    as: :get_profiles
 
   protected
 
   def exec(course_id)
     course = Entity::Course.find(course_id)
     run(:get_teacher_users, course)
-    run(:get_full_names, outputs[:teachers])
-    outputs[:teacher_names] = outputs[:teacher_names].collect(&:full_name).sort
+    run(:get_profiles, users: outputs[:teachers])
+    outputs[:teacher_names] = outputs[:profiles].collect(&:full_name).sort
   end
 end
