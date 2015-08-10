106466f21f154fe88575832c8078276f551c474d
Joe Sak <joe@joesak.com>
2015-08-10 14:35:17 -0400

    Merge GetProfiles into SearchProfiles

--- spec/subsystems/user_profile/search_profiles_spec.rb
+++ spec/subsystems/user_profile/search_profiles_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe UserProfile::SearchProfiles do
                                      full_name: 'Stan Dup' }
 
   it 'searches username and name' do
-    results = described_class[search_term: '%ch%']
+    results = described_class[search: '%ch%']
     expect(results.total_items).to eq 2
     expect(results.items).to eq [ {
       'id' => user_2.id,
@@ -39,7 +39,7 @@ RSpec.describe UserProfile::SearchProfiles do
   end
 
   it 'paginates results' do
-    results = described_class[search_term: '%', per_page: 2, page: 1]
+    results = described_class[search: '%', per_page: 2, page: 1]
     expect(results.total_items).to eq 3
     expect(results.items).to eq [ {
       'id' => user_2.id,
@@ -57,9 +57,29 @@ RSpec.describe UserProfile::SearchProfiles do
       'username' => 'student'
     } ]
 
-    results = described_class[search_term: '%', per_page: 2, page: 2]
+    results = described_class[search: '%', per_page: 2, page: 2]
     expect(results.total_items).to eq 3
     expect(results.items).to eq [ {
+      'id' => admin.id,
+      'account_id' => admin.account.id,
+      'entity_user_id' => admin.entity_user.id,
+      'full_name' => 'Administrator User',
+      'name' => 'Administrator User',
+      'username' => 'admin'
+    } ]
+  end
+
+  it 'returns profiles for their entity users' do
+    profiles = described_class[search: [admin.entity_user, user_1.entity_user]]
+    expect(profiles.total_items).to eq(2)
+    expect(profiles.items).to eq [ {
+      'id' => user_1.id,
+      'account_id' => user_1.account.id,
+      'entity_user_id' => user_1.entity_user.id,
+      'full_name' => 'Chris Mass',
+      'name' => 'Chris Mass',
+      'username' => 'student'
+    }, {
       'id' => admin.id,
       'account_id' => admin.account.id,
       'entity_user_id' => admin.entity_user.id,
