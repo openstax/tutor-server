505608d646838c7b0f25d1528cd0cf15384b9b18
Joe Sak <joe@joesak.com>
2015-08-14 09:31:33 -0400

    Add tests for book tocs, book toc, chapter toc, page toc representers

--- spec/representers/api/v1/book_toc_representer_spec.rb
+++ spec/representers/api/v1/book_toc_representer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BookTocRepresenter, type: :representer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book) { { id: 1,
+                 cnx_id: '123abc',
+                 title: 'Good book',
+                 type: 'foo',
+                 chapter_section: [4, 1] } }
+
+  subject(:represented) { described_class.new(Hashie::Mash.new(book)).to_hash }
+
+  it 'sets the type to part' do
+    expect(represented['type']).to eq('part')
+  end
 end
