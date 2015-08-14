48b74b8593220f716652284aa5932b76a52326e3
Dante Soares <dms3@rice.edu>
2015-08-13 19:39:31 -0500

    Ecosystem mapping is now validated when an ecosystem is added to a course

--- app/subsystems/course_content/add_ecosystem_to_course.rb
+++ app/subsystems/course_content/add_ecosystem_to_course.rb
@@ -1,9 +1,12 @@
 class CourseContent::AddEcosystemToCourse
-  lev_routine
+
+  lev_routine express_output: :ecosystem_map
 
   protected
 
-  def exec(course:, ecosystem:, remove_other_ecosystems: false)
+  def exec(course:, ecosystem:, remove_other_ecosystems: false,
+           ecosystem_strategy_class: ::Content::Strategies::Direct::Ecosystem,
+           map_strategy_class: ::Content::Strategies::Generated::Map)
     fatal_error(code: :ecosystem_already_set,
                 message: 'The given ecosystem is already active for the given course') \
       if course.reload.ecosystems.first.try(:id) == ecosystem.id
@@ -14,5 +17,23 @@ class CourseContent::AddEcosystemToCourse
     )
     course.course_ecosystems << course_ecosystem
     transfer_errors_from(course_ecosystem, {type: :verbatim}, true)
+
+    # Create a mapping from the old course ecosystems to the new one and validate it
+    from_ecosystems = course.course_ecosystems.collect do |course_ecosystem|
+      strategy = ecosystem_strategy_class.new(course_ecosystem.ecosystem)
+      ::Content::Ecosystem.new(strategy: strategy)
+    end
+    to_ecosystem = case ecosystem
+    when ::Content::Ecosystem
+      ecosystem
+    else
+      strategy = ecosystem_strategy_class.new(ecosystem)
+      ::Content::Ecosystem.new(strategy: strategy)
+    end
+
+    outputs[:ecosystem_map] = ::Content::Map.create!(from_ecosystems: from_ecosystems,
+                                                     to_ecosystem: to_ecosystem,
+                                                     strategy_class: map_strategy_class)
   end
+
 end
