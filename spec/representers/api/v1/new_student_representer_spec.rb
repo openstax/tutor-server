168062d510e2bd4cc9f6862234861c98de40b4da
Joe Sak <joe@joesak.com>
2015-08-14 10:24:42 -0400

    Add tests for new student, answer, exercise stats representers

--- spec/representers/api/v1/new_student_representer_spec.rb
+++ spec/representers/api/v1/new_student_representer_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::NewStudentRepresenter, type: :representer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:student) { { id: 1,
+                    course_membership_period_id: 2,
+                    entity_role_id: 3,
+                    email: 'new@student.com',
+                    username: 'student',
+                    password: 'secret',
+                    first_name: 'Bob',
+                    last_name: 'Student',
+                    full_name: 'Bob A. Student',
+                    deidentifier: 'uhhhhhh',
+                    'active?' => true } }
+
+  subject(:represented) { described_class.new(Hashie::Mash.new(student)).to_hash }
+
+  it 'renames course_membership_period_id to period_id' do
+    expect(represented['period_id']).to eq('2')
+  end
+
+  it 'renames entity_role_id to role_id' do
+    expect(represented['role_id']).to eq('3')
+  end
+
+  it 'renames active? to is_active' do
+    expect(represented['is_active']).to eq(true)
+  end
 end
