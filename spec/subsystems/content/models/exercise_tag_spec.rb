d3857edaf138ffba08557dfccc4460d40a7628de
Dante Soares <dms3@rice.edu>
2015-08-17 16:35:54 -0500

    Disabled ExerciseTag validations and added a comment explaining why

--- spec/subsystems/content/models/exercise_tag_spec.rb
+++ spec/subsystems/content/models/exercise_tag_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Content::Models::ExerciseTag, :type => :model do
+RSpec.describe Content::Models::ExerciseTag, type: :model do
   subject { FactoryGirl.create :content_exercise_tag }
 
   it { is_expected.to belong_to(:exercise) }
   it { is_expected.to belong_to(:tag) }
 
-  it { is_expected.to validate_presence_of(:exercise) }
-  it { is_expected.to validate_presence_of(:tag) }
+  # These validations are by far the most consuming for all specs and the demo script
+  # They are also enforced by the DB through non-null columns,
+  # foreign key constraints and unique indices
+  # Therefore, we decided to disable them until a bulk validation solution is available
+  # it { is_expected.to validate_presence_of(:exercise) }
+  # it { is_expected.to validate_presence_of(:tag) }
 
-  it { is_expected.to validate_uniqueness_of(:tag)
-                        .scoped_to(:content_exercise_id) }
+  # it { is_expected.to validate_uniqueness_of(:tag).scoped_to(:content_exercise_id) }
 end
