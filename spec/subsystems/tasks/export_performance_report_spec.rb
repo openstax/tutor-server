9821132c2a78eebd166f64b317eb9a4178c7333f
Dante Soares <dms3@rice.edu>
2015-08-19 18:39:40 -0500

    Made ExportPerformanceReport able to handle missing tasks

--- spec/subsystems/tasks/export_performance_report_spec.rb
+++ spec/subsystems/tasks/export_performance_report_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 require 'vcr_helper'
 
-RSpec.describe Tasks::ExportPerformanceReport, speed: :slow, vcr: VCR_OPTS do
-  let(:course) { CreateCourse[name: 'Physics'] }
-  let(:teacher) { FactoryGirl.create :user_profile }
+RSpec.describe Tasks::ExportPerformanceReport, speed: :slow do
 
-  before(:each) do
-    ecosystem = FetchAndImportBookAndCreateEcosystem[id: '93e2b09d-261c-4007-a987-0b3062fe154b']
-    SetupPerformanceReportData[course: course, teacher: teacher, ecosystem: ecosystem]
+  before(:all) do
+    DatabaseCleaner.start
+
+    VCR.use_cassette("Tasks_ExportPerformanceReport/with_book", VCR_OPTS) do
+      @ecosystem = FetchAndImportBookAndCreateEcosystem[
+        id: '93e2b09d-261c-4007-a987-0b3062fe154b'
+      ]
+    end
+    @course = CreateCourse[name: 'Physics']
+    CourseContent::AddEcosystemToCourse.call(course: @course, ecosystem: @ecosystem)
+
+    @teacher = FactoryGirl.create :user_profile
+    SetupPerformanceReportData[course: @course, teacher: @teacher, ecosystem: @ecosystem]
+    @role = GetUserCourseRoles[course: @course, user: @teacher.entity_user].first
+  end
+
+  after(:each) do
+    File.delete(@output_filename) if !@output_filename.nil? && File.exist?(@output_filename)
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
   end
 
   it 'does not blow up' do
-    role = GetUserCourseRoles[course: course, user: teacher.entity_user].first
     expect {
-      output_filename = described_class[role: role, course: course]
-      File.delete(output_filename) if File.exist? output_filename
+      @output_filename = described_class[role: @role, course: @course]
     }.not_to raise_error
   end
+
+  it 'does not blow up when a student was not assigned a particular task' do
+    @course.students.first.role.taskings.first.task.destroy
+    expect {
+      @output_filename = described_class[role: @role, course: @course]
+    }.not_to raise_error
+  end
+
 end
