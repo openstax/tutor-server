af2f66c6033b8f201a49ba9fe6ee23bdc75a36b0
Joe Sak <joe@joesak.com>
2015-08-20 12:34:41 -0400

    Add tests for HtmlTreeOperations, remove redundant comments

--- lib/html_tree_operations.rb
+++ lib/html_tree_operations.rb
@@ -3,57 +3,37 @@ module HtmlTreeOperations
   def recursive_compact(node, stop_node)
     return if node == stop_node
 
-    # Get parent
     parent = node.parent
-
-    # Remove current node
     node.remove
 
-    # Remove parent if empty
-    recursive_compact(parent, stop_node) if parent.content.blank?
+    if parent && parent.content.blank?
+      recursive_compact(parent, stop_node)
+    end
   end
 
   # Recursively removes all siblings before a node and its parents
   # Returns the stop_node
   def remove_before(node, stop_node)
-    # Stopping condition
     return if node == stop_node
 
-    # Get parent
-    parent = node.parent
-
-    # Get siblings
-    siblings = parent.children
-
-    # Get node's index
-    index = siblings.index(node)
-
-    # Remove siblings before node
-    parent.children = siblings.slice(index..-1)
-
-    # Remove nodes after the parent
-    remove_before(parent, stop_node)
+    if parent = node.parent
+      siblings = parent.children
+      index = siblings.index(node)
+      parent.children = siblings.slice(index..-1)
+      remove_before(parent, stop_node)
+    end
   end
 
   # Recursively removes all siblings after a node and its parents
   # Returns the stop_node
   def remove_after(node, stop_node)
-    # Stopping condition
     return if node == stop_node
 
-    # Get parent
-    parent = node.parent
-
-    # Get siblings
-    siblings = parent.children
-
-    # Get node's index
-    index = siblings.index(node)
-
-    # Remove siblings after node
-    parent.children = siblings.slice(0..index)
-
-    # Remove nodes after the parent
-    remove_after(parent, stop_node)
+    if parent = node.parent
+      siblings = parent.children
+      index = siblings.index(node)
+      parent.children = siblings.slice(0..index)
+      remove_after(parent, stop_node)
+    end
   end
 end
