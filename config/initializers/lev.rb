0616297bbb18bbe1baf44e6593845027ee9c54c6
Joe Sak <joe@joesak.com>
2015-08-31 11:41:22 -0400

    Clear lev jobs after 1 week

--- config/initializers/lev.rb
+++ config/initializers/lev.rb
@@ -4,6 +4,7 @@ Lev.configure do |config|
   config.raise_fatal_errors = false
   config.job_store = ActiveSupport::Cache::RedisStore.new(
     url: redis_secrets['url'],
-    namespace: redis_secrets['namespaces']['lev']
+    namespace: redis_secrets['namespaces']['lev'],
+    expires_in: 1.week
   )
 end
