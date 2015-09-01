b4fe6a84ab5f637f6124919162323e7d4c3aef78
Dante Soares <dms3@rice.edu>
2015-09-01 10:47:13 -0500

    DistributeTasks no longer deletes assignments after the open date; Added specs

--- app/routines/distribute_tasks.rb
+++ app/routines/distribute_tasks.rb
@@ -22,25 +22,36 @@ class DistributeTasks
     Entity::Task.import! entity_tasks, recursive: true
   end
 
-  def exec(task_plan)
+  def exec(task_plan, publish_time = Time.now)
     # Lock the TaskPlan to prevent concurrent update/publish
     task_plan.lock!
 
-    # Delete pre-existing assignments
-    task_plan.tasks.each{ |tt| tt.entity_task.destroy } unless task_plan.tasks.empty?
+    tasks = task_plan.tasks.preload(:entity_task, { taskings: :role })
+
+    # Delete pre-existing (unopened) assignments
+    tasks.each do |tt|
+      next if tt.opens_at <= publish_time
+      tt.entity_task.destroy
+    end
+
+    tasked_taskees = tasks.select{ |tt| !tt.destroyed? }
+                          .flat_map{ |tt| tt.taskings.collect{ |tk| tk.role } }
 
     tasking_plans = run(:get_tasking_plans, task_plan).outputs.tasking_plans
 
-    taskees = tasking_plans.collect { |tp| tp.target }
-    opens_ats = tasking_plans.collect { |tp| tp.opens_at }
-    due_ats = tasking_plans.collect { |tp| tp.due_at }
+    taskees = tasking_plans.collect{ |tp| tp.target }
+    opens_ats = tasking_plans.collect{ |tp| tp.opens_at }
+    due_ats = tasking_plans.collect{ |tp| tp.due_at }
+
+    # Exclude students that already had the assignment
+    untasked_taskees = taskees - tasked_taskees
 
     assistant = task_plan.assistant
 
     # Call the assistant code to create Tasks, then distribute them
-    entity_tasks = assistant.build_tasks(task_plan: task_plan, taskees: taskees)
+    entity_tasks = assistant.build_tasks(task_plan: task_plan, taskees: untasked_taskees)
     entity_tasks.each_with_index do |entity_task, ii|
-      role = taskees[ii]
+      role = untasked_taskees[ii]
       tasking = Tasks::Models::Tasking.new(
         task: entity_task,
         role: role,
@@ -56,7 +67,8 @@ class DistributeTasks
 
     save(entity_tasks)
 
-    task_plan.update_column(:published_at, Time.now) if task_plan.persisted?
+    task_plan.update_column(:published_at, publish_time) \
+      if task_plan.published_at.nil? && task_plan.persisted?
 
     outputs[:entity_tasks] = entity_tasks
   end
--- spec/routines/distribute_tasks_spec.rb
+++ spec/routines/distribute_tasks_spec.rb
@@ -11,10 +11,11 @@ RSpec.describe DistributeTasks, type: :routine do
   }
   let!(:task_plan) {
     task_plan = FactoryGirl.build(:tasks_task_plan, owner: course)
-    task_plan.tasking_plans.first.target = user
+    task_plan.tasking_plans.first.target = period.to_model
     task_plan.save!
     task_plan
   }
+  let!(:new_user)  { FactoryGirl.create :user_profile }
 
   context 'unpublished task_plan' do
     it "creates tasks for the task_plan" do
@@ -24,7 +25,7 @@ RSpec.describe DistributeTasks, type: :routine do
       expect(task_plan.tasks.size).to eq 1
     end
 
-    it "sets the published_at field" do
+    it 'sets the published_at field' do
       result = DistributeTasks.call(task_plan)
       expect(result.errors).to be_empty
       expect(task_plan.reload.published_at).to be_within(1.second).of(Time.now)
@@ -35,22 +36,59 @@ RSpec.describe DistributeTasks, type: :routine do
     before(:each) do
       DistributeTasks.call(task_plan)
       task_plan.reload
+      AddUserAsPeriodStudent.call(user: new_user.entity_user, period: period)
     end
 
-    it "rebuilds the tasks for the task_plan" do
-      expect(task_plan.tasks.size).to eq 1
-      old_task = task_plan.tasks.first
+    context 'before the open date' do
+      before(:each) do
+        opens_at = Time.now.tomorrow
+        task_plan.tasking_plans.each{ |tp| tp.update_attribute(:opens_at, opens_at) }
+        task_plan.tasks.each{ |tt| tt.update_attribute(:opens_at, opens_at) }
+      end
 
-      result = DistributeTasks.call(task_plan)
-      expect(result.errors).to be_empty
-      expect(task_plan.reload.tasks.size).to eq 1
-      expect(task_plan.tasks.first).not_to eq old_task
+      it "rebuilds the tasks for the task_plan" do
+        expect(task_plan.tasks.size).to eq 1
+        old_task = task_plan.tasks.first
+
+        result = DistributeTasks.call(task_plan)
+        expect(result.errors).to be_empty
+        expect(task_plan.reload.tasks.size).to eq 2
+        expect(task_plan.tasks).not_to include old_task
+      end
+
+      it 'does not set the published_at field' do
+        old_published_at = task_plan.published_at
+        publish_time = Time.now
+        result = DistributeTasks.call(task_plan, publish_time)
+        expect(result.errors).to be_empty
+        expect(task_plan.reload.published_at).to eq old_published_at
+      end
     end
 
-    it "sets the published_at field" do
-      result = DistributeTasks.call(task_plan)
-      expect(result.errors).to be_empty
-      expect(task_plan.reload.published_at).to be_within(1.second).of(Time.now)
+    context 'after the open date' do
+      before(:each) do
+        opens_at = Time.now.yesterday
+        task_plan.tasking_plans.each{ |tp| tp.update_attribute(:opens_at, opens_at) }
+        task_plan.tasks.each{ |tt| tt.update_attribute(:opens_at, opens_at) }
+      end
+
+      it 'does not rebuild existing tasks for the task_plan' do
+        expect(task_plan.tasks.size).to eq 1
+        old_task = task_plan.tasks.first
+
+        result = DistributeTasks.call(task_plan)
+        expect(result.errors).to be_empty
+        expect(task_plan.reload.tasks.size).to eq 2
+        expect(task_plan.tasks).to include old_task
+      end
+
+      it 'does not set the published_at field' do
+        old_published_at = task_plan.published_at
+        publish_time = Time.now
+        result = DistributeTasks.call(task_plan, publish_time)
+        expect(result.errors).to be_empty
+        expect(task_plan.reload.published_at).to eq old_published_at
+      end
     end
   end
 end
