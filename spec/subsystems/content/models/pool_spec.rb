e17fad6ece2702fefc0bdd02ce790b678eb4c577
Dante Soares <dms3@rice.edu>
2015-09-01 15:24:19 -0500

    Added Content::Models::Pool specs

---
+++ spec/subsystems/content/models/pool_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Content::Models::Pool, type: :model do
+  subject(:pool) { FactoryGirl.create :content_pool }
+
+  it { is_expected.to belong_to(:ecosystem) }
+
+  it { is_expected.to validate_presence_of(:ecosystem) }
+  it { is_expected.to validate_presence_of(:pool_type) }
+  it { is_expected.to validate_presence_of(:uuid) }
+
+  it { is_expected.to validate_uniqueness_of(:uuid) }
+
+  it 'returns exercises' do
+    exercises = 10.times.collect{ FactoryGirl.create :content_exercise }
+    pool.content_exercise_ids = exercises.collect(&:id)
+    expect(Set.new pool.exercises).to eq Set.new exercises
+  end
+end
