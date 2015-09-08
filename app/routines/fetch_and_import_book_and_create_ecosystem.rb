20c6d40e6d341b5b200a4248d69651d0733fb113
Dante Soares <dms3@rice.edu>
2015-09-08 14:09:03 -0500

    Addressed PR comments

--- app/routines/fetch_and_import_book_and_create_ecosystem.rb
+++ app/routines/fetch_and_import_book_and_create_ecosystem.rb
@@ -10,8 +10,8 @@ class FetchAndImportBookAndCreateEcosystem
   # Returns a Content::Ecosystem containing a book obtained from the given CNX id
   def exec(book_cnx_id:, ecosystem_title: nil, exercise_uids: nil)
     cnx_book = OpenStax::Cnx::V1.book(id: book_cnx_id)
-    ecosystem_title ||= "#{cnx_book.title} (#{cnx_book.uuid}@#{cnx_book.version}) - #{Time.now}"
-    outputs[:ecosystem] = Content::Ecosystem.create!(title: ecosystem_title)
+    eco_title ||= "#{cnx_book.title} (#{cnx_book.uuid}@#{cnx_book.version}) - #{Time.now.utc}"
+    outputs[:ecosystem] = Content::Ecosystem.create!(title: eco_title)
     run(:import_book, cnx_book: cnx_book,
                       ecosystem: outputs[:ecosystem],
                       exercise_uids: exercise_uids)
