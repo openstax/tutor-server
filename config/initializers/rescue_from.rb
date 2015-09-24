4087e8891c434589ee19941034b4ce2c78051f43
Joe Sak <joe@joesak.com>
2015-09-24 16:38:01 -0400

    Update the default initializer

--- config/initializers/rescue_from.rb
+++ config/initializers/rescue_from.rb
@@ -17,17 +17,6 @@ OpenStax::RescueFrom.configure do |config|
   #                           %{"OpenStax Tutor" <noreply@openstax.org>}
   # config.exception_recipients = ENV['EXCEPTION_RECIPIENTS'] ||
   #                                 %w{tutor-notifications@openstax.org}
-
-  # Append to the exception lists and maps
-  #
-  # config.exception_status_codes['ArgumentError'] = :unprocessable_entity
-  # config.friendly_status_messages[:bad_request] = 'Your request was not good.'
-  # config.non_notifying_exceptions += ['ArgumentError']
-  # config.exception_extras['ArgumentError'] = ->(exception) {
-  #   { headers: exception.response.headers,
-  #     status: exception.response.status,
-  #     body: exception.response.body }
-  # }
 end
 
 # OpenStax::RescueFrom#register_exception default options:
