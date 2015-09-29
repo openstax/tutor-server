8764fb8a83046ca522f56e3026d76e24f81acbab
Nathan Stitt <nathan@stitt.org>
2015-09-28 21:26:12 -0500

    Add auto_assign option to demo importer
    Generates a mass import of assignments

--- lib/tasks/demo/work.rb
+++ lib/tasks/demo/work.rb
@@ -14,40 +14,49 @@ class DemoWork < DemoBase
     set_random_seed(random_seed)
 
     ContentConfiguration[book.to_sym].each do | content |
+
       content.assignments.each do | assignment |
         next if assignment.draft # Draft plans haven't been distributed so can't be worked
-
-        task_plan = Tasks::Models::TaskPlan.where(owner: content.course, title: assignment.title)
-                                           .order(created_at: :desc).first!
-
-        tasks_profile = build_tasks_profile(
-          students: assignment.periods.flat_map{ |period| period.students.map(&:to_a) },
-          assignment_type: assignment.type.to_sym,
-          step_types: assignment.step_types,
-        )
-        log("Working assignment: #{assignment.title}")
-        task_plan.tasks.preload([{taskings: {role: {profile: :account}}},
-                                 {task_steps: [:tasked, :task]}])
-                       .each_with_index do | task, index |
-          profile = task.taskings.first.role.profile
-          strategy = ::User::Strategies::Direct::User.new(profile)
-          user = ::User::User.new(strategy: strategy)
-          task_profile = tasks_profile[user.id]
-
-          unless task_profile
-            raise "#{assignment.title} period #{period.id} has no responses for task #{index} for user #{user.id} #{user.username}"
-          end
-          lateness = assignment.late ? assignment.late[task_profile.initials] : nil
-          worked_at = task.due_at + (lateness ? lateness : -60)
-
-          Timecop.freeze(worked_at) do
-            work_task(task: task, responses: task_profile.responses)
-          end
-        end
-
+        work_assignment(content, assignment)
       end
+
+      content.auto_assign.each do | settings |
+        each_from_auto_assignment(content, settings) do | assignment |
+          work_assignment(content, assignment)
+        end
+      end
+
     end
 
   end
+
+  def work_assignment(content, assignment)
+    task_plan = Tasks::Models::TaskPlan.where(owner: content.course, title: assignment.title)
+                .order(created_at: :desc).first!
+
+    tasks_profile = build_tasks_profile(
+      students: assignment.periods.flat_map{ |period| period.students.map(&:to_a) },
+      assignment_type: assignment.type.to_sym,
+      step_types: assignment.step_types,
+    )
+    log("Working assignment: #{assignment.title}")
+    task_plan.tasks.preload([{taskings: {role: {user: {profile: :account}}}},
+                             {task_steps: [:tasked, :task]}])
+      .each_with_index do | task, index |
+      user = task.taskings.first.role.user
+      profile = tasks_profile[ user.profile.id ]
+
+      unless profile
+        raise "#{assignment.title} period #{period.id} has no responses for task #{index} for user #{user.profile.id} #{user.username}"
+      end
+      lateness = assignment.late ? assignment.late[profile.initials] : nil
+      worked_at = task.due_at + (lateness ? lateness : -60)
+
+      Timecop.freeze(worked_at) do
+        work_task(task: task, responses: profile.responses)
+      end
+    end
+  end
+
 
 end
