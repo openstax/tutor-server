f9dbe43cfb862b925bc56d8173621a8bc2a005b4
Amanda Shih <amanda.shih@rice.edu>
2015-09-30 12:57:45 -0500

    revert indentation changes

--- app/subsystems/tasks/assistants/event_assistant.rb
+++ app/subsystems/tasks/assistants/event_assistant.rb
@@ -1,29 +1,31 @@
-class Tasks::Assistants::EventAssistant
+module Tasks
+  module Assistants
+    class EventAssistant
+      def self.schema
+        '{
+          "type": "object",
+          "required": [],
+          "properties": {},
+          "additionalProperties": false
+        }'
+      end
 
-  def self.schema
-    '{
-      "type": "object",
-      "required": [],
-      "properties": {},
-      "additionalProperties": false
-    }'
+      def initialize(task_plan:, taskees:)
+        @task_plan = task_plan
+        @taskees = taskees
+      end
+
+      def build_tasks
+        taskees.collect {
+          Tasks::BuildTask[task_plan: task_plan,
+                           task_type: :event,
+                           title: task_plan.title,
+                           description: task_plan.description]
+        }.flat_map(&:entity_task)
+      end
+
+      private
+      attr_reader :task_plan, :taskees
+    end
   end
-
-  def initialize(task_plan:, taskees:)
-    @task_plan = task_plan
-    @taskees = taskees
-  end
-
-  def build_tasks
-    taskees.collect {
-      Tasks::BuildTask[task_plan: task_plan,
-                       task_type: :event,
-                       title: task_plan.title,
-                       description: task_plan.description]
-    }.flat_map(&:entity_task)
-  end
-
-  private
-  attr_reader :task_plan, :taskees
-
 end
