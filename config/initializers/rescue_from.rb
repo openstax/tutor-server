5583122bb0b006c679ff4b4435f6162207c6e2cf
JP Slavinsky <jpslav@gmail.com>
2015-10-06 19:16:37 -0700

    aligned secrets with deployment file

--- config/initializers/rescue_from.rb
+++ config/initializers/rescue_from.rb
@@ -1,11 +1,13 @@
 require 'openstax_rescue_from'
 
+secrets = Rails.application.secrets
+
 OpenStax::RescueFrom.configure do |config|
   config.raise_exceptions = Rails.application.config.consider_all_requests_local
 
   config.app_name = 'Tutor'
-  config.app_env = ENV['APP_ENV']
-  config.contact_name = Rails.application.secrets['exceptions']['contact_name']
+  config.app_env = secrets.environment_name
+  config.contact_name = secrets.exception[:contact_name]
 
   # config.notifier = ExceptionNotifier
 
@@ -13,8 +15,8 @@ OpenStax::RescueFrom.configure do |config|
   # config.html_error_template_layout_name = 'application'
 
   # config.email_prefix = "[#{app_name}] (#{app_env}) "
-  config.sender_address = Rails.application.secrets['exceptions']['sender']
-  config.exception_recipients = Rails.application.secrets['exceptions']['recipients']
+  config.sender_address = secrets.exception[:sender]
+  config.exception_recipients = secrets.exception[:recipients]
 end
 
 # OpenStax::RescueFrom.register_exception('ExampleException',
