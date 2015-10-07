1cecf20cca7a401f24278ca558d91caafef58eac
Dante Soares <dms3@rice.edu>
2015-10-07 18:32:18 -0500

    Improved the trouble flag query to only display the trouble flag on the calendar if some page for some period would also have the flag

--- app/routines/get_course_task_plans.rb
+++ app/routines/get_course_task_plans.rb
@@ -8,10 +8,15 @@ class GetCourseTaskPlans
     outputs[:plans] = Tasks::Models::TaskPlan.where(owner: course)
     return unless include_trouble_flags
 
-    outputs[:trouble_plan_ids] = Set.new Tasks::Models::TaskPlan.joins(:tasks).group(:id).having{
-      (sum(tasks.completed_exercise_steps_count) > sum(tasks.exercise_steps_count)/4) & \
-      (sum(tasks.correct_exercise_steps_count) < sum(tasks.completed_exercise_steps_count)/2)
-    }.pluck(:id)
+    outputs[:trouble_plan_ids] = Set.new Tasks::Models::TaskPlan
+      .joins(tasks: [:taskings, tasked_exercises: :exercise])
+      .group([:id,
+              {tasks: {taskings: :course_membership_period_id}},
+              {tasks: {tasked_exercises: {exercise: :content_page_id}}}])
+      .having{
+        (sum(tasks.completed_exercise_steps_count) > sum(tasks.exercise_steps_count)/4) & \
+        (sum(tasks.correct_exercise_steps_count) < sum(tasks.completed_exercise_steps_count)/2)
+      }.distinct.pluck(:id)
   end
 
 end
