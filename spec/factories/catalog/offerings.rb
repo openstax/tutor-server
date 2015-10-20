0d448fa1e6ec44c9d5b0691f79f04bf565b3265e
Nathan Stitt <nathan@stitt.org>
2015-10-20 14:47:10 -0500

    Set pdf_url

--- spec/factories/catalog/offerings.rb
+++ spec/factories/catalog/offerings.rb
@@ -2,8 +2,9 @@ FactoryGirl.define do
   factory :catalog_offering, class: '::Catalog::Models::Offering' do
 
     identifier  { Faker::Lorem.word   }
-    webview_url { Faker::Internet.url }
     description { Faker::Company.bs   }
+    webview_url { Faker::Internet.url }
+    pdf_url     { Faker::Internet.url }
 
   end
 end
