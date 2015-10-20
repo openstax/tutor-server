271d5faa1a86fdce31e35cd4906725c50b4ff5d2
Dante Soares <dante.m.soares@rice.edu>
2015-10-20 15:51:09 -0500

    Readded enrollment_id field to enrollment_change

--- app/subsystems/course_membership/models/enrollment_change.rb
+++ app/subsystems/course_membership/models/enrollment_change.rb
@@ -1,13 +1,17 @@
 class CourseMembership::Models::EnrollmentChange < Tutor::SubSystems::BaseModel
   belongs_to :profile, subsystem: :user
-  belongs_to :period
+  belongs_to :enrollment # from
+  belongs_to :period     # to
 
   enum status: [ :pending, :succeeded, :failed ]
 
   validates :profile, presence: true
+  validates :enrollment, uniqueness: { allow_nil: true }
   validates :period, presence: true
 
-  def latest?
-    created_at >= profile.enrollment_changes.maximum(:created_at)
+  def from_period
+    enrollment.try(:period)
   end
+
+  alias_method :to_period, :period
 end
--- db/migrate/20151020183551_create_course_membership_enrollment_changes.rb
+++ db/migrate/20151020183551_create_course_membership_enrollment_changes.rb
@@ -5,6 +5,11 @@ class CreateCourseMembershipEnrollmentChanges < ActiveRecord::Migration
                    null: false,
                    foreign_key: { on_update: :cascade, on_delete: :cascade },
                    index: true
+      t.references :course_membership_enrollment,
+                   foreign_key: { on_update: :cascade, on_delete: :nullify },
+                   index: {
+                     name: 'index_course_membership_enrollments_on_enrollment_id', unique: true
+                   }
       t.references :course_membership_period,
                    null: false,
                    foreign_key: { on_update: :cascade, on_delete: :cascade },
@@ -14,9 +19,6 @@ class CreateCourseMembershipEnrollmentChanges < ActiveRecord::Migration
       t.datetime :enrollee_approved_at
 
       t.timestamps null: false
-
-      t.index [:user_profile_id, :created_at],
-              name: 'index_course_membership_enrollment_changes_on_p_id_and_c_at', unique: true
     end
   end
 end
