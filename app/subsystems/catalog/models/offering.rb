144c21d1df397e8c43fb53293dc1721368b2b8d9
Nathan Stitt <nathan@stitt.org>
2015-10-20 14:13:23 -0500

    Validate that identifier is unique

--- app/subsystems/catalog/models/offering.rb
+++ app/subsystems/catalog/models/offering.rb
@@ -2,7 +2,7 @@ class Catalog::Models::Offering < Tutor::SubSystems::BaseModel
 
   belongs_to :ecosystem, subsystem: :content
 
-  validates :identifier,  presence: true
+  validates :identifier,  presence: true, uniqueness: true
   validates :webview_url, presence: true
   validates :description, presence: true
 
--- spec/subsystems/catalog/models/offering_spec.rb
+++ spec/subsystems/catalog/models/offering_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Catalog::Models::Offering, :type => :model do
   it { is_expected.to validate_presence_of(:webview_url) }
   it { is_expected.to validate_presence_of(:description) }
 
+  it 'disallows duplicate identifiers' do
+    FactoryGirl.create(:catalog_offering, identifier: 'test')
+    expect{
+      FactoryGirl.create(:catalog_offering, identifier: 'test')
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
