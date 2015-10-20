aa720aceb272dde4f83cdb829e1ee413baed2a30
Dante Soares <dante.m.soares@rice.edu>
2015-10-20 15:59:07 -0500

    Added some EnrollmentChange validations

--- app/subsystems/course_membership/models/enrollment_change.rb
+++ app/subsystems/course_membership/models/enrollment_change.rb
@@ -8,10 +8,25 @@ class CourseMembership::Models::EnrollmentChange < Tutor::SubSystems::BaseModel
   validates :profile, presence: true
   validates :enrollment, uniqueness: { allow_nil: true }
   validates :period, presence: true
+  validate :same_profile, :different_period
 
   def from_period
     enrollment.try(:period)
   end
 
   alias_method :to_period, :period
+
+  protected
+
+  def same_profile
+    return if profile.nil? || enrollment.nil? || enrollment.student.role.profile == profile
+    errors.add(:base, 'cannot have an enrollment that belongs to a different user profile')
+    false
+  end
+
+  def different_period
+    return if enrollment.nil? || period.nil? || enrollment.period != period
+    errors.add(:base, 'cannot have an enrollment that belongs to the same period')
+    false
+  end
 end
