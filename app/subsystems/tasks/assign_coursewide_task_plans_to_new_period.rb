9592852992d7eb4be51f6f61a6c326ca96419bb7
Joe Sak <joe@joesak.com>
2015-10-20 16:15:26 -0400

    Clarify the private methods with more clarifying methods

--- app/subsystems/tasks/assign_coursewide_task_plans_to_new_period.rb
+++ app/subsystems/tasks/assign_coursewide_task_plans_to_new_period.rb
@@ -7,29 +7,32 @@ module Tasks
       @existing_period_ids = period.course.periods.flat_map(&:id) - [period.id]
 
       task_plans_with_matching_tasking_plans_across_periods.each do |task_plan|
-        create_tasking_plan(period, task_plan)
+        Models::TaskingPlan.create(target: period.to_model,
+                                   opens_at: task_plan.tasking_plans.first.opens_at,
+                                   due_at: task_plan.tasking_plans.first.due_at,
+                                   tasks_task_plan_id: task_plan.id)
       end
     end
 
     private
     attr_reader :existing_period_ids
 
-    def create_tasking_plan(period, task_plan)
-      Models::TaskingPlan.create(target: period.to_model,
-                                 opens_at: task_plan.tasking_plans.first.opens_at,
-                                 due_at: task_plan.tasking_plans.first.due_at,
-                                 tasks_task_plan_id: task_plan.id)
+    def task_plans_with_matching_tasking_plans_across_periods
+      task_plans_across_periods.select { |task_plan|
+
+        assigned_to_all_existing_periods?(task_plan.tasking_plans) &&
+          tasking_plans_have_same_dates?(task_plan)
+
+      }
     end
 
-    def task_plans_with_matching_tasking_plans_across_periods
-      task_plans_across_periods.reject do |task_plan|
-        target_ids = task_plan.tasking_plans.flat_map(&:target_id).uniq.sort
-        period_ids = existing_period_ids.uniq.sort
-        due_dates = task_plan.tasking_plans.flat_map(&:due_at).uniq
-        open_dates = task_plan.tasking_plans.flat_map(&:opens_at).uniq
+    def assigned_to_all_existing_periods?(tasking_plans)
+      tasking_plans.flat_map(&:target_id).sort == existing_period_ids.sort
+    end
 
-        target_ids != period_ids || due_dates.size != 1 || open_dates.size != 1
-      end
+    def tasking_plans_have_same_dates?(task_plan)
+      task_plan.tasking_plans.flat_map(&:due_at).uniq.one? &&
+        task_plan.tasking_plans.flat_map(&:opens_at).uniq.one?
     end
 
     def task_plans_across_periods
