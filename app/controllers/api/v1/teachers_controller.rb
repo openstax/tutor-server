d351ac07ea3f5c7bd3db4ef1e22a592125f1457a
karen chan <karen@karen-chan.com>
2015-10-22 16:55:59 +0100

    Add stub remove teacher API
    The API doesn't remove the teacher from the course at this point, it
    just checks that the user has permission to remove a teacher.

---
+++ app/controllers/api/v1/teachers_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::TeachersController < Api::V1::ApiController
+  before_filter :get_teacher, only: [:destroy]
+
+  resource_description do
+    api_versions 'v1'
+    short_description 'Represents a teacher in a course'
+    description <<-EOS
+      Other teachers and admins are allowed to create and remove teachers from a course.
+    EOS
+  end
+
+  api :DELETE, '/teachers/:teacher_id', 'Removes a teacher from the course'
+  description <<-EOS
+    Removes a teacher from the course.
+  EOS
+  def destroy
+    OSU::AccessPolicy.require_action_allowed!(:destroy, current_api_user, @teacher)
+    head :no_content
+  end
+
+  protected
+
+  def get_teacher
+    @teacher = CourseMembership::Models::Teacher.find(params[:id])
+  end
+
+end
---
+++ spec/controllers/api/v1/teachers_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe Api::V1::TeachersController, type: :controller, api: true, version: :v1 do
+  let!(:application)       { FactoryGirl.create :doorkeeper_application }
+
+  let!(:course)            { Entity::Course.create }
+  let!(:period)            { CreatePeriod[course: course] }
+
+  let!(:student_user)      { FactoryGirl.create(:user) }
+  let!(:student_role)      { AddUserAsPeriodStudent[user: student_user, period: period] }
+  let!(:student)           { student_role.student }
+  let!(:student_token)     { FactoryGirl.create :doorkeeper_access_token,
+                                                application: application,
+                                                resource_owner_id: student_user.id }
+
+  let!(:teacher_user)      { FactoryGirl.create(:user) }
+  let!(:teacher_role)      { AddUserAsCourseTeacher[user: teacher_user, course: course] }
+  let!(:teacher)           { teacher_role.teacher }
+  let!(:teacher_token)     { FactoryGirl.create :doorkeeper_access_token,
+                                                application: application,
+                                                resource_owner_id: teacher_user.id }
+
+  describe '#destroy' do
+    context 'anonymous user' do
+      it 'raises SecurityTransgression' do
+        expect {
+          api_delete :destroy, nil, parameters: { id: teacher.id }
+        }.to raise_error(SecurityTransgression)
+      end
+    end
+
+    context 'user is a student' do
+      it 'raises SecurityTransgression' do
+        expect {
+          api_delete :destroy, student_token, parameters: { id: teacher.id }
+        }.to raise_error(SecurityTransgression)
+      end
+    end
+
+    context 'user is a teacher' do
+      it 'removes the teacher' do
+        api_delete :destroy, teacher_token, parameters: { id: teacher.id }
+        # TODO assert the teacher has been removed
+      end
+    end
+  end
+end
