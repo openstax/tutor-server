001be84093c5e1b909e7e3fb41a4d743762ec117
karen chan <karen@karen-chan.com>
2015-10-23 12:04:06 +0100

    Add spec UpdateCourse routine

---
+++ spec/routines/update_course_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe UpdateCourse do
+  let!(:course) { CreateCourse[name: 'A course'] }
+
+  it 'updates the course name' do
+    UpdateCourse.call(course.id, { name: 'Physics' })
+    expect(course.reload.name).to eq 'Physics'
+  end
+end
