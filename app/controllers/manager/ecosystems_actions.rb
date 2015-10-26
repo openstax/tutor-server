a72b6f18ffc628fd30e11982de3afdc0c1236377
karen chan <karen@karen-chan.com>
2015-10-26 15:39:25 +0000

    Move admin ecosystems controller to Manager::EcosystemsAction
    This allows sharing of code between the admin ecosystems page, the
    content analyst ecosystems page and the customer service ecosystems
    page.

---
+++ app/controllers/manager/ecosystems_actions.rb
@@ -0,0 +1,30 @@
+module Manager::EcosystemsActions
+
+  def index
+    @ecosystems = Content::ListEcosystems[]
+    @incomplete_jobs = Lev::BackgroundJob.incomplete.select do |job|
+      job.respond_to?(:ecosystem_import_url)
+    end
+  end
+
+  def import
+    @default_archive_url = OpenStax::Cnx::V1.archive_url_base
+    import_ecosystem if request.post?
+  end
+
+  protected
+
+  def import_ecosystem
+    archive_url = params[:archive_url].present? ? params[:archive_url] : @default_archive_url
+
+    OpenStax::Cnx::V1.with_archive_url(url: archive_url) do
+      job_id = FetchAndImportBookAndCreateEcosystem.perform_later(book_cnx_id: params[:cnx_id])
+      job = Lev::BackgroundJob.find(job_id)
+      import_url = OpenStax::Cnx::V1.url_for(params[:cnx_id])
+      job.save(ecosystem_import_url: import_url)
+      flash[:notice] = 'Ecosystem import job queued.'
+    end
+    redirect_to admin_ecosystems_path
+  end
+
+end
