62d1bb3b29304767faa2d2f46349383fec6cee4c
Joe Sak <joe@joesak.com>
2015-10-28 09:28:53 -0400

    Allow blank on the new comments field?

--- app/subsystems/content/ecosystem.rb
+++ app/subsystems/content/ecosystem.rb
@@ -159,7 +159,10 @@ module Content
     end
 
     def comments
-      verify_and_return @strategy.comments, klass: String, allow_nil: true, error: StrategyError
+      verify_and_return @strategy.comments, klass: String
+                                            allow_blank: true,
+                                            allow_nil: true,
+                                            error: StrategyError
     end
 
     def imported_at
