0ef4d17cb8b3d0ec2265b988a75a0e20f5d600a9
Joe Sak <joe@joesak.com>
2015-10-28 09:07:33 -0400

    Respond to some PR feedback about private methods in Manager::EcosystemsActions

--- app/controllers/manager/ecosystems_actions.rb
+++ app/controllers/manager/ecosystems_actions.rb
@@ -15,31 +15,25 @@ module Manager::EcosystemsActions
   protected
 
   def import_ecosystem
-    OpenStax::Cnx::V1.with_archive_url(url: archive_url) do
+    url = params[:archive_url] || @default_archive_url
+
+    OpenStax::Cnx::V1.with_archive_url(url: url) do
+      job = create_book_import_job
+      import_url = OpenStax::Cnx::V1.url_for(params[:cnx_id])
+
       job.save(ecosystem_import_url: import_url)
       flash[:notice] = 'Ecosystem import job queued.'
     end
+
     redirect_to ecosystems_path
   end
 
-  def archive_url
-    if params[:archive_url].present?
-      params[:archive_url]
-    else
-      @default_archive_url
-    end
-  end
-
-  def job
+  def create_book_import_job
     job_id = FetchAndImportBookAndCreateEcosystem.perform_later(
       book_cnx_id: params[:cnx_id],
       comments: params[:comments]
     )
     Lev::BackgroundJob.find(job_id)
-  end
-
-  def import_url
-    OpenStax::Cnx::V1.url_for(params[:cnx_id])
   end
 
 end
