4e8cb267bfb85d00ba6695cc4c55107cc34cd916
Nathan Stitt <nathan@stitt.org>
2015-11-02 16:41:53 -0600

    Remove references to tokens controller

--- lib/doorkeeper_session_grant.rb
+++ lib/doorkeeper_session_grant.rb
@@ -1,26 +1,7 @@
 module Doorkeeper
 
   # Let Doorkeeper accept a custom grant type of "session"
-
   configuration.token_grant_types << "session"
-
-  # The TokensController is bare bones; add knowledge of helpers and cookies
-  # as well as how to get the session user
-
-  module TokensControllerExtensions
-    def self.included(base)
-      base.include ActionController::Helpers
-      base.include ActionController::Cookies
-    end
-
-    def current_user
-      @current_user_manager ||= \
-         OpenStax::Accounts::CurrentUserManager.new(request, session, cookies)
-      @current_user_manager.current_user
-    end
-  end
-
-  TokensController.send(:include, TokensControllerExtensions)
 
   # The new grant type of "session" is used by Doorkeeper to infer the
   # existence of a Doorkeeper::Request::Session object that handles this
@@ -40,8 +21,7 @@ module Doorkeeper
       def request
         return @request if @request
 
-        tokens_controller = @server.context
-        current_user = tokens_controller.current_user
+        current_user = @server.context.current_user
         current_user = nil if current_user.is_anonymous?
 
         @request = OAuth::PasswordAccessTokenRequest.new(
