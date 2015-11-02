7b6ec9b6d38b7316a7a89fd3d66d27c75eb3836b
Dante Soares <dante.m.soares@rice.edu>
2015-11-02 17:13:46 -0600

    Added ConceptCoachTask model spec

--- spec/factories/tasks/concept_coach_tasks.rb
+++ spec/factories/tasks/concept_coach_tasks.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :tasks_concept_coach_task do
+  factory :tasks_concept_coach_task, class: '::Tasks::Models::ConceptCoachTask' do
     association :task, factory: :entity_task
     association :page, factory: :content_page
   end
--- spec/subsystems/tasks/models/concept_coach_task_spec.rb
+++ spec/subsystems/tasks/models/concept_coach_task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tasks::Models::ConceptCoachTask, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:concept_coach_task) { FactoryGirl.create(:tasks_concept_coach_task) }
+
+  it { is_expected.to belong_to(:task) }
+  it { is_expected.to belong_to(:page) }
+
+  it { is_expected.to validate_presence_of(:task) }
+  it { is_expected.to validate_presence_of(:page) }
+
+  it { is_expected.to validate_uniqueness_of(:task) }
 end
+
