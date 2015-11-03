9bf7528b9ba0c42e2d4c362a46f1f805b92434cc
Joe Sak <joe@joesak.com>
2015-11-03 14:54:47 -0500

    Add unique tokenable with multiple token generator modes

--- lib/generate_token.rb
+++ lib/generate_token.rb
@@ -1,12 +1,18 @@
+require 'generate_token/token_generator'
+require 'generate_token/secure_random_token_generator'
+require 'generate_token/babbler_token_generator'
+
 class GenerateToken
-  def self.apply(record:, attribute:, mode: :hex)
+  def self.apply(record:, attribute:, mode: nil)
+    @mode = mode || :hex
+
     begin
-      record[attribute] = SecureRandom.send(mode)
+      record[attribute] = token_generator.generate_with(@mode)
     end while record.class.exists?(attribute => record[attribute])
   end
 
-  def self.apply!(record:, attribute:, mode: :hex)
-    apply(record: record, attribute: attribute, mode: mode)
-    record.save!
+  private
+  def self.token_generator
+    TokenGenerator.selected_generator(@mode)
   end
 end
