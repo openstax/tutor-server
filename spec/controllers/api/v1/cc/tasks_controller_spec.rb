5dc7987bb143104fab2443c209f9aa76cfcf8ced
Dante Soares <dante.m.soares@rice.edu>
2015-11-03 17:00:07 -0600

    Added specs for the CC TasksController error handling

--- spec/controllers/api/v1/cc/tasks_controller_spec.rb
+++ spec/controllers/api/v1/cc/tasks_controller_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe Api::V1::Cc::TasksController, type: :controller, api: true, versi
                                     book_location: book_location]
     end
 
-    book = chapter.book
-    Content::Routines::PopulateExercisePools[book: book]
+    @book = chapter.book
+    Content::Routines::PopulateExercisePools[book: @book]
 
     @page = Content::Page.new(strategy: page_model.reload.wrap)
 
-    ecosystem_model = book.ecosystem
+    ecosystem_model = @book.ecosystem
     ecosystem = Content::Ecosystem.new(strategy: ecosystem_model.wrap)
 
     period_model = FactoryGirl.create(:course_membership_period)
@@ -40,7 +40,7 @@ RSpec.describe Api::V1::Cc::TasksController, type: :controller, api: true, versi
     @role_1 = AddUserAsPeriodStudent[user: user_1, period: period]
     @role_2 = AddUserAsPeriodStudent[user: user_2, period: period]
 
-    @book_uuid = chapter.book.uuid
+    @book_uuid = @book.uuid
     @page_uuid = @page.uuid
 
     @user_1_token = FactoryGirl.create :doorkeeper_access_token,
@@ -59,13 +59,13 @@ RSpec.describe Api::V1::Cc::TasksController, type: :controller, api: true, versi
     DatabaseCleaner.clean
   end
 
-  def api_call(token)
-    api_get :show, token, parameters: { cnx_book_id: @book_uuid, cnx_page_id: @page_uuid }
+  def api_call(token, cnx_book_id: @book_uuid, cnx_page_id: @page_uuid)
+    api_get :show, token, parameters: { cnx_book_id: cnx_book_id, cnx_page_id: cnx_page_id }
   end
 
   describe "#show" do
     context 'no existing task' do
-      it 'should create a new task' do
+      it 'should create a new task if params are valid' do
         expect{ api_call(@user_1_token) }.to change{ Tasks::Models::Task.count }.by(1)
         expect(response).to have_http_status(:ok)
 
@@ -75,6 +75,37 @@ RSpec.describe Api::V1::Cc::TasksController, type: :controller, api: true, versi
         expect(response.body_as_hash).to include(title: cc_task.title)
         expect(response.body_as_hash).to have_key(:steps)
         expect(response.body_as_hash[:steps].length).to eq 4
+      end
+
+      it 'should return 422 with code :invalid_book if the book is invalid' do
+        expect{ api_call(@user_1_token, cnx_book_id: 'invalid') }.not_to(
+          change{ Tasks::Models::Task.count }
+        )
+        expect(response).to have_http_status(:unprocessable_entity)
+        body = response.body_as_hash
+        expect(body[:errors].map{ |error| error[:code] }).to eq ['invalid_book']
+        expect(body[:valid_books]).to eq [@book.url]
+      end
+
+      it 'should return 422 with code :invalid_page if the page is invalid' do
+        expect{ api_call(@user_1_token, cnx_page_id: 'invalid') }.not_to(
+          change{ Tasks::Models::Task.count }
+        )
+        expect(response).to have_http_status(:unprocessable_entity)
+        body = response.body_as_hash
+        expect(body[:errors].map{ |error| error[:code] }).to eq ['invalid_page']
+        expect(body[:valid_books]).to eq [@book.url]
+      end
+
+      it 'should return 422 with code :not_a_cc_student if the user is not in a CC course' do
+        @role_1.destroy
+        expect{ api_call(@user_1_token) }.not_to(
+          change{ Tasks::Models::Task.count }
+        )
+        expect(response).to have_http_status(:unprocessable_entity)
+        body = response.body_as_hash
+        expect(body[:errors].map{ |error| error[:code] }).to eq ['not_a_cc_student']
+        expect(body[:valid_books]).to eq []
       end
     end
 
