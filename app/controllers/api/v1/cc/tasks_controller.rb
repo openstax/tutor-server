d30c84fa1ba49d130889811ad904782c69441896
Dante Soares <dante.m.soares@rice.edu>
2015-11-03 16:45:59 -0600

    Improved Cc TasksController error handling

--- app/controllers/api/v1/cc/tasks_controller.rb
+++ app/controllers/api/v1/cc/tasks_controller.rb
@@ -16,6 +16,11 @@ class Api::V1::Cc::TasksController < Api::V1::ApiController
             'Gets the Concept Coach Task for the given CNX page'
   description <<-EOS
     The `cnx_book_id` and `cnx_page_id` should not contain version information.
+
+    Possible error codes:
+      invalid_book
+      invalid_page
+      not_a_cc_student
     #{json_schema(Api::V1::TaskRepresenter, include: :readable)}
   EOS
   def show
@@ -38,7 +43,28 @@ class Api::V1::Cc::TasksController < Api::V1::ApiController
 
     # If page_models.size > 1, the user is in 2 courses with the same CC book (not allowed)
     page_model = page_models.order(:created_at).last
-    return head(:unprocessable_entity) if page_model.blank?
+
+    if page_model.blank?
+      valid_books = Content::Models::Book.where(content_ecosystem_id: ecosystem_id_role_map.keys)
+                                         .to_a
+      valid_book_with_cnx_book_id = valid_books.select{ |book| book.uuid == params[:cnx_book_id] }
+                                               .first
+
+      if !valid_book_with_cnx_book_id.nil?
+        # Book is valid for the user, but page is invalid
+        code = :invalid_page
+        valid_books = [valid_book_with_cnx_book_id]
+      elsif !valid_books.empty?
+        # Book is invalid for the user, but there are other valid books
+        code = :invalid_book
+      else
+        # Not a CC student
+        code = :not_a_cc_student
+      end
+      
+      json_hash = { errors: [{code: code}], valid_books: valid_books.map(&:url) }
+      return render(json: json_hash, status: :unprocessable_entity)
+    end
 
     ecosystem_id = page_model.book.content_ecosystem_id
     roles = ecosystem_id_role_map[ecosystem_id]
