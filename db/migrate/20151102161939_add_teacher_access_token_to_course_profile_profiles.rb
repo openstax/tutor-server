520b30dae0ebc2a5de54face0d6fb8098fcaa0b6
Joe Sak <joe@joesak.com>
2015-11-05 09:49:05 -0500

    Fix the teacher access token migration

--- db/migrate/20151102161939_add_teacher_access_token_to_course_profile_profiles.rb
+++ db/migrate/20151102161939_add_teacher_access_token_to_course_profile_profiles.rb
@@ -4,9 +4,8 @@ class AddTeacherAccessTokenToCourseProfileProfiles < ActiveRecord::Migration
 
     reversible do |direction|
       direction.up do
-        CourseProfile::Models::Profile.where(teacher_access_token: nil).find_each do |p|
-          GenerateToken.apply!(record: p, attribute: :teacher_access_token)
-        end
+        # the before_validation callback will apply the token
+        CourseProfile::Models::Profile.where(teacher_access_token: nil).find_each(&:save)
 
         change_column :course_profile_profiles, :teacher_access_token, :string, null: false
       end
