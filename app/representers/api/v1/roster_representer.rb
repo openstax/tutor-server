23da092d412fd9cf3f7580723afd96ac82e9a6d9
Joe Sak <joe@joesak.com>
2015-11-11 11:19:22 -0500

    Update the roster representer properties with schema info

--- app/representers/api/v1/roster_representer.rb
+++ app/representers/api/v1/roster_representer.rb
@@ -1,15 +1,24 @@
 module Api::V1
   class RosterRepresenter < Roar::Decorator
+    include Roar::JSON
     include Representable::JSON::Collection
 
     property :teacher_join_url,
              type: String,
              writeable: false,
-             readable: true
+             readable: true,
+             schema_info: {
+               required: true,
+               description: "The unique join URL for users to become teachers of the course. This is a private URL and must be kept secret."
+             }
 
     collection :students,
                writeable: false,
                readable: true,
-               decorator: StudentRepresenter
+               extend: StudentRepresenter,
+               schema_info: {
+                 required: true,
+                 description: "The list of students in the course."
+               }
   end
 end
