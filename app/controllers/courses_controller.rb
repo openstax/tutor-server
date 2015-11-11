ecf062c3971b68498124565bfb35ae1023960b15
Joe Sak <joe@joesak.com>
2015-11-11 14:39:02 -0500

    Use after_transaction block so the handler can raise handled exceptions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,10 +2,6 @@ class CoursesController < ApplicationController
   include Lev::HandleWith
 
   def join
-    handle_with(CoursesJoin, success: -> { redirect_to dashboard_path },
-                             failure: -> {
-                               error_code = @handler_result.errors.first.code
-                               raise CoursesJoin.handled_exceptions[error_code]
-                             })
+    handle_with(CoursesJoin, complete: -> { redirect_to dashboard_path })
   end
 end
--- app/handlers/courses_join.rb
+++ app/handlers/courses_join.rb
@@ -4,17 +4,12 @@ class CoursesJoin
   uses_routine AddUserAsCourseTeacher, as: :add_teacher
   uses_routine GetCourseProfile
 
-  def self.handled_exceptions
-    @@handled_exceptions ||= {
-      profile_not_found: InvalidTeacherJoinToken,
-      user_is_already_teacher_of_course: UserAlreadyCourseTeacher
-    }
-  end
-
   protected
   def authorized?; true; end
 
   def handle
+    after_transaction { raise_handled_exceptions! }
+
     course = find_course_by_join_token
     run(:add_teacher, course: course, user: caller)
   end
@@ -26,6 +21,17 @@ class CoursesJoin
     }).outputs.profile
 
     Entity::Course.find(profile.course_id)
+  end
+
+  def raise_handled_exceptions!
+    raise self.class.handled_exceptions[errors.first.code] if errors.any?
+  end
+
+  def self.handled_exceptions
+    @@handled_exceptions ||= {
+      profile_not_found: InvalidTeacherJoinToken,
+      user_is_already_teacher_of_course: UserAlreadyCourseTeacher
+    }
   end
 end
 
