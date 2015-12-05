f9772d288b9121b43f9ed3fd3fea893c91269213
Dante Soares <dante.m.soares@rice.edu>
2015-12-04 18:12:16 -0600

    Added the finished search routine and its spec

--- app/routines/search_courses.rb
+++ app/routines/search_courses.rb
@@ -1,10 +1,26 @@
 class SearchCourses
-  lev_routine express_output: :courses
+  lev_routine express_output: :items
 
   protected
 
   def exec(query:)
-    outputs[:courses] = Entity::Course.all
+    if query.nil?
+      courses = Entity::Course.joins(:profile)
+    else
+      q = "%#{query}%"
+      courses = Entity::Course.joins{
+        [profile.school.outer, teachers.outer.role.outer.profile.outer.account.outer]
+      }.where{
+        (profile.name.like q) | \
+        (profile.school.name.like q) | \
+        (teachers.role.profile.account.username.like q) | \
+        (teachers.role.profile.account.first_name.like q) | \
+        (teachers.role.profile.account.last_name.like q) | \
+        (teachers.role.profile.account.full_name.like q)
+      }
+    end
+
+    outputs.items = courses.order{ profile.name }
   end
 
 end
--- spec/routines/search_courses_spec.rb
+++ spec/routines/search_courses_spec.rb
@@ -2,12 +2,63 @@ require 'rails_helper'
 
 RSpec.describe SearchCourses, type: :routine do
 
-  let!(:course_1) { FactoryGirl.create(:course_profile_profile).course }
-  let!(:course_2) { FactoryGirl.create(:course_profile_profile).course }
-  let!(:course_3) { FactoryGirl.create(:course_profile_profile).course }
+  let!(:tutor_school) { FactoryGirl.create(:school, name: 'TTS') }
+  let!(:cc_school)    { FactoryGirl.create(:school, name: 'CCS') }
 
-  it 'returns all courses if no query is given' do
-    courses = described_class[query: nil]
-    expect(courses).to contain_exactly(course_1, course_2, course_3)
+  let!(:course_1) { FactoryGirl.create(
+    :course_profile_profile, name: 'Physics', school: tutor_school
+  ).course }
+  let!(:course_2) { FactoryGirl.create(
+    :course_profile_profile, name: 'Biology', school: tutor_school
+  ).course }
+  let!(:course_3) { FactoryGirl.create(
+    :course_profile_profile, name: 'Concept Coach', school: cc_school
+  ).course }
+
+  let!(:teacher_user) { FactoryGirl.create(:user, first_name: 'Charles') }
+
+  before do
+    AddUserAsCourseTeacher[course: course_1, user: teacher_user]
+    AddUserAsCourseTeacher[course: course_3, user: teacher_user]
+  end
+
+  it 'returns all courses in alphabetical order if the query is nil' do
+    courses = described_class[query: nil].to_a
+    expect(courses).to eq [course_2, course_3, course_1]
+  end
+
+  it 'returns all courses in alphabetical order if the query is blank' do
+    courses = described_class[query: ''].to_a
+    expect(courses).to eq [course_2, course_3, course_1]
+  end
+
+  it 'returns courses whose name matches the given query, in alphabetical order' do
+    courses = described_class[query: 'i'].to_a
+    expect(courses).to eq [course_2, course_1]
+
+    courses = described_class[query: 'o'].to_a
+    expect(courses).to eq [course_2, course_3]
+
+    courses = described_class[query: 'bIo'].to_a
+    expect(courses).to eq [course_2]
+
+    courses = described_class[query: 'physics'].to_a
+    expect(courses).to eq [course_1]
+  end
+
+  it 'returns courses whose school\'s name matches the given query, in alphabetical order' do
+    courses = described_class[query: 'ts'].to_a
+    expect(courses).to eq [course_2, course_1]
+
+    courses = described_class[query: 'ccs'].to_a
+    expect(courses).to eq [course_3]
+  end
+
+  it 'returns courses whose teacher\'s name matches the given query, in alphabetical order' do
+    courses = described_class[query: 'cHaRlEs'].to_a
+    expect(courses).to eq [course_3, course_1]
+
+    courses = described_class[query: 'rLe'].to_a
+    expect(courses).to eq [course_3, course_1]
   end
 end
