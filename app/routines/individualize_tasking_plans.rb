6c4b01e2fa20c48e7157df244f0bd849cbf1c135
Joe Sak <joe@joesak.com>
2015-12-07 11:52:54 -0500

    Revert non-necessary changes

--- app/routines/individualize_tasking_plans.rb
+++ app/routines/individualize_tasking_plans.rb
@@ -13,25 +13,25 @@ class IndividualizeTaskingPlans
       target = tasking_plan.target
 
       roles = case target
-              when Entity::Role
-                target
-              when User::Models::Profile
-                strategy = ::User::Strategies::Direct::User.new(target)
-                user = ::User::User.new(strategy: strategy)
-                Role::GetDefaultUserRole[user]
-              when Entity::Course
-                run(:get_course_roles, course: target, types: :student).roles
-              when CourseMembership::Models::Period
-                run(:get_period_roles, periods: target, types: :student).roles
-              else
-                raise NotYetImplemented
-              end
+      when Entity::Role
+        target
+      when User::Models::Profile
+        strategy = ::User::Strategies::Direct::User.new(target)
+        user = ::User::User.new(strategy: strategy)
+        Role::GetDefaultUserRole[user]
+      when Entity::Course
+        run(:get_course_roles, course: target, types: :student).roles
+      when CourseMembership::Models::Period
+        run(:get_period_roles, periods: target, types: :student).roles
+      else
+        raise NotYetImplemented
+      end
 
       [roles].flatten.collect do |role|
         Tasks::Models::TaskingPlan.new(task_plan: task_plan, target: role,
                                        opens_at: tasking_plan.opens_at,
                                        due_at: tasking_plan.due_at)
       end
-    end.flatten.uniq(&:target))
+    end.flatten.uniq { |ii| ii.target }
   end
 end
