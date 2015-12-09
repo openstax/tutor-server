025c1a7085f749e1186999bbba42144dc2e97e62
Dante Soares <dante.m.soares@rice.edu>
2015-12-09 15:02:21 -0600

    Block students from accessing performance report API's

--- app/controllers/api/v1/performance_reports_controller.rb
+++ app/controllers/api/v1/performance_reports_controller.rb
@@ -44,10 +44,11 @@ class Api::V1::PerformanceReportsController < Api::V1::ApiController
   end
 
   protected
-  def get_course_role(types: :any)
+
+  def get_course_role
     result = ChooseCourseRole.call(user: current_human_user,
                                    course: Entity::Course.find(params[:id]),
-                                   allowed_role_type: types,
+                                   allowed_role_type: :teacher,
                                    role_id: params[:role_id])
     raise(SecurityTransgression, :invalid_role) if result.errors.any?
     result.outputs.role
