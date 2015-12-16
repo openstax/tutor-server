9f5eceb7571380a90066b4abcd1d5510c84a3fec
JP Slavinsky <jpslav@gmail.com>
2015-12-16 14:44:21 -0800

    Add validated return_to's on login URLs

--- config/initializers/openstax_accounts.rb
+++ config/initializers/openstax_accounts.rb
@@ -5,6 +5,12 @@ secrets = Rails.application.secrets['openstax']['accounts']
 # By default, stub unless in the production environment
 stub = secrets['stub'].nil? ? !Rails.env.production? : secrets['stub']
 
+APPROVED_HOST_REGEXES = [
+  /.*openstax\.org$/,
+  /.*cnx\.org$/,
+  /localhost$/,
+]
+
 OpenStax::Accounts.configure do |config|
   config.openstax_application_id = secrets['client_id']
   config.openstax_application_secret = secrets['secret']
@@ -14,6 +20,14 @@ OpenStax::Accounts.configure do |config|
   config.account_user_mapper = MapUsersAccounts
   config.logout_redirect_url = ->(request) {
     LogoutRedirectChooser.new(request.url).choose(default: config.default_logout_redirect_url)
+  }
+  config.return_to_url_approver = ->(url) {
+    begin
+      uri = Addressable::URI.parse(url)
+      APPROVED_HOST_REGEXES.any?{|regex| regex.match(uri.host)}
+    rescue
+      false
+    end
   }
 end
 
---
+++ spec/requests/login_return_to_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Login with explicit return_to", type: :request do
+  let(:user)        { FactoryGirl.create(:user) }
+
+  good_return_tos = %w(http://www.cnx.org?blah http://localhost:3001 http://openstax.org)
+
+  good_return_tos.each do |good_return_to|
+    it "stores '#{good_return_to}'return_to redirect after login for approved return_tos" do
+      stub_current_user(user)
+      allow(OpenStax::Accounts.configuration).to receive(:enable_stubbing?) { false }
+      host! 'tutor-blah.openstax.org'
+      get("/accounts/login?return_to=#{good_return_to}")
+      expect(session["accounts_return_to"]).to eq good_return_to
+    end
+  end
+
+  it "does not store return_to redirect after login for non-approved return_tos" do
+    stub_current_user(user)
+    allow(OpenStax::Accounts.configuration).to receive(:enable_stubbing?) { false }
+    host! 'tutor-blah.openstax.org'
+    get("/accounts/login?return_to=http://www.openstax.org.spamsite.net")
+    expect(session["accounts_return_to"]).to eq nil
+  end
+end
