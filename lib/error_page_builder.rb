bd77758403dd95ff63c60e5ace59f29d8aad602e
Dante Soares <dante.m.soares@rice.edu>
2016-01-08 13:58:42 -0600

    Fixed the static error pages

--- lib/error_page_builder.rb
+++ lib/error_page_builder.rb
@@ -3,9 +3,10 @@ require 'render_anywhere'
 class ErrorPageBuilder
   include RenderAnywhere
 
-  def self.build(code:, heading:)
+  def self.build(view:, code:, heading:)
+    stylesheet_tag = view.stylesheet_link_tag 'application', media: 'all'
     new.render template: 'layouts/static_error',
                layout: false,
-               locals: {code: code, heading: heading}
+               locals: { stylesheet_tag: stylesheet_tag, code: code, heading: heading }
   end
 end
