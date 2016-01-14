1ae052cfb699611ad4209f7e4baa76c7dc1314ec
Dante Soares <dante.m.soares@rice.edu>
2016-01-14 17:36:24 -0600

    Added a migration to change all default_group steps to recovery_group

---
+++ db/migrate/20160114232922_change_default_group_task_steps_to_recovery_group.rb
@@ -0,0 +1,13 @@
+class ChangeDefaultGroupTaskStepsToRecoveryGroup < ActiveRecord::Migration
+  def up
+    Tasks::Models::TaskStep
+      .where(group_type: Tasks::Models::TaskStep.group_types[:default_group])
+      .update_all(group_type: Tasks::Models::TaskStep.group_types[:recovery_group])
+  end
+
+  def down
+    Tasks::Models::TaskStep
+      .where(group_type: Tasks::Models::TaskStep.group_types[:recovery_group])
+      .update_all(group_type: Tasks::Models::TaskStep.group_types[:default_group])
+  end
+end
