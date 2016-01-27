f8c8a4a25c64cc402609824f840ec0a3ac7ae9a5
Dante Soares <dante.m.soares@rice.edu>
2016-01-27 10:05:34 -0600

    Added some controller specs for the stats controllers

---
+++ spec/controllers/admin/stats_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Admin::StatsController, type: :controller do
+
+  let!(:admin) { FactoryGirl.create(:user, :administrator) }
+
+  context "GET #courses" do
+    let!(:course)        { Entity::Course.create! }
+    let!(:periods)       do
+      3.times.map { FactoryGirl.create :course_membership_period, course: course }
+    end
+
+    let!(:teacher_user)  { FactoryGirl.create :user }
+    let!(:teacher_role)  { AddUserAsCourseTeacher[course: course, user: teacher_user] }
+
+    let!(:student_roles) do
+      5.times.map do
+        user = FactoryGirl.create :user
+        AddUserAsPeriodStudent[period: periods.sample, user: user]
+      end
+    end
+
+    it "returns http success" do
+      controller.sign_in admin
+
+      get :courses
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
