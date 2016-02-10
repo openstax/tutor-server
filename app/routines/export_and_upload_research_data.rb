bc777e5906c74e068c650e71869481fb56637ffa
Dante Soares <dante.m.soares@rice.edu>
2016-02-10 16:59:19 -0600

    Removed some debug code

--- app/routines/export_and_upload_research_data.rb
+++ app/routines/export_and_upload_research_data.rb
@@ -156,7 +156,7 @@
     auth = { username: own_cloud_secrets['username'], password: own_cloud_secrets['password'] }
 
     File.open(filepath, 'r') do |file|
-      @req = HTTParty.put(webdav_url, basic_auth: auth, body_stream: file).success?
+      HTTParty.put(webdav_url, basic_auth: auth, body_stream: file).success?
     end
   end
 
