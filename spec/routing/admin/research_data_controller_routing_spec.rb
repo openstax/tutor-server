e576cb633f413a318b534e318de92fde2c7925d0
Dante Soares <dante.m.soares@rice.edu>
2016-02-10 16:50:24 -0600

    Added ResearchDataController routing spec

---
+++ spec/routing/admin/research_data_controller_routing_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe Admin::ResearchDataController, type: :routing do
+
+  describe "GET /admin/research_data" do
+    it "routes to #index" do
+      expect(get '/admin/research_data').to route_to('admin/research_data#index')
+    end
+  end
+
+  describe "POST /admin/research_data" do
+    it "routes to #create" do
+      expect(post '/admin/research_data').to route_to('admin/research_data#create')
+    end
+  end
+
+end
