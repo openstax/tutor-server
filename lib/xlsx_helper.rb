954fe2e58616aeae0718aa82728dab30aaee6932
JP Slavinsky <jpslav@gmail.com>
2016-03-02 15:17:23 -0800

    more specs, fixes, documentation, reuse from older workbooks

--- lib/xlsx_helper.rb
+++ lib/xlsx_helper.rb
@@ -6,10 +6,10 @@ class XlsxHelper
     @truncated_name_count = 0
   end
 
-  def standard_package_settings(package)
-    package.use_shared_strings = true # OS X Numbers interoperability
-    package.workbook.styles.fonts.first.name = 'Helvetica Neue'
-    package.workbook.styles.fonts.first.sz = 11
+  def standard_package_settings(axlsx_package)
+    axlsx_package.use_shared_strings = true # OS X Numbers interoperability
+    axlsx_package.workbook.styles.fonts.first.name = 'Helvetica Neue'
+    axlsx_package.workbook.styles.fonts.first.sz = 11
   end
 
   # Worksheet name ostensible is "#{name}#{suffix}".  Both name and suffix
@@ -20,6 +20,9 @@ class XlsxHelper
   def sanitized_worksheet_name(name:, suffix: nil)
     # worksheet names cannot contain characters :\/?*[]
 
+    raise(IllegalArgument, "`name` must be provided") if name.nil?
+    suffix ||= ""
+
     name = name.gsub(INVALID_WORKSHEET_NAME_CHARS_REGEX, '-')
     suffix = suffix.gsub(INVALID_WORKSHEET_NAME_CHARS_REGEX, '-')
 
@@ -27,13 +30,35 @@ class XlsxHelper
     # names that will be 31 characters or longer when the suffix is included.
     # we also add a counter to avoid collisions between truncated names.
 
-    if name.size < 31 - suffix.size
+    if name.size <= 31 - suffix.size
       "#{name}#{suffix}"
     else
-      "#{name[0..(31-3-suffix.size)]}-#{'%02d' % next_truncated_name_count}#{suffix}"
+      raise(IllegalArgument, "`suffix` is limited to 27 characters") if suffix.size > 27
+      "#{name[0..(30-3-suffix.size)]}-#{'%02d' % next_truncated_name_count}#{suffix}"
     end
   end
 
+  # Normally, when adding a row in Axlsx you have to specify the values separately
+  # from the values, e.g.:
+  #
+  #   sheet.add_row(["Foo", "Bar", "Hi"], style: [black, blue, red])
+  #
+  # which is a little error prone when your rows get long.  This `add_row` method lets
+  # you specify :style, :type, :comment, :cols right next to the value, e.g.
+  #
+  #   helper.add_row(
+  #     "Unstyled text",
+  #     ["Foo", {style: black}],
+  #     ["Bar", {comment: "I use to hate saying 'foo' and 'bar'"}]
+  #   )
+  #
+  # Options:
+  #
+  #   :style -- just like the normal Axlsx :style
+  #   :type -- just like the normal Axlsx :type
+  #   :comment -- text of a comment to add to the cell
+  #   :cols -- a number specifying the width of the column (triggers `merge_cells`)
+  #
   def add_row(sheet, optioned_values)
     optioned_values = OptionedValues.new(optioned_values)
 
@@ -62,6 +87,9 @@ class XlsxHelper
   def next_truncated_name_count
     @truncated_name_count += 1
   end
+
+  # OptionedValues and its inner class OptionedValue are used to parse and expand
+  # the options passed to the `add_row` method above.
 
   class OptionedValues
 
---
+++ spec/lib/xlsx_helper_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe XlsxHelper, type: :lib do
+
+  describe '#sanitized_worksheet_name' do
+    let!(:helper) { described_class.new }
+
+    it 'objects if no name provided' do
+      expect{helper.sanitized_worksheet_name(name: nil)}.to raise_error
+    end
+
+    it 'returns short names verbatim' do
+      expect(helper.sanitized_worksheet_name(name: "Short name")).to eq "Short name"
+    end
+
+    it 'returns short names with suffixes with no truncation' do
+      expect(helper.sanitized_worksheet_name(name: "Short name", suffix:"WOW")).to eq "Short nameWOW"
+    end
+
+    it 'replaces bad chars' do
+      expect(helper.sanitized_worksheet_name(name: 'T:][\\/', suffix: '*?b:b')).to eq 'T-------b-b'
+    end
+
+    it 'leaves max length names alone' do
+      expect(helper.sanitized_worksheet_name(name: '1234567890123456789012345678901')).to eq '1234567890123456789012345678901'
+    end
+
+    it 'truncates & numbers overlong names and resulting length is good' do
+      expect(helper.sanitized_worksheet_name(name: '12345678901234567890123456789012')).to eq '1234567890123456789012345678-01'
+    end
+
+    it 'increments numbers for truncated names' do
+      expect(helper.sanitized_worksheet_name(name: '12345678901234567890123456789012')).to eq '1234567890123456789012345678-01'
+      expect(helper.sanitized_worksheet_name(name: "Short name")).to eq "Short name"
+      expect(helper.sanitized_worksheet_name(name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')).to eq 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa-02'
+    end
+
+    it 'maintains full length suffixes when truncating' do
+      expect(helper.sanitized_worksheet_name(name: '12345678901234567890123456789012', suffix: 'howdy')).to eq '12345678901234567890123-01howdy'
+    end
+
+    it 'objects if suffix too long' do
+      expect{helper.sanitized_worksheet_name(name: '12345', suffix: '1234567890123456789012345678')}.to raise_error
+    end
+
+    it 'does not object if suffix is max length' do
+      expect{helper.sanitized_worksheet_name(name: '12345', suffix: '123456789012345678901234567')}.not_to raise_error
+    end
+  end
+
+  describe '#cell_ref' do
+    it 'finds simple cells' do
+      expect(described_class.cell_ref(row: 5, column: 0)).to eq('A5')
+      expect(described_class.cell_ref(row: 99, column: 2)).to eq('C99')
+      expect(described_class.cell_ref(row: 137, column: 25)).to eq('Z137')
+    end
+
+    it 'finds double-letter cells' do
+      expect(described_class.cell_ref(row: 5, column: 26)).to eq('AA5')
+      expect(described_class.cell_ref(row: 69, column: 27)).to eq('AB69')
+      expect(described_class.cell_ref(row: 420, column: 57)).to eq('BF420')
+    end
+  end
+
+end
