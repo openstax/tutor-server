b0ff632eebe3e575f7dd57cc38fe8b03a555d3ef
Dante Soares <dante.m.soares@rice.edu>
2016-03-09 19:52:38 -0600

    Added routing spec and another test to course profile spec

---
+++ spec/routing/admin/courses_controller_routing_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe Admin::CoursesController, type: :routing do
+
+  describe "DELETE /admin/courses/:course_id" do
+    it "routes to #destroy" do
+      expect(delete '/admin/courses/42').to route_to('admin/courses#destroy', id: '42')
+    end
+  end
+
+end
--- spec/subsystems/course_profile/models/profile_spec.rb
+++ spec/subsystems/course_profile/models/profile_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CourseProfile::Models::Profile, type: :model do
   it { is_expected.to belong_to(:course) }
 
+  it { is_expected.to validate_presence_of(:course) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:timezone) }
 
