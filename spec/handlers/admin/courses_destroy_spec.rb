ccb5f9077edaf7eff6e528665e833f0dfef424b3
Dante Soares <dante.m.soares@rice.edu>
2016-03-09 20:40:24 -0600

    Added a CoursesDestroy handler spec

---
+++ spec/handlers/admin/courses_destroy_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Admin::CoursesDestroy, type: :handler do
+  let!(:course) { FactoryGirl.create :entity_course }
+
+  context 'destroyable course' do
+    it 'deletes the course and the course profile' do
+      courses_count = Entity::Course.count
+      profiles_count = CourseProfile::Models::Profile.count
+
+      result = described_class.call(params: {id: course.id})
+      expect(result.errors).to be_empty
+      expect(Entity::Course.count).to eq courses_count - 1
+      expect(CourseProfile::Models::Profile.count).to eq profiles_count - 1
+    end
+  end
+
+  context 'non-destroyable course' do
+    before{ CreatePeriod[course: course] }
+
+    it 'returns an error' do
+      result = described_class.call(params: {id: course.id})
+      expect(result.errors.first.code).to eq :course_not_empty
+    end
+  end
+end
