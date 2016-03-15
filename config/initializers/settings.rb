b26c11b413b0193af306805f516801fb97fb17fb
Dante Soares <dante.m.soares@rice.edu>
2016-03-14 19:02:33 -0500

    Added exercise exclusion settings to admin UI; Biglearn pool UUID is automatically set when the exclusion list is updated

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -3,6 +3,9 @@
 class Settings::Db::Store < RailsSettings::CachedSettings
 end
 Settings::Db.store = Settings::Db::Store
+Settings::Db.store.defaults[:excluded_uids] = ''
+Settings::Db.store.defaults[:excluded_pool_uuid] = ''
+Settings::Db.store.defaults[:import_real_salesforce_courses] = false
 
 redis_secrets = Rails.application.secrets['redis']
 Settings::Redis.store = Redis::Store.new(
