fde2bc6486b7f2c81fb2d67f9c48abffa587d4ff
Dante Soares <dante.m.soares@rice.edu>
2016-03-16 14:52:39 -0500

    Optimize FilterExcludedExercises to use Sets

--- app/routines/filter_excluded_exercises.rb
+++ app/routines/filter_excluded_exercises.rb
@@ -3,12 +3,12 @@ class FilterExcludedExercises
   lev_routine express_output: :exercises
 
   def exec(exercises:, course: nil, additional_excluded_numbers: [])
-    admin_excluded_uids = Settings::Exercises.excluded_uids.split(',').map(&:strip)
+    admin_excluded_uids_set = Set.new Settings::Exercises.excluded_uids.split(',').map(&:strip)
     course_excluded_numbers = course.nil? ? [] : course.excluded_exercises.pluck(:exercise_number)
-    excluded_exercise_numbers = course_excluded_numbers + additional_excluded_numbers
+    excluded_exercise_numbers_set = Set.new(course_excluded_numbers + additional_excluded_numbers)
 
     outputs[:exercises] = exercises.reject do |ex|
-      ex.number.in?(excluded_exercise_numbers) || ex.uid.in?(admin_excluded_uids)
+      excluded_exercise_numbers_set.include?(ex.number) || admin_excluded_uids_set.include?(ex.uid)
     end
   end
 
