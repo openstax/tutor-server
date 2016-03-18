e9e8ed39215134dbd76c7181c951fef9bd390402
Dante Soares <dante.m.soares@rice.edu>
2016-03-18 13:56:52 -0500

    Potentially made the TaskedExercise migration way faster

--- db/migrate/20160225204521_fix_tasked_exercise_answer_orders.rb
+++ db/migrate/20160225204521_fix_tasked_exercise_answer_orders.rb
@@ -1,10 +1,7 @@
 class FixTaskedExerciseAnswerOrders < ActiveRecord::Migration
   def up
-    Tasks::Models::TaskedExercise.find_each do |tasked_exercise|
-      content_hash = JSON.parse(tasked_exercise.content)
-      answers = content_hash['questions'].first['answers']
-      content_hash['questions'].first['answers'] = answers.sort_by{ |answer| answer['id'] }
-      tasked_exercise.update_attribute(:content, content_hash.to_json)
+    Tasks::Models::TaskedExercise.preload(:exercise).find_each do |tasked_exercise|
+      tasked_exercise.update_attribute(:content, tasked_exercise.exercise.content)
     end
   end
 
