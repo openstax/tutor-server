3be0f43aa346eee5e1cb043c06f67df805f5cb5c
Dante Soares <dante.m.soares@rice.edu>
2016-03-28 19:58:09 -0500

    Moved some code around; some very minor optimizations

--- app/routines/get_student_guide.rb
+++ app/routines/get_student_guide.rb
@@ -12,12 +12,23 @@ class GetStudentGuide
 
   private
 
+  def get_completed_exercise_task_steps(task_steps)
+    task_steps.select{ |ts| ts.exercise? && ts.completed? }
+  end
+
   def completed_exercise_task_steps_for_role(role)
     get_completed_exercise_task_steps(
       role.taskings
         .preload(task: {task: :task_steps})
         .flat_map{ |tasking| tasking.task.task.task_steps }
     )
+  end
+
+  def get_tasked_exercises_from_completed_exercise_task_steps(completed_exercise_task_steps)
+    tasked_exercise_ids = completed_exercise_task_steps.collect{ |ts| ts.tasked_id }
+    Tasks::Models::TaskedExercise.where(id: tasked_exercise_ids).preload(
+      [{task_step: {task: {taskings: {role: :profile}}}}, :exercise]
+    ).to_a.group_by{ |te| te.task_step.id }
   end
 
   def gather_role_stats(role)
