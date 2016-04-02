7c6a2592530225cb0f3ea0a1a1c958424119d7d3
Dante Soares <dante.m.soares@rice.edu>
2016-04-01 19:56:04 -0500

    Changed collect to map

--- app/routines/get_student_guide.rb
+++ app/routines/get_student_guide.rb
@@ -25,7 +25,7 @@ class GetStudentGuide
   end
 
   def get_tasked_exercises_from_completed_exercise_task_steps(completed_exercise_task_steps)
-    tasked_exercise_ids = completed_exercise_task_steps.collect{ |ts| ts.tasked_id }
+    tasked_exercise_ids = completed_exercise_task_steps.map(&:tasked_id)
     Tasks::Models::TaskedExercise.where(id: tasked_exercise_ids).preload(
       [{task_step: {task: {taskings: {role: :profile}}}}, :exercise]
     ).to_a.group_by{ |te| te.task_step.id }
