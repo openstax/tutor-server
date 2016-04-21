27f7daeca91a75b57e7d9b0f1946991f0851ef2b
karen chan <karen@karen-chan.com>
2016-04-21 19:15:18 +0100

    Make period default open time and due time editable via the API

--- spec/controllers/api/v1/periods_controller_spec.rb
+++ spec/controllers/api/v1/periods_controller_spec.rb
@@ -76,6 +76,22 @@ RSpec.describe Api::V1::PeriodsController, type: :controller, api: true, version
 
       expect(response).to have_http_status(403)
     end
+
+    it 'allows teachers to change the default open time' do
+      api_patch :update, teacher_token,
+        parameters: { id: period.id, period: { default_open_time: '18:32' } }
+
+      expect(response).to have_http_status(200)
+      expect(response.body_as_hash[:default_open_time]).to eq('18:32')
+    end
+
+    it 'allows teachers to change the default due time' do
+      api_patch :update, teacher_token,
+        parameters: { id: period.id, period: { default_due_time: '18:33' } }
+
+      expect(response).to have_http_status(200)
+      expect(response.body_as_hash[:default_due_time]).to eq('18:33')
+    end
   end
 
   describe '#destroy' do
