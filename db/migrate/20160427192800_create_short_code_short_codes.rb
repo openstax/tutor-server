fcecdc6b93a879bdefd1e3122a293f0a213cc933
karen chan <karen@karen-chan.com>
2016-04-27 21:10:33 +0100

    Add short codes table with index for code

---
+++ db/migrate/20160427192800_create_short_code_short_codes.rb
@@ -0,0 +1,10 @@
+class CreateShortCodeShortCodes < ActiveRecord::Migration
+  def change
+    create_table :short_code_short_codes do |t|
+      t.string :code, null: false
+      t.text :uri, null: false
+    end
+
+    add_index :short_code_short_codes, :code, unique: true
+  end
+end
