fb7031424e3f523bb5dff403a228ef6537c4ca01
karen chan <karen@karen-chan.com>
2016-05-01 18:13:27 +0100

    Add ShortCode::ShortCodeRedirect handler
    The handler is going to be used by the short code controller and just
    calls ShortCode::GetShortCodeUrl routine.

---
+++ app/subsystems/short_code/short_code_redirect.rb
@@ -0,0 +1,16 @@
+class ShortCode::ShortCodeRedirect
+  lev_handler
+
+  uses_routine ShortCode::GetShortCodeUrl, translations: { outputs: { type: :verbatim } }
+
+  protected
+
+  def authorized?
+    true
+  end
+
+  def handle
+    run(:short_code_get_short_code_url,
+        short_code: params[:short_code], user: caller)
+  end
+end
