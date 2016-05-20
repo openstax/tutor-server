cabbd278ecbe4b73383ff0a01ed26445acee05b4
Dante Soares <dante.m.soares@rice.edu>
2016-05-20 18:02:08 -0500

    Fixed MapUsersAccounts race condition

--- lib/map_users_accounts.rb
+++ lib/map_users_accounts.rb
@@ -3,7 +3,7 @@ module MapUsersAccounts
   class << self
     def account_to_user(account)
       @account = account
-      anonymous_user || find_user || create_user
+      anonymous_user || find_or_create_user
     end
 
     def user_to_account(user)
@@ -16,18 +16,24 @@ module MapUsersAccounts
       User::User.anonymous if @account.is_anonymous?
     end
 
-    def find_user
-      User::User.find_by_account_id(@account.id)
-    end
+    def find_or_create_user
+      retry_count = 0
+      begin
+        user = User::User.find_by_account_id(@account.id)
+        return user if user.present?
 
-    def create_user
-      result = User::CreateUser.call(account_id: @account.id)
+        result = User::CreateUser.call(account_id: @account.id)
 
-      if (error = result.errors.first)
-        raise error.message
+        error = result.errors.first
+        raise error.message unless error.nil?
+
+        result.outputs.user
+      rescue RuntimeError, ActiveRecord::RecordNotUnique, ::PG::UniqueViolation
+        raise if retry_count >= 3
+
+        retry_count += 1
+        retry
       end
-
-      result.outputs.user
     end
   end
 
