02cf13399464a7214c32b5309d68c5f2fac414e9
Dante Soares <dante.m.soares@rice.edu>
2016-05-20 15:49:14 -0500

    Fix background job exception rescue

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -21,3 +21,44 @@ Delayed::Worker.delay_jobs = Rails.env.production? || (
                                EnvUtilities.load_boolean(name: 'USE_REAL_BACKGROUND_JOBS',
                                                          default: false)
                              )
+
+# https://github.com/smartinez87/exception_notification/issues/195#issuecomment-31257207
+Delayed::Worker.class_exec do
+  ALWAYS_FAIL = ->(exception) { true }
+
+  INSTANT_FAILURE_PROCS = {
+    'ActiveRecord::RecordInvalid' => ALWAYS_FAIL,
+    'ActiveRecord::RecordNotFound' => ALWAYS_FAIL,
+    'Addressable::URI::InvalidURIError' => ALWAYS_FAIL,
+    'ArgumentError' => ALWAYS_FAIL,
+    'Content::MapInvalidError' => ALWAYS_FAIL,
+    'JSON::ParserError' => ALWAYS_FAIL,
+    'NoMethodError' => ALWAYS_FAIL,
+    'NotYetImplemented' => ALWAYS_FAIL,
+    # http://stackoverflow.com/a/31928089
+    'ActiveJob::DeserializationError' => ->(exception) do
+      exception.original_exception.is_a? ActiveRecord::RecordNotFound
+    end,
+    'OAuth2::Error'       => ->(exception) do
+      status = exception.response.status
+      400 <= status && status < 500
+    end,
+    'OpenStax::HTTPError' => ->(exception) do
+      status = exception.message.to_i
+      400 <= status && status < 500
+    end,
+    'OpenURI::HTTPError'  => ->(exception) do
+      status = exception.message.to_i
+      400 <= status && status < 500
+    end
+  }
+
+  def handle_failed_job_with_instant_failures(job, exception)
+    fail_proc = INSTANT_FAILURE_PROCS[exception.class.name]
+    job.fail! if fail_proc.present? && fail_proc.call(exception)
+
+    handle_failed_job_without_instant_failures(job, exception)
+  end
+
+  alias_method_chain :handle_failed_job, :instant_failures
+end
