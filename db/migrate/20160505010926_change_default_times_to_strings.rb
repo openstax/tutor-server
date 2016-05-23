6c839093ec780153c3ad26c6484911316a0144d9
Dante Soares <dante.m.soares@rice.edu>
2016-05-23 15:42:36 -0500

    Makes migrations not yet in production reversible (allows us to revert QA/Dev servers)

--- db/migrate/20160505010926_change_default_times_to_strings.rb
+++ db/migrate/20160505010926_change_default_times_to_strings.rb
@@ -1,9 +1,9 @@
 class ChangeDefaultTimesToStrings < ActiveRecord::Migration
   def change
-    remove_column :course_membership_periods, :default_open_time
-    remove_column :course_membership_periods, :default_due_time
-    remove_column :course_profile_profiles, :default_open_time
-    remove_column :course_profile_profiles, :default_due_time
+    remove_column :course_membership_periods, :default_open_time, :time
+    remove_column :course_membership_periods, :default_due_time, :time
+    remove_column :course_profile_profiles, :default_open_time, :time
+    remove_column :course_profile_profiles, :default_due_time, :time
 
     add_column :course_membership_periods, :default_open_time, :string
     add_column :course_membership_periods, :default_due_time, :string
--- db/migrate/20160505221319_add_defaults_for_null_counts.rb
+++ db/migrate/20160505221319_add_defaults_for_null_counts.rb
@@ -1,17 +1,23 @@
 class AddDefaultsForNullCounts < ActiveRecord::Migration
-  def change
-    reversible do |dir|
-      dir.up do
-        Tasks::Models::Task.preload(task_steps: :tasked).find_each(&:update_step_counts!)
-      end
-    end
-
-    change_column_null :tasks_tasks, :correct_on_time_exercise_steps_count, false
-    change_column_null :tasks_tasks, :completed_on_time_exercise_steps_count, false
-    change_column_null :tasks_tasks, :completed_on_time_steps_count, false
+  def up
+    Tasks::Models::Task.preload(task_steps: :tasked).find_each(&:update_step_counts!)
 
     change_column_default :tasks_tasks, :correct_on_time_exercise_steps_count, 0
     change_column_default :tasks_tasks, :completed_on_time_exercise_steps_count, 0
     change_column_default :tasks_tasks, :completed_on_time_steps_count, 0
+
+    change_column_null :tasks_tasks, :correct_on_time_exercise_steps_count, false
+    change_column_null :tasks_tasks, :completed_on_time_exercise_steps_count, false
+    change_column_null :tasks_tasks, :completed_on_time_steps_count, false
+  end
+
+  def down
+    change_column_null :tasks_tasks, :correct_on_time_exercise_steps_count, true
+    change_column_null :tasks_tasks, :completed_on_time_exercise_steps_count, true
+    change_column_null :tasks_tasks, :completed_on_time_steps_count, true
+
+    change_column_default :tasks_tasks, :correct_on_time_exercise_steps_count, nil
+    change_column_default :tasks_tasks, :completed_on_time_exercise_steps_count, nil
+    change_column_default :tasks_tasks, :completed_on_time_steps_count, nil
   end
 end
