e108b69b1e5bd64dbbd9257a27d304a98ab96568
Dante Soares <dante.m.soares@rice.edu>
2016-05-26 17:33:45 -0500

    Added tasks controller and task plans controller routing specs for the delete/undelete actions

---
+++ spec/routing/api/v1/task_plans_controller_routing_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe Api::V1::TaskPlansController, type: :routing, api: true, version: :v1 do
+
+  context 'DELETE /api/task_plans/:id' do
+    it "routes to #destroy" do
+      expect(delete '/api/plans/42').to(
+        route_to('api/v1/task_plans#destroy', format: 'json', id: '42')
+      )
+    end
+  end
+
+  context 'PUT /api/task_plans/:id/restore' do
+    it "routes to #restore" do
+      expect(put '/api/plans/42/restore').to(
+        route_to('api/v1/task_plans#restore', format: 'json', id: '42')
+      )
+    end
+  end
+
+end
---
+++ spec/routing/api/v1/tasks_controller_routing_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe Api::V1::TasksController, type: :routing, api: true, version: :v1 do
+
+  context 'DELETE /api/tasks/:id' do
+    it "routes to #destroy" do
+      expect(delete '/api/tasks/42').to route_to('api/v1/tasks#destroy', format: 'json', id: '42')
+    end
+  end
+
+end
