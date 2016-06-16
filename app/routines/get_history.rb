5ce76be833ad2e9c200650013c50e2f95d3514b5
Dante Soares <dante.m.soares@rice.edu>
2016-06-16 15:09:01 -0500

    Fix broken GetHistory specs

--- app/routines/get_history.rb
+++ app/routines/get_history.rb
@@ -18,7 +18,7 @@ class GetHistory
 
     # Find reading pages without dynamic exercises
     reading_page_ids = tasks.select(&:reading?)
-                            .flat_map{ |task| task.task_plan.settings['page_ids'] }.uniq
+                            .flat_map{ |task| task.task_plan.settings['page_ids'] }.compact.uniq
     reading_pages = Content::Models::Page.where(id: reading_page_ids).preload(:reading_dynamic_pool)
     non_dynamic_reading_pages = reading_pages.to_a.select{ |page| page.reading_dynamic_pool.empty? }
     non_dynamic_reading_page_ids_set = Set.new non_dynamic_reading_pages.map(&:id)
@@ -28,7 +28,7 @@ class GetHistory
       next true if task.id == current_task_id
       next false unless task.task_type == 'reading'
 
-      reading_page_ids_set = Set.new task.task_plan.settings['page_ids']
+      reading_page_ids_set = Set.new task.task_plan.settings['page_ids'].map(&:to_i)
       reading_page_ids_set.subset? non_dynamic_reading_page_ids_set
     end
 
