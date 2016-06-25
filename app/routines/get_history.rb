df32cf8901489f1d1f5661a3dff075f100a67af8
Dante Soares <dante.m.soares@rice.edu>
2016-06-25 01:54:07 -0500

    Optimize GetHistory some more

--- app/routines/get_history.rb
+++ app/routines/get_history.rb
@@ -1,99 +1,116 @@
 class GetHistory
-  MAX_ROLES_PER_QUERY = 10
+  TASK_BATCH_SIZE = 1000
 
   lev_routine express_output: :history
 
   protected
 
   def exec(roles:, type: :all)
+    roles = [roles].flatten.compact
+
     all_history = Hashie::Mash.new
+    roles.each do |role|
+      all_history[role] = Hashie::Mash.new(
+        total_count: 0, ecosystem_ids: [], core_page_ids: [], exercise_numbers: []
+      )
+    end
 
-    [roles].flatten.compact.each_slice(MAX_ROLES_PER_QUERY) do |roles_slice|
-      roles_by_id = roles_slice.index_by(&:id)
-      role_ids = roles_by_id.keys
+    roles_by_id = roles.index_by(&:id)
+    role_ids = roles_by_id.keys
 
-      all_tasks = Tasks::Models::Task
-        .joins{[task_plan.outer, taskings]}
-        .where(taskings: { entity_role_id: role_ids })
-        .preload([:task_plan, :concept_coach_task, {tasked_exercises: :exercise}])
-        .select([Tasks::Models::Task.arel_table[Arel.star],
-                 Tasks::Models::Tasking.arel_table[:entity_role_id]])
+    if type == :all || type == :reading
+      # Exclude reading tasks that only have pages without dynamic exercises
+      reading_task_plan_id_to_page_ids_map = {}
+      Tasks::Models::TaskPlan.joins(tasks: :taskings)
+                             .where(type: 'reading', tasks: {taskings: {entity_role_id: role_ids}})
+                             .uniq.pluck(:id, :settings).each do |task_plan_id, settings|
+        page_ids = (settings['page_ids'] || []).compact.map(&:to_i).uniq
 
-      all_tasks = all_tasks.where(task_type: Tasks::Models::Task.task_types[type]) unless type == :all
-
-      reading_tasks, other_tasks = all_tasks.partition(&:reading?)
-
-      # Find reading pages without dynamic exercises
-      grouped_reading_tasks = reading_tasks.group_by(&:task_plan).map do |task_plan, tasks|
-        [(task_plan.settings['page_ids'] || []).compact, tasks]
-      end
-      reading_page_ids = grouped_reading_tasks.map(&:first).flatten.uniq
-      reading_pages = Content::Models::Page.where(id: reading_page_ids).preload(:reading_dynamic_pool)
-      non_dynamic_reading_pages = reading_pages.to_a.select{ |page| page.reading_dynamic_pool.empty? }
-      non_dynamic_reading_page_ids_set = non_dynamic_reading_pages.map(&:id)
-
-      # Remove reading tasks without dynamic exercises from the history
-      filtered_grouped_reading_tasks = grouped_reading_tasks.reject do |page_ids, tasks|
-        reading_page_ids = page_ids.map(&:to_i)
-        reading_page_ids.all?{ |page_id| non_dynamic_reading_page_ids_set.include? page_id }
-      end
-      filtered_reading_tasks = filtered_grouped_reading_tasks.flat_map(&:second)
-
-      filtered_tasks = filtered_reading_tasks + other_tasks
-      grouped_tasks = filtered_tasks.group_by(&:entity_role_id)
-
-      # Add an empty array for roles with no tasks
-      taskless_role_ids = role_ids.reject{ |role_id| grouped_tasks.has_key? role_id }
-      taskless_role_ids.each{ |role_id| grouped_tasks[role_id] = [] }
-
-      # Since getting page_ids for homework tasks requires DB access, get them all at once
-      exercise_id_to_page_id_map = {}
-      homework_tasks = all_tasks.select(&:homework?)
-      all_hw_exercise_ids = homework_tasks.flat_map{ |task| task.task_plan.settings['exercise_ids'] }
-      all_hw_exercises = Content::Models::Exercise.where(id: all_hw_exercise_ids)
-                                                  .select([:id, :content_page_id])
-      all_hw_exercises.each do |exercise|
-        exercise_id_to_page_id_map[exercise.id] = exercise.content_page_id
+        reading_task_plan_id_to_page_ids_map[task_plan_id] = page_ids
       end
 
-      grouped_tasks.each do |entity_role_id, tasks|
-        history = Hashie::Mash.new
+      reading_page_ids = reading_task_plan_id_to_page_ids_map.values.flatten
+      reading_pages = Content::Models::Page.where(id: reading_page_ids)
+                                           .preload(:reading_dynamic_pool)
+                                           .to_a
+      non_dynamic_reading_pages = reading_pages.select{ |page| page.reading_dynamic_pool.empty? }
+      non_dynamic_reading_page_ids = non_dynamic_reading_pages.map(&:id)
 
-        role = roles_by_id[entity_role_id]
+      excluded_reading_task_plan_ids = reading_task_plan_id_to_page_ids_map
+                                         .select do |task_plan_id, page_ids|
+        page_ids.all?{ |page_id| non_dynamic_reading_page_ids.include? page_id }
+      end.map(&:first)
+    end
 
-        sorted_tasks = tasks.sort_by do |task|
-          due_date = task.due_at_ntz.to_f
-          open_date = task.opens_at_ntz.to_f
-          tie_breaker = (task.task_plan || task).created_at.to_f
+    if type == :all || type == :homework
+      # Since getting page_ids for homework task plans requires DB access, get them all at once
+      homework_task_plan_id_to_page_ids_map = {}
+      homework_exercise_id_to_task_plan_id_map = {}
+      Tasks::Models::TaskPlan.joins(tasks: :taskings)
+                             .where(type: 'homework', tasks: {taskings: {entity_role_id: role_ids}})
+                             .uniq.pluck(:id, :settings).each do |task_plan_id, settings|
+        exercise_ids = (settings['exercise_ids'] || []).compact.map(&:to_i)
+        exercise_ids.each do |exercise_id|
+          homework_exercise_id_to_task_plan_id_map[exercise_id] ||= []
+          homework_exercise_id_to_task_plan_id_map[exercise_id] << task_plan_id
+        end
+      end
+      homework_exercise_ids = homework_exercise_id_to_task_plan_id_map.keys.flatten
+      homework_exercises = Content::Models::Exercise.where(id: homework_exercise_ids)
+                                                    .select([:id, :content_page_id])
+      homework_exercises.each do |exercise|
+        task_plan_ids = homework_exercise_id_to_task_plan_id_map[exercise.id]
+        task_plan_ids.each do |task_plan_id|
+          homework_task_plan_id_to_page_ids_map[task_plan_id] ||= []
+          homework_task_plan_id_to_page_ids_map[task_plan_id] << exercise.content_page_id
+        end
+      end
 
-          [due_date, open_date, tie_breaker]
-        end.reverse!
+      homework_task_plan_id_to_page_ids_map.each do |task_plan_id, page_ids|
+        homework_task_plan_id_to_page_ids_map[task_plan_id] = page_ids.uniq
+      end
+    end
 
-        history.total_count = sorted_tasks.size
+    query = Tasks::Models::Task.joins{[task_plan.outer, taskings]}
+                               .where(taskings: {entity_role_id: role_ids})
+    query = query.where{task_plan.id.not_in excluded_reading_task_plan_ids} \
+      if type == :all || type == :reading
+    query = query.where(task_type: Tasks::Models::Task.task_types[type]) unless type == :all
 
-        history.ecosystem_ids = sorted_tasks.map{ |task| task.task_plan.try(:content_ecosystem_id) }
+    task_count = query.count
+
+    # http://stackoverflow.com/a/15190294
+    # This logic works because we don't modify the tasks at all here
+    (0..[task_count - TASK_BATCH_SIZE, 0].max).step(TASK_BATCH_SIZE) do |offset|
+      tasks = query.order{[due_at_ntz.desc, opens_at_ntz.desc,
+                           task_plan.created_at.desc, created_at.desc]}
+                   .limit(TASK_BATCH_SIZE).offset(offset)
+                   .select([Tasks::Models::Task.arel_table[Arel.star],
+                            Tasks::Models::Tasking.arel_table[:entity_role_id]])
+                   .preload([:task_plan, :concept_coach_task, {tasked_exercises: :exercise}])
+
+      tasks.each do |task|
+        role = roles_by_id[task.entity_role_id]
+        history = all_history[role]
+
+        history.total_count += 1
+
+        history.ecosystem_ids << task.task_plan.try(:content_ecosystem_id)
 
         # The core page ids exclude spaced practice/personalized pages
-        history.core_page_ids = sorted_tasks.map do |task|
-          case task.task_type.to_sym
-          when :reading
-            task.task_plan.settings['page_ids'].compact.map(&:to_i)
-          when :homework
-            exercise_ids = task.task_plan.settings['exercise_ids'].compact.map(&:to_i)
-            exercise_ids.map{ |exercise_id| exercise_id_to_page_id_map[exercise_id] }.compact.uniq
-          when :concept_coach
-            [task.concept_coach_task.content_page_id]
-          else
-            task.tasked_exercises.map{ |te| te.exercise.content_page_id }.uniq
-          end
+        history.core_page_ids << case task.task_type.to_sym
+        when :reading
+          reading_task_plan_id_to_page_ids_map[task.task_plan.id]
+        when :homework
+          homework_task_plan_id_to_page_ids_map[task.task_plan.id]
+        when :concept_coach
+          [task.concept_coach_task.content_page_id]
+        else
+          task.tasked_exercises.map{ |te| te.exercise.content_page_id }.uniq
         end
 
         # The exercise numbers include spaced practice/personalized exercises
-        history.exercise_numbers = sorted_tasks.map do |task|
-          task.tasked_exercises.map{ |te| te.exercise.number }
-        end
-
-        all_history[role] = history
+        history.exercise_numbers << task.tasked_exercises.map{ |te| te.exercise.number }
       end
     end
 
