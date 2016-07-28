9908d5ee6f1d23be556d3cc9c2ed270400098148
JP Slavinsky <jpslav@gmail.com>
2016-07-28 13:55:23 -0700

    limit imports to admin-specified term years

--- lib/settings/salesforce.rb
+++ lib/settings/salesforce.rb
@@ -11,6 +11,14 @@ module Settings
         Settings::Db.store.import_real_salesforce_courses = bool
       end
 
+      def term_years_to_import
+        Settings::Db.store.term_years_to_import
+      end
+
+      def term_years_to_import=(string)
+        Settings::Db.store.term_years_to_import = string
+      end
+
     end
 
   end
