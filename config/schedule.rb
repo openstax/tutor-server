e7cedcfadb20cbf81c4f66786ec4c97b8fdd9438
Dantemss <dante.m.soares@rice.edu>
2016-08-12 11:47:36 -0500

    Remove UpdateClues from cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,11 +11,6 @@ set :runner_command, "#{bundle_command} rails runner"
 
 every 1.minute do
   rake 'openstax:accounts:sync:all'
-  rake 'openstax:biglearn:clues:update:recent'
-end
-
-every 1.day, at: '9:00 AM' do  # ~ 3am central
-  rake 'openstax:biglearn:clues:update:all'
 end
 
 every 1.hour do
