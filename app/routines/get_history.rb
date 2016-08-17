3dd1de1e0f87862e3097eb6e4ec94382b7b4f09e
Dante Soares <dante.m.soares@rice.edu>
2016-08-17 18:33:26 -0500

    Use SQL select and pluck to make GetHistory faster

--- app/routines/get_history.rb
+++ app/routines/get_history.rb
@@ -31,11 +31,11 @@ class GetHistory
       end
 
       reading_page_ids = reading_task_plan_id_to_page_ids_map.values.flatten
-      reading_pages = Content::Models::Page.where(id: reading_page_ids)
-                                           .preload(:reading_dynamic_pool)
-                                           .to_a
-      non_dynamic_reading_pages = reading_pages.select{ |page| page.reading_dynamic_pool.empty? }
-      non_dynamic_reading_page_ids = non_dynamic_reading_pages.map(&:id)
+      non_dynamic_reading_page_ids = Content::Models::Page
+                                       .joins(:reading_dynamic_pool)
+                                       .where(id: reading_page_ids)
+                                       .where{reading_dynamic_pool.content_exercise_ids == '[]'}
+                                       .pluck(:id)
 
       excluded_reading_task_plan_ids = reading_task_plan_id_to_page_ids_map
                                          .select do |task_plan_id, page_ids|
@@ -72,7 +72,7 @@ class GetHistory
       end
     end
 
-    query = Tasks::Models::Task.joins{[task_plan.outer, taskings]}
+    query = Tasks::Models::Task.joins{[task_plan.outer, concept_coach_task.outer, taskings]}
                                .where(taskings: {entity_role_id: role_ids})
     query = query.where{task_plan.id.not_in excluded_reading_task_plan_ids} \
       if type == :all || type == :reading
@@ -85,10 +85,17 @@ class GetHistory
     (0..[task_count - TASK_BATCH_SIZE, 0].max).step(TASK_BATCH_SIZE) do |offset|
       tasks = query.order{[opens_at_ntz.desc, due_at_ntz.desc, created_at.desc, id.desc]}
                    .limit(TASK_BATCH_SIZE).offset(offset)
-                   .select([Tasks::Models::Task.arel_table[Arel.star],
+                   .select([Tasks::Models::Task.arel_table[:id],
+                            Tasks::Models::Task.arel_table[:task_type],
+                            Tasks::Models::Task.arel_table[:time_zone_id],
+                            Tasks::Models::Task.arel_table[:created_at],
+                            Tasks::Models::Task.arel_table[:opens_at_ntz],
+                            Tasks::Models::Task.arel_table[:due_at_ntz],
+                            Tasks::Models::TaskPlan.arel_table[:id].as('task_plan_id'),
+                            Tasks::Models::TaskPlan.arel_table[:content_ecosystem_id],
+                            Tasks::Models::ConceptCoachTask.arel_table[:content_page_id],
                             Tasks::Models::Tasking.arel_table[:entity_role_id]])
-                   .preload([:task_plan, :concept_coach_task,
-                             :time_zone, {tasked_exercises: :exercise}])
+                   .preload([:time_zone, {tasked_exercises: :exercise}])
 
       tasks.each do |task|
         role = roles_by_id[task.entity_role_id]
@@ -100,16 +107,16 @@ class GetHistory
 
         history.task_types << task.task_type.to_sym
 
-        history.ecosystem_ids << task.task_plan.try(:content_ecosystem_id)
+        history.ecosystem_ids << task.content_ecosystem_id
 
         # The core page ids exclude spaced practice/personalized pages
         history.core_page_ids << case task.task_type.to_sym
         when :reading
-          reading_task_plan_id_to_page_ids_map[task.task_plan.id]
+          reading_task_plan_id_to_page_ids_map[task.task_plan_id]
         when :homework
-          homework_task_plan_id_to_page_ids_map[task.task_plan.id]
+          homework_task_plan_id_to_page_ids_map[task.task_plan_id]
         when :concept_coach
-          [task.concept_coach_task.content_page_id]
+          [task.content_page_id]
         else
           task.tasked_exercises.map{ |te| te.exercise.content_page_id }.uniq
         end
