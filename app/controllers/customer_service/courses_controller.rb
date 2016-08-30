c58e3194cb2d6ceb3c7188c191f95a036da5ac8e
bryanelidimas <bryanelidimas@gmail.com>
2016-08-30 15:19:23 -0500

    Remove try-s as they are not necessary

--- app/controllers/customer_service/courses_controller.rb
+++ app/controllers/customer_service/courses_controller.rb
@@ -7,15 +7,14 @@ class CustomerService::CoursesController < CustomerService::BaseController
     @query = params[:query]
     courses = SearchCourses.call(query: params[:query], order_by: params[:order_by]).outputs
     params[:per_page] = courses.total_count if params[:per_page] == "all"
-    params_for_pagination = {page: params.fetch(:page, 1), per_page: params.fetch(:per_page, 25)}
-    @total_courses = courses.try(:total_count)
+    @total_courses = courses.total_count
     @course_infos = courses.items.preload(
       [
         :profile, { teachers: { role: [:role_user, :profile] }, periods_with_deleted: :latest_enrollments_with_deleted }
       ],
       [ ecosystems: [:books] ],
       [ :periods ]
-    ).try(:paginate, params_for_pagination)
+    ).paginate(page: params.fetch(:page, 1), per_page: params.fetch(:per_page, 25))
   end
 
   def show
