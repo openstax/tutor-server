2c099c49c534fcc641f8a9e782fbe93037c01661
Dante Soares <dante.m.soares@rice.edu>
2016-09-01 20:38:18 -0500

    Increased maximum allowed memory usage to half of the dev server RAM

--- config/puma.rb
+++ config/puma.rb
@@ -41,7 +41,7 @@ before_fork do
   PumaWorkerKiller.config do |config|
     config.ram           = 2048 # mb
     config.frequency     = 5    # seconds
-    config.percent_usage = 0.25
+    config.percent_usage = 0.5
     config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
   end
 
