5ddd5ad904d835140a8a3888da84756d3df7dac6
Dante Soares <dante.m.soares@rice.edu>
2016-09-08 00:16:53 -0500

    Don't explode when trying to initialize the biglearn client before the database is migrated

--- lib/settings/biglearn.rb
+++ lib/settings/biglearn.rb
@@ -4,12 +4,15 @@ module Settings
     class << self
 
       def client
+        # This code can be called when creating a database (before the settings table is created).
+        # In that case, the setting has never been set, so the correct value is the default value.
+        return Settings::Db.store.defaults[:biglearn_client] \
+          unless ActiveRecord::Base.connection.table_exists? 'settings'
+
         # Changes to this setting in the middle of a test aren't always
         # reflected in the cached value, so reset it. Similar to calling
         # `reload!` on an activerecord in a spec, just doing it here because
         # it is easy to forget in specs.
-
-        # Settings::Db.store.object('biglearn_client').try(:rewrite_cache) if Rails.env.test?
         Settings::Db.store.object('biglearn_client').try(:expire_cache) if Rails.env.test?
 
         Settings::Db.store.biglearn_client
