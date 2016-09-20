ab2a382930b536cd2ddb0b696f57319f3947fdc0
Dante Soares <dante.m.soares@rice.edu>
2016-09-20 11:18:07 -0500

    Added :new field to Salesforce Opportunities

--- app/subsystems/salesforce/remote/opportunity.rb
+++ app/subsystems/salesforce/remote/opportunity.rb
@@ -3,11 +3,12 @@ class Salesforce::Remote::Opportunity < ActiveForce::SObject
   field :term_year,                    from: "TermYear__c"
   field :book_name,                    from: "Book_Text__c"
   field :contact_id,                   from: "Contact__c"
+  field :new,                          from: "New__c"
 
   self.table_name = 'Opportunity'
 
   def term_year_object
-    @term_year_object ||= Salesforce::Remote::TermYear.from_string(self.term_year)
+    @term_year_object ||= Salesforce::Remote::TermYear.from_string(term_year)
   end
 
 end
