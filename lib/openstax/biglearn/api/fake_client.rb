0619b95ff51fb3466cc203f994cd2954196a6b05
Dante Soares <dante.m.soares@rice.edu>
2016-09-30 15:56:06 -0500

    FakeClient now properly stores and retrieves PEs from and for assignments

--- lib/openstax/biglearn/api/fake_client.rb
+++ lib/openstax/biglearn/api/fake_client.rb
@@ -77,8 +77,86 @@ class OpenStax::Biglearn::Api::FakeClient
   # Creates or updates a task in Biglearn
   # Requests are hashes containing the following key: :task
   def create_update_assignments(requests)
+    task_plans = requests.map{ |request| request[:task].task_plan }
+
+    # The following code mimics GetHistory to get the core pages for each task
+    homework_exercise_id_to_task_plans_map = {}
+    reading_page_id_to_task_plans_map = {}
+
+    # Collect all exercise and page ids first
+    task_plans.each do |task_plan|
+      task_plan_type = task_plan.try!(:type) || 'nil'
+
+      case task_plan_type
+      when 'homework'
+        exercise_ids = (task_plan.settings['exercise_ids'] || []).compact.map(&:to_i)
+        exercise_ids.each do |exercise_id|
+          homework_exercise_id_to_task_plans_map[exercise_id] ||= []
+          homework_exercise_id_to_task_plans_map[exercise_id] << task_plan
+        end
+      when 'reading', 'practice'
+        page_ids = (task_plan.settings['page_ids'] || []).compact.map(&:to_i).uniq
+
+        page_ids.each do |page_id|
+          reading_page_id_to_task_plans_map[page_id] ||= []
+          reading_page_id_to_task_plans_map[page_id] << task_plan
+        end
+      else
+        raise NotImplementedError,
+              "Biglearn FakeClient does not yet support #{task_plan_type} task_plans"
+      end
+    end
+
+    homework_exercise_ids = homework_exercise_id_to_task_plans_map.keys.flatten
+    reading_page_ids = reading_page_id_to_task_plans_map.keys.flatten
+
+    # Do some queries to get the dynamic exercises for each assignment type
+    task_plan_to_pe_ids_map = {}
+
+    Content::Models::Exercise.joins(page: :homework_dynamic_pool)
+                             .where(id: homework_exercise_ids)
+                             .select([:id, Content::Models::Pool.arel_table[:content_exercise_ids]])
+                             .find_each do |exercise|
+      task_plans = homework_exercise_id_to_task_plans_map[exercise.id]
+
+      task_plans.each do |task_plan|
+        task_plan_to_pe_ids_map[task_plan] ||= []
+        task_plan_to_pe_ids_map[task_plan] += JSON.parse(exercise.content_exercise_ids)
+      end
+    end
+
+    Content::Models::Page.joins(:reading_dynamic_pool)
+                         .where(id: reading_page_ids)
+                         .select([:id, Content::Models::Pool.arel_table[:content_exercise_ids]])
+                         .each do |page|
+      task_plans = reading_page_id_to_task_plans_map[page.id]
+
+      task_plans.each do |task_plan|
+        task_plan_to_pe_ids_map[task_plan] ||= []
+        task_plan_to_pe_ids_map[task_plan] += JSON.parse(page.content_exercise_ids)
+      end
+    end
+
+    all_pe_ids = task_plan_to_pe_ids_map.values.flatten
+
+    # Get the uuids for each dynamic exercise id
+    pe_id_to_pe_uuid_map = Content::Models::Exercise.where(id: all_pe_ids).pluck(:id, :uuid).to_h
+
+    task_plan_to_pe_uuids_map = {}
+    task_plan_to_pe_ids_map.each do |task_plan, pe_ids|
+      task_plan_to_pe_uuids_map[task_plan] = []
+
+      pe_ids.each do |pe_id|
+        task_plan_to_pe_uuids_map[task_plan] << pe_id_to_pe_uuid_map[pe_id]
+      end
+    end
+
     requests.map do |request|
       task = request[:task]
+      task_key = "tasks/#{task.uuid}/pe_uuids"
+      exercise_uuids = task_plan_to_pe_uuids_map[task.task_plan]
+
+      store.write task_key, exercise_uuids.to_json
 
       {
         request_uuid: request[:request_uuid],
@@ -92,13 +170,34 @@ class OpenStax::Biglearn::Api::FakeClient
   # May return less than the given number if there aren't enough exercises
   # Requests are hashes containing the following keys: :task and :max_exercises_to_return
   def fetch_assignment_pes(requests)
+    request_task_keys_map = {}
+    requests.each do |request|
+      request_task_keys_map[request] = "tasks/#{request[:task].uuid}/pe_uuids"
+    end
+
+    exercise_uuids_map = store.read_multi(*request_task_keys_map.values)
+
     requests.map do |request|
-      {
-        request_uuid: request[:request_uuid],
-        assignment_uuid: request[:task].uuid,
-        exercise_uuids: [],
-        assignment_status: :assignment_ready
-      }
+      task_key = request_task_keys_map[request]
+      all_exercise_uuids_json = exercise_uuids_map[task_key]
+
+      if all_exercise_uuids_json.nil?
+        {
+          request_uuid: request[:request_uuid],
+          assignment_uuid: request[:task].uuid,
+          exercise_uuids: [],
+          assignment_status: :assignment_unknown
+        }
+      else
+        all_exercise_uuids = JSON.parse all_exercise_uuids_json
+
+        {
+          request_uuid: request[:request_uuid],
+          assignment_uuid: request[:task].uuid,
+          exercise_uuids: all_exercise_uuids.sample(request[:max_exercises_to_return]),
+          assignment_status: :assignment_ready
+        }
+      end
     end
   end
 
@@ -128,27 +227,28 @@ class OpenStax::Biglearn::Api::FakeClient
 
   protected
 
-  def random_clue
-    aggregate = rand(0.0..1.0)
-    confidence_left  = [aggregate - 0.1, 0.0].max
-    confidence_right = [aggregate + 0.1, 1.0].min
-    level = aggregate >= 0.8 ? 'high' : (aggregate >= 0.3 ? 'medium' : 'low')
-    confidence = ['good', 'bad'].sample
-    samples = 7
-    threshold = 'above'
-    unique_learner_count = 1
+  def random_clue(options = {})
+    options[:value] ||= rand(0.0..1.0)
+    options[:value_interpretation] ||= options[:value] >= 0.8 ?
+                                         'high' : (options[:value] >= 0.3 ? 'medium' : 'low')
+    options[:confidence_interval_left]  ||= [options[:value] - 0.1, 0.0].max
+    options[:confidence_interval_right] ||= [options[:value] + 0.1, 1.0].min
+    options[:confidence_interval_interpretation] = ['good', 'bad'].sample
+    options[:sample_size] = 7
+    options[:sample_size_interpretation] = 'above'
+    options[:unique_learner_count] = 1
 
     {
-      value: aggregate,
-      value_interpretation: level,
+      value: options[:value],
+      value_interpretation: options[:value_interpretation],
       confidence_interval: [
-        confidence_left,
-        confidence_right
+        options[:confidence_interval_left],
+        options[:confidence_interval_right]
       ],
-      confidence_interval_interpretation: confidence,
-      sample_size: samples,
-      sample_size_interpretation: threshold,
-      unique_learner_count: unique_learner_count
+      confidence_interval_interpretation: options[:confidence_interval_interpretation],
+      sample_size: options[:sample_size],
+      sample_size_interpretation: options[:sample_size_interpretation],
+      unique_learner_count: options[:unique_learner_count]
     }
   end
 
