8cadacfcfa34a82a794e6d5a469437c66f3a799f
Dante Soares <dante.m.soares@rice.edu>
2016-10-03 16:03:33 -0500

    Removed the Biglearn local query client and local clue

--- lib/settings/biglearn.rb
+++ lib/settings/biglearn.rb
@@ -9,12 +9,6 @@ module Settings
         return Settings::Db.store.defaults[:biglearn_client] \
           unless ActiveRecord::Base.connection.table_exists? 'settings'
 
-        # Changes to this setting in the middle of a test aren't always
-        # reflected in the cached value, so reset it. Similar to calling
-        # `reload!` on an activerecord in a spec, just doing it here because
-        # it is easy to forget in specs.
-        Settings::Db.store.object('biglearn_client').try(:expire_cache) if Rails.env.test?
-
         Settings::Db.store.biglearn_client
       end
 
