6e661d0e51f88e4d3b36b34fcbe6173f2711cb64
Dante Soares <dante.m.soares@rice.edu>
2016-10-07 19:32:29 -0500

    Added a spec for the course_profile active? method and start/end date validations

--- app/subsystems/course_profile/models/profile.rb
+++ app/subsystems/course_profile/models/profile.rb
@@ -12,7 +12,7 @@ class CourseProfile::Models::Profile < Tutor::SubSystems::BaseModel
   validates :course, :time_zone, presence: true, uniqueness: true
   validates :name, :starts_at, :ends_at, presence: true
 
-  validate :default_times_have_good_values
+  validate :default_times_have_good_values, :ends_after_it_starts
 
   delegate :name, to: :school, prefix: true, allow_nil: true
 
@@ -24,7 +24,15 @@ class CourseProfile::Models::Profile < Tutor::SubSystems::BaseModel
     read_attribute(:default_open_time) || Settings::Db.store[:default_open_time]
   end
 
-  def is_active?(current_time = Time.current)
+  def active?(current_time = Time.current)
     starts_at <= current_time && current_time <= ends_at
+  end
+
+  protected
+
+  def ends_after_it_starts
+    return if starts_at.nil? || ends_at.nil? || ends_at > starts_at
+    errors.add :base, 'cannot end before it starts'
+    false
   end
 end
