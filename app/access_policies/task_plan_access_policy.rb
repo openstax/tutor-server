4d0e6cafcdbe756c3e16ef5f5bd02c0ff288e4bc
Dante Soares <dante.m.soares@rice.edu>
2016-10-10 15:44:56 -0500

    Added course start/end dates and active info to representer; Disallow creating assignments for inactive courses

--- app/access_policies/task_plan_access_policy.rb
+++ app/access_policies/task_plan_access_policy.rb
@@ -1,23 +1,19 @@
 class TaskPlanAccessPolicy
+  VALID_ACTIONS = [:read, :create, :update, :destroy, :restore]
+
   def self.action_allowed?(action, requestor, task_plan)
-    case action
-    when :read, :create, :update, :destroy, :restore
-      if task_plan.owner.is_a?(Entity::Course)
-        UserIsCourseTeacher[user: requestor, course: task_plan.owner] rescue false
-      elsif requestor.is_human?
-        requestor_is_task_plan_owner?(requestor, task_plan.owner)
-      else
-        false
-      end
+    return false if !VALID_ACTIONS.include?(action)
+
+    owner = task_plan.owner
+
+    if owner.is_a?(Entity::Course)
+      return false if action == :create && !owner.active?
+
+      UserIsCourseTeacher[user: requestor, course: owner]
+    elsif requestor.is_human?
+      requestor == owner || requestor.to_model == owner
     else
       false
     end
   end
-
-  private
-
-  def self.requestor_is_task_plan_owner?(requestor, owner)
-    return true if requestor == owner || requestor.to_model == owner
-  end
-
 end
