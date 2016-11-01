0a566e3fbc257c401f42042cba3cb4253cf8fe67
Nathan Stitt <nathan@stitt.org>
2016-11-01 16:25:13 -0500

    Add inverse_of to AR association declarations

--- app/subsystems/user/models/tour.rb
+++ app/subsystems/user/models/tour.rb
@@ -1,6 +1,7 @@
 module User
   module Models
     class Tour < Tutor::SubSystems::BaseModel
+      has_many :tour_views, dependent: :destroy, inverse_of: :tour
 
       validates :identifier, presence: true, uniqueness: true, format: /\A[a-z\-]+\z/
     end
--- app/subsystems/user/models/tour_view.rb
+++ app/subsystems/user/models/tour_view.rb
@@ -2,9 +2,14 @@ module User
   module Models
     class TourView < Tutor::SubSystems::BaseModel
 
-      belongs_to :tour
+      belongs_to :tour, inverse_of: :tour_views
 
       belongs_to :profile, -> { with_deleted }, inverse_of: :tour_views
+
+      validates :tour,       presence: true, uniqueness: { scope: :user_tour_id }
+      validates :view_count, presence: true
+      validates :profile,    presence: true
+
 
     end
   end
