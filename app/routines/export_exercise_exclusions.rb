558d13499f0505e441ff11f63a9899231341daa1
Dante Soares <dante.m.soares@rice.edu>
2016-11-16 13:40:32 -0600

    Fixed the exercise exclusions report

--- app/routines/export_exercise_exclusions.rb
+++ app/routines/export_exercise_exclusions.rb
@@ -19,8 +19,8 @@ class ExportExerciseExclusions
         upload_by_exercise_csv
       end
     else
-      outputs[:by_course] = ee_by_course
-      outputs[:by_exercise] = ee_by_exercise
+      outputs[:by_course] = exercises_by_course
+      outputs[:by_exercise] = exercises_by_exercise
     end
   ensure
     remove_exported_files
@@ -32,110 +32,120 @@ class ExportExerciseExclusions
                             ).sort_by(&:exercise_number)
   end
 
-  def exercises_hash_by_page_uuid
-    @exercises_hash_by_page_uuid ||= Content::Models::Exercise
-                                       .where(number: all_excluded_exercise_numbers)
-                                       .preload(:page).group_by{ |ex| ex.page.uuid }
+  def exercises_by_ecosystems_and_pages
+    @exercises_by_ecosystems_and_pages ||= {}.tap do |hash|
+      excluded_exercises = Content::Models::Exercise.where(number: all_excluded_exercise_numbers)
+                                                    .preload(page: :ecosystem)
+
+      excluded_exercises.each do |excluded_exercise|
+        page = excluded_exercise.page
+        ecosystem = page.ecosystem
+
+        hash[ecosystem] ||= {}
+        hash[ecosystem][page] ||= []
+        hash[ecosystem][page] << excluded_exercise
+      end
+    end
   end
 
   def all_excluded_exercise_numbers
     @excluded_exercise_numbers ||= excluded_exercises.map(&:exercise_number).uniq
   end
 
-  def get_page_url_by_page_uuid
-    @get_page_url_by_page_uuid ||= all_page_urls_by_page_uuids
-  end
-
-  def all_page_urls_by_page_uuids
-    cnx_page_urls_by_page_uuids = {}
-    exercises_hash_by_page_uuid.each do |page_uuid, exercises|
-      page_url = OpenStax::Cnx::V1.webview_url_for(page_uuid) # URL
-      cnx_page_urls_by_page_uuids[page_uuid] = page_url
-    end
-    cnx_page_urls_by_page_uuids
-  end
-
-  def exercise_urls_by_exercise_number
-    @exercise_urls_by_exercise_numbers ||= all_exercise_urls_by_exercise_numbers
-  end
-
-  def all_exercise_urls_by_exercise_numbers
-    exercise_urls = {}
-    all_excluded_exercise_numbers.each do |number|
-      exercise_url = OpenStax::Exercises::V1.uri_for("/exercises/#{number}").to_s # URL
-      exercise_urls[number] = exercise_url
-    end
-    exercise_urls
-  end
-
-  def page_uuids_by_exercise_numbers
-    @page_uuids_by_exercise_numbers ||= all_page_uuids_by_exercise_numbers
-  end
-
-  def all_page_uuids_by_exercise_numbers
-    uuids = Hash.new{ |hash, key| hash[key] = [] }
-    exercises_hash_by_page_uuid.map do |page_uuid, exercises|
-      exercises.map(&:number).uniq.each do |number|
-        uuids[number] << page_uuid
+  def page_urls_by_pages
+    @page_urls_by_pages ||= {}.tap do |hash|
+      exercises_by_ecosystems_and_pages.each do |ecosystem, exercises_by_pages|
+        exercises_by_pages.each do |page, exercises|
+          hash[page] = OpenStax::Cnx::V1.webview_url_for(page.uuid)
+        end
       end
     end
-    uuids
   end
 
-  def get_ee_numbers_with_urls_by_ee(e_numbers = [])
-    ee_and_urls = []
-    e_numbers.each do |number|
-      ee_and_urls << {
-        ee_number: number,
-        ee_url: exercise_urls_by_exercise_number[number]
+  def exercise_urls_by_exercise_numbers
+    @exercise_urls_by_exercise_numbers ||= {}.tap do |hash|
+      all_excluded_exercise_numbers.each do |number|
+        hash[number] = OpenStax::Exercises::V1.uri_for("/exercises/#{number}").to_s
+      end
+    end
+  end
+
+  def pages_by_ecosystems_and_exercise_numbers
+    @pages_by_ecosystems_and_exercise_numbers ||= {}.tap do |hash|
+      exercises_by_ecosystems_and_pages.each do |ecosystem, exercises_by_pages|
+        hash[ecosystem] ||= {}
+
+        exercises_by_pages.each do |page, exercises|
+          exercises.map(&:number).uniq.each do |number|
+            hash[ecosystem][number] ||= []
+            hash[ecosystem][number] << page
+          end
+        end
+      end
+    end
+  end
+
+  def get_exercise_numbers_and_urls_for_exercise_numbers(numbers:)
+    numbers.map do |number|
+      {
+        exercise_number: number,
+        exercise_url: exercise_urls_by_exercise_numbers[number]
       }
     end
-    ee_and_urls
   end
 
-  def get_page_uuids_and_urls_by_ee_numbers(numbers = [])
-    uuids_and_urls = []
-    numbers.map do |e_number|
-      page_uuids = page_uuids_by_exercise_numbers[e_number] || []
+  def get_page_uuids_and_urls_for_ecosystems_and_exercise_numbers(numbers:, ecosystems: nil)
+    (ecosystems || pages_by_ecosystems_and_exercise_numbers.keys).flat_map do |ecosystem|
+      pages_by_exercise_numbers = pages_by_ecosystems_and_exercise_numbers[ecosystem] || {}
 
-      page_uuids.each do |page_uuid|
-        uuids_and_urls << {
-          page_uuid: page_uuid,
-          page_url: get_page_url_by_page_uuid[page_uuid]
-        }
+      numbers.flat_map do |number|
+        pages = pages_by_exercise_numbers[number] || []
+
+        pages.map do |page|
+          {
+            page_uuid: page.uuid,
+            page_url: page_urls_by_pages[page]
+          }
+        end
       end
     end
-    uuids_and_urls
   end
 
-  def get_teachers_by_course(course)
-    return "" unless course && course.teachers
-    course.teachers.map{ |teacher| teacher.role.name }.join(', ')
+  def get_teachers_by_course(course:)
+    course.teachers.map(&:name).join(', ')
   end
 
-  def ee_by_course
+  def exercises_by_course
     excluded_exercises.group_by(&:course).map do |course, excluded_exercises|
       numbers = excluded_exercises.flat_map(&:exercise_number)
+      ecosystems = [course.ecosystems.first]
+      exercises_hash = get_exercise_numbers_and_urls_for_exercise_numbers(numbers: numbers)
+      pages_hash = get_page_uuids_and_urls_for_ecosystems_and_exercise_numbers(
+        ecosystems: ecosystems, numbers: numbers
+      )
 
       Hashie::Mash.new(
         course_id: course.id,
         course_name: course.name,
         course_type: course.is_concept_coach ? 'CC' : 'Tutor',
-        teachers: get_teachers_by_course(course),
+        teachers: get_teachers_by_course(course: course),
         excluded_exercises_count: excluded_exercises.length,
-        excluded_exercises_numbers_with_urls: get_ee_numbers_with_urls_by_ee(numbers),
-        page_uuids_with_urls: get_page_uuids_and_urls_by_ee_numbers(numbers)
+        excluded_exercises_numbers_with_urls: exercises_hash,
+        page_uuids_with_urls: pages_hash
       )
     end
   end
 
-  def ee_by_exercise
+  def exercises_by_exercise
     excluded_exercises.group_by(&:exercise_number).map do |number, excluded_exercises|
+      exercises_hash = get_exercise_numbers_and_urls_for_exercise_numbers(numbers: [number])
+      pages_hash = get_page_uuids_and_urls_for_ecosystems_and_exercise_numbers(numbers: [number])
+
       Hashie::Mash.new(
         exercise_number: number,
-        exercise_url: get_ee_numbers_with_urls_by_ee([number]).first[:ee_url],
+        exercise_url: exercises_hash.first[:exercise_url],
         excluded_exercises_count: excluded_exercises.length,
-        pages_with_uuids_and_urls: get_page_uuids_and_urls_by_ee_numbers([number])
+        pages_with_uuids_and_urls: pages_hash
       )
     end
   end
@@ -154,17 +164,17 @@ class ExportExerciseExclusions
         "CNX Section UUID URLs"
       ])
 
-      ee_by_course.each do |ee|
+      exercises_by_course.each do |mash|
         file.add_row([
-          ee.course_id,
-          ee.course_name,
-          ee.course_type,
-          ee.teachers,
-          ee.excluded_exercises_count,
-          ee.excluded_exercises_numbers_with_urls.map(&:ee_number).join(", "),
-          ee.excluded_exercises_numbers_with_urls.map(&:ee_url).join(", "),
-          ee.page_uuids_with_urls.map(&:page_uuid).join(", "),
-          ee.page_uuids_with_urls.map(&:page_url).join(", ")
+          mash.course_id,
+          mash.course_name,
+          mash.course_type,
+          mash.teachers,
+          mash.excluded_exercises_count,
+          mash.excluded_exercises_numbers_with_urls.map(&:exercise_number).join(", "),
+          mash.excluded_exercises_numbers_with_urls.map(&:exercise_url).join(", "),
+          mash.page_uuids_with_urls.map(&:page_uuid).join(", "),
+          mash.page_uuids_with_urls.map(&:page_url).join(", ")
         ])
       end
     end
@@ -180,13 +190,13 @@ class ExportExerciseExclusions
         "CNX Section UUID(s) URLs"
       ])
 
-      ee_by_exercise.each do |ee|
+      exercises_by_exercise.each do |mash|
         file.add_row([
-          ee.exercise_number,
-          ee.exercise_url,
-          ee.excluded_exercises_count,
-          ee.pages_with_uuids_and_urls.map(&:page_uuid).join(", "),
-          ee.pages_with_uuids_and_urls.map(&:page_url).join(", ")
+          mash.exercise_number,
+          mash.exercise_url,
+          mash.excluded_exercises_count,
+          mash.pages_with_uuids_and_urls.map(&:page_uuid).join(", "),
+          mash.pages_with_uuids_and_urls.map(&:page_url).join(", ")
         ])
       end
     end
--- spec/routines/export_exercise_exclusions_spec.rb
+++ spec/routines/export_exercise_exclusions_spec.rb
@@ -13,18 +13,34 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
       @teacher_user = FactoryGirl.create :user, first_name: "Bob", last_name: "Martin"
       @teacher_role = AddUserAsCourseTeacher[course: @course, user: @teacher_user]
 
-      @exercise = FactoryGirl.create :content_exercise
+      chapter = FactoryGirl.create :content_chapter
+
+      @page_1 = FactoryGirl.create :content_page, chapter: chapter
+      @page_2 = FactoryGirl.create :content_page, chapter: chapter
+      @page_3 = FactoryGirl.create :content_page, chapter: chapter
+
+      @exercise_1 = FactoryGirl.create :content_exercise, page: @page_1
+      @exercise_2 = FactoryGirl.create :content_exercise, page: @page_2
+      @exercise_3 = FactoryGirl.create :content_exercise, page: @page_3
+
+      @exercise_another_eco = FactoryGirl.create :content_exercise, number: @exercise_1.number
+
+      ecosystem_model = chapter.ecosystem
+      ecosystem = Content::Ecosystem.new(strategy: ecosystem_model.wrap)
+      AddEcosystemToCourse[course: @course, ecosystem: ecosystem]
 
       @ee_1 = FactoryGirl.create :course_content_excluded_exercise,
-                                 course: @course, exercise_number: @exercise.number
-      @ee_2 = FactoryGirl.create :course_content_excluded_exercise, course: @course
-      @ee_3 = FactoryGirl.create :course_content_excluded_exercise, course: @course
+                                 course: @course, exercise_number: @exercise_1.number
+      @ee_2 = FactoryGirl.create :course_content_excluded_exercise,
+                                 course: @course, exercise_number: @exercise_2.number
+      @ee_3 = FactoryGirl.create :course_content_excluded_exercise,
+                                 course: @course, exercise_number: @exercise_3.number
     end
 
     let(:outputs){ described_class.call.outputs }
 
     context "output by course" do
-      let(:output){ outputs.by_course.first }
+      let(:output) { outputs.by_course.first }
 
       it "includes the correct keys" do
         expect(output).to include(
@@ -39,15 +55,15 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
         end
 
         specify "course id" do
-          expect(output).to include(course_id: @ee_1.course.id)
+          expect(output).to include(course_id: @course.id)
         end
 
         specify "course name" do
-          expect(output).to include(course_name: @ee_1.course.name)
+          expect(output).to include(course_name: @course.name)
         end
 
         specify "course teachers" do
-          expect(output).to include(teachers: "Bob Martin")
+          expect(output).to include(teachers: @teacher_user.name)
         end
 
         specify "excluded exercises count" do
@@ -58,7 +74,7 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
           specify do
             ee_numbers = @course.excluded_exercises.map(&:exercise_number)
             expect(ee_numbers).to_not be_empty
-            expect(output[:excluded_exercises_numbers_with_urls].map(&:ee_number)).to(
+            expect(output[:excluded_exercises_numbers_with_urls].map(&:exercise_number)).to(
               match_array ee_numbers
             )
           end
@@ -68,7 +84,7 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
             ee_numbers_urls = ee_numbers.map do |number|
               OpenStax::Exercises::V1.uri_for("/exercises/#{number}").to_s
             end
-            expect(output[:excluded_exercises_numbers_with_urls].map(&:ee_url)).to(
+            expect(output[:excluded_exercises_numbers_with_urls].map(&:exercise_url)).to(
               match_array ee_numbers_urls
             )
           end
@@ -76,12 +92,20 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
 
         context "page uuids" do
           specify do
-            expect(output[:page_uuids_with_urls].map(&:page_uuid)).to include(@exercise.page.uuid)
+            page_uuids = output[:page_uuids_with_urls].map(&:page_uuid)
+            expect(page_uuids).to include(@exercise_1.page.uuid)
+            expect(page_uuids).to include(@exercise_2.page.uuid)
+            expect(page_uuids).to include(@exercise_3.page.uuid)
+            expect(page_uuids).not_to include(@exercise_another_eco.page.uuid)
           end
 
           specify "with urls" do
-            expect(output[:page_uuids_with_urls].map(&:page_url)).to(
-              include(OpenStax::Cnx::V1.webview_url_for(@exercise.page.uuid))
+            page_urls = output[:page_uuids_with_urls].map(&:page_url)
+            expect(page_urls).to include(OpenStax::Cnx::V1.webview_url_for(@exercise_1.page.uuid))
+            expect(page_urls).to include(OpenStax::Cnx::V1.webview_url_for(@exercise_2.page.uuid))
+            expect(page_urls).to include(OpenStax::Cnx::V1.webview_url_for(@exercise_3.page.uuid))
+            expect(page_urls).not_to include(
+              OpenStax::Cnx::V1.webview_url_for(@exercise_another_eco.page.uuid)
             )
           end
         end
@@ -93,24 +117,22 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
           ee_numbers_urls = ee_numbers.map do |number|
             OpenStax::Exercises::V1.uri_for("/exercises/#{number}").to_s
           end
+          page_uuids = [@page_1, @page_2, @page_3].map(&:uuid)
+          page_urls = page_uuids.map{ |page_uuid| OpenStax::Cnx::V1.webview_url_for(page_uuid) }
 
           with_rows_from_csv("by_course") do |rows|
             headers = rows.first
             values = rows.second
             data = Hash[headers.zip(values)]
 
-            expect(data["Course ID"]).to eq @ee_1.course.id.to_s
-            expect(data["Course Name"]).to eq @ee_1.course.name
-            expect(data["Teachers"]).to eq "Bob Martin"
+            expect(data["Course ID"]).to eq @course.id.to_s
+            expect(data["Course Name"]).to eq @course.name
+            expect(data["Teachers"]).to eq @teacher_user.name
             expect(data["# Exclusions"]).to eq "3"
-            expect(data["Excluded Numbers"].split(", ")).to(
-              match_array ee_numbers.map{|numb| "#{numb}"}
-            )
+            expect(data["Excluded Numbers"].split(", ")).to match_array ee_numbers.map(&:to_s)
             expect(data["Excluded Numbers URLs"].split(", ")).to match_array ee_numbers_urls
-            expect(data["CNX Section UUID"]).to eq @exercise.page.uuid
-            expect(data["CNX Section UUID URLs"]).to(
-              eq OpenStax::Cnx::V1.webview_url_for(@exercise.page.uuid)
-            )
+            expect(data["CNX Section UUID"].split(", ")).to match_array page_uuids
+            expect(data["CNX Section UUID URLs"].split(", ")).to match_array page_urls
           end
         end
 
@@ -133,64 +155,91 @@ RSpec.describe ExportExerciseExclusions, type: :routine do
     end
 
     context "output by exercise" do
-      let!(:output){ outputs.by_exercise.first }
+      let!(:outputs_by_exercise) { outputs.by_exercise }
 
       it "includes the correct keys" do
-        expect(output).to include(
-          :exercise_number, :exercise_url, :excluded_exercises_count, :pages_with_uuids_and_urls
-        )
+        outputs_by_exercise.each do |output|
+          expect(output).to include(
+            :exercise_number, :exercise_url, :excluded_exercises_count, :pages_with_uuids_and_urls
+          )
+        end
       end
 
       context "returns a (Hashie Mash) hash with the correct" do
         specify do
-          expect(output).to be_a Hashie::Mash
+          outputs_by_exercise.each{ |output| expect(output).to be_a Hashie::Mash }
         end
 
         specify "exercise number" do
-          expect(output[:exercise_number]).to eq @ee_1.exercise_number
+          exercise_numbers = [@ee_1, @ee_2, @ee_3].map(&:exercise_number)
+          outputs_by_exercise.each do |output|
+            expect(output[:exercise_number]).to be_in exercise_numbers
+          end
         end
 
         specify "exercise url" do
-          expected_uri = OpenStax::Exercises::V1.uri_for("/exercises/#{@ee_1.exercise_number}").to_s
-          expect(output[:exercise_url]).to eq expected_uri
+          exercise_urls = [@ee_1, @ee_2, @ee_3].map do |ee|
+            OpenStax::Exercises::V1.uri_for("/exercises/#{ee.exercise_number}").to_s
+          end
+          outputs_by_exercise.each{ |output| expect(output[:exercise_url]).to be_in exercise_urls }
         end
 
         specify "exclusions count" do
-          expect(output).to include(excluded_exercises_count: 1)
+          outputs_by_exercise.each do |output|
+            expect(output).to include(excluded_exercises_count: 1)
+          end
         end
 
         context "pages with uuids and urls" do
           it "includes the appropriate uuids" do
-            expect(output[:pages_with_uuids_and_urls].map(&:page_uuid)).to(
-              include(@exercise.page.uuid)
-            )
+            page_uuids = [@page_1, @page_2, @page_3, @exercise_another_eco.page].map(&:uuid)
+
+            outputs_by_exercise.each do |output|
+              output[:pages_with_uuids_and_urls].map(&:page_uuid).each do |page_uuid|
+                expect(page_uuid).to be_in page_uuids
+              end
+            end
           end
 
           specify "with urls" do
-            expected_webview_url = OpenStax::Cnx::V1.webview_url_for(@exercise.page.uuid)
-            expect(output[:pages_with_uuids_and_urls].map(&:page_url)).to(
-              include(expected_webview_url)
-            )
+            page_urls = [@page_1, @page_2, @page_3, @exercise_another_eco.page].map do |page|
+              OpenStax::Cnx::V1.webview_url_for(page.uuid)
+            end
+
+            outputs_by_exercise.each do |output|
+              output[:pages_with_uuids_and_urls].map(&:page_url).each do |page_url|
+                expect(page_url).to be_in page_urls
+              end
+            end
           end
         end
       end
 
       context "as csv" do
         it "includes all the correct data" do
+          exercise_numbers = [@ee_1, @ee_2, @ee_3].map(&:exercise_number)
+          exercise_urls = exercise_numbers.map do |exercise_number|
+            OpenStax::Exercises::V1.uri_for("/exercises/#{exercise_number}").to_s
+          end
+          page_uuids = [@page_1, @page_2, @page_3, @exercise_another_eco.page].map(&:uuid)
+          page_urls = page_uuids.map{ |page_uuid| OpenStax::Cnx::V1.webview_url_for(page_uuid) }
+
           with_rows_from_csv("by_exercise") do |rows|
             headers = rows.first
-            values = rows.second
-            data = Hash[headers.zip(values)]
+            value_rows = rows[1..-1]
+            value_rows.each do |values|
+              data = Hash[headers.zip(values)]
 
-            expect(data["Exercise Number"]).to eq "#{@exercise.number}"
-            expect(data["Exercise Number URL"]).to(
-              eq OpenStax::Exercises::V1.uri_for("/exercises/#{@exercise.number}").to_s
-            )
-            expect(data["# Exclusions"]).to eq "1"
-            expect(data["CNX Section UUID(s)"]).to eq @exercise.page.uuid
-            expect(data["CNX Section UUID(s) URLs"]).to(
-              eq OpenStax::Cnx::V1.webview_url_for(@exercise.page.uuid)
-            )
+              expect(data["Exercise Number"]).to be_in exercise_numbers.map(&:to_s)
+              expect(data["Exercise Number URL"]).to be_in exercise_urls
+              expect(data["# Exclusions"]).to eq "1"
+              data["CNX Section UUID(s)"].split(", ").each do |page_uuid|
+                expect(page_uuid).to be_in page_uuids
+              end
+              data["CNX Section UUID(s) URLs"].split(", ").each do |page_url|
+                expect(page_url).to be_in page_urls
+              end
+            end
           end
         end
 
