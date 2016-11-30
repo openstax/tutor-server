7f9302537d0dd340a2573f7dfa4f69e2c3aa0c2d
Dante Soares <dante.m.soares@rice.edu>
2016-11-30 10:50:33 -0600

    Some role/enrollment routine cleanup

--- app/handlers/courses_enroll.rb
+++ app/handlers/courses_enroll.rb
@@ -1,10 +1,13 @@
 class CoursesEnroll
   lev_handler
 
-  uses_routine CourseMembership::GetPeriod, as: :get_period,       translations: { outputs: { type: :verbatim } }
-  uses_routine CollectCourseInfo,           as: :get_course_info,  translations: { outputs: { type: :verbatim } }
-  uses_routine UserIsCourseStudent,         as: :is_student,       translations: { outputs: { type: :verbatim } }
+  uses_routine CourseMembership::GetPeriod, as: :get_period,
+                                            translations: { outputs: { type: :verbatim } }
+  uses_routine CollectCourseInfo,           as: :get_course_info
+  uses_routine UserIsCourseStudent,         as: :is_student,
+                                            translations: { outputs: { type: :verbatim } }
   uses_routine GetUserCourses,              as: :get_courses
+
   protected
 
   def authorized?; true; end
@@ -13,19 +16,15 @@ class CoursesEnroll
     run(:get_period, enrollment_code: params[:enroll_token])
     fatal_error(code: :enrollment_code_not_found) if outputs.period.nil?
 
-    run(:get_course_info, with: [:teacher_names], courses: outputs.period.course)
-    outputs.course = outputs.courses.first
-
-    outputs.current_courses = run(:get_courses, user: caller).outputs.courses.reject{|c| c.id == outputs.period.course.id }
-
     run(:is_student, course: outputs.period.course, user: caller,
         include_dropped: true, include_archived: false)
 
-    fatal_error(code: :period_is_archived) if outputs.period.deleted?
-    fatal_error(code: :user_is_dropped)    if outputs.is_dropped
+    fatal_error(code: :period_is_archived)               if outputs.period.deleted?
+    fatal_error(code: :user_is_dropped)                  if outputs.is_dropped
+    fatal_error(code: :user_is_already_a_course_student) if outputs.user_is_course_student
 
-    fatal_error(code: :user_is_already_a_course_student) \
-               if outputs.user_is_course_student && !outputs.is_dropped
+    outputs.course = run(:get_course_info, with: [:teacher_names],
+                                           courses: outputs.period.course).outputs.courses.first
   end
 
 end
--- app/subsystems/role/get_user_roles.rb
+++ app/subsystems/role/get_user_roles.rb
@@ -5,16 +5,16 @@ class Role::GetUserRoles
 
   protected
 
-  def exec(users_or_user_ids, role_types=:any)
+  def exec(users_or_user_ids, role_types = :any)
     user_ids = verify_and_get_id_array(users_or_user_ids, User::User)
 
-    role_users = Role::Models::RoleUser.includes(:role).where{user_profile_id.in user_ids}
+    role_users = Role::Models::RoleUser.preload(:role).where(user_profile_id: user_ids)
     roles = role_users.map(&:role)
 
-    if role_types != :any
-      role_types = [role_types].flatten.map(&:to_s)
-      roles = roles.select{|role| role_types.include?(role.role_type)}
-    end
+    role_types = [role_types].flatten.map(&:to_s)
+
+    roles = roles.select{ |role| role_types.include?(role.role_type) } \
+      unless role_types.any?{ |role_type| role_type == 'any' }
 
     outputs[:roles] = roles
   end
