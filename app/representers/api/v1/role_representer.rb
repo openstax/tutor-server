28f735751aab7c2adbca7a77ed1e82a414c0e6a9
Nathan Stitt <nathan@stitt.org>
2016-12-05 15:17:48 -0600

    Add joined_at to role
    Needed so the FE can start to nag for student id 1 week after the user joins

--- app/representers/api/v1/role_representer.rb
+++ app/representers/api/v1/role_representer.rb
@@ -16,5 +16,12 @@ module Api::V1
              writeable: false,
              schema_info: { required: true }
 
+    property :joined_at,
+             type: String,
+             readable: true,
+             writeable: false,
+             getter: ->(*) { DateTimeUtilities.to_api_s(created_at) },
+             schema_info: { required: true }
+
   end
 end
