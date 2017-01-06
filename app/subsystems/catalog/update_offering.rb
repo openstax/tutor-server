42ea0e6d9bc4addbca713db8056849dc571f78cd
Dante Soares <dante.m.soares@rice.edu>
2017-01-06 12:09:21 -0600

    Automatically update associated courses' ecosystems when updating a catalog offering

--- app/subsystems/catalog/update_offering.rb
+++ app/subsystems/catalog/update_offering.rb
@@ -6,8 +6,22 @@ module Catalog
 
     def exec(id, attributes)
       offering = Catalog::Models::Offering.find(id)
+      old_ecosystem = offering.ecosystem
       offering.update_attributes(attributes)
+      new_ecosystem = offering.ecosystem
       transfer_errors_from(offering, {type: :verbatim}, true)
+
+      return if old_ecosystem == new_ecosystem
+
+      wrapped_ecosystem = Marshal.dump(Content::Ecosystem.new(strategy: new_ecosystem.wrap))
+
+      offering.courses.each do |course|
+        job_id = CourseContent::AddEcosystemToCourse.perform_later(
+          course: course, ecosystem: wrapped_ecosystem
+        )
+        job = Jobba.find(job_id)
+        job.save(course_ecosystem: new_ecosystem.title, course_id: course.id)
+      end
     end
 
   end
---
+++ spec/subsystems/catalog/update_offering_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe Catalog::UpdateOffering, type: :routine do
+
+  let(:old_ecosystem) { FactoryGirl.create :content_ecosystem }
+  let(:new_ecosystem) { FactoryGirl.create :content_ecosystem }
+
+  let(:offering)      { FactoryGirl.create :catalog_offering, ecosystem: old_ecosystem }
+
+  let(:courses)       do
+    3.times.map do
+      FactoryGirl.create(:course_profile_course, offering: offering).tap do |course|
+        course.ecosystems << old_ecosystem
+      end
+    end
+  end
+
+  let(:new_attributes) do
+    {
+      salesforce_book_name: 'new_book',
+      appearance_code: 'new',
+      is_tutor: true,
+      is_concept_coach: false,
+      is_available: true,
+      title: 'New Book',
+      description: 'Newest of the new',
+      webview_url: 'http://www.example.com',
+      pdf_url: 'http://www.example.pdf'
+    }
+  end
+
+  it "updates the offering's attributes" do
+    described_class.call(offering.id, new_attributes)
+    str_attributes = new_attributes.stringify_keys
+    expect(offering.reload.attributes.slice(*str_attributes.keys)).to eq str_attributes
+  end
+
+  it "updates associated courses' ecosystems if the ecosystem is changed" do
+    courses.each{ |course| expect(course.ecosystems.first).to eq old_ecosystem }
+
+    described_class.call(offering.id, ecosystem: new_ecosystem)
+
+    courses.each{ |course| expect(course.reload.ecosystems.first).to eq new_ecosystem }
+  end
+
+end
