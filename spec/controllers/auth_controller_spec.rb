0eaac210246f99e54c413753f039571bcf159d0c
Nathan Stitt <nathan@stitt.org>
2017-01-30 13:59:25 -0600

    Allow client to specify type of signup via :go param

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -23,12 +23,12 @@ RSpec.describe AuthController, type: :controller do
       }
 
       it 'allows access to popup and redirects to accounts' do
-        get :popup, go: 'faster' # "faster" isn't a valid option
-        expect(response).to redirect_to(controller.openstax_accounts.login_url)
+        get :popup, go: 'faster' # "faster" isn't a valid option, but will still be passed on
+        expect(response).to redirect_to(controller.openstax_accounts.login_url(go: 'faster'))
       end
 
       it 'allows access to popup and redirects to accounts signin' do
-        get :popup, go: 'signup'
+        get :popup, go: 'student_signup'
         expect(response).to redirect_to(controller.openstax_accounts.login_url(go: 'student_signup'))
       end
     end
