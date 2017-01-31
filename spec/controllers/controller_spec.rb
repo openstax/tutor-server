f7942272ea05626a16294465e8128e65a9a7ca8d
JP Slavinsky <jpslav@gmail.com>
2017-01-31 10:02:40 -0800

    send exception emails for UrlGenerationErrors

--- spec/controllers/controller_spec.rb
+++ spec/controllers/controller_spec.rb
@@ -6,16 +6,26 @@ class TestExceptionsController < ApplicationController
   def bad_action
     RaiseUnknownConstantException
   end
+
+  def url_generation_error
+    raise ActionController::UrlGenerationError
+  end
 end
 
 RSpec.describe TestExceptionsController, type: :controller do
   context 'rescuing exceptions' do
-    it 'fires off an email' do
+    before(:each) do
       ActionMailer::Base.deliveries.clear
-
       allow(request).to receive(:remote_ip) { '96.21.0.39' }
+    end
 
+    it 'fires off an email for some error' do
       expect { get :bad_action }.to raise_error(NameError)
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+    end
+
+    it 'fires off an email for a UrlGenerationError' do
+      expect { get :url_generation_error }.to raise_error(ActionController::UrlGenerationError)
       expect(ActionMailer::Base.deliveries.size).to eq(1)
     end
   end
