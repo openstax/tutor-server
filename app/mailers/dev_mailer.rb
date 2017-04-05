ffc3422579696343912ad9a7036ab772429d8427
Dante Soares <dante.m.soares@rice.edu>
2017-04-05 11:44:37 -0500

    Exception email fix for production

--- app/mailers/dev_mailer.rb
+++ app/mailers/dev_mailer.rb
@@ -1,8 +1,9 @@
 class DevMailer < ApplicationMailer
 
-  def inspect_object(object:, to: nil, subject:)
+  def inspect_object(object:, to: nil, from: nil, subject:)
     @object = object
     mail to: to || Rails.application.secrets.exception['recipients'],
+         from: from || Rails.application.secrets.exception['sender'],
          subject: subject
   end
 
