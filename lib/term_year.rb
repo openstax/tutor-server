e2b04872db711b9edbb67c5a237a95bf144c4009
Dante Soares <dante.m.soares@rice.edu>
2017-04-20 17:29:15 -0500

    Make demo and spring courses end on June 30th instead

--- lib/term_year.rb
+++ lib/term_year.rb
@@ -18,10 +18,10 @@ TermYear = Struct.new(:term, :year) do
 
   const_set 'TERM_END_DATES', {
     legacy:  ->(year) { TermYear::LEGACY_TERM_ENDS_AT     },
-    demo:    ->(year) { DateTime.new(year + 1, 7)         }, # July 1st of the year after
+    demo:    ->(year) { DateTime.new(year + 1, 6, 30)     },  # June 30th of the year after
     preview: ->(year) { DateTime.current + 8.weeks        }, # 8 weeks after today
     winter:  ->(year) { DateTime.new(year    , 5)         }, # May 1st of given year
-    spring:  ->(year) { DateTime.new(year    , 7)         }, # July 1st of given year
+    spring:  ->(year) { DateTime.new(year    , 6, 30)     }, # June 30th of given year
     summer:  ->(year) { DateTime.new(year    , 9)         }, # September 1st of given year
     fall:    ->(year) { DateTime.new(year + 1   )         }  # January 1st of the year after
   }
