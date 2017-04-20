9539ebaedf0917321e456231bca1bcc4068792be
Dante Soares <dante.m.soares@rice.edu>
2017-04-20 16:31:45 -0500

    Made the Biglearn algorithm names migration also remove local_query_with_ from the default biglearn client name

--- db/migrate/20170307230726_add_biglearn_algorithm_names_to_course_profile_courses.rb
+++ db/migrate/20170307230726_add_biglearn_algorithm_names_to_course_profile_courses.rb
@@ -1,5 +1,5 @@
 class AddBiglearnAlgorithmNamesToCourseProfileCourses < ActiveRecord::Migration
-  def change
+  def up
     add_column :course_profile_courses, :biglearn_student_clues_algorithm_name, :string
     add_column :course_profile_courses, :biglearn_teacher_clues_algorithm_name, :string
     add_column :course_profile_courses, :biglearn_assignment_spes_algorithm_name, :string
@@ -26,5 +26,17 @@ class AddBiglearnAlgorithmNamesToCourseProfileCourses < ActiveRecord::Migration
                        :biglearn_practice_worst_areas_algorithm_name,
                        false,
                        Settings::Biglearn.practice_worst_areas_algorithm_name
+
+    # Remove local_query_with_ from the default biglearn client, since those are no longer valid
+    biglearn_client = Settings::Db.store.biglearn_client
+    Settings::Db.store.biglearn_client = biglearn_client.to_s.sub('local_query_with_', '').to_sym
+  end
+
+  def down
+    remove_column :course_profile_courses, :biglearn_practice_worst_areas_algorithm_name, :string
+    remove_column :course_profile_courses, :biglearn_assignment_pes_algorithm_name, :string
+    remove_column :course_profile_courses, :biglearn_assignment_spes_algorithm_name, :string
+    remove_column :course_profile_courses, :biglearn_teacher_clues_algorithm_name, :string
+    remove_column :course_profile_courses, :biglearn_student_clues_algorithm_name, :string
   end
 end
