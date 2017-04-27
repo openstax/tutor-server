075cddca2451377627f50d18a27c4f84d8421ac2
Dante Soares <dante.m.soares@rice.edu>
2017-04-26 20:46:48 -0500

    Updated CLUe format in the teacher course guide and fixed specs

--- spec/representers/api/v1/teacher_course_guide_representer_spec.rb
+++ spec/representers/api/v1/teacher_course_guide_representer_spec.rb
@@ -18,36 +18,46 @@ RSpec.describe Api::V1::TeacherCourseGuideRepresenter, type: :representer do
   end
 
   it 'recurses children with the child representer' do
-    guide = [Hashie::Mash.new({ children: [{ title: 'my cool title',
-                                             book_location: [1, 4],
-                                             questions_answered_count: 25,
-                                             clue: {
-                                               value: 0.89,
-                                               value_interpretation: 'high',
-                                               confidence_interval: [0.7, 0.9],
-                                               confidence_interval_interpretation: 'good',
-                                               sample_size: 16,
-                                               sample_size_interpretation: 'above'
-                                             },
-                                             practice_count: 3,
-                                             page_ids: [4, 5, 6] }] })]
+    guide = [
+      Hashie::Mash.new(
+        {
+          children: [
+            {
+              title: 'my cool title',
+              book_location: [1, 4],
+              questions_answered_count: 25,
+              clue: {
+                minimum: 0.7,
+                most_likely: 0.89,
+                maximum: 0.9,
+                is_real: true
+              },
+              practice_count: 3,
+              page_ids: [4, 5, 6]
+            }
+          ]
+        }
+      )
+    ]
 
     decorated = described_class.new(guide).to_hash.first
 
-    expect(decorated['children']).to eq([{
-      'title' => 'my cool title',
-      'chapter_section' => [1, 4],
-      'questions_answered_count' => 25,
-      "clue" => {
-        "value" => 0.89,
-        "value_interpretation" => 'high',
-        "confidence_interval" => [0.7, 0.9],
-        "confidence_interval_interpretation" => 'good',
-        "sample_size" => 16,
-        "sample_size_interpretation" => 'above'
-      },
-      'practice_count' => 3,
-      'page_ids' => ['4', '5', '6']
-    }])
+    expect(decorated['children']).to eq(
+      [
+        {
+          title: 'my cool title',
+          chapter_section: [1, 4],
+          questions_answered_count: 25,
+          clue: {
+            minimum: 0.7,
+            most_likely: 0.89,
+            maximum: 0.9,
+            is_real: true
+          },
+          practice_count: 3,
+          page_ids: ['4', '5', '6']
+        }.deep_stringify_keys
+      ]
+    )
   end
 end
