3c9fff3cfac3136e882a4e3737f8f8da7b60aec3
Dante Soares <dante.m.soares@rice.edu>
2017-04-26 20:46:48 -0500

    Updated CLUe format in the teacher course guide and fixed specs

--- spec/subsystems/content/routines/import_exercises_spec.rb
+++ spec/subsystems/content/routines/import_exercises_spec.rb
@@ -168,44 +168,6 @@ RSpec.describe Content::Routines::ImportExercises, type: :routine, speed: :slow,
     end
   end
 
-  context 'adding lo:uuid tags' do
-    before(:all) do
-      chapter = FactoryGirl.create :content_chapter
-      @ecosystem = chapter.book.ecosystem
-
-      cnx_page = OpenStax::Cnx::V1::Page.new(id: '0e58aa87-2e09-40a7-8bf3-269b2fa16509',
-                                             title: 'Acceleration')
-
-      @page = VCR.use_cassette('Content_Routines_ImportExercises/with_custom_tags', VCR_OPTS) do
-        Content::Routines::ImportPage[cnx_page: cnx_page, chapter: chapter,
-                                      number: 2, book_location: [3, 1]]
-      end
-    end
-
-    it 'adds an lo:uuid tag when there are no LOs or APLOs' do
-      stub_exercise_query([{tags: ['some-id-tag']}])
-      described_class.call(ecosystem: @ecosystem, page: @page, query_hash: {tags: ['some-id-tag']})
-      exercise = Content::Models::Exercise.order(:created_at).last
-      expect(exercise.tags.map(&:value)).to include "lo:0e58aa87-2e09-40a7-8bf3-269b2fa16509"
-      expect(exercise.tags.map(&:tag_type)).to include "lo"
-      expect(exercise.los.first.value).to eq "lo:0e58aa87-2e09-40a7-8bf3-269b2fa16509"
-    end
-
-    it 'does not add an lo:uuid tag when there is an LO' do
-      stub_exercise_query([{tags: ['some-id-tag', 'lo:stax-phys:1-2-3']}])
-      described_class.call(ecosystem: @ecosystem, page: @page, query_hash: {tags: ['some-id-tag']})
-      exercise = Content::Models::Exercise.order(:created_at).last
-      expect(exercise.tags.map(&:value)).not_to include "lo:0e58aa87-2e09-40a7-8bf3-269b2fa16509"
-    end
-
-    it 'does not add an lo:uuid tag when there is an APLO' do
-      stub_exercise_query([{tags: ['some-id-tag', 'aplo:stax-bio:4-5-6']}])
-      described_class.call(ecosystem: @ecosystem, page: @page, query_hash: {tags: ['some-id-tag']})
-      exercise = Content::Models::Exercise.order(:created_at).last
-      expect(exercise.tags.map(&:value)).not_to include "lo:0e58aa87-2e09-40a7-8bf3-269b2fa16509"
-    end
-  end
-
   context 'incoming free response exercises' do
     before(:all) do
       chapter = FactoryGirl.create :content_chapter
@@ -227,38 +189,6 @@ RSpec.describe Content::Routines::ImportExercises, type: :routine, speed: :slow,
                              query_hash: {tags: ['some-id-tag']})
       end.to change{Content::Models::Exercise.count}.by(0)
     end
-  end
-
-  context 'MutableWrapper' do
-    it "can add LOs" do
-      original_lo = 'lo:stax-phys:1-2-3'
-      original_lo_hash = {value: original_lo, name: nil, type: :lo}
-
-      new_lo = 'lo:somethingrandom'
-      new_lo_hash = {value: new_lo, name: nil, type: :lo}
-
-      hash = OpenStax::Exercises::V1::FakeClient.new_exercise_hash(tags: [original_lo])
-      exercise = OpenStax::Exercises::V1::Exercise.new(content: hash.to_json)
-      mutable = described_class::MutableWrapper.new(exercise)
-
-      expect(mutable.los).to eq [original_lo]
-      expect(mutable.tags).to eq [original_lo]
-      expect(mutable.import_tags).to eq [original_lo]
-      expect(mutable.tag_hashes).to eq [original_lo_hash]
-      expect(mutable.lo_hashes).to eq [original_lo_hash]
-      expect(mutable.import_tag_hashes).to eq [original_lo_hash]
-
-      mutable.add_lo(new_lo)
-
-      expect(mutable.los).to eq [original_lo, new_lo]
-      expect(mutable.tags).to eq [original_lo, new_lo]
-      expect(mutable.import_tags).to eq [original_lo, new_lo]
-
-      expect(mutable.tag_hashes).to eq [original_lo_hash, new_lo_hash]
-      expect(mutable.lo_hashes).to eq [original_lo_hash, new_lo_hash]
-      expect(mutable.import_tag_hashes).to eq [original_lo_hash, new_lo_hash]
-    end
-
   end
 
   def stub_exercise_query(array_of_exercise_options={})
