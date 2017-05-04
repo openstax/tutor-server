75675c7b8e7ec13acee4de47e19d177bf1bfe32a
Nathan Stitt <nathan@stitt.org>
2017-05-04 17:21:41 -0500

    default estimated_student_count to nil

--- app/routines/create_course.rb
+++ app/routines/create_course.rb
@@ -17,7 +17,7 @@ class CreateCourse
   def exec(name:, term:, year:, is_preview:, is_college:, is_concept_coach: nil, num_sections: 0,
            catalog_offering: nil, appearance_code: nil, starts_at: nil, ends_at: nil,
            school: nil, time_zone: nil, cloned_from: nil, default_open_time: nil,
-           default_due_time: nil, estimated_student_count: 0)
+           default_due_time: nil, estimated_student_count: nil)
     # TODO eventually, making a course part of a school should be done independently
     # with separate admin controller interfaces and all work done in the SchoolDistrict SS
 
