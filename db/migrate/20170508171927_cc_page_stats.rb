ba02be6a0c94bbbe3a18ec30b695f0f436529a61
Nathan Stitt <nathan@stitt.org>
2017-05-12 12:32:51 -0500

    read page id from content_exercises table
    This way it reflects proper page for spaced practice

--- db/migrate/20170508171927_cc_page_stats.rb
+++ db/migrate/20170508171927_cc_page_stats.rb
@@ -5,7 +5,7 @@ class CcPageStats < ActiveRecord::Migration
       create materialized view cc_page_stats as
 
       select
-        content_page_id
+        content_exercises.content_page_id
         ,course_membership_periods.course_profile_course_id as course_id
         ,course_membership_periods.id as course_period_id
         ,tasks_task_steps.group_type
@@ -15,25 +15,24 @@ class CcPageStats < ActiveRecord::Migration
         ,count(tasks_task_steps.first_completed_at) filter (
           where tasks_tasked_exercises.answer_id = tasks_tasked_exercises.correct_answer_id
         ) AS correct_count
-        ,array_agg(tasks_taskings.entity_role_id) as role_ids
-        ,array_agg(tasks_tasks.id) as task_ids
+        ,array_agg(distinct(tasks_taskings.entity_role_id)) as role_ids
+        ,array_agg(distinct(tasks_tasks.id)) as task_ids
       from
-        tasks_concept_coach_tasks
-      join tasks_tasks on tasks_tasks.id = tasks_concept_coach_tasks.tasks_task_id
-        and tasks_tasks.deleted_at is null
-      join tasks_task_steps on tasks_task_steps.tasks_task_id = tasks_tasks.id
+        content_exercises
+      join tasks_tasked_exercises on tasks_tasked_exercises.content_exercise_id = content_exercises.id
+      join tasks_task_steps on tasks_task_steps.tasked_id = tasks_tasked_exercises.id
         and tasks_task_steps.tasked_type = 'Tasks::Models::TaskedExercise'
-      join tasks_tasked_exercises on tasks_task_steps.tasked_id = tasks_tasked_exercises.id
+      join tasks_tasks on tasks_tasks.id = tasks_task_steps.tasks_task_id
+        and tasks_tasks.deleted_at is null
+        and tasks_tasks.completed_exercise_steps_count > 0
       join tasks_taskings on tasks_taskings.tasks_task_id = tasks_tasks.id
         and tasks_taskings.deleted_at is null
       join course_membership_periods on course_membership_periods.id = tasks_taskings.course_membership_period_id
         and course_membership_periods.deleted_at is null
-      where
-        tasks_concept_coach_tasks.deleted_at is null
       group by
         course_id
         ,course_period_id
-        ,tasks_concept_coach_tasks.content_page_id
+        ,content_exercises.content_page_id
         ,tasks_task_steps.group_type
 
       EOS
