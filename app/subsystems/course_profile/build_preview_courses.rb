33004bbc0fc210a010c883e31f80324f95d944c4
Nathan Stitt <nathan@stitt.org>
2017-05-19 16:29:05 -0500

    Populate and build in background

--- app/subsystems/course_profile/build_preview_courses.rb
+++ app/subsystems/course_profile/build_preview_courses.rb
@@ -1,22 +1,22 @@
 class CourseProfile::BuildPreviewCourses
   lev_routine
 
-  uses_routine ::CreateCourse, translations: { outputs: { type: :verbatim } },
-               as: :create_course
+  uses_routine ::CreateCourse, as: :create_course
+  uses_routine PopulatePreviewCourseContent, as: :populate_preview_course_content
 
   protected
 
   def exec(desired_count: Settings::Db.store.prebuilt_preview_course_count)
 
     while (
-      offerings = self.class.offerings_that_need_previews(desired_count: desired_count).to_a
+      offerings = self.class.offerings_that_need_previews(desired_count: desired_count)
     ).any?
       term = TermYear.visible_term_years.first
 
       offerings.each do |offering|
         (desired_count - offering.course_preview_count).times do
 
-          run(:create_course, {
+          course = run(:create_course, {
                 name: "#{offering.description} Preview",
                 term: term.term,
                 year: term.year,
@@ -25,17 +25,24 @@ class CourseProfile::BuildPreviewCourses
                 num_sections: 2,
                 time_zone: "Central Time (US & Canada)",
                 catalog_offering: offering
-              })
-          outputs.course.update_attributes(is_preview_claimed: false)
+              }).outputs.course
+
+          run(:populate_preview_course_content, course: course)
         end
       end
     end
+  end
 
+
+  def self.start_in_background
+    if offerings_that_need_previews.any? && Jobba.where(job_name: self.name).none?
+      self.perform_later
+    end
   end
 
   def self.offerings_that_need_previews(desired_count: Settings::Db.store.prebuilt_preview_course_count)
     courses = CourseProfile::Models::Course
-                .where("is_preview = 't' and is_preview_claimed = 'f'")
+                .where("is_preview = 't' and preview_claimed_at is null")
                 .group(:catalog_offering_id)
                 .select([:catalog_offering_id, 'count(*) as course_preview_count'])
 
@@ -51,6 +58,7 @@ class CourseProfile::BuildPreviewCourses
       .select(
         'catalog_offerings.*, coalesce(course_preview_count, 0) as course_preview_count'
       )
+      .to_a
   end
 
 
