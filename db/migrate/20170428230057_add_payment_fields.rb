2933da639ef74bcea5338ed42cbfdfe5bb544443
JP Slavinsky <jpslav@gmail.com>
2017-05-25 14:25:09 -0700

    removed duplicate uuid migration

--- db/migrate/20170428230057_add_payment_fields.rb
+++ db/migrate/20170428230057_add_payment_fields.rb
@@ -7,10 +7,5 @@ class AddPaymentFields < ActiveRecord::Migration
     add_column :course_membership_students, :is_paid, :boolean, default: false, null: false
     add_column :course_membership_students, :is_comped, :boolean, default: false, null: false
     add_column :course_membership_students, :payment_due_at, :datetime
-
-    # remove on rebase already in BL PR
-    enable_extension 'pgcrypto'
-    add_column :course_membership_students, :uuid, :uuid, null: false, default: 'gen_random_uuid()'
-    add_index :course_membership_students, :uuid, unique: true
   end
 end
