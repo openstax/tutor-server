3d758cce8cb15a351af350f49d3f4fd99ee6e817
Nathan Stitt <nathan@stitt.org>
2017-05-26 10:08:34 -0500

    access policy for events

--- app/access_policies/track_tutor_onboarding_event_policy.rb
+++ app/access_policies/track_tutor_onboarding_event_policy.rb
@@ -1,8 +1,13 @@
 class TrackTutorOnboardingEventPolicy
 
+  TEACHER_EVENTS = %w{
+    arrived_my_courses created_preview_course created_real_course
+    like_preview_ask_later like_preview_yes made_adoption_decision
+  }
   def self.action_allowed?(event, requestor, force)
+    return false if requestor.is_anonymous? || requestor.account.student?
 
-    return false
+    TEACHER_EVENTS.include?(event)
   end
 
 end
---
+++ spec/access_policies/track_tutor_onboarding_event_policy_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe TrackTutorOnboardingEventPolicy, type: :access_policy do
+
+  let(:anon) { User::User.anonymous }
+  let(:user) { FactoryGirl.create(:user) }
+
+
+  it 'cannot be accessed by anonymous users' do
+    expect(TrackTutorOnboardingEventPolicy.action_allowed?('created_real_course', anon,
+                                                           TrackTutorOnboardingEvent)).to eq false
+  end
+
+  it 'cannot be accessed by student users' do
+    user.account.role = :student
+    expect(TrackTutorOnboardingEventPolicy.action_allowed?('created_real_course', user,
+                                                           TrackTutorOnboardingEvent)).to eq false
+  end
+
+  context 'accessed by a teacher' do
+    before(:each) { user.account.role = :instructor }
+
+    it 'rejects invalid events' do
+      expect(TrackTutorOnboardingEventPolicy.action_allowed?('wrong_bad_incorrect', user,
+                                                             TrackTutorOnboardingEvent)).to eq false
+    end
+
+    it 'succeeds with a valid event code' do
+      expect(TrackTutorOnboardingEventPolicy.action_allowed?('created_real_course', user,
+                                                             TrackTutorOnboardingEvent)).to eq true
+    end
+
+  end
+
+end
