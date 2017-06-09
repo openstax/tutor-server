bb09d699478cdf058baab0e57b16b45c732e4216
Dante Soares <dante.m.soares@rice.edu>
2017-06-09 12:03:14 -0500

    Fix set_payment_due_at_not_null due to paranoia gem

--- db/migrate/20170609150609_set_payment_due_at_not_null.rb
+++ db/migrate/20170609150609_set_payment_due_at_not_null.rb
@@ -1,6 +1,7 @@
 class SetPaymentDueAtNotNull < ActiveRecord::Migration
   def change
-    CourseMembership::Models::Student.where(payment_due_at: nil)
+    CourseMembership::Models::Student.unscoped
+                                     .where(payment_due_at: nil)
                                      .preload(course: :time_zone)
                                      .find_each do |student|
       payment_due_at = student.created_at.in_time_zone(student.course.time_zone.to_tz).midnight +
