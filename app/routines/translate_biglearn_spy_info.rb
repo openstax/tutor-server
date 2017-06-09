87fbd4e81fa2af6b0d89e587877fd27384f996af
Dante Soares <dante.m.soares@rice.edu>
2017-06-09 15:31:50 -0500

    TranslateBiglearnSpyInfo now properly stringifies incoming spy_info hash keys

--- app/routines/translate_biglearn_spy_info.rb
+++ app/routines/translate_biglearn_spy_info.rb
@@ -5,21 +5,23 @@ class TranslateBiglearnSpyInfo
   protected
 
   def exec(spy_info:)
-    task_uuids = spy_info.values.map { |hash| hash['assignment_uuid'] }.compact
+    outputs.spy_info = {}
+
+    return if spy_info.nil?
+
+    stringified_spy_info = spy_info.deep_stringify_keys
+
+    task_uuids = stringified_spy_info.values.map { |hash| hash['assignment_uuid'] }.compact
 
     task_id_by_uuid = Tasks::Models::Task.where(uuid: task_uuids).pluck(:uuid, :id).to_h
 
-    translated_spy_info = {}
-    spy_info.each do |key, value|
+    stringified_spy_info.each do |key, value|
       assignment_uuid = value['assignment_uuid']
 
-      translated_spy_info[key] = assignment_uuid.nil? ? value : value.except('assignment_uuid')
-                                                                     .merge(
+      outputs.spy_info[key] = assignment_uuid.nil? ? value : value.except('assignment_uuid').merge(
         'task_id' => task_id_by_uuid[assignment_uuid],
         'task_uuid' => assignment_uuid
       )
     end
-
-    outputs.spy_info = translated_spy_info
   end
 end
