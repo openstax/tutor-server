55de5edf071b6b196e3ca65148ee0f6763865e88
Dante Soares <dante.m.soares@rice.edu>
2017-06-24 00:48:12 -0500

    Added a message when the Biglearn JobFailed exception is raised

--- app/jobs/openstax/biglearn/api/job.rb
+++ app/jobs/openstax/biglearn/api/job.rb
@@ -10,8 +10,11 @@ class OpenStax::Biglearn::Api::Job < ApplicationJob
       responses = [response].flatten
 
       responses.each do |response|
-        raise OpenStax::Biglearn::Api::JobFailed \
-          if !accepted_response_status.include?(response[response_status_key])
+        raise(
+          OpenStax::Biglearn::Api::JobFailed,
+          "[#{method}] Expected #{response_status_key} to be in #{
+          accepted_response_status.inspect} but was #{response[response_status_key]} instead"
+        ) unless accepted_response_status.include?(response[response_status_key])
       end
     end
   end
