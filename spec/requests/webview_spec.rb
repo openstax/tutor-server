0ec21371a489ca1200460de82ae994f1c6d7b5c3
Nathan Stitt <nathan@stitt.org>
2017-07-11 18:37:10 -0500

    test welcome screen

--- spec/requests/webview_spec.rb
+++ spec/requests/webview_spec.rb
@@ -3,13 +3,19 @@ require "rails_helper"
 RSpec.describe "Webview", type: :request do
 
   describe 'GET /courses/enroll/blah' do
-    it 'does not direct accounts to use the alternate signup' do
+
+    it 'renders welcome screen' do
       get '/enroll/whatever'
+      expect(response).to render_template(:enroll)
+    end
+
+    it 'start does not direct accounts to use the alternate signup' do
+      get '/enroll/start/whatever'
       expect(redirect_query_hash).not_to have_key(:signup_at)
     end
 
     it 'directs accounts to go straight to signup for student' do
-      get '/enroll/whatever'
+      get '/enroll/start/whatever'
       expect(redirect_query_hash[:go]).to eq 'student_signup'
     end
   end
