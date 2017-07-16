fb0f7656124838942f32fb77f453fe447add6470
JP Slavinsky <jpslav@gmail.com>
2017-07-16 14:53:09 -0600

    bootstrap spec fixes

--- spec/representers/api/v1/bootstrap_data_representer_spec.rb
+++ spec/representers/api/v1/bootstrap_data_representer_spec.rb
@@ -13,15 +13,15 @@ RSpec.describe Api::V1::BootstrapDataRepresenter, type: :representer do
   end
 
   it "generates a JSON representation of data for a user to start work with" do
-    expect(JSON.parse representation).to eq(
+    expect(JSON.parse representation).to match(
       "user" =>  Api::V1::UserRepresenter.new(user).as_json,
       "courses" => [], # not testing this since it's too expensive to generate meaningful course data
       "accounts_api_url" => OpenStax::Accounts.configuration.openstax_accounts_url + 'api',
       "accounts_profile_url" => OpenStax::Accounts.configuration.openstax_accounts_url + 'profile',
       "errata_form_url" => 'https://oscms.openstax.org/errata/form',
       "tutor_api_url" => 'https://example.com/api',
-      "payments_base_url" => 'https://payments-dev.openstax.org',
-      "payments_embed_js_url" => 'http://localhost:8001/pay/embed.js',
+      "payments_base_url" => a_string_starting_with('http'),
+      "payments_embed_js_url" => a_string_starting_with('http'),
       "payments_product_uuid" => '6d60ab29-3b3d-575a-93ef-57d62e30984c',
       "ui_settings" => {},
       "flash" => { "alert" => 'Nothing!' }
