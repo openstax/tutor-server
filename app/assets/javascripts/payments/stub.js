c083e724bba2b59add6d766043d2df2258a4f1f0
Nathan Stitt <nathan@stitt.org>
2017-07-18 13:55:26 -0500

    add missing ; remove ,

--- app/assets/javascripts/payments/stub.js
+++ app/assets/javascripts/payments/stub.js
@@ -1,26 +1,18 @@
 window.postMessage(JSON.stringify({
   type: 'ready'
-}), '*')
+}), '*');
 
 window.postMessage(JSON.stringify({
-  type: 'size',
-  data: {
+  type: 'size', data: {
     height: document.body.scrollHeight,
     width: document.body.scrollWidth
   }
 }), '*');
 
-document.querySelector('button.success').addEventListener('click', () => {
-  window.postMessage(JSON.stringify({
-    type: 'payment',
-    data: {
-      // TODO: fill in once we know what data is returned
-    }
-  }), '*');
+document.querySelector('button.success').addEventListener('click', function() {
+  window.postMessage(JSON.stringify({ type: 'payment', data: { } }), '*');
 });
 
-document.querySelector('button.cancel').addEventListener('click', () => {
-  window.postMessage(JSON.stringify({
-    type: 'cancel',
-  }), '*');
+document.querySelector('button.cancel').addEventListener('click', function() {
+  window.postMessage(JSON.stringify({ type: 'cancel' }), '*');
 });
