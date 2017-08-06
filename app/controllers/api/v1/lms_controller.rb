3c32b4f1c406049343a8bc2c680dc32f97002e89
Nathan Stitt <nathan@stitt.org>
2017-08-06 17:38:44 -0500

    Add API controller endpoint for find-or-create course keys

---
+++ app/controllers/api/v1/lms_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::LmsController < Api::V1::ApiController
+
+  before_filter :get_course
+
+
+  resource_description do
+    api_versions "v1"
+    short_description 'Get secrets for LMS'
+    description <<-EOS
+    EOS
+  end
+
+  api :GET, '/terms', 'Returns info on to connect a course to a LMS system'
+  description <<-EOS
+    TODO
+  EOS
+  def index
+    if @course.blank? || !UserIsCourseTeacher[user: current_human_user, course: @course]
+      head :forbidden and return
+    end
+
+    # TODO: use polymorphic belongs to?
+    condition = { owner_id: @course.id, name: @course.name }
+    consumer = Lms::Models::ToolConsumer.where(condition).first || Lms::Models::ToolConsumer.create(condition)
+    render json: consumer.as_json.merge(url: lms_launch_url)
+  end
+
+  protected
+
+  def get_course
+    @course = CourseProfile::Models::Course.find(params[:course_id])
+  end
+
+end
