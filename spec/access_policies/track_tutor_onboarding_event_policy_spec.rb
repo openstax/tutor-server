664008934dbbe61000e2482dfd2bafe4ffc5ed8f
Dante Soares <dante.m.soares@rice.edu>
2017-08-07 16:08:48 -0500

    Fix 403 error loop if a student account becomes verified faculty

--- spec/access_policies/track_tutor_onboarding_event_policy_spec.rb
+++ spec/access_policies/track_tutor_onboarding_event_policy_spec.rb
@@ -6,19 +6,13 @@ RSpec.describe TrackTutorOnboardingEventPolicy, type: :access_policy do
   let(:user) { FactoryGirl.create(:user) }
 
 
-  it 'cannot be accessed by anonymous users' do
+  it 'cannot be accessed by non-confirmed-faculty' do
     expect(TrackTutorOnboardingEventPolicy.action_allowed?('created_real_course', anon,
                                                            TrackTutorOnboardingEvent)).to eq false
   end
 
-  it 'cannot be accessed by student users' do
-    user.account.role = :student
-    expect(TrackTutorOnboardingEventPolicy.action_allowed?('created_real_course', user,
-                                                           TrackTutorOnboardingEvent)).to eq false
-  end
-
-  context 'accessed by a teacher' do
-    before(:each) { user.account.role = :instructor }
+  context 'accessed by confirmed faculty' do
+    before(:each) { user.account.faculty_status = :confirmed_faculty }
 
     it 'rejects invalid events' do
       expect(TrackTutorOnboardingEventPolicy.action_allowed?('wrong_bad_incorrect', user,
