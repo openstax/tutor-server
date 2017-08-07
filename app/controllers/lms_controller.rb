74cbcb33e4dacf001b8ddb10de910aae7268e6e8
JP Slavinsky <jpslav@gmail.com>
2017-08-07 09:06:18 -0500

    print launch data on launch, lazy load exercises client

--- app/controllers/lms_controller.rb
+++ app/controllers/lms_controller.rb
@@ -1,6 +1,9 @@
 class LmsController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, only: [:launch]
+  skip_before_filter :authenticate_user!, only: [:configuration, :launch]
+
+  layout false
 
   def configuration
     debugger
@@ -8,7 +11,7 @@ class LmsController < ApplicationController
 
   def launch
     # Check that the request specifies a valid tool consumer
-debugger
+    # debugger
     consumer = Lms::Models::ToolConsumer.find_by(key: params[:oauth_consumer_key])
     return redirect_to action: :launch_failed if consumer.nil?
 
@@ -21,17 +24,20 @@ debugger
     )
     return redirect_to action: :launch_failed if !authenticator.valid_signature?
 
+    @launch_message = authenticator.message
     # Check that we haven't seen this nonce yet
 
-    begin
-      Lms::Models::Nonce.create!({ lms_tool_consumer_id: consumer.id, value: params['oauth_nonce'] })
-    rescue ActiveRecord::RecordNotUnique => ee
-      return redirect_to action: :launch_failed
-    end
+    # begin
+    #   Lms::Models::Nonce.create!({ lms_tool_consumer_id: consumer.id, value: params['oauth_nonce'] })
+    # rescue ActiveRecord::RecordNotUnique => ee
+    #   return redirect_to action: :launch_failed
+    # end
 
     # All checks passed, move along
 
-    redirect_to lti_someplace_url
+    respond_to do |format|
+      format.html
+    end
   end
 
   def someplace
