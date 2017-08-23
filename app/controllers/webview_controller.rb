89c482460bf2b52bffc9050adbad6a5abeebe153
m1yag1 <mike.arbelaez@rice.edu>
2017-08-23 16:15:56 -0500

    added cors_preflight_check to the webview controller for hypothesis

--- app/controllers/webview_controller.rb
+++ app/controllers/webview_controller.rb
@@ -4,7 +4,15 @@ class WebviewController < ApplicationController
 
   layout :resolve_layout
 
-  skip_before_filter :authenticate_user!, only: [:home, :enroll]
+  # Requested by an OPTIONS request type
+  def cors_preflight_check # the other CORS headers are set by the before_filter
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Origin'] = Rails.application.secrets[:hypothesis]['client_url']
+    render text: '', content_type: 'text/plain'
+  end
+
+
+  skip_before_filter :authenticate_user!, only: [:home, :enroll, :cors_preflight_check]
 
   def home
     if params[:cc] == "1"
