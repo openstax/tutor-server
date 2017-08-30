b43ab50cf3f40f87b0aa46d14d9d02c7cb3a0bda
JP Slavinsky <jpslav@gmail.com>
2017-08-29 23:05:26 -0600

    move process launch to handler, bug fixes

--- app/subsystems/lms/models/course_grade_callback.rb
+++ app/subsystems/lms/models/course_grade_callback.rb
@@ -1,4 +1,11 @@
 class Lms::Models::CourseGradeCallback < Tutor::SubSystems::BaseModel
+  # Records data needed to post a course grade for a student back to an LMS.
+  # Though not intended, a student could have multiple of these if a teacher
+  # adds more than one Tutor assignment in their LMS.  Since we just report
+  # course grades all such assignments would get the same grade; but we can
+  # at least deal with this situation until the teacher realizes that is not
+  # useful.
+
   belongs_to :student, subsystem: :course_membership
 
   validates :outcome_url, presence: true
