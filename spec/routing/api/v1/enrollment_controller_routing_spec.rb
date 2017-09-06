372abc8a3a8138ece938b977ea798afe6c85ec42
Nathan Stitt <nathan@stitt.org>
2017-09-06 17:33:58 -0500

    fix routing spec

---
+++ spec/routing/api/v1/enrollment_controller_routing_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::EnrollmentController, type: :routing, api: true, version: :v1 do
+
+  context '/api/enrollment' do
+    it 'routes to #create' do
+      expect(post '/api/enrollment').to route_to('api/v1/enrollment#create', format: 'json')
+    end
+  end
+
+  context '/api/enrollment/:course_uuid/choices' do
+    it 'routes to #choices' do
+      uuid = 'cc3c6ff9-83d8-4375-94be-8c7ae3024938'
+      expect(get "/api/enrollment/#{uuid}/choices").to(
+        route_to('api/v1/enrollment#choices', format: 'json', id: guuid)
+      )
+    end
+  end
+
+  context '/api/enrollment/:enrollment_change_id/approve' do
+    it 'routes to #approve' do
+      expect(put '/api/enrollment/42/approve').to(
+        route_to('api/v1/enrollment#approve', format: 'json', id: '42')
+      )
+    end
+  end
+
+end
