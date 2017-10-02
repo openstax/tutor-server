363e11fc90ed788da346bd31d7949d8179461267
JP Slavinsky <jpslav@gmail.com>
2017-10-02 11:20:46 -0600

    more simulator specs

--- spec/requests/lms_launch_spec.rb
+++ spec/requests/lms_launch_spec.rb
@@ -7,37 +7,43 @@ RSpec.describe 'LMS Launch', type: :request do
   let(:lms_app) { FactoryGirl.create(:lms_app, owner: course) }
   let(:user) { FactoryGirl.create(:user) }
 
-  before(:each) { pretend_launch_signature_valid! }
+  let(:simulator) { Lms::Simulator.new(self) }
+  let(:launch_helper) { Lms::LaunchHelper.new(self) }
+
+  before(:each) {
+    simulator.install_tutor(app: lms_app, course: "physics")
+    simulator.set_launch_defaults(course: "physics")
+  }
 
   context "student launches" do
     context "not enrolled" do
       it 'redirects the student to enrollment' do
         expect_any_instance_of(Lms::Launch).to receive(:update_tool_consumer_metadata!)
 
-        lr = FactoryGirl.build(:launch_request, :assignment, roles: :student, app: lms_app)
-        post "/lms/launch", lr.request_parameters
-
-        expect_and_fake_trip_to_accounts_and_back(user)
+        simulator.add_student("bob")
+        simulator.launch(user: "bob", assignment: "tutor")
+        bob_user = launch_helper.complete_the_launch_locally
 
         expect(response.body).to match("/enroll/#{course.uuid}")
         expect(UserIsCourseStudent[course: course, user: user]).to eq false
-        expect_course_score_callback_count(1)
+        expect_course_score_callback_count(user: bob_user, count: 1)
       end
     end
 
     context "already enrolled" do
       it 'redirects the student to course' do
-        expect_any_instance_of(Lms::Launch).to receive(:update_tool_consumer_metadata!)
+        # Launch once so we know about bob in all places, which is what would happen anyway
+        simulator.add_student("bob")
+        simulator.launch(user: "bob", assignment: "tutor")
+        bob_user = launch_helper.complete_the_launch_locally
 
-        AddUserAsPeriodStudent[period: period, user: user]
+        AddUserAsPeriodStudent[period: period, user: bob_user]
 
-        lr = FactoryGirl.build(:launch_request, :assignment, roles: :student, app: lms_app)
-        post "/lms/launch", lr.request_parameters
-
-        expect_and_fake_trip_to_accounts_and_back(user)
+        simulator.launch(user: "bob", assignment: "tutor")
+        bob_user = launch_helper.complete_the_launch_locally
 
         expect(response.body).to match("/course/#{course.id}")
-        expect_course_score_callback_count(1)
+        expect_course_score_callback_count(user: bob_user, count: 1)
       end
     end
   end
@@ -47,14 +53,13 @@ RSpec.describe 'LMS Launch', type: :request do
       it 'makes the user a teacher and redirects to course' do
         expect_any_instance_of(Lms::Launch).to receive(:update_tool_consumer_metadata!)
 
-        lr = FactoryGirl.build(:launch_request, :assignment, roles: :instructor, app: lms_app)
-        post "/lms/launch", lr.request_parameters
-
-        expect_and_fake_trip_to_accounts_and_back(user)
+        simulator.add_teacher("teacher")
+        simulator.launch(user: "teacher", assignment: "tutor")
+        teacher_user = launch_helper.complete_the_launch_locally
 
         expect(response.body).to match("/course/#{course.id}")
-        expect(UserIsCourseTeacher[course: course, user: user]).to eq true
-        expect_course_score_callback_count(0)
+        expect(UserIsCourseTeacher[course: course, user: teacher_user]).to eq true
+        expect_course_score_callback_count(user: teacher_user, count: 0)
       end
     end
 
@@ -64,10 +69,9 @@ RSpec.describe 'LMS Launch', type: :request do
 
         AddUserAsCourseTeacher[user: user, course: course]
 
-        lr = FactoryGirl.build(:launch_request, :assignment, roles: :instructor, app: lms_app)
-        post "/lms/launch", lr.request_parameters
-
-        expect_and_fake_trip_to_accounts_and_back(user)
+        simulator.add_teacher("teacher")
+        simulator.launch(user: "teacher", assignment: "tutor")
+        teacher_user = launch_helper.complete_the_launch_locally(log_in_as: user)
 
         expect(response.body).to match("/course/#{course.id}")
         expect(CourseMembership::Models::Teacher.count).to eq 1 # doesn't readd
@@ -76,52 +80,47 @@ RSpec.describe 'LMS Launch', type: :request do
   end
 
   it 'gives an error for unsupported role' do
-    lr = FactoryGirl.build(:launch_request, roles: :administrator, app: lms_app)
-    post "/lms/launch", lr.request_parameters
+    simulator.add_administrator("admin")
+    simulator.launch(user: "admin", assignment: "tutor")
     expect_error("only supports")
   end
 
   context 'missing required fields' do
     context "tool_consumer_instance_guid" do
-      let(:lr) { FactoryGirl.build(:launch_request, roles: role, app: lms_app,
-                                    tool_consumer_instance_guid: nil) }
-
       context "teacher launch" do
-        let(:role) { :instructor }
+        before(:each) { simulator.add_teacher("teacher") }
 
         it 'errors' do
-          post "/lms/launch", lr.request_parameters
+          simulator.launch(user: "teacher", drop_these_fields: :tool_consumer_instance_guid)
           expect_error("try to include the following fields.*tool_consumer_instance_guid")
         end
       end
 
       context "student launch" do
-        let(:role) { :student }
+        before(:each) { simulator.add_student("student") }
 
         it 'errors' do
-          post "/lms/launch", lr.request_parameters
+          simulator.launch(user: "student", drop_these_fields: :tool_consumer_instance_guid)
           expect_error("to see instructions for providing these fields")
         end
       end
     end
 
     context "context_id" do
-      let(:lr) { FactoryGirl.build(:launch_request, roles: role, app: lms_app, context_id: nil) }
-
       context "teacher launch" do
-        let(:role) { :instructor }
+        before(:each) { simulator.add_teacher("teacher") }
 
         it 'errors' do
-          post "/lms/launch", lr.request_parameters
+          simulator.launch(user: "teacher", drop_these_fields: :context_id)
           expect_error("try to include the following fields.*context_id")
         end
       end
 
       context "student launch" do
-        let(:role) { :student }
+        before(:each) { simulator.add_student("student") }
 
         it 'errors' do
-          post "/lms/launch", lr.request_parameters
+          simulator.launch(user: "student", drop_these_fields: :context_id)
           expect_error("to see instructions for providing these fields")
         end
       end
@@ -130,17 +129,23 @@ RSpec.describe 'LMS Launch', type: :request do
 
   context "lms disabled in course" do
     # test context existing or not because can hit this error on two different paths
+    before(:each) { simulator.add_teacher("user") }
 
     it "errors if context already exists" do
-      run_new_launch(:assignment, roles: :instructor, app: lms_app) # builds context
+      # launch once to build context
+      simulator.launch(user: "user")
+      launch_helper.complete_the_launch_locally
+
       course.update_attribute(:is_lms_enabled, false)
-      run_new_launch(:assignment, roles: :instructor, app: lms_app, go_to_accounts: false)
+
+      simulator.launch(user: "user")
       expect_error("fail_lms_disabled")
     end
 
     it "errors if context doesn't exist yet" do
       course.update_attribute(:is_lms_enabled, false)
-      run_new_launch(:assignment, roles: :instructor, app: lms_app, go_to_accounts: false)
+
+      simulator.launch(user: "user")
       expect_error("fail_lms_disabled")
     end
   end
@@ -151,48 +156,30 @@ RSpec.describe 'LMS Launch', type: :request do
     # admin-setup of apps)
 
     it "gives an instructor-specific error" do
-      run_new_launch(:assignment, roles: :instructor, app: lms_app)
-      run_new_launch(:assignment, roles: :instructor, app: lms_app, go_to_accounts: false)
+      simulator.add_teacher("teacher")
+      simulator.launch(user: "teacher", course: "physics")
+      launch_helper.complete_the_launch_locally
+
+      simulator.install_tutor(app: lms_app, course: "biology")
+      simulator.launch(user: "teacher", course: "biology")
 
       expect_error("Message for teachers")
     end
 
     it "gives a student-specific error" do
-      run_new_launch(:assignment, roles: :student, app: lms_app)
-      run_new_launch(:assignment, roles: :student, app: lms_app, go_to_accounts: false)
+      simulator.add_student("student")
+      simulator.launch(user: "student", course: "physics")
+      launch_helper.complete_the_launch_locally
+
+      simulator.install_tutor(app: lms_app, course: "biology")
+      simulator.launch(user: "student", course: "biology")
 
       expect_error("Message for students")
     end
   end
 
-  def pretend_launch_signature_valid!
-    allow(OpenStax::Accounts.configuration).to receive(:openstax_application_secret) { 'secret' }
-    allow_any_instance_of(IMS::LTI::Services::MessageAuthenticator).to receive(:valid_signature?) { true }
-  end
-
-  def expect_and_fake_trip_to_accounts_and_back(user)
-    expect(response.status).to eq 200
-    get response.body.match(/href=\"(.*)\"/)[1] # 'click' open in new tab
-
-    expect(redirect_path).to eq "/accounts/login"
-    expect(redirect_query_hash[:sp]["signature"]).not_to be_blank
-
-    stub_current_user(user)
-    get redirect_query_hash[:return_to]
-  end
-
-  def expect_course_score_callback_count(count)
+  def expect_course_score_callback_count(user: user, count:)
     expect(Lms::Models::CourseScoreCallback.where(course: course).where(profile: user.to_model).count).to eq count
-  end
-
-  def run_new_launch(*args, **keyword_args)
-    go_to_accounts = keyword_args.delete(:go_to_accounts)
-    go_to_accounts = true if go_to_accounts.nil?
-
-    lr = FactoryGirl.build(:launch_request, *args, **keyword_args)
-    post "/lms/launch", lr.request_parameters
-    expect_and_fake_trip_to_accounts_and_back(user) if go_to_accounts
-    lr
   end
 
   def expect_error(message, status_code=422)
