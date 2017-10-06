c64c69469f135ec1827695ac4692eb77b95e79c3
JP Slavinsky <jpslav@gmail.com>
2017-10-06 16:06:27 -0600

    style launch page that forces a new tab to be opened

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -8,5 +8,10 @@ class Admin::TestsController < Admin::BaseController
 
   def minimal_error_iframe; end
 
+  def launch_iframe; end
+
+  def launch
+    render template: 'lms/launch', layout: false
+  end
 
 end
