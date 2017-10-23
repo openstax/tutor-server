7ff84dcda361de01424f45b317f9b47bf93f9a16
Nathan Stitt <nathan@stitt.org>
2017-10-23 12:27:32 -0500

    convert unsuported browser tests to request type

--- spec/controllers/webview_controller_spec.rb
+++ spec/controllers/webview_controller_spec.rb
@@ -33,44 +33,6 @@ RSpec.describe WebviewController, type: :controller do
 
   end
 
-  describe "supported browser check" do
-    # list of browser UA
-    # https://github.com/fnando/browser/blob/master/test/ua.yml
-    [
-      ['IE 9', 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'],
-      ['IE 10', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; EIE10;ENUSMSN)'],
-      ['IE Mobile', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows CE; IEMobile 6.12)'],
-      ['Blackberry', 'BlackBerry8100/4.2.1 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/103'],
-      ['Safari 9', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'],
-
-    ].each do |name, ua|
-      it "#{name} is not supported" do
-        request.headers['HTTP_USER_AGENT'] = ua
-        controller.sign_in new_user
-        get :home
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(browser_upgrade_path)
-      end
-    end
-
-    [
-      ['MS Edge', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0'],
-      ['Firefox', 'Mozilla/5.0 (X11; Ubuntu; Linux armv7l; rv:17.0) Gecko/20100101 Firefox/17.0'],
-      ['Chrome', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.99 Safari/533.4'],
-      ['Safari 10', 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'],
-      ['Safari 11', 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38']
-    ].each do |name, ua|
-      it "#{name} is supported" do
-        request.headers['HTTP_USER_AGENT'] = ua
-        controller.sign_in new_user
-        get :home
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(dashboard_path)
-      end
-    end
-
-  end
-
   describe 'GET *anything' do
     it 'requires a user' do
       get :index
