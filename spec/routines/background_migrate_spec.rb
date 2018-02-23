5ecf48f557c831a537fa2503bcd932cf37b6011a
Dante Soares <dante.m.soares@rice.edu>
2018-02-22 19:26:54 -0600

    Added specs for BackgroundMigrate

---
+++ spec/routines/background_migrate_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'rake'
+
+RSpec.describe BackgroundMigrate, type: :routine do
+  let(:direction) { ['up', 'down'].sample }
+  let(:version)   { Time.current.strftime("%Y%m%d%H%M%S") }
+
+  before(:all)    { Rails.application.load_tasks }
+
+  it 'calls ActiveRecord::Migrator.run with the correct arguments and the db:_dump rake task' do
+    Rake::Task['db:load_config'].invoke
+    expect(Rake::Task['db:load_config']).to receive(:invoke)
+
+    paths = ActiveRecord::Migrator.migrations_paths.map do |path|
+      path.sub 'migrate', 'background_migrate'
+    end
+
+    expect(ActiveRecord::Migrator).to receive(:run).with(direction.to_sym, paths, version.to_i)
+
+    expect(Rake::Task['db:_dump']).to receive(:invoke)
+
+    described_class.call direction, version
+  end
+end
