6de9d28992f67689040788ca01871adbcc107ef5
JP Slavinsky <jpslav@gmail.com>
2018-02-25 17:07:28 -0700

    basics for first research manipulation

--- app/subsystems/research/models/study.rb
+++ app/subsystems/research/models/study.rb
@@ -2,6 +2,13 @@ class Research::Models::Study < IndestructibleRecord
   has_many :survey_plans, inverse_of: :study
   has_many :study_courses, inverse_of: :study
   has_many :courses, through: :study_courses, subsystem: :course_profile, inverse_of: :studies
+  has_many :cohorts, inverse_of: :study
 
   validates :name, presence: true
+
+  def active?
+    raise "nyi"
+  end
+
+  # TODO don't allow activate_at to be changed when active
 end
---
+++ db/migrate/20180222225553_add_research_models_2.rb
@@ -0,0 +1,28 @@
+class AddResearchModels2 < ActiveRecord::Migration
+  def change
+    create_table :research_cohorts do |t|
+      t.references :research_study,             null: false, index: true,
+                                                foreign_key: { on_update: :cascade, on_delete: :cascade }
+
+      t.string :name,                           null: false
+
+      t.integer :cohort_members_count,          null: false, default: 0
+
+      t.timestamps                              null: false
+    end
+
+    create_table :research_cohort_members do |t|
+      t.references :research_cohort,            null: false, index: true,
+                                                foreign_key: { on_update: :cascade, on_delete: :cascade }
+      t.references :course_membership_student,  null: false, index: true,
+                                                foreign_key: { on_update: :cascade, on_delete: :cascade }
+
+      t.timestamps                              null: false
+    end
+
+    add_column :research_studies, :activate_at, :datetime, null: false
+    add_column :research_studies, :deactivate_at, :datetime
+    add_column :research_studies, :killed_at, :datetime
+
+  end
+end
