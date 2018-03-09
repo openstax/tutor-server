5e3d683665db38c2bbc81f0294ca643573b83fdb
Dante Soares <dante.m.soares@rice.edu>
2018-03-09 13:46:29 -0600

    Fixed 2 of 4 N+1 queries in BootstrapDataRepresenter

--- app/subsystems/legal/get_targeted_contracts.rb
+++ app/subsystems/legal/get_targeted_contracts.rb
@@ -6,7 +6,8 @@ class Legal::GetTargetedContracts
   def exec(ids: :all, applicable_to: nil)
     models =
       if applicable_to.present?
-        targeted_contract_models_for_gid(Legal::Utils.gid(applicable_to))
+        gids = [ applicable_to ].flatten.map { |item| Legal::Utils.gid(item) }
+        targeted_contract_models_for_gids(gids)
       elsif :all == ids
         Legal::Models::TargetedContract.all
       else
@@ -17,13 +18,10 @@ class Legal::GetTargetedContracts
     outputs.contracts = models.map(&:as_poro)
   end
 
-  def targeted_contract_models_for_gid(gid)
-    parent_gids = Legal::Models::TargetedContractRelationship.all_parent_gids_of(gid)
-    potential_target_gids = [gid] + parent_gids
+  def targeted_contract_models_for_gids(gids)
+    parent_gids = Legal::Models::TargetedContractRelationship.all_parent_gids_of(gids)
+    potential_target_gids = gids + parent_gids
 
     Legal::Models::TargetedContract.where(target_gid: potential_target_gids).all
   end
 end
-
-
-
