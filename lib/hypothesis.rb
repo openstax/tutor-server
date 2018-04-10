2a865e2b547e615392195d9fd358e3a8d7631609
Dante Soares <dante.m.soares@rice.edu>
2018-04-10 13:54:48 -0500

    Added comment about openstax_uid and uuid

--- lib/hypothesis.rb
+++ lib/hypothesis.rb
@@ -1,6 +1,8 @@
 module Hypothesis
   def self.generate_grant_token(account)
       now = Time.now.to_i
+      # openstax_uid is almost always present except for some fake accounts,
+      # in which case we fall back to the account's uuid
       user_id = "acct:" + (account.openstax_uid || account.uuid).abs.to_s + "@openstax.org"
 
       payload = {
