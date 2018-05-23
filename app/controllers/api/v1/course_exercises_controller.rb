123f49e0d996a449ae1f3e100c0ca9cd7c2504e4
Nathan Stitt <nathan@stitt.org>
2018-05-23 13:19:40 -0500

    remove exercise fetch from courses controller

--- app/controllers/api/v1/course_exercises_controller.rb
+++ app/controllers/api/v1/course_exercises_controller.rb
@@ -4,36 +4,7 @@ class Api::V1::CourseExercisesController < Api::V1::ApiController
 
   resource_description do
     api_versions "v1"
-    short_description 'Provides ways to get course exercises and set options on them'
-    description <<-EOS
-      Provides ways to get course exercises and set options on them
-    EOS
-  end
-
-  api :GET, '/courses/:course_id/exercises(/:pool_types)',
-            "Returns exercises for a given course's ecosystem, filtered by the following params: " +
-            "ecosystem_id, page_ids, pool_types"
-  description <<-EOS
-    Returns a list of assignable exercises in the given course's ecosystem.
-    Old ecosystems can be accessed by specifying the ecosystem_id param.
-    The list is filtered by pages matching the given page_ids array
-    and pools matching the given pool_types array, if given.
-    Exercises are marked as excluded based on the given course.
-
-    #{json_schema(Api::V1::ExerciseSearchRepresenter, include: :readable)}
-  EOS
-  def show
-    ecosystem = params[:ecosystem_id].present? ? Content::Ecosystem.find(params[:ecosystem_id]) :
-                                                 GetCourseEcosystem[course: @course]
-
-    OSU::AccessPolicy.require_action_allowed!(:exercises, current_api_user, ecosystem)
-
-    exercises = GetExercises[ecosystem: ecosystem,
-                             course: @course,
-                             page_ids: params[:page_ids],
-                             pool_types: params[:pool_types]]
-
-    respond_with exercises, represent_with: Api::V1::ExerciseSearchRepresenter
+    short_description 'Provides ways to get set options on exercises for the given course'
   end
 
   api :PATCH, '/courses/:course_id/exercises',
