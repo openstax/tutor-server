8972d7b1b3f6d89c3b178be30bbeaf8b9bbd9d42
JP Slavinsky <jpslav@gmail.com>
2018-07-01 12:21:25 -0600

    activation times in study form

--- app/assets/javascripts/research.js
+++ app/assets/javascripts/research.js
@@ -1,5 +1,15 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery.datetimepicker
 //= require jquery-ui-1.11.4.custom.min
 //= require bootstrap-sprockets
 //= require manager
+
+//=============== Date Time Picker ============//
+$(document).ready(function() {
+  var datepicker_format = 'Y-m-d H:i:s';
+
+  $('.datepicker').datetimepicker({
+    format: datepicker_format
+  });
+});
