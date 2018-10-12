059f4b227eddf7e084df3b3deff0ace8c96e849f
Nathan Stitt <nathan@stitt.org>
2018-10-12 13:08:45 -0500

    pile db export into a new db

--- lib/pgdb.rb
+++ lib/pgdb.rb
@@ -1,13 +1,6 @@
 module Pgdb
   extend self
 
-  def with_config
-    yield Rails.application.class.parent_name.underscore,
-          ActiveRecord::Base.connection_config[:host] || 'localhost',
-          ActiveRecord::Base.connection_config[:database],
-          ActiveRecord::Base.connection_config[:username]
-  end
-
   def name
     ActiveRecord::Base.connection_config[:database]
   end
