e48244337d37c88333ff0e4c22df4aa62f36f653
Nathan Stitt <nathan@stitt.org>
2018-11-02 15:16:22 -0500

    set free-response on individual steps

--- spec/research-studies/remove_free_response_format_spec.rb
+++ spec/research-studies/remove_free_response_format_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Task steps without free response field', type: :request, api: tr
   before(:all) do
     @course = FactoryBot.create :course_profile_course
     @course.ecosystem.books << FactoryBot.create(:content_book, :standard_contents_1 )
+    page_ids = @course.ecosystem.books.last.pages.pluck(:id)
     period = FactoryBot.create :course_membership_period, course: @course
     user = FactoryBot.create(:user)
     user_role = AddUserAsPeriodStudent[user: user, period: period]
@@ -15,10 +16,12 @@ RSpec.describe 'Task steps without free response field', type: :request, api: tr
 
     task_plan = FactoryBot.create :tasks_task_plan,
       owner: @course, ecosystem: @course.ecosystem,
-      settings: { page_ids:  @course.ecosystem.books.last.pages.pluck(:id) }
+      settings: { page_ids:  page_ids }
 
     @task = FactoryBot.create :tasks_task, task_plan: task_plan,
-                             step_types: [:tasks_tasked_exercise]
+                              step_types: [:tasks_tasked_exercise]
+
+    @task.task_steps.each{|ts| ts.update_attributes(content_page_id: page_ids.first) }
     FactoryBot.create :tasks_tasking, role: user_role, task: @task
 
     @token = FactoryBot.create :doorkeeper_access_token,
@@ -31,7 +34,6 @@ RSpec.describe 'Task steps without free response field', type: :request, api: tr
   }
 
   it "can hide free-response formats when displaying a task" do
-    @course.ecosystem.books.last.pages.pluck(:book_location).map{|bl|bl.join('.')}
 
     brain = FactoryBot.create :research_modified_task_for_display,
                               study: @study,
@@ -43,27 +45,34 @@ unless task.reading? || task.homework?
  return {}
 end
 
-chapter_sections = Content::Models::Page.where(
-  id: task.task_plan.settings['page_ids']
-).pluck(:book_location).map{|cs|cs.join('.')}.uniq
+chosen_sections = %w{
+  1,1 1,2 1,3 1,4 2,1 2,2 2,3 2,4 2,5 2,6 2,7 2,8 3,1 3,2 3,3 3,4 3,5
+}
+.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
+.map{|cs| '['+cs+']'}
 
-manipulated_sections = %w{
-  1.1 1.2 1.3 1.4 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 3.1 3.2 3.3 3.4 3.5
-}.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
+manipulated_sections = Set.new(
+  Content::Models::Page.where({
+    id: task.task_plan.settings['page_ids'],
+    book_location: chosen_sections,
+  }).pluck(:id)
+)
 
-task_step_ids = []
+manipulated_task_step_ids = []
 
-if (manipulated_sections & chapter_sections).any?
+if manipulated_sections.any?
   task.task_steps.each do |ts|
-    if ts.exercise?
-      task_step_ids << ts.id
-      ts.tasked.parser.questions_for_students.each{|q| q['formats'] -= ['free-response'] }
+    if ts.exercise? && manipulated_sections.include?(ts.content_page_id)
+      manipulated_task_step_ids << ts.id
+      ts.tasked.parser.questions_for_students.each{|q|
+        q['formats'] -= ['free-response']
+      }
     end
   end
 end
 
-if task_step_ids.any?
-  manipulation.context[:task_step_ids] = task_step_ids
+if manipulated_task_step_ids.any?
+  manipulation.context[:task_step_ids] = manipulated_task_step_ids
   manipulation.record!
 else
   manipulation.ignore!
@@ -85,21 +94,15 @@ EOC
                       name: 'chapter 1-3 save without free-response',
                       code: <<~EOC
 
-task = tasked.task_step.task
-unless task.reading? || task.homework?
- manipulation.ignore!
- return {}
-end
+chosen_sections = %w{
+  1,1 1,2 1,3 1,4 2,1 2,2 2,3 2,4 2,5 2,6 2,7 2,8 3,1 3,2 3,3 3,4 3,5
+}
+.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
+.map{|cs| '['+cs+']'}
 
-chapter_sections = Content::Models::Page.where(
-  id: task.task_plan.settings['page_ids']
-).pluck(:book_location).map{|cs|cs.join('.')}.uniq
-
-manipulated_sections = %w{
-  1.1 1.2 1.3 1.4 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 3.1 3.2 3.3 3.4 3.5
-}.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
-
-if (manipulated_sections & chapter_sections).any?
+if Content::Models::Page.where({
+  id: tasked.task_step.content_page_id, book_location: chosen_sections
+}).exists?
   tasked.parser.questions_for_students.each{|q|
     q['formats'] -= ['free-response']
   }
