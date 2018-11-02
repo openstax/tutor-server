c50e79ccbf983d3d2e2e5f7c85bda68673b46f91
Nathan Stitt <nathan@stitt.org>
2018-11-02 10:36:20 -0500

    update free response study brain to filter by section

--- spec/research-studies/remove_free_response_format_spec.rb
+++ spec/research-studies/remove_free_response_format_spec.rb
@@ -3,16 +3,20 @@ require "rails_helper"
 RSpec.describe 'Task steps without free response field', type: :request, api: true, version: :v1 do
 
   before(:all) do
-    course = FactoryBot.create :course_profile_course
-    period = FactoryBot.create :course_membership_period, course: course
+    @course = FactoryBot.create :course_profile_course
+    @course.ecosystem.books << FactoryBot.create(:content_book, :standard_contents_1 )
+    period = FactoryBot.create :course_membership_period, course: @course
     user = FactoryBot.create(:user)
     user_role = AddUserAsPeriodStudent[user: user, period: period]
     @study = FactoryBot.create :research_study
-    FactoryBot.create :research_cohort, name: 'control', study: @study
+    FactoryBot.create :research_cohort, name: 'A', study: @study
 
-    Research::AddCourseToStudy[course: course, study: @study]
+    Research::AddCourseToStudy[course: @course, study: @study]
 
-    task_plan = FactoryBot.create :tasks_task_plan, owner: course
+    task_plan = FactoryBot.create :tasks_task_plan,
+      owner: @course, ecosystem: @course.ecosystem,
+      settings: { page_ids:  @course.ecosystem.books.last.pages.pluck(:id) }
+
     @task = FactoryBot.create :tasks_task, task_plan: task_plan,
                              step_types: [:tasks_tasked_exercise]
     FactoryBot.create :tasks_tasking, role: user_role, task: @task
@@ -20,34 +24,56 @@ RSpec.describe 'Task steps without free response field', type: :request, api: tr
     @token = FactoryBot.create :doorkeeper_access_token,
                        application: FactoryBot.create(:doorkeeper_application),
                        resource_owner_id: user.id
+    @change_me_ecosystem_id = @task.content_ecosystem_id
   end
 
   before(:each) {
     @study.deactivate!
   }
 
-  it "can hide free-response formats" do
+  it "can hide free-response formats when displaying a task" do
+    @course.ecosystem.books.last.pages.pluck(:book_location).map{|bl|bl.join('.')}
+
     brain = FactoryBot.create :research_modified_task_for_display,
                       study: @study,
                       code: <<~EOC
-manipulation.ignore! and return unless cohort.name == 'control'
+
+unless task.content_ecosystem_id == #{@change_me_ecosystem_id} && (task.reading? || task.homework?)
+ manipulation.ignore!
+ return {}
+end
+
+chapter_sections = Content::Models::Page.where(
+  id: task.task_plan.settings['page_ids']
+).pluck(:book_location).map{|cs|cs.join('.')}.uniq
+
+manipulated_sections = %w{
+  1.1 2.1 1.2
+}.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
+
 task_step_ids = []
-task.task_steps.each do |ts|
-  if ts.exercise?
-    task_step_ids << ts.id
-    ts.tasked.parser.questions_for_students.each{|q| q['formats'] -= ['free-response'] }
+
+if (manipulated_sections & chapter_sections).any?
+  task.task_steps.each do |ts|
+    if ts.exercise?
+      task_step_ids << ts.id
+      ts.tasked.parser.questions_for_students.each{|q| q['formats'] -= ['free-response'] }
+    end
   end
 end
+
 if task_step_ids.any?
   manipulation.context[:task_step_ids] = task_step_ids
   manipulation.record!
 else
   manipulation.ignore!
 end
+
 EOC
     @study.activate!
-
-    api_get "/api/tasks/#{@task.id}", @token
+    expect {
+      api_get "/api/tasks/#{@task.id}", @token
+    }.to change{ Research::Models::Manipulation.count }.by 1
     formats = response.body_as_hash[:steps].flat_map{|ts| ts.dig(:content, :questions, 0, :formats) }
     expect(formats).to_not include(:free_response)
     expect(brain.manipulations.last.target).to eq @task
@@ -57,16 +83,38 @@ EOC
     FactoryBot.create :research_modified_tasked_for_update,
                       study: @study,
                       code: <<~EOC
-manipulation.ignore! and return unless tasked.exercise? && cohort.name == 'control'
-tasked.parser.questions_for_students.each{|q|
-  q['formats'] -= ['free-response']
-}
-manipulation.record!
+
+task = tasked.task_step.task
+unless task.content_ecosystem_id == #{@change_me_ecosystem_id} && (task.reading? || task.homework?)
+ manipulation.ignore!
+ return {}
+end
+
+chapter_sections = Content::Models::Page.where(
+  id: task.task_plan.settings['page_ids']
+).pluck(:book_location).map{|cs|cs.join('.')}.uniq
+
+manipulated_sections = %w{
+  1.1 2.1 1.2
+}.select.with_index{|_, i| 'A' == cohort.name ? i.even? : i.odd? }
+
+debugger
+if (manipulated_sections & chapter_sections).any?
+  tasked.parser.questions_for_students.each{|q|
+    q['formats'] -= ['free-response']
+  }
+  manipulation.record!
+else
+  manipulation.ignore!
+end
+
 EOC
     @study.activate!
 
-    api_put "/api/steps/#{@task.task_steps.first.id}", @token,
-            raw_post_data: { answer_id: @task.task_steps.first.tasked.question_answer_ids[0][0] }
+    expect {
+      api_put "/api/steps/#{@task.task_steps.first.id}", @token,
+              raw_post_data: { answer_id: @task.task_steps.first.tasked.question_answer_ids[0][0] }
+    }.to change{ Research::Models::Manipulation.count }.by 1
 
     expect(response.body_as_hash[:errors]).to be_nil
   end
