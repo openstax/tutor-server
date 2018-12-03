968797f6d12c2d32658d017560ad536360011770
Dante Soares <dante.m.soares@rice.edu>
2018-12-03 16:34:58 -0600

    Restored .css instead of .at_css

--- lib/openstax/cnx/v1/baked.rb
+++ lib/openstax/cnx/v1/baked.rb
@@ -1,7 +1,7 @@
 module OpenStax::Cnx::V1::Baked
   def self.parse_title(title)
     part = Nokogiri::HTML.fragment(title)
-    text_node = part.at_css('.os-text')
+    text_node = part.css('.os-text')
     if text_node.present?
       { text: text_node.text, book_location: part.css('.os-number').text.split('.') }
     else
