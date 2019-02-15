65b13628b2bba1a450741266fbcfcca1746549af
Nathan Stitt <nathan@stitt.org>
2019-02-15 12:25:46 -0600

    fixup specs

--- spec/factories/lms/app.rb
+++ spec/factories/lms/app.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :lms_app, class: '::Lms::Models::App' do
     association :owner, factory: :course_profile_course
-    # after(:build) do |app, evaluator|
-    #   app.owner.lms_context = FactoryBot.create :lms_context, course: app.owner
-    # end
   end
 end
--- spec/subsystems/lms/willo_labs_spec.rb
+++ spec/subsystems/lms/willo_labs_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Lms::WilloLabs do
 
-
+  it 'reads from secrets' do
+    expect(Rails.application.secrets).to receive(:dig).with(:lms, 'willo_labs').at_least(:once).and_return(
+                                           'key' => '1234key', 'secret' => '1234secret'
+                                        )
+    wl = described_class.new
+    expect(wl.key).to eq '1234key'
+    expect(wl.secret).to eq '1234secret'
+  end
 
 end
