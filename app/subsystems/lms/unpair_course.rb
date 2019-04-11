27fb57ab8ca9b9a496ae9653db6846a174611110
Nathan Stitt <nathan@stitt.org>
2019-04-11 14:31:13 -0500

    use destroy_all; make clear that they're being removed

--- app/subsystems/lms/unpair_course.rb
+++ app/subsystems/lms/unpair_course.rb
@@ -7,7 +7,7 @@ class Lms::UnpairCourse
       fatal_error(code: :course_access_is_locked, message: "Course access is locked")
     end
     if course.lms_contexts.any?
-      course.lms_contexts.clear
+      course.lms_contexts.destroy_all
     end
     transfer_errors_from(course, {type: :verbatim})
   end
