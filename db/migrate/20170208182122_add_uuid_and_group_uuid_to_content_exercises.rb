3ee0339a27e682a117a93dfda1a0c2ce62319804
Dante Soares <dante.m.soares@rice.edu>
2019-05-02 13:40:40 -0500

    Fix old migration that had been broken

--- db/migrate/20170208182122_add_uuid_and_group_uuid_to_content_exercises.rb
+++ db/migrate/20170208182122_add_uuid_and_group_uuid_to_content_exercises.rb
@@ -2,9 +2,10 @@ class AddUuidAndGroupUuidToContentExercises < ActiveRecord::Migration
   def change
     enable_extension 'pgcrypto'
 
-    add_column :content_exercises, :uuid, :uuid, null: false, index: true
+    add_column :content_exercises, :uuid, :uuid, null: false
     add_column :content_exercises, :group_uuid, :uuid, null: false
 
+    add_index :content_exercises, :uuid
     add_index :content_exercises, [:group_uuid, :version]
   end
 end
