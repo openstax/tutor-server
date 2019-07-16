3dd0a3897a35e6010dedf92ed8fdecf42ebd7503
Nathan Stitt <nathan@stitt.org>
2019-07-16 08:30:14 -0500

    dropping cc_page_stats needs MATERIALIZED keyword

--- db/migrate/20190711001122_drop_cc_page_stats.rb
+++ db/migrate/20190711001122_drop_cc_page_stats.rb
@@ -1,5 +1,5 @@
 class DropCcPageStats < ActiveRecord::Migration[5.2]
   def change
-    execute 'DROP VIEW IF EXISTS cc_page_stats;'
+    execute 'DROP MATERIALIZED VIEW IF EXISTS cc_page_stats;'
   end
 end
