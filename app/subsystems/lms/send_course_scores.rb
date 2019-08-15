d4fcc8b8f48863ff1ded92cfd6aaa52bc429aa77
Dante Soares <dante.m.soares@rice.edu>
2019-08-15 12:04:25 -0500

    Fix lms/salesforce exception reporting again

--- app/subsystems/lms/send_course_scores.rb
+++ app/subsystems/lms/send_course_scores.rb
@@ -131,9 +131,9 @@ class Lms::SendCourseScores
       exception = error[:exception]
 
       if exception.nil?
-        Raven.capture_message error[:message], error.except(:message)
+        Raven.capture_message error[:message], extra: error.except(:message)
       else
-        Raven.capture_exception exception, error.except(:exception)
+        Raven.capture_exception exception, extra: error.except(:exception)
       end
     end
   end
