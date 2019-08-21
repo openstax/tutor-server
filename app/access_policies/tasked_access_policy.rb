e263b285d60665abf0eadcfa47df4d46646e9269
Nathan Stitt <nathan@stitt.org>
2019-08-21 11:21:23 -0500

    add parenthesis so || is evaluated properly

--- app/access_policies/tasked_access_policy.rb
+++ app/access_policies/tasked_access_policy.rb
@@ -4,16 +4,14 @@ class TaskedAccessPolicy
     task = tasked.task_step.task
     case action
     when :read
-      return true if DoesTaskingExist[task_component: tasked, user: requestor] &&
-        tasked.task_step.task.past_open?
+      return true if DoesTaskingExist[task_component: tasked, user: requestor] && task.past_open?
       period = task.taskings.first.try(:period)
       return false if period.nil?
       UserIsCourseTeacher[user: requestor, course: period.course]
     when :update
-      DoesTaskingExist[task_component: tasked, user: requestor] &&
-        task.roles.first.teacher_student? || (
-          task.past_open? && !task.withdrawn?
-        )
+      DoesTaskingExist[task_component: tasked, user: requestor] && (
+        task.roles.first.teacher_student? || (task.past_open? && !task.withdrawn?)
+      )
     else
       false
     end
