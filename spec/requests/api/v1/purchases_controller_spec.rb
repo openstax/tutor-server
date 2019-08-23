679521abb908e7a82f524cf107026c3b7af2606c
Dante Soares <dante.m.soares@rice.edu>
2019-08-23 10:40:43 -0500

    Fixed refunds

--- spec/requests/api/v1/purchases_controller_spec.rb
+++ spec/requests/api/v1/purchases_controller_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, version: :v1 do
-
+RSpec.describe Api::V1::PurchasesController, type: :request, api: true, version: :v1 do
   let(:application)       { FactoryBot.create :doorkeeper_application }
 
   let(:period)            { FactoryBot.create :course_membership_period }
@@ -22,30 +21,30 @@ RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, versi
     it 'gives accepted status when the student exists' do
       student = FactoryBot.create(:course_membership_student)
       expect(UpdatePaymentStatus).to receive(:perform_later).with(uuid: student.uuid)
-      api_put :check, nil, params: { id: student.uuid }
+      api_put check_api_purchase_url(student.uuid), nil
       expect(response).to have_http_status(:accepted)
     end
 
     it 'gives not found status when the student does not exist' do
-      api_put :check, nil, params: { id: 'some UUID' }
+      api_put check_api_purchase_url('some UUID'), nil
       expect(response).to have_http_status(:not_found)
     end
   end
 
   context "#refund" do
     it "gives not found status when the student does not exist" do
-      api_put :refund, student_token, params: { id: 'some UUID' }
+      api_put refund_api_purchase_url('some UUID'), student_token
       expect(response).to have_http_status(:not_found)
     end
 
     it "gives 403 when user does not own student" do
       expect do
-        api_put :refund, other_user_token, params: { id: student.uuid }
+        api_put refund_api_purchase_url(student.uuid), other_user_token
       end.to raise_error(SecurityTransgression)
     end
 
     it "gives 422 not paid if not paid" do
-      api_put :refund, student_token, params: { id: student.uuid }
+      api_put refund_api_purchase_url(student.uuid), student_token
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body_as_hash[:errors]).to match a_collection_containing_exactly(
         a_hash_including(code: 'not_paid')
@@ -56,7 +55,7 @@ RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, versi
       Timecop.freeze(Time.now - 14.days - 2.hours) do
         student.update_attributes!(is_paid: true)
       end
-      api_put :refund, student_token, params: { id: student.uuid }
+      api_put refund_api_purchase_url(student.uuid), student_token
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body_as_hash[:errors]).to match a_collection_containing_exactly(
         a_hash_including(code: 'refund_period_elapsed')
@@ -65,9 +64,13 @@ RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, versi
 
     it "gives 202 accepted if all good" do
       student.update_attributes!(is_paid: true)
-      survey = { 'why' => "too-expensive", "comments" => "gimme my money back"}
-      expect(RefundPayment).to receive(:perform_later).with(uuid: student.uuid, survey: survey)
-      api_put :refund, student_token, params: { id: student.uuid, survey: survey }
+
+      survey = { 'why' => "too-expensive", "comments" => "gimme my money back" }
+      expect(RefundPayment).to receive(:perform_later) do |uuid:, survey:|
+        expect(uuid).to eq student.uuid
+        expect(survey.to_h).to eq survey
+      end
+      api_put refund_api_purchase_url(student.uuid, survey: survey), student_token
       expect(response).to have_http_status(:accepted)
     end
   end
@@ -79,7 +82,7 @@ RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, versi
           .with(student_user.account)
           .and_return(orders: [1, 2, 3])
       )
-      api_get :index, student_token
+      api_get api_purchases_url, student_token
       expect(response).to have_http_status(:ok)
       expect(response.body_as_hash).to eq(orders: [1, 2, 3])
     end
@@ -87,11 +90,10 @@ RSpec.describe Api::V1::PurchasesController, type: :controller, api: true, versi
 
   context "#create_fake" do
     it 'creates new fake purchased items' do
-      uuids = 2.times.map{ SecureRandom.uuid }
-      api_post :create_fake, nil, body: uuids.to_json
+      uuids = 2.times.map { SecureRandom.uuid }
+      api_post fake_api_purchases_url, nil, params: uuids.to_json
       expect(OpenStax::Payments::FakePurchasedItem.find(uuids[0])).to be_present
       expect(OpenStax::Payments::FakePurchasedItem.find(uuids[1])).to be_present
     end
   end
-
 end
