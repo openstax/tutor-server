0f32974ad3f1285e25901c113bccdfd9500cb6a2
Dante Soares <dante.m.soares@rice.edu>
2019-10-11 18:03:44 -0500

    Note API changes

--- spec/routing/api/v1/notes_controller_routing_spec.rb
+++ spec/routing/api/v1/notes_controller_routing_spec.rb
@@ -1,64 +1,45 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::NotesController, type: :routing, api: true, version: :v1 do
-  context 'GET /api/courses/:course_id/notes/:chapter.:section' do
+  context 'GET /api/pages/:page_id/notes' do
     it 'routes to #index' do
-      expect(get '/api/courses/21/notes/4.2').to route_to(
-        'api/v1/notes#index', format: 'json', course_id: '21', chapter: '4', section: '2'
+      expect(get '/api/pages/b2f8c9a0-46f1-4a45-822f-e7c4a5d22849/notes').to route_to(
+        'api/v1/notes#index', format: 'json', page_id: 'b2f8c9a0-46f1-4a45-822f-e7c4a5d22849'
       )
     end
   end
 
-  context 'POST /api/courses/:course_id/notes/:chapter.:section(/role/:role_id)' do
-    context 'no role_id' do
-      it 'routes to #create' do
-        expect(post '/api/courses/21/notes/4.2').to route_to(
-          'api/v1/notes#create', format: 'json', course_id: '21', chapter: '4', section: '2'
-        )
-      end
-    end
-
-    context 'role_id in path' do
-      it 'routes to #create' do
-        expect(post '/api/courses/21/notes/4.2/role/84').to route_to(
-          'api/v1/notes#create',
-          format: 'json', course_id: '21', chapter: '4', section: '2', role_id: '84'
-        )
-      end
-    end
-
-    context 'role_id in query params' do
-      it 'routes to #create' do
-        expect(post '/api/courses/21/notes/4.2?role_id=84').to route_to(
-          'api/v1/notes#create',
-          format: 'json', course_id: '21', chapter: '4', section: '2', role_id: '84'
-        )
-      end
+  context 'POST /api/pages/:page_id/notes' do
+    it 'routes to #create' do
+      expect(post '/api/pages/b2f8c9a0-46f1-4a45-822f-e7c4a5d22849/notes').to route_to(
+        'api/v1/notes#create', format: 'json', page_id: 'b2f8c9a0-46f1-4a45-822f-e7c4a5d22849'
+      )
     end
   end
 
-  context 'PATCH /api/courses/:course_id/notes/:chapter.:section/:id' do
+  context 'PATCH /api/notes/:id' do
     it 'routes to #update' do
-      expect(patch '/api/courses/21/notes/4.2/84').to route_to(
-        'api/v1/notes#update',
-        format: 'json', course_id: '21', chapter: '4', section: '2', id: '84'
-      )
+      expect(patch '/api/notes/42').to route_to 'api/v1/notes#update', format: 'json', id: '42'
     end
   end
 
-  context 'DELETE /api/courses/:course_id/notes/:chapter.:section/:id' do
+  context 'PUT /api/notes/:id' do
+    it 'routes to #update' do
+      expect(put '/api/notes/42').to route_to 'api/v1/notes#update', format: 'json', id: '42'
+    end
+  end
+
+  context 'DELETE /api/notes/:id' do
     it 'routes to #destroy' do
-      expect(delete '/api/courses/21/notes/4.2/84').to route_to(
-        'api/v1/notes#destroy',
-        format: 'json', course_id: '21', chapter: '4', section: '2', id: '84'
-      )
+      expect(delete '/api/notes/42').to route_to 'api/v1/notes#destroy', format: 'json', id: '42'
     end
   end
 
-  context 'GET /api/courses/:course_id/highlighted_sections' do
+  context 'GET /api/books/:book_uuid/highlighted_sections' do
     it 'routes to #highlighted_sections' do
-      expect(get '/api/courses/42/highlighted_sections').to route_to(
-        'api/v1/notes#highlighted_sections', format: 'json', course_id: '42'
+      expect(get '/api/books/b9fdc912-6ea6-451a-912c-b52ea6238bb2/highlighted_sections').to(
+        route_to 'api/v1/notes#highlighted_sections',
+                 format: 'json', book_uuid: 'b9fdc912-6ea6-451a-912c-b52ea6238bb2'
       )
     end
   end
