e0126ef89c2e0487406e94f5fd77df0c0d08bf6f
Nathan Stitt <nathan@stitt.org>
2020-01-15 11:56:54 -0600

    params.slice is not a method, use permit and to_h

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -8,13 +8,13 @@ class Admin::NotificationsController < Admin::BaseController
     redirect_to :admin_notifications, error: "Invalid notification type \"#{type}\"" \
       unless Settings::Notifications.valid_type?(type: type)
 
-    Settings::Notifications.add **params.slice(:type, :message, :from, :to).symbolize_keys
+    Settings::Notifications.add(**(params.permit(:type, :message, :from, :to).to_h.symbolize_keys))
 
     redirect_to :admin_notifications, notice: "#{type.humanize} notification created"
   end
 
   def destroy
-    Settings::Notifications.remove **params.slice(:type, :id).symbolize_keys
+    Settings::Notifications.remove(**(params.permit(:type, :id).to_h.symbolize_keys))
 
     redirect_to :admin_notifications, notice: "#{params[:type].humanize} notification deleted"
   end
