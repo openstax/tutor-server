1b991429bc9ff8f7b07885e733b6f88b78cd0846
Dante Soares <dante.m.soares@rice.edu>
2020-01-17 12:50:03 -0600

    Fix admin creating catalog offerings when Salesforce is not configured

--- app/routines/get_salesforce_book_names.rb
+++ app/routines/get_salesforce_book_names.rb
@@ -3,11 +3,17 @@ class GetSalesforceBookNames
 
   def exec(force_cache_miss = false)
     unless force_cache_miss
-      outputs[:book_names] = ActiveForce.cache_store.get('book_names')
-      return unless outputs[:book_names].nil?
+      outputs.book_names = ActiveForce.cache_store.get('book_names')
+      return unless outputs.book_names.nil?
     end
 
-    outputs[:book_names] = OpenStax::Salesforce::Remote::Book.all.map(&:name)
-    ActiveForce.cache_store.set('book_names', outputs[:book_names])
+    begin
+      outputs.book_names = OpenStax::Salesforce::Remote::Book.all.map(&:name)
+    rescue IllegalState
+      # Ignore IllegalState exceptions thrown by openstax_salesforce
+    else
+      # This block runs if no exceptions were raised
+      ActiveForce.cache_store.set('book_names', outputs.book_names)
+    end
   end
 end
--- spec/routines/get_salesforce_book_names_spec.rb
+++ spec/routines/get_salesforce_book_names_spec.rb
@@ -1,39 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe GetSalesforceBookNames, type: :routine do
-
   let(:book_names) { ['Biology', 'College Physics (Algebra)'] }
 
-  before(:each) do
-    ActiveForce.cache_store.set('book_names', nil)
-  end
+  before { ActiveForce.cache_store.set('book_names', nil) }
 
   it 'queries the salesforce book model to get book names' do
     expect(OpenStax::Salesforce::Remote::Book).to(
-      receive(:all).once.and_return(book_names.map{ |name| OpenStruct.new(name: name) })
+      receive(:all).once.and_return(book_names.map { |name| OpenStruct.new(name: name) })
     )
 
-    expect(GetSalesforceBookNames[]).to eq book_names
+    expect(described_class[]).to eq book_names
   end
 
   it 'returns cached results in subsequent calls' do
     expect(OpenStax::Salesforce::Remote::Book).to(
-      receive(:all).once.and_return(book_names.map{ |name| OpenStruct.new(name: name) })
+      receive(:all).once.and_return(book_names.map { |name| OpenStruct.new(name: name) })
     )
 
-    expect(GetSalesforceBookNames[]).to eq book_names
-    expect(GetSalesforceBookNames[]).to eq book_names
-    expect(GetSalesforceBookNames[]).to eq book_names
+    expect(described_class[]).to eq book_names
+    expect(described_class[]).to eq book_names
+    expect(described_class[]).to eq book_names
   end
 
   it 'does not use the cached value if a forced cache miss is requested' do
     expect(OpenStax::Salesforce::Remote::Book).to(
-      receive(:all).twice.and_return(book_names.map{ |name| OpenStruct.new(name: name) })
+      receive(:all).twice.and_return(book_names.map { |name| OpenStruct.new(name: name) })
     )
 
-    expect(GetSalesforceBookNames[]).to eq book_names
-    expect(GetSalesforceBookNames[true]).to eq book_names
-    expect(GetSalesforceBookNames[]).to eq book_names
+    expect(described_class[]).to eq book_names
+    expect(described_class[true]).to eq book_names
+    expect(described_class[]).to eq book_names
   end
 
+  it 'does not blow up if Salesforce is not configured' do
+    expect(OpenStax::Salesforce::Remote::Book).to receive(:all).once do
+      raise IllegalState, 'The Salesforce username is missing'
+    end
+
+    expect(described_class[]).to be_nil
+  end
 end
