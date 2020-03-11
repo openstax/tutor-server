24271a3c38d9ae90ba0587989bacd476ff3966a4
Dante Soares <dante.m.soares@rice.edu>
2020-03-11 13:28:35 -0500

    Always update preview course ecosystems regardless of options selected and end dates

--- spec/subsystems/catalog/update_offering_spec.rb
+++ spec/subsystems/catalog/update_offering_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Catalog::UpdateOffering, type: :routine do
+  let(:old_ecosystem)   { FactoryBot.create :content_ecosystem }
+  let(:new_ecosystem)   { FactoryBot.create :content_ecosystem }
 
-  let(:old_ecosystem) { FactoryBot.create :content_ecosystem }
-  let(:new_ecosystem) { FactoryBot.create :content_ecosystem }
+  let(:offering)        { FactoryBot.create :catalog_offering, ecosystem: old_ecosystem }
 
-  let(:offering)      { FactoryBot.create :catalog_offering, ecosystem: old_ecosystem }
-
-  let(:courses)       do
+  let(:courses)        do
     3.times.map do
       FactoryBot.create(:course_profile_course, offering: offering).tap do |course|
-        AddEcosystemToCourse.call(course: course, ecosystem: old_ecosystem)
+        AddEcosystemToCourse.call course: course, ecosystem: old_ecosystem
       end
     end
   end
+  let(:preview_course) do
+    FactoryBot.create(:course_profile_course, offering: offering, is_preview: true).tap do |course|
+      AddEcosystemToCourse.call course: course, ecosystem: old_ecosystem
+    end
+  end
 
-  let(:new_attributes) do
+
+  let(:new_attributes)  do
     {
       salesforce_book_name: 'new_book',
       appearance_code: 'new',
@@ -30,29 +35,32 @@ RSpec.describe Catalog::UpdateOffering, type: :routine do
   end
 
   it "updates the offering's attributes" do
-    described_class.call(offering.id, new_attributes)
+    described_class.call offering.id, new_attributes
     str_attributes = new_attributes.stringify_keys
     expect(offering.reload.attributes.slice(*str_attributes.keys)).to eq str_attributes
   end
 
   context 'update_courses is true' do
     it "updates associated courses' ecosystems if the ecosystem is changed" do
-      courses.each{ |course| expect(course.ecosystem).to eq old_ecosystem }
+      courses.each { |course| expect(course.ecosystem).to eq old_ecosystem }
+      expect(preview_course.ecosystem).to eq old_ecosystem
 
-      described_class.call(offering.id, {ecosystem: new_ecosystem}, true)
+      described_class.call offering.id, { ecosystem: new_ecosystem }, true
 
-      courses.each{ |course| expect(course.reload.ecosystem).to eq new_ecosystem }
+      courses.each { |course| expect(course.reload.ecosystem).to eq new_ecosystem }
+      expect(preview_course.ecosystem).to eq new_ecosystem
     end
   end
 
   context 'update_courses is false' do
     it "does not updates associated courses' ecosystems" do
-      courses.each{ |course| expect(course.ecosystem).to eq old_ecosystem }
+      courses.each { |course| expect(course.ecosystem).to eq old_ecosystem }
+      expect(preview_course.ecosystem).to eq old_ecosystem
 
-      described_class.call(offering.id, {ecosystem: new_ecosystem}, false)
+      described_class.call offering.id, { ecosystem: new_ecosystem }, false
 
-      courses.each{ |course| expect(course.reload.ecosystem).to eq old_ecosystem }
+      courses.each { |course| expect(course.reload.ecosystem).to eq old_ecosystem }
+      expect(preview_course.ecosystem).to eq new_ecosystem
     end
   end
-
 end
