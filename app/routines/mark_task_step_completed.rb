ed888a0bbd89a4c675925c3245603338abde3a19
Dante Soares <dante.m.soares@rice.edu>
2020-05-01 13:53:57 -0500

    Added comments about setting the course variable

--- app/routines/mark_task_step_completed.rb
+++ app/routines/mark_task_step_completed.rb
@@ -29,6 +29,7 @@ class MarkTaskStepCompleted
     role = task.taskings.first&.role
     period = role&.course_member&.period
     course = period&.course
+    # course will only be set if role and period were found
     return if course.nil?
 
     queue = task.is_preview ? 'preview' : 'dashboard'
