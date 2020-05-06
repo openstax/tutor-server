ca495164b5ff454d7262992b311cb66565d3fd48
Dante Soares <dante.m.soares@rice.edu>
2020-05-06 11:48:59 -0500

    Changed Summer courses to start April 1st

--- lib/term_year.rb
+++ lib/term_year.rb
@@ -12,7 +12,7 @@ TermYear = Struct.new(:term, :year) do
     preview: ->(year) { DateTime.current.monday - 2.weeks }, # 2 weeks before the previous monday
     winter:  ->(year) { DateTime.new(year    , 11)        }, # November 1st of given year
     spring:  ->(year) { DateTime.new(year        )        }, # January 1st of given year
-    summer:  ->(year) { DateTime.new(year    , 5 )        }, # May 1st of given year
+    summer:  ->(year) { DateTime.new(year    , 4 )        }, # April 1st of given year
     fall:    ->(year) { DateTime.new(year    , 7 )        }  # July 1st of given year
   }
 
