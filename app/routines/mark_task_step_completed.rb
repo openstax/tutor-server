4478f6966bd365f4a5d1f4793f40aaa5712436f8
Dante Soares <dante.m.soares@rice.edu>
2020-05-13 14:40:53 -0500

    Only run Glicko once when a task is completed

--- app/routines/mark_task_step_completed.rb
+++ app/routines/mark_task_step_completed.rb
@@ -12,6 +12,8 @@ class MarkTaskStepCompleted
       task.lock!
     end
 
+    task_was_completed = task.completed?(use_cache: true)
+
     task_step.complete completed_at: completed_at
     transfer_errors_from task_step, { type: :verbatim }, true
 
@@ -32,7 +34,7 @@ class MarkTaskStepCompleted
     # course will only be set if role and period were found
     return if course.nil?
 
-    if task.completed?(use_cache: true)
+    if !task_was_completed && task.completed?(use_cache: true)
       queue = task.is_preview ? 'preview' : 'dashboard'
       role_run_at = task.feedback_available? ? completed_at :
                                                [ task.feedback_at, completed_at ].compact.max
