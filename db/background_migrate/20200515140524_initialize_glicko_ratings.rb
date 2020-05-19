eff472fa3dc1ff490fb5a0a983c2bcdbc0ca1d01
Dante Soares <dante.m.soares@rice.edu>
2020-05-19 15:36:06 -0500

    Changed Glicko background migration to run based on max_due_at Split it into several transactions Store max_due_at on Redis

--- db/background_migrate/20200515140524_initialize_glicko_ratings.rb
+++ db/background_migrate/20200515140524_initialize_glicko_ratings.rb
@@ -1,35 +1,54 @@
 class InitializeGlickoRatings < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   include Ratings::Concerns::RatingJobs
 
+  redis_secrets = Rails.application.secrets.redis
+  # STORE needs to be an ActiveSupport::Cache::RedisStore to support TTL
+  STORE = ActiveSupport::Cache::RedisStore.new(
+    url: redis_secrets[:url],
+    namespace: 'migration',
+    expires_in: 1.year
+  )
   LIMIT = 1000
 
   def up
     current_time = Time.current
 
-    offset = 0
-    tasks = Tasks::Models::Task.preload(
-      taskings: { role: [ { student: :period }, { teacher_student: :period } ] }
-    ).order(:due_at_ntz).limit(LIMIT)
+    max_due_at = STORE.read 'max_due_at'
 
-    until tasks.empty?
-      tasks = Tasks::Models::Task.preload(
-        taskings: { role: [ { student: :period }, { teacher_student: :period } ] }
-      ).order(:due_at_ntz).offset(offset).first(LIMIT)
-      offset += tasks.size
+    loop do
+      Tasks::Models::Task.transaction do
+        tasks = Tasks::Models::Task.preload(
+          taskings: { role: [ { student: :period }, { teacher_student: :period } ] }
+        ).order(:due_at_ntz)
 
-      tasks.each do |task|
-        role = task.taskings.first.role
-        period = role.course_member.period
+        tasks = tasks.where(Tasks::Models::Task.arel_table[:due_at_ntz].gt(max_due_at)) \
+          unless max_due_at.nil?
 
-        perform_rating_jobs_later(
-          task: task,
-          role: role,
-          period: period,
-          current_time: current_time,
-          queue: 'migration'
-        )
+        tasks = tasks.first(LIMIT)
+
+        break if tasks.empty?
+
+        tasks.each do |task|
+          role = task.taskings.first.role
+          period = role.course_member.period
+
+          perform_rating_jobs_later(
+            task: task,
+            role: role,
+            period: period,
+            current_time: current_time,
+            queue: 'migration'
+          )
+        end
+
+        max_due_at = tasks.last.due_at_ntz
+        STORE.write 'max_due_at', max_due_at
       end
     end
+
+    STORE.delete 'max_due_at'
   end
 
   def down
