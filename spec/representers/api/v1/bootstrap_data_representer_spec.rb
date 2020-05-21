9e9904a634df26eb46bc1c7cdde87dd1aee93e8d
Nathan Stitt <nathan@stitt.org>
2020-05-21 15:31:56 -0500

    include force reload flag

--- spec/representers/api/v1/bootstrap_data_representer_spec.rb
+++ spec/representers/api/v1/bootstrap_data_representer_spec.rb
@@ -37,7 +37,8 @@ RSpec.describe Api::V1::BootstrapDataRepresenter, type: :representer do
         feature_flags: {
           is_payments_enabled: Settings::Payments.payments_enabled,
           teacher_student_enabled: Settings::Db[:teacher_student_enabled],
-          pulse_insights: Settings::Db[:pulse_insights]
+          pulse_insights: Settings::Db[:pulse_insights],
+          force_browser_reload: Settings::Db[:force_browser_reload]
         },
         ui_settings: {},
         flash: { alert: 'Nothing!' }
