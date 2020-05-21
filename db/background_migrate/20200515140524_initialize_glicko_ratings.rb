13cd95574d58c02b06d160761eecb437f884d1a0
Dante Soares <dante.m.soares@rice.edu>
2020-05-21 17:36:47 -0500

    Updated the Glicko migration to run preview courses first Preview and test courses and accounts no longer update exercise ratings

--- db/background_migrate/20200515140524_initialize_glicko_ratings.rb
+++ db/background_migrate/20200515140524_initialize_glicko_ratings.rb
@@ -13,15 +13,30 @@ class InitializeGlickoRatings < ActiveRecord::Migration[5.2]
   LIMIT = 1100
 
   def up
+    cc = CourseProfile::Models::Course.arel_table
+    active_preview_course_ids = CourseProfile::Models::Course
+      .where(preview_claimed_at: nil)
+      .or(CourseProfile::Models::Course.where(cc[:ends_at].gt(Time.current)))
+      .where(is_preview: true)
+      .pluck(:id)
+    migrate_course_tasks course_ids: active_preview_course_ids, key: 'preview'
+
+    other_course_ids =  CourseProfile::Models::Course.pluck(:id) - active_preview_course_ids
+    migrate_course_tasks course_ids: other_course_ids, key: 'other'
+  end
+
+  def migrate_course_tasks(course_ids:, key:)
     current_time = Time.current
 
     tt = Tasks::Models::Task.arel_table
 
-    last_worked_at = STORE.read 'last_worked_at'
+    last_worked_at_key = "#{key}_last_worked_at"
+    last_worked_at = STORE.read last_worked_at_key
 
     loop do
       tasks = Tasks::Models::Task.transaction do
         tasks = Tasks::Models::Task
+          .where(course_profile_course_id: course_ids)
           .where(tt[:completed_exercise_steps_count].gt(0))
           .where('"steps_count" <= "completed_steps_count"')
           .order(:last_worked_at)
@@ -47,7 +62,7 @@ class InitializeGlickoRatings < ActiveRecord::Migration[5.2]
         end
 
         last_worked_at = tasks.last.last_worked_at
-        STORE.write 'last_worked_at', last_worked_at
+        STORE.write last_worked_at_key, last_worked_at
 
         tasks
       end
@@ -55,11 +70,13 @@ class InitializeGlickoRatings < ActiveRecord::Migration[5.2]
       break if tasks.empty?
     end
 
-    max_due_at = STORE.read 'max_due_at'
+    max_due_at_key = "#{key}_max_due_at"
+    max_due_at = STORE.read max_due_at_key
 
     loop do
       tasks = Tasks::Models::Task.transaction do
         tasks = Tasks::Models::Task
+          .where(course_profile_course_id: course_ids)
           .where(tt[:completed_exercise_steps_count].gt(0))
           .where('"steps_count" > "completed_steps_count"')
           .where.not(due_at_ntz: nil)
@@ -86,18 +103,20 @@ class InitializeGlickoRatings < ActiveRecord::Migration[5.2]
         end
 
         max_due_at = tasks.last.due_at_ntz
-        STORE.write 'max_due_at', max_due_at
+        STORE.write max_due_at_key, max_due_at
 
         tasks
       end
 
       break if tasks.empty?
     end
-
-    STORE.delete 'last_worked_at'
-    STORE.delete 'max_due_at'
   end
 
   def down
+    STORE.delete 'preview_last_worked_at'
+    STORE.delete 'preview_max_due_at'
+
+    STORE.delete 'other_last_worked_at'
+    STORE.delete 'other_max_due_at'
   end
 end
