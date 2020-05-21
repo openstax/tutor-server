24f22d2de3f189bdaa8f8408af6940f35d0a4895
Nathan Stitt <nathan@stitt.org>
2020-05-21 15:29:40 -0500

    feature flags from settings, include in updates

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,4 +1,12 @@
 module Settings
+  def self.feature_flags
+      {
+        is_payments_enabled: Payments.payments_enabled,
+        teacher_student_enabled: Db[:teacher_student_enabled],
+        force_browser_reload: Db[:force_browser_reload],
+        pulse_insights: Db[:pulse_insights]
+      }
+  end
 end
 
 Dir[File.join(__dir__, 'settings', '*.rb')].each{ |file| require file }
