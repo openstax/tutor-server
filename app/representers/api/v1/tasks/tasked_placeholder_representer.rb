b93dfd40b719bf99371029290b4f834c1c41f926
Dante Soares <dante.m.soares@rice.edu>
2020-06-01 13:00:55 -0500

    Assign correct available_points for placeholder steps

--- app/representers/api/v1/tasks/tasked_placeholder_representer.rb
+++ app/representers/api/v1/tasks/tasked_placeholder_representer.rb
@@ -6,9 +6,17 @@ module Api::V1::Tasks
              writeable: false,
              readable: true,
              schema_info: {
-                required: true,
-                description: "What this Placeholder is standing in for"
+               required: true,
+               description: 'What this Placeholder is standing in for'
              }
 
+    property :available_points,
+             type: Float,
+             readable: true,
+             writeable: false,
+             schema_info: {
+               required: true,
+               description: 'How many points this step will be worth once assigned'
+             }
   end
 end
