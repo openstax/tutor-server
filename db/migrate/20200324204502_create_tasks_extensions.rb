59d53255222bc56c2cdc89d49816f4605fa41361
Dante Soares <dante.m.soares@rice.edu>
2020-06-02 14:23:47 -0500

    Load extensions dynamically from the task_plan and try to preload them in places that use multiple tasks

--- db/migrate/20200324204502_create_tasks_extensions.rb
+++ db/migrate/20200324204502_create_tasks_extensions.rb
@@ -13,20 +13,17 @@ class CreateTasksExtensions < ActiveRecord::Migration[5.2]
 
     add_index :tasks_extensions, [ :tasks_task_plan_id, :entity_role_id ], unique: true
 
-    add_reference :tasks_tasks, :tasks_extension,
-                  foreign_key: { on_update: :cascade, on_delete: :nullify }
-
     Tasks::Models::Task.where.not(tasks_task_plan_id: nil)
                        .where.not(accepted_late_at: nil)
                        .where.not(time_zone_id: nil)
-                       .preload(:taskings, :time_zone)
+                       .preload(:task_plan, :time_zone, taskings: :role)
                        .find_in_batches do |tasks|
       extensions = tasks.map do |task|
         accepted_late_at_ntz = task.accepted_late_at.in_time_zone(task.time_zone.name)
 
         Tasks::Models::Extension.new(
-          tasks_task_plan_id: task.tasks_task_plan_id,
-          entity_role_id: task.taskings.first.entity_role_id,
+          task_plan: task.task_plan,
+          role: task.taskings.first.role,
           time_zone: task.time_zone,
           due_at_ntz: [ accepted_late_at_ntz, task.due_at_ntz ].compact.max,
           closes_at_ntz: [ accepted_late_at_ntz, task.closes_at_ntz ].compact.max
@@ -34,10 +31,6 @@ class CreateTasksExtensions < ActiveRecord::Migration[5.2]
       end
 
       Tasks::Models::Extension.import extensions, validate: false
-
-      Tasks::Models::Task.import tasks, validate: false, on_duplicate_key_update: {
-        conflict_target: [ :id ], columns: [ :tasks_extension_id ]
-      }
     end
 
     remove_column :tasks_tasks, :accepted_late_at
