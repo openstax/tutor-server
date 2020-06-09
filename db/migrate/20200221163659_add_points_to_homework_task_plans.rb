b3155c8d39d964c1bf2deb135958ae40f890b71c
Dante Soares <dante.m.soares@rice.edu>
2020-06-09 13:16:02 -0500

    Fix the points migration to add the correct number of points for multipart questions

--- db/migrate/20200221163659_add_points_to_homework_task_plans.rb
+++ db/migrate/20200221163659_add_points_to_homework_task_plans.rb
@@ -2,9 +2,17 @@ class AddPointsToHomeworkTaskPlans < ActiveRecord::Migration[5.2]
   def up
     Tasks::Models::TaskPlan.reset_column_information
     Tasks::Models::TaskPlan.where(type: 'homework').find_in_batches do |task_plans|
+      all_exercise_ids = task_plans.flat_map { |task_plan| task_plan.settings['exercise_ids'] }
+      num_questions_by_exercise_id = {}
+      Content::Models::Exercise.where(
+        id: all_exercise_ids
+      ).pluck(:id, :question_answer_ids).each do |id, question_answer_ids|
+        num_questions_by_exercise_id[id] = question_answer_ids.size
+      end
+
       task_plans.each do |task_plan|
         task_plan.settings['exercises'] = task_plan.settings['exercise_ids'].map do |id|
-          { 'id' => id.to_s, 'points' => [ 1 ] }
+          { 'id' => id.to_s, 'points' => [ 1.0 ] * num_questions_by_exercise_id[id] }
         end
         task_plan.settings.delete 'exercise_ids'
       end
