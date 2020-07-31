0235ad5e2a3077b6d7f0d42c78add0e11d46b0d2
Dante Soares <dante.m.soares@rice.edu>
2020-07-31 16:29:47 -0500

    Updated openstax_accounts and lev Disabled tests that require signed params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_162633) do
+ActiveRecord::Schema.define(version: 2020_07_31_211543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -551,6 +551,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_162633) do
     t.integer "school_type", default: 0, null: false
     t.boolean "is_kip"
     t.integer "school_location", default: 0, null: false
+    t.boolean "grant_tutor_access"
     t.index ["access_token"], name: "index_openstax_accounts_accounts_on_access_token", unique: true
     t.index ["faculty_status"], name: "index_openstax_accounts_accounts_on_faculty_status"
     t.index ["first_name"], name: "index_openstax_accounts_accounts_on_first_name"
