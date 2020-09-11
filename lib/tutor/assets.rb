e7723af7f4476aafbded8251bad05533e258b188
Nathan Stitt <nathan@stitt.org>
2020-09-11 14:34:50 -0500

    fetch and use manifest.json for JS assets

--- lib/tutor/assets.rb
+++ lib/tutor/assets.rb
@@ -3,13 +3,13 @@ require_relative 'assets/manifest'
 module Tutor
   module Assets
 
-    def self.[](asset, ext)
-      if @manifest.present?
-        asset = @manifest["#{asset}.min.#{ext}"] # manifest assets are minimized
-      else
-        asset = "#{asset}.#{ext}"
+    def self.tags(asset)
+      if @manifest.nil?
+        return "<script type='text/javascript' src='#{asset}' async></script>".html_safe
       end
-      "#{Rails.application.secrets.assets_url}/#{asset}"
+      @manifest[asset].map do |chunk|
+        "<script type='text/javascript' src='#{chunk['src']}'  async></script>"
+      end.join("\n").html_safe
     end
 
     # called by assets initializer as it boots
@@ -18,13 +18,6 @@ module Tutor
       unless @manifest.present?
         Rails.logger.info "assets manifest is missing, running in development mode with assets served by webpack at #{Rails.application.secrets.assets_url}"
         @manifest = nil
-      end
-    end
-
-
-    module Scripts
-      def self.[](asset)
-        Tutor::Assets["#{asset}", 'js']
       end
     end
 
--- lib/tutor/assets/manifest.rb
+++ lib/tutor/assets/manifest.rb
@@ -1,23 +1,41 @@
 module Tutor
   module Assets
     module Manifest
-      # A remote manifest is used in development with Docker
-      # The assets will be inside another container and loaded over http
-      class Remote
+      # A remote manifest is used in production from s3/cloudfront
+      #
+      class ManifestParser
         def [](asset)
-          assets[asset]
+          assets[asset] || []
         end
+
+        def parse_source(source)
+          contents = JSON.parse(source)
+          contents.default_proc = proc do |_, asset|
+            raise("Asset #{asset} does not exist")
+          end
+          contents['entrypoints'].reduce({}) do |assets, (entry_key, types) |
+            assets[entry_key] = types['js'].map do |chunk|
+              asset = contents.find{ |_, attributes| attributes['src'] == chunk }
+              puts asset
+              asset.present? ? asset.last.tap{ |a| a['src'] = "#{url}#{a['src']}" } : nil
+            end.compact
+            assets
+          end
+        end
+      end
+
+      class Remote < ManifestParser
 
         def url
           Rails.application.secrets.assets_manifest_url
         end
 
         def assets
-          RequestStore.store[:assets_manifest] ||= Manifest.parse_source(fetch)
+          RequestStore.store[:assets_manifest] ||= parse_source(fetch)
         end
 
         def fetch
-          response = Faraday.get url
+          response = Faraday.get "#{url}manifest.json"
           if response.success?
             response.body
           else
@@ -35,9 +53,13 @@ module Tutor
         end
       end
 
-      # A local manifest is used in production. care is taken
-      # to only re-parse the file when needed
-      class Local
+      # A local manifest is use when assets are copied into the public/assets directory. docker uses this
+      class Local < ManifestParser
+
+        def url
+          "/assets/"
+        end
+
         SOURCE = Rails.root.join('public', 'assets', 'rev-manifest.json')
         def [](asset)
           read if SOURCE.mtime != @mtime
@@ -45,7 +67,7 @@ module Tutor
         end
 
         def read
-          @contents = Manifest.parse_source SOURCE.read
+          @contents = parse_source SOURCE.read
           @mtime = SOURCE.mtime
           Rails.logger.info "read assets manifest at #{SOURCE.expand_path}"
         end
@@ -53,15 +75,6 @@ module Tutor
         def present?
           SOURCE.exist?
         end
-      end
-
-
-      def self.parse_source(source)
-        contents = JSON.parse(source)
-        contents.default_proc = proc do |_, asset|
-          raise("Asset #{asset} does not exist")
-        end
-        contents
       end
 
       def self.pick_local_or_remote
