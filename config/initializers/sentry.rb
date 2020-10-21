adeaea57a4a80c1860df9083651ca75d7cc34d5f
Dante Soares <dante.m.soares@rice.edu>
2020-10-21 16:14:57 -0500

    Make sure scout and sentry don't print anything in development and test modes

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,10 @@
-sentry_secrets = Rails.application.secrets.sentry
-
 Raven.configure do |config|
+  sentry_secrets = Rails.application.secrets.sentry
+
   config.dsn = sentry_secrets[:dsn]
   config.current_environment = Rails.application.secrets.environment_name
 
   # Send POST data and cookies to Sentry
   config.processors -= [ Raven::Processor::Cookies, Raven::Processor::PostData ]
   config.release = sentry_secrets[:release]
-end
+end if Rails.env.production?
