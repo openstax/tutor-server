1ac965c0eac603e0146c01d31820d74ced5c7464
Dante Soares <dante.m.soares@rice.edu>
2020-10-22 10:11:21 -0500

    Namespaced Puma env vars and added pool setting to database.yml

--- config/puma.rb
+++ config/puma.rb
@@ -1,25 +1,27 @@
 # https://gitlab.com/gitlab-org/gitlab-foss/-/blob/multi-threading/config/puma.rb.example
 
-APP_DIR = File.expand_path('../', __dir__)
+APP_DIR = File.expand_path('..', __dir__)
 directory APP_DIR
-
-stdout_redirect(
-  "#{APP_DIR}/log/puma.stdout.log", "#{APP_DIR}/log/puma.stderr.log", true
-) if ENV.fetch('REDIRECT_STDOUT', false)
 
 tag 'OpenStax Tutor Puma'
 
-worker_timeout ENV.fetch('TIMEOUT', 300)
+NUM_WORKERS = ENV.fetch('PUMA_NUM_WORKERS') { Etc.nprocessors }.to_i
 
-NUM_WORKERS = ENV.fetch('WEB_CONCURRENCY') { Etc.nprocessors }.to_i
+worker_timeout ENV.fetch('PUMA_WORKER_TIMEOUT', 60).to_i
+
+stdout_redirect(
+  ENV.fetch('PUMA_STDOUT_LOGFILE', "#{APP_DIR}/log/puma.stdout.log"),
+  ENV.fetch('PUMA_STDERR_LOGFILE', "#{APP_DIR}/log/puma.stderr.log"),
+  true
+) if ENV.fetch('PUMA_REDIRECT_STDOUT', false)
 
 before_fork do
   require 'puma_worker_killer'
 
   PumaWorkerKiller.config do |config|
     # Restart workers when they start consuming more than 1G each
-    config.ram = ENV.fetch('MAX_MEMORY') do
-      ENV.fetch('MAX_MEMORY_PER_WORKER', 1024).to_i * NUM_WORKERS
+    config.ram = ENV.fetch('PUMA_MAX_MEMORY') do
+      ENV.fetch('PUMA_MAX_WORKER_MEMORY', 1024).to_i * NUM_WORKERS
     end.to_i
 
     config.frequency = 10
@@ -42,20 +44,26 @@ end
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
-threads min_threads_count, max_threads_count
+max_threads = ENV.fetch('PUMA_MAX_THREADS', 5).to_i
+threads ENV.fetch('PUMA_MIN_THREADS', max_threads_count).to_i, max_threads
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch('PORT', 3001)
+if ENV['PUMA_SOCKET']
+  # Specifies the `socket` to which Puma will bind to receive requests.
+  #
+  bind ENV['PUMA_SOCKET']
+else
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  #
+  port ENV.fetch('PUMA_PORT', 3001)
+end
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch('RAILS_ENV', 'development')
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE', 'tmp/pids/puma.pid')
+#
+pidfile ENV.fetch('PUMA_PIDFILE', 'tmp/pids/puma.pid')
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -70,7 +78,8 @@ workers NUM_WORKERS
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app! if ENV.fetch('PRELOAD_APP', false)
+preload_app! if ENV.fetch('PUMA_PRELOAD_APP', false)
 
 # Allow puma to be restarted by `rails restart` command.
+#
 plugin :tmp_restart
