1b4b76ce6297f13078ee7c21a70f987c22eadf03
Josiah Ivey <josiah.ivey@gmail.com>
2020-10-23 11:06:25 -0700

    remove base_ prefix

--- app/subsystems/tasks/get_practice_task.rb
+++ app/subsystems/tasks/get_practice_task.rb
@@ -4,15 +4,15 @@ class Tasks::GetPracticeTask
   protected
 
   def exec(role:, task_type:, page_ids: nil, id: nil)
-    base_query = Tasks::Models::Task
+    query = Tasks::Models::Task
                  .joins(:taskings)
                  .where(taskings: { entity_role_id: role.id }, task_type: task_type)
                  .where('completed_steps_count < steps_count')
                  .order(:created_at)
 
-    base_query = base_query.where(core_page_ids: [page_ids].flatten) if page_ids
-    base_query = base_query.where(id: id) if id
+    query = query.where(core_page_ids: [page_ids].flatten) if page_ids
+    query = query.where(id: id) if id
 
-    outputs.task = base_query.first
+    outputs.task = query.first
   end
 end
