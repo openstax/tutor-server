0efc9027edd75c0e673fbd0ec9441c35e9e1a63b
Dante Soares <dante.m.soares@rice.edu>
2020-10-28 15:55:11 -0500

    Split Redis url secret Connect to the DB via SSL

--- config/initializers/jobba.rb
+++ config/initializers/jobba.rb
@@ -1,6 +1,5 @@
-redis_secrets = Rails.application.secrets.redis
-
 Jobba.configure do |config|
+  redis_secrets = Rails.application.secrets.redis
   config.redis_options = { url: redis_secrets[:url] }
   config.namespace = redis_secrets[:namespaces][:jobba]
 end
--- config/initializers/openstax_payments.rb
+++ config/initializers/openstax_payments.rb
@@ -1,14 +1,16 @@
-payments_secrets = Rails.application.secrets.openstax[:payments] || { stub: true }
-redis_secrets = Rails.application.secrets[:redis]
-
 OpenStax::Payments::Api.configure do |config|
+  payments_secrets = Rails.application.secrets.openstax[:payments] || { stub: true }
   config.server_url = payments_secrets[:url]
   config.client_id  = payments_secrets[:client_id]
   config.secret     = payments_secrets[:secret]
+
   config.stub       = ActiveAttr::Typecasting::BooleanTypecaster.new.call(payments_secrets[:stub])
+
+  redis_secrets     = Rails.application.secrets.redis
   config.fake_store = ActiveSupport::Cache::RedisStore.new(
     url: redis_secrets[:url],
     namespace: redis_secrets[:namespaces][:fake_payments]
   )
+
   config.embed_js_url = payments_secrets[:embed_js_url]
 end
