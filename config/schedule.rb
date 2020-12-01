308b733158d456826eac814af6739ed60fd650ba
Dante Soares <dante.m.soares@rice.edu>
2020-11-30 18:19:51 -0600

    Removed obsolete whenever configuration

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,3 @@
-bundle_command = ENV['BUNDLE_COMMAND'] || 'bundle exec'
-
-set :bundle_command, bundle_command
-set :runner_command, "#{bundle_command} rails runner"
-
 # Server time is UTC; times below are interpreted that way.
 # Ideally we'd have a better way to specify times relative to Central
 # time, independent of the server time.  Maybe there's something here:
