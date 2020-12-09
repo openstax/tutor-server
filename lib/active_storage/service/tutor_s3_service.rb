165f90ebbb50243993fb39d4d7303ff00b1a33b0
Dante Soares <dante.m.soares@rice.edu>
2020-12-08 18:41:11 -0600

    Set the key when creating a new blob so files get uploaded to the correct path

--- lib/active_storage/service/tutor_s3_service.rb
+++ lib/active_storage/service/tutor_s3_service.rb
@@ -11,15 +11,26 @@ module ActiveStorage
     def initialize(bucket:, upload: {}, **options)
       bucket_parts = bucket.split('/')
       bucket_name = bucket_parts.shift
-      @path = bucket_parts.empty? ? '' : bucket_parts.join('/')
+      @path = bucket_parts.join('/')
       super(bucket: bucket_name, upload: upload, **options)
     end
+  end
+end
 
-    private
+ActiveStorage::Blob.class_exec do
+  # Returns the key pointing to the file on the service that's associated with this blob. The key is the
+  # secure-token format from Rails in lower case. So it'll look like: xtapjjcjiudrlk3tmwyjgpuobabd.
+  # This key is not intended to be revealed directly to the user.
+  # Always refer to blobs using the signed_id or a verified form of the key.
+  def key
+    return self[:key] unless self[:key].nil?
 
-    def object_for(key)
-      path = path.present? ? File.join(path, key) : key
-      bucket.object(path)
-    end
+    path = service.respond_to?(:path) ? "#{service.path}/" : ''
+
+    # We can't wait until the record is first saved to have a key for it
+    # Rails 6: self[:key] = path + self.class.generate_unique_secure_token(
+    #            length: MINIMUM_TOKEN_LENGTH
+    #          )
+    self[:key] = path + self.class.generate_unique_secure_token
   end
 end
