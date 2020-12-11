4ce4bc9ba21428299f6edbe048453f53914bcf59
Josiah Ivey <josiah.ivey@gmail.com>
2020-12-11 09:00:03 -0800

    protect against nil exercises setting

--- app/routines/update_assigned_exercise_version.rb
+++ app/routines/update_assigned_exercise_version.rb
@@ -24,7 +24,9 @@ class UpdateAssignedExerciseVersion
       plan = Tasks::Models::TaskPlan.find(plan_id)
       next if plan.out_to_students?
 
-      plan.settings['exercises'].each {|ex| ex['id'] = new_version.id.to_s if ex['id'].in?(old_ids) }
+      (plan.settings['exercises'] ||= []).each do |ex|
+        ex['id'] = new_version.id.to_s if ex['id'].in?(old_ids)
+      end
       (plan.settings['page_ids'] ||= []) << new_version.page.id.to_s
       plan.settings['page_ids'].uniq!
       plan.save
