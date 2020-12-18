cae1737798efb817bbb1e4063f1b2d8261bd0745
Josiah Ivey <josiah.ivey@gmail.com>
2020-12-17 17:41:23 -0800

    use one query two get both eco and page ids instead of two

--- app/subsystems/content/routines/remap_teacher_exercises.rb
+++ app/subsystems/content/routines/remap_teacher_exercises.rb
@@ -5,27 +5,24 @@ class Content::Routines::RemapTeacherExercises
 
   protected
 
-  def exec(ecosystem:, save: false)    
+  def exec(ecosystem:, save: false)
     updated_exercise_ids_by_page_id = {}
 
-    ecosystem_ids = Content::Models::Exercise
-                      .created_by_teacher
-                      .joins(:book)
-                      .distinct
-                      .pluck(:content_ecosystem_id)
+    ecosystem_ids, page_ids = Content::Models::Exercise
+                                .created_by_teacher
+                                .joins(:book)
+                                .distinct
+                                .pluck(:content_ecosystem_id, :content_page_id)
+                                .transpose.map(&:uniq)
     ecosystems = Content::Models::Ecosystem.where(id: ecosystem_ids)
-    page_ids   = Content::Models::Exercise
-                   .created_by_teacher
-                   .distinct
-                   .pluck(:content_page_id)
-    
+
     map = Content::Map.find_or_create_by(
       from_ecosystems: ecosystems, to_ecosystem: ecosystem
     )
     mapped = map.map_page_ids(page_ids: page_ids).compact.reject do |k, v|
       k.to_s == v.to_s
     end
-    
+
     mapped.each do |from, to|
       exercises = Content::Models::Exercise.created_by_teacher.where(content_page_id: from)
       updated_exercise_ids_by_page_id[from.to_s] = exercises.map(&:id)
