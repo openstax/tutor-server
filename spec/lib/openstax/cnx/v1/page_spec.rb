176acac35a17fa92f5719d84c9deaa5e4dddbee1
Nathan Stitt <nathan@stitt.org>
2021-02-14 14:34:20 -0600

    use mini-ecosystem phys book, hs phys is unused

--- spec/lib/openstax/cnx/v1/page_spec.rb
+++ spec/lib/openstax/cnx/v1/page_spec.rb
@@ -3,136 +3,11 @@ require 'rails_helper'
 require 'vcr_helper'
 
 RSpec.describe OpenStax::Cnx::V1::Page, type: :external, vcr: VCR_OPTS do
-  context 'with parts of k12phys' do
+  context 'with parts of college phys' do
     before(:all) do
-      cnx_page_infos = [
-        {
-          id: '3005b86b-d993-4048-aff0-500256001f42',
-          title: '<span class="os-number">1.1</span><span class="os-divider"> </span><span class="os-text">The Science of Biology</span>',
-          expected: {
-            los: ['k12phys-ch03-s02-lo01', 'k12phys-ch03-s02-lo02'],
-            tags: [
-              {
-                value: 'context-cnxmod:3005b86b-d993-4048-aff0-500256001f42',
-                type: :cnxmod
-              },
-              {
-                value: 'teks-112-39-c-4a',
-                type: :teks,
-                name: '(4A)',
-                description: 'Generate and interpret graphs and charts describing different types of motion, including the use of real-time technology such as motion detectors or photogates.'
-              },
-              {
-                value: 'teks-112-39-c-4b',
-                type: :teks,
-                name: '(4B)',
-                description: 'Describe and analyze motion in one dimension using equations with the concepts of distance, displacement, speed, average velocity, instantaneous velocity, and acceleration.'
-              },
-              {
-                value: 'k12phys-ch03-s02-lo01',
-                type: :lo,
-                description: 'Explain the kinematic equations related to acceleration and illustrate them with graphs.',
-                teks: 'teks-112-39-c-4a'
-              },
-              {
-                value: 'k12phys-ch03-s02-lo02',
-                type: :lo,
-                description: 'Apply the kinematic equations and related graphs to problems involving acceleration.',
-                teks: 'teks-112-39-c-4a'
-              }
-            ],
-            is_intro: false
-          }
-        },
-        {
-          id: '1bb611e9-0ded-48d6-a107-fbb9bd900851',
-          title: '<span class="os-text">Introduction</span>',
-          expected: {
-            los: [],
-            tags: [
-              {
-                value: 'context-cnxmod:1bb611e9-0ded-48d6-a107-fbb9bd900851',
-                type: :cnxmod
-              }
-            ],
-            is_intro: true
-          }
-        },
-        {
-          id: '95e61258-2faf-41d4-af92-f62e1414175a',
-          title: 'Force',
-          expected: {
-            los: ['k12phys-ch04-s01-lo01', 'k12phys-ch04-s01-lo02'],
-            tags: [
-              {
-                value: 'context-cnxmod:95e61258-2faf-41d4-af92-f62e1414175a',
-                type: :cnxmod
-              },
-              {
-                value: 'k12phys-ch04-s01-lo01',
-                type: :lo,
-                description: 'Differentiate between force, net force, and dynamics',
-                teks: 'teks-112-39-c-4c'
-              },
-              {
-                value: 'k12phys-ch04-s01-lo02',
-                type: :lo,
-                description: 'Draw a free-body diagram',
-                teks: 'teks-112-39-c-4e'
-              },
-              {
-                value: 'teks-112-39-c-4c',
-                type: :teks,
-                name: '(4C)',
-                description: 'analyze and describe accelerated motion in two dimensions using equations, including projectile and circular examples'
-              },
-              {
-                value: 'teks-112-39-c-4e',
-                type: :teks,
-                name: '(4E)',
-                description: 'develop and interpret free-body diagrams'
-              }
-            ],
-            is_intro: false
-          }
-        },
-        {
-          id: '640e3e84-09a5-4033-b2a7-b7fe5ec29dc6',
-          title: 'Newton\'s First Law of Motion: Inertia',
-          expected: {
-            los: ['k12phys-ch04-s02-lo01', 'k12phys-ch04-s02-lo02'],
-            tags: [
-              {
-                value: 'context-cnxmod:640e3e84-09a5-4033-b2a7-b7fe5ec29dc6',
-                type: :cnxmod
-              },
-              {
-                value: 'k12phys-ch04-s02-lo01',
-                type: :lo,
-                description: 'Describe Newton’s first law and friction',
-                teks: 'teks-112-39-c-4d'
-              },
-              {
-                value: 'k12phys-ch04-s02-lo02',
-                type: :lo,
-                description: 'Discuss the relationship between mass and inertia',
-                teks: 'teks-112-39-c-4d'
-              },
-              {
-                value: 'teks-112-39-c-4d',
-                type: :teks,
-                name: '(4D)',
-                description: 'calculate the effect of forces on objects, including the law of inertia, the relationship between force and acceleration, and the nature of force pairs between objects'
-              }
-            ],
-            is_intro: false
-          }
-        }
-      ]
-
-      @hashes_with_pages = VCR.use_cassette('OpenStax_Cnx_V1_Page/with_parts_of_k12phys',
+      @hashes_with_pages = VCR.use_cassette('OpenStax_Cnx_V1_Page/with_mini_ecosystem_pages',
                                             VCR_OPTS) do
-        cnx_page_infos.map do |hash|
+        PopulateMiniEcosystem.cnx_page_hashes.map do |hash|
           [hash, page_for(hash).tap{ |page| page.full_hash }]
         end
       end
@@ -173,13 +48,13 @@ RSpec.describe OpenStax::Cnx::V1::Page, type: :external, vcr: VCR_OPTS do
 
     it "extracts the LO's from the page" do
       @hashes_with_pages.each do |hash, page|
-        expect(page.los).to eq hash[:expected][:los]
+        expect(page.los).to eq []
       end
     end
 
     it 'extracts tag names and descriptions from the page' do
       @hashes_with_pages.each do |hash, page|
-        expect(Set.new page.tags).to eq Set.new(hash[:expected][:tags])
+        expect(Set.new page.tags).to eq Set.new([{ :value=>"context-cnxmod:#{hash[:id]}", :type=>:cnxmod }])
       end
     end
   end
@@ -209,27 +84,6 @@ RSpec.describe OpenStax::Cnx::V1::Page, type: :external, vcr: VCR_OPTS do
       expect(page.title).to eq 'Hello World!'
     end
 
-  end
-
-  context 'with The Scientific Method' do
-    before(:all) do
-      page_info = { id: '9545b9a2-c371-4a31-abb9-3a4a1fff497b@8', title: 'The Scientific Method' }
-      @page = VCR.use_cassette('OpenStax_Cnx_V1_Page/with_The_Scientific_Method', VCR_OPTS) do
-        page_for(page_info).tap{ |page| page.full_hash }
-      end
-    end
-
-    it 'extracts snap lab notes' do
-      snap_labs = @page.snap_lab_nodes
-      expect(snap_labs.length).to eq 1
-      expect(snap_labs.first[:id]).to eq 'fs-id1164355841632'
-    end
-
-    it 'extracts feature nodes by id' do
-      feature_id = 'fs-id1164355841632'
-      feature_node = described_class.feature_node(@page.root, feature_id)
-      expect(feature_node[:id]).to eq feature_id
-    end
   end
 
   protected
