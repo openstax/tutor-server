0c99d3ecb715fcd1ed3527f6bc1442e83311ad1f
Dante Soares <dante.m.soares@rice.edu>
2021-04-21 12:42:38 -0500

    Made practice worst areas respect teacher exercise ecosystems

--- app/subsystems/tasks/fetch_practice_worst_areas_exercises.rb
+++ app/subsystems/tasks/fetch_practice_worst_areas_exercises.rb
@@ -16,23 +16,26 @@ class Tasks::FetchPracticeWorstAreasExercises
       role_book_part.clue['is_real'] ? role_book_part.clue['most_likely'] : 1.5
     end.first(max_num_exercises).map(&:book_part_uuid)
 
-    exercise_ids_by_page_uuid = ecosystem
-      .pages
-      .where(uuid: page_uuids)
-      .pluck(:uuid, :practice_widget_exercise_ids)
-      .to_h
+    pages = ecosystem.pages.where(uuid: page_uuids)
+
+    # Get page_ids while preserving the order from page_uuids
+    page_id_by_uuid = pages.pluck(:uuid, :id).to_h
+    page_ids = page_id_by_uuid.values_at(*page_uuids)
+
+    # Get valid practice widget exercises for each page
+    exercise_ids_by_page_id = pages.pluck(:id, :practice_widget_exercise_ids).to_h
 
     # Add teacher-created exercises
     Content::Models::Exercise
-      .joins(:page)
-      .where(user_profile_id: course.related_teacher_profile_ids, page: { uuid: page_uuids })
-      .pluck(:id, Arel.sql('"content_pages"."uuid"'))
-      .group_by(&:first).each do |exercise_id, page_uuid|
-      exercise_ids_by_page_uuid[page_uuid] ||= []
-      exercise_ids_by_page_uuid[page_uuid] << exercise_id
+      .where(content_page_id: page_ids, user_profile_id: course.related_teacher_profile_ids)
+      .pluck(:content_page_id, :id)
+      .each do |page_id, exercise_id|
+      exercise_ids_by_page_id[page_id] ||= []
+      exercise_ids_by_page_id[page_id] << exercise_id
     end
 
-    pools = exercise_ids_by_page_uuid.values_at(*page_uuids).reject(&:blank?)
+    # Create ordered pools of practice widget exercises
+    pools = exercise_ids_by_page_id.values_at(*page_ids).reject(&:blank?)
     num_pools = pools.size
 
     return if num_pools == 0
