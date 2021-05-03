6b73f284913e320b7a4a1bdcf98212a1c98c1194
Dante Soares <dante.m.soares@rice.edu>
2021-05-03 10:34:32 -0500

    Redirect to login if non-logged-in user tries to access status page

--- config/initializers/openstax_utilities.rb
+++ config/initializers/openstax_utilities.rb
@@ -1,3 +1,9 @@
 OpenStax::Utilities.configure do |config|
-  config.status_authenticate = -> { raise SecurityTransgression unless current_user.is_admin? }
+  config.status_authenticate = -> do
+    authenticate_user!
+
+    next if performed? || current_user.is_admin?
+
+    raise SecurityTransgression
+  end
 end
