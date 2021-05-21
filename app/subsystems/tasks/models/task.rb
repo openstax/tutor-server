173b609ede8b93007e9ce8ae4ecad65bb44a6f79
Dante Soares <dante.m.soares@rice.edu>
2021-05-21 11:00:08 -0500

    Handle nil updated_at during task creation

--- app/subsystems/tasks/models/task.rb
+++ app/subsystems/tasks/models/task.rb
@@ -232,7 +232,7 @@ class Tasks::Models::Task < ApplicationRecord
   end
 
   def cached?
-    task_plan.nil? || updated_at >= task_plan.updated_at
+    updated_at.nil? || task_plan.nil? || updated_at >= task_plan.updated_at
   end
 
   def completion
