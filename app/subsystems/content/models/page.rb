79530c0da4553413218f66df2788e69c62cd4661
Dante Soares <dante.m.soares@rice.edu>
2021-07-08 12:15:49 -0500

    Added compatibility layer for pages with serialized fragments

--- app/subsystems/content/models/page.rb
+++ app/subsystems/content/models/page.rb
@@ -224,3 +224,8 @@ class Content::Models::Page < IndestructibleRecord
     )
   end
 end
+
+# Compatibility: Many existing pages have serialized fragments saved as OpenStax::CNX::V1::Fragment
+module OpenStax::CNX
+  V1 = ::OpenStax::Content
+end
