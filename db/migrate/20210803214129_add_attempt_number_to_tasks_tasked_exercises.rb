72cd43f7c608eac8cf8e7cbd3a8ed0324cf7db8b
Josiah Ivey <josiah.ivey@gmail.com>
2021-08-10 16:38:36 -0700

    change migration to also set existing tasked exercises to 0 attempts

--- db/migrate/20210803214129_add_attempt_number_to_tasks_tasked_exercises.rb
+++ db/migrate/20210803214129_add_attempt_number_to_tasks_tasked_exercises.rb
@@ -1,6 +1,5 @@
 class AddAttemptNumberToTasksTaskedExercises < ActiveRecord::Migration[5.2]
   def change
-    add_column :tasks_tasked_exercises, :attempt_number, :integer
-    change_column_default :tasks_tasked_exercises, :attempt_number, 0
+    add_column :tasks_tasked_exercises, :attempt_number, :integer, default: 0
   end
 end
