beecaf1ff99a3161de99fce39920c5a0c13548e1
Dante Soares <dante.m.soares@rice.edu>
2021-09-29 16:48:53 -0500

    Added remaining spec placeholder files

--- spec/models/lti/score_spec.rb
+++ spec/models/lti/score_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Lti::ResourceLink, type: :model do
+RSpec.describe Lti::Score, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/lti_controller_spec.rb
+++ spec/requests/lti_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Lti::ResourceLink, type: :model do
+RSpec.describe LtiController, type: :request do
   pending "add some examples to (or delete) #{__FILE__}"
 end
