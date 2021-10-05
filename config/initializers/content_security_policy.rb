c0010a4e6e05f1e3cc151e807c6f14f6c9bdceed
Dante Soares <dante.m.soares@rice.edu>
2021-10-05 18:24:23 -0500

    Added a Tutor CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,24 +4,69 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+# We currently use at least the following sources:
+# self - connect, script, style, img
+# https://accounts.openstax.org - connect
+# https://ajax.googleapis.com - script
+# https://random-looking-domain.salesforceliveagent.com- script
+# https://cdnjs.cloudflare.com- connect, script, font
+# https://js.pulseinsights.com- script
+# https://www.googletagmanager.com- script
+# https://survey.pulseinsights.com- script
+# https://snap.licdn.com- script
+# https://www.googleadservices.com- script
+# https://static.ads-twitter.com- script
+# https://cdn.abrankings.com- connect, script
+# https://googleads.g.doubleclick.net - connect, script
+# https://analytics.twitter.com- script
+# https://www.google-analytics.com - connect, script, img
+# https://fonts.googleapis.com - style
+# https://www.google.com - img
+# https://px.ads.linkedin.com - img
+# https://t.co - img
+# https://fonts.gstatic.com - font
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self
+
+  default_src = [ :self ]
+  connect_src = [ :self, :https ]
+  font_src = [ :self, :https, :data ] # The frontend loads font data blobs
+  img_src = [ :self, :https ]
+  # Once we are in Rails 6 we can try getting rid of unsafe_inline by using nonces
+  script_src = [ :self, :https, :unsafe_inline ]
+  style_src = [ :self, :https, :unsafe_inline ]
+
   if Rails.env.development?
-    policy.connect_src :self, :https,
-      "http://localhost:2999/api/user",
-      "http://localhost:8000",
-      "http://localhost:3035",
-      "ws://localhost:3035",
-      "ws://localhost:8000"
+    # Local ports:
+    # 2999 - OpenStax Accounts
+    # 3035 - Webpacker using dev_server configuration
+    # 8000 - Webpacker using default configuration
+    connect_src += [
+      'http://localhost:2999',
+      'http://localhost:3035',
+      'http://localhost:8000',
+      'ws://localhost:3035',
+      'ws://localhost:8000'
+    ]
+    font_src += [ 'http://localhost:3035', 'http://localhost:8000',  ]
+    img_src += [ 'http://localhost:3035', 'http://localhost:8000' ]
+    script_src += [ 'http://localhost:3035', 'http://localhost:8000' ]
+
+    # This doesn't seem to be currently necessary
+    # style_src += [ 'http://localhost:3035', 'http://localhost:8000' ]
   end
+
+  policy.connect_src *connect_src
+
+  policy.font_src *font_src
+  policy.img_src *img_src
+
+  policy.script_src *script_src
+  policy.style_src *style_src
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
