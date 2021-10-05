c6a45668274c8e1784838faa1f08e2fdd845a459
Dante Soares <dante.m.soares@rice.edu>
2021-10-05 13:32:38 -0500

    Remove serialized nokogiri nodes

---
+++ db/background_migrate/20211005155727_remove_serialized_nokogiri_nodes_in_background.rb
@@ -0,0 +1,20 @@
+class RemoveSerializedNokogiriNodesInBackground < ActiveRecord::Migration[5.2]
+  BATCH_SIZE = 100
+
+  def up
+    loop do
+      break if Content::Models::Page.where(
+        '"fragments" ILIKE \'%\\\\nnode: !ruby/object:Nokogiri::HTML::DocumentFragment {}\\\\n%\''
+      ).limit(BATCH_SIZE).update_all(
+        <<~UPDATE_SQL
+          "fragments" = REPLACE(
+            "fragments", '\\nnode: !ruby/object:Nokogiri::HTML::DocumentFragment {}\\n', '\\n'
+          )
+        UPDATE_SQL
+      ) < BATCH_SIZE
+    end
+  end
+
+  def down
+  end
+end
