cba3e58a90c5bd887b934053a47bbc52eb36f3e3
Dante Soares <dante.m.soares@rice.edu>
2021-10-06 10:40:51 -0500

    Allow script unsafe eval in the admin controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,10 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_admin!
 
+  content_security_policy do |policy|
+    policy.script_src :self, :https, :unsafe_inline, :unsafe_eval
+  end
+
   layout 'admin'
 
   protected
